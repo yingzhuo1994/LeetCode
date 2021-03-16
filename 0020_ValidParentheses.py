c64107685bdfe00a5cd4998292140fca25df0a0b
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-03-16 13:33:04 -0500

    Created the second solution for Q20

--- 0020_ValidParentheses.py
+++ 0020_ValidParentheses.py
@@ -6,16 +6,28 @@ class Solution(object):
         """
         dic ={'(': ')', '[': ']', '{': '}'}
         n = len(s)
-        if n == 2:
-            if s[0] in dic and dic[s[0]] == s[1]:
-                return True
-            else:
-                return False
+        # if n == 2:
+        #     if s[0] in dic and dic[s[0]] == s[1]:
+        #         return True
+        #     else:
+        #         return False
         if n % 2 == 1:
             return False
-        for i in range(n):
-            if s[i] not in dic:
-                if i == 0 or dic[s[i-1]] != s[i]:
+        # for i in range(n):
+        #     if s[i] not in dic:
+        #         if i == 0 or dic[s[i-1]] != s[i]:
+        #             return False
+        #         else:
+        #             return self.isValid(s[:i - 1] + s[i + 1:])
+
+        stack = []
+        for paren in s:
+            if paren in dic:
+                stack.append(paren)
+            else:
+                if len(stack) == 0:
                     return False
-                else:
-                    return self.isValid(s[:i - 1] + s[i + 1:])
+                last_open = stack.pop()
+                if dic[last_open] != paren:
+                    return False
+        return len(stack) == 0
