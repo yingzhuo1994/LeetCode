5e6de2090a946341d3a209918c9163fc420f8574
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-03-23 19:37:00 -0500

    Improved the solution if Q70

--- 0070_ClimbingStairs.py
+++ 0070_ClimbingStairs.py
@@ -1,11 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dic = {-1: 0, 0: 1}
-        k = 1
-        while k <= n:
-            oneStep = dic[k - 1]
-            twoStep = dic[k - 2]
-            total = oneStep + twoStep
-            dic[k] = total
-            k += 1
-        return dic[n]
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a + b
+        return b
