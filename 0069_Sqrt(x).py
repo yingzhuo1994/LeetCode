e7f5ff55f5509e482ca6994fd32e05c87fb93ee8
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-03-25 22:59:06 -0500

    Simplified the solution of Q69.

--- 0069_Sqrt(x).py
+++ 0069_Sqrt(x).py
@@ -3,12 +3,11 @@ class Solution:
         def f(k):
             return k * k - x
         a, b = 0, x
-        c = (a + b) / 2
         while a <= b:
+            c = (a + b) // 2
             if f(c) <= 0 and f(c + 1) > 0:
                 return c
             if f(c) < 0:
                 a = c + 1
             else:
                 b = c - 1
-            c = (a + b) // 2
