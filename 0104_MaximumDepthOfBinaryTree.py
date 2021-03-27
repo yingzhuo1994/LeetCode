31dc8b51b791c3bf3df76d6d6217507913cc0f62
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-03-26 19:22:07 -0500

    Created the iterative solution.

--- 0104_MaximumDepthOfBinaryTree.py
+++ 0104_MaximumDepthOfBinaryTree.py
@@ -9,3 +9,18 @@ class Solution:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    # Iterative solution
+        depth = 0
+        level = [root] if root else []
+        while level:
+            depth += 1
+            queue = []
+            for el in level:
+                if el.left:
+                    queue.append(el.left)
+                if el.right:
+                    queue.append(el.right)
+            level = queue
+
+        return depth
