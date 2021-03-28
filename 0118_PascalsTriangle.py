b81b241810797f7ad6325d1e489d7cdc7841e0c1
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-03-27 22:01:41 -0500

    2nd solution for Q118

--- 0118_PascalsTriangle.py
+++ 0118_PascalsTriangle.py
@@ -1,14 +1,25 @@
 class Solution:
     # 1st Recursive Solution
+    # def generate(self, numRows: int) -> List[List[int]]:
+    #     def helper(n):
+    #         if n == 1:
+    #             return [1]
+    #         else:
+    #             lst = [1 for _ in range(n)]
+    #             lastRow = helper(n - 1)
+    #             for i in range(1, n - 1):
+    #                 lst[i] = lastRow[i - 1] + lastRow[i]
+    #             return lst
+    #     lst = [helper(r) for r in range(1, numRows + 1)]
+    #     return lst
+
+    # 2nd Iterative Solution
     def generate(self, numRows: int) -> List[List[int]]:
-        def helper(n):
-            if n == 1:
-                return [1]
-            else:
-                lst = [1 for _ in range(n)]
-                lastRow = helper(n - 1)
-                for i in range(1, n - 1):
-                    lst[i] = lastRow[i - 1] + lastRow[i]
-                return lst
-        lst = [helper(r) for r in range(1, numRows + 1)]
+        lst = [[1]]
+        for i in range(2, numRows + 1):
+            curRow = [1 for _ in range(i)]
+            lastRow = lst[i-2]
+            for j in range(1, i - 1):
+                curRow[j] = lastRow[j - 1] + lastRow[j]
+            lst.append(curRow)
         return lst
