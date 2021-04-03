9bf248420c258b91b8472a91b785008f07d49eb4
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-02 19:24:34 -0500

    Solution for Q155.

---
+++ 0155_MinStack.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if not self.stack:
+            return None
+        else:
+            minVal = self.stack[0]
+            for elem in self.stack:
+                if minVal > elem:
+                    minVal = elem
+            return minVal
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
