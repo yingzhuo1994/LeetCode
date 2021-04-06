3a81822309f06f72d979e50b02eb96616740f635
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-06 18:43:05 -0500

    2nd solution for Q69.

--- 0069_Sqrt(x).py
+++ 0069_Sqrt(x).py
@@ -1,13 +1,20 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        def f(k):
-            return k * k - x
-        a, b = 0, x
-        while a <= b:
-            c = (a + b) // 2
-            if f(c) <= 0 and f(c + 1) > 0:
-                return c
-            if f(c) < 0:
-                a = c + 1
-            else:
-                b = c - 1
+        # 1st solution
+        # def f(k):
+        #     return k * k - x
+        # a, b = 0, x
+        # while a <= b:
+        #     c = (a + b) // 2
+        #     if f(c) <= 0 and f(c + 1) > 0:
+        #         return c
+        #     if f(c) < 0:
+        #         a = c + 1
+        #     else:
+        #         b = c - 1
+
+        # 2nd Newton Iteration method
+        r = x
+        while r*r > x:
+            r = (r + x//r) // 2
+        return r
