99910c3183bbeaa5643f5f2ebae76a43e3f50cae
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-10 23:11:02 -0500

    2nd Solution for Q122.

--- 0122_BestTimeToBuyAndSellStock(2).py
+++ 0122_BestTimeToBuyAndSellStock(2).py
@@ -1,16 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        start = prices[0]
-        end = start
-        for i, price in enumerate(prices):
-            if price >= end and i < len(prices) - 1:
-                end = price
-            else:
-                if price >= end:
-                    profit += price - start
-                elif end > start:
-                    profit += end - start
-                start = price
-                end = start
-        return profit
+        # profit = 0
+        # start = prices[0]
+        # end = start
+        # for i, price in enumerate(prices):
+        #     if price >= end and i < len(prices) - 1:
+        #         end = price
+        #     else:
+        #         if price >= end:
+        #             profit += price - start
+        #         elif end > start:
+        #             profit += end - start
+        #         start = price
+        #         end = start
+        # return profit
+
+        # 2nd Solution
+        total = 0
+        for i in range(len(prices) - 1):
+            curProfit = prices[i + 1] - prices[i]
+            if curProfit > 0:
+                total += curProfit
+        return total
