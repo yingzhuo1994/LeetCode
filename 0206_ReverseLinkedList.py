f0ce458519f417d9263b73fe311ab6670a5913ab
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-11 13:42:11 -0500

    Renew the solution of Q206.

--- 0206_ReverseLinkedList.py
+++ 0206_ReverseLinkedList.py
@@ -5,12 +5,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        p = ListNode(0)
-        p.next = ListNode(0)
+        prev = None
         while head:
-            p.next.val = head.val
-            temp = ListNode(0)
-            temp.next = p
-            p = temp
+            curr = head
             head = head.next
-        return p.next.next
+            curr.next = prev
+            prev = curr
+        return prev
