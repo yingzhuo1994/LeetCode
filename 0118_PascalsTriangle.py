101fe0f4c6b6274f6449888194ae8fdfca22ec6a
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-11 23:30:07 -0500

    Solution for Q118

--- 0118_PascalsTriangle.py
+++ 0118_PascalsTriangle.py
@@ -14,12 +14,19 @@ class Solution:
     #     return lst
 
     # 2nd Iterative Solution
-    def generate(self, numRows: int) -> List[List[int]]:
-        lst = [[1]]
-        for i in range(2, numRows + 1):
-            curRow = [1 for _ in range(i)]
-            lastRow = lst[i-2]
-            for j in range(1, i - 1):
-                curRow[j] = lastRow[j - 1] + lastRow[j]
-            lst.append(curRow)
-        return lst
+    # def generate(self, numRows: int) -> List[List[int]]:
+    #     lst = [[1]]
+    #     for i in range(2, numRows + 1):
+    #         curRow = [1 for _ in range(i)]
+    #         lastRow = lst[i-2]
+    #         for j in range(1, i - 1):
+    #             curRow[j] = lastRow[j - 1] + lastRow[j]
+    #         lst.append(curRow)
+    #     return lst
+
+    # 3rd Solution
+    def generate(self, numRows):
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [map(lambda x, y: x+y, res[-1] + [0], [0] + res[-1])]
+        return res
