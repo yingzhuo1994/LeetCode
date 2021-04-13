d3a77efeb48db3e2cbf536c42ad6d090b89903c6
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-13 00:14:12 -0500

    Solution for Q242.

---
+++ 0242_ValidAnagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dic = {}
+        for ch in s:
+            if ch not in dic:
+                dic[ch] = 1
+            else:
+                dic[ch] += 1
+        for ch in t:
+            if ch not in dic or dic[ch] == 0:
+                return False
+            else:
+                dic[ch] -= 1
+        return True
