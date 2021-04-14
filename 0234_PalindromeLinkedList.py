878d57834c48f01e69fb0f762bbe4dac9c8eeba8
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-13 23:59:44 -0500

    Solution with restore.

--- 0234_PalindromeLinkedList.py
+++ 0234_PalindromeLinkedList.py
@@ -19,24 +19,38 @@ class Solution:
         # return True
 
         # 2nd Solution
-        slow = fast = head
-        while fast.next != None and fast.next.next != None:
-            slow = slow.next
-            fast = fast.next.next
-        secondHalf = self.reverseLinkedList(slow.next)
-        firstHalf = head
-        while firstHalf and secondHalf:
-            if firstHalf.val != secondHalf.val:
-                return False
-            firstHalf = firstHalf.next
-            secondHalf = secondHalf.next
-        return True
+    #     slow = fast = head
+    #     while fast.next != None and fast.next.next != None:
+    #         slow = slow.next
+    #         fast = fast.next.next
+    #     secondHalf = self.reverseLinkedList(slow.next)
+    #     firstHalf = head
+    #     while firstHalf and secondHalf:
+    #         if firstHalf.val != secondHalf.val:
+    #             return False
+    #         firstHalf = firstHalf.next
+    #         secondHalf = secondHalf.next
+    #     return True
+    #
+    # def reverseLinkedList(self, head: ListNode) -> ListNode:
+    #     prev = None
+    #     while head:
+    #         p = head
+    #         head = head.next
+    #         p.next = prev
+    #         prev = p
+    #     return prev
 
-    def reverseLinkedList(self, head: ListNode) -> ListNode:
-        prev = None
-        while head:
-            p = head
-            head = head.next
-            p.next = prev
-            prev = p
-        return prev
+        #3rd solution
+    def isPalindrome(self, head):
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+        return not rev
