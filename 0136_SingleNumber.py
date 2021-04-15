646303c4a8875188b343b6223afa9d335f7bbe0d
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-15 12:26:45 -0500

    Add complexity analysis.

--- 0136_SingleNumber.py
+++ 0136_SingleNumber.py
@@ -1,8 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # 1st hash table
+        # O(n) time | O(n) space
         # for i in range(len(nums)):
         #     if nums[i] not in nums[:i] and nums[i] not in nums[i+1:]:
         #         return nums[i]
 
         # using set
+        # O(n) time | O(n) space
         return 2 * sum(set(nums)) - sum(nums)
