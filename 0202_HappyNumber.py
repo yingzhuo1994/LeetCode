0f9a29b6b32ea7c53f78fc723594f2e29b373d72
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-17 00:04:08 -0500

    Add 2nd solution.

--- 0202_HappyNumber.py
+++ 0202_HappyNumber.py
@@ -1,14 +1,30 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        dic = []
-        while True:
-            if n == 1:
-                return True
-            if n not in dic:
-                dic.append(n)
-                newN = 0
-                for num in str(n):
-                    newN += int(num) * int(num)
-                n = newN
-            else:
-                return False
+        # 1st solution
+        # dic = []
+        # while True:
+        #     if n == 1:
+        #         return True
+        #     if n not in dic:
+        #         dic.append(n)
+        #         newN = 0
+        #         for num in str(n):
+        #             newN += int(num) * int(num)
+        #         n = newN
+        #     else:
+        #         return False
+
+        # 2nd solution
+        def digitSquareSum(n):
+            squareSum = 0
+            for num in str(n):
+                squareSum += int(num) * int(num)
+            return squareSum
+
+        slow = digitSquareSum(n)
+        fast = digitSquareSum(slow)
+        while slow != fast:
+            slow = digitSquareSum(slow)
+            fast = digitSquareSum(fast)
+            fast = digitSquareSum(fast)
+        return slow == 1
