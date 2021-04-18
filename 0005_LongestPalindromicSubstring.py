84a70e4e4c30e129e5180f9d9d8941282494fbee
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-18 13:38:13 -0500

    1st solution and complexity analysis.

---
+++ 0005_LongestPalindromicSubstring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # 1st solution
+        # O(n^2) time | O(1) space
+        def helper(s, start, end):
+            while start >= 0 and end < len(s) and s[start] == s[end]:
+                start -= 1
+                end += 1
+            return [start + 1, end - 1]
+
+        pair = [0, 0]
+        for i in range(len(s) - 1):
+            pair0 = helper(s, i, i)
+            pair1 = helper(s, i, i + 1)
+            if pair0[1] - pair0[0] > pair[1] - pair[0]:
+                pair = pair0
+            if pair1[1] - pair1[0] > pair[1] - pair[0]:
+                pair = pair1
+        return s[pair[0]:pair[1] + 1]
