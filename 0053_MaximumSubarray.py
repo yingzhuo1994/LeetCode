8f94abf1d9f22d3dab416b18aea9da602fe2f421
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-18 17:10:11 -0500

    Add complexity analysis.

--- 0053_MaximumSubarray.py
+++ 0053_MaximumSubarray.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        for i in range(1, len(nums)):
-            if nums[i-1] > 0:
-                nums[i] += nums[i-1]
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's algorithm
+        # O(n) time | O(1) space
+        for i in range(len(nums) - 1):
+            if nums[i] > 0:
+                nums[i + 1] += nums[i]
         return max(nums)
-            
