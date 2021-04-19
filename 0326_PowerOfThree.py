a77c7bcb68cf8c274a2f6dd9c60d3000ce6a80ab
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-18 21:27:15 -0500

    Add complexity analysis.

--- 0326_PowerOfThree.py
+++ 0326_PowerOfThree.py
@@ -1,3 +1,13 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        # 1st solution
+        # O(1) time | O(1) space
         return n > 0 and 3 ** 19 % n == 0
+
+        # 2nd loop iteration solution
+        # O(logn) time | O(1) space
+        if n < 1:
+            return False
+        while n % 3 == 0:
+            n = n // 3
+        return n == 1
