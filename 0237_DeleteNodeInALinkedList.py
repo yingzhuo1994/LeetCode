fc83480966f6ca6b0f646f6147045161be2b6969
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-18 21:04:54 -0500

    Add complexity analysis.

--- 0237_DeleteNodeInALinkedList.py
+++ 0237_DeleteNodeInALinkedList.py
@@ -10,5 +10,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        # O(1) time | O(1) space
         node.val = node.next.val
         node.next = node.next.next
