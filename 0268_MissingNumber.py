26eeed961cbcb075db86562f3b443fc576337c4c
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-18 21:13:35 -0500

    Add complexity analysis.

--- 0268_MissingNumber.py
+++ 0268_MissingNumber.py
@@ -1,4 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # Gauss' Formula
+        # O(n) time | O(1) space
+        # sum() function costs O(n) time
         n = len(nums)
         return (1 + n) * n // 2 - sum(nums)
