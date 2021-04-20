cfe04b186faf20fd1ca16be26093a02a8e0eea7e
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-20 13:41:18 -0500

    2 solutions and complexity analysis.

---
+++ 0011_ContainerWithMostWater.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # 1st brute-force solution
+        # O(n^2) time | O(1) space
+        area_largest = 0
+        for left in range(len(height) - 1):
+            for right in range(left + 1, len(height)):
+                area = (right - left) * min(height[left], height[right])
+                if area > area_largest:
+                    area_largest = area
+        return area_largest
+
+        # 2nd solution
+        # O(n) time | O(1) space
+        area = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            area = max(area, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return area
