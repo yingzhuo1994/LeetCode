50c2b480334e00811aa98351d03b1b8ec09d2663
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-23 18:19:38 -0500

    Add complexity analysis.

--- 0088_MergeSortedArray.py
+++ 0088_MergeSortedArray.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # O(n) time | O(1) space
         while n > 0:
             if m <= 0 or nums2[n-1] >= nums1[m-1]:
                 nums1[m+n-1] = nums2[n-1]
