b98ad3ebd03c9350dd0277389d9a2e36f76cf83a
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-24 13:33:47 -0500

    Add iterative solution.

--- 0101_SymmetricTree.py
+++ 0101_SymmetricTree.py
@@ -11,6 +11,8 @@ class Solution:
         else:
             return self.helper(root.left, root.right)
 
+    # 1st recursive solution
+    # O(n) time | O(h) time (worst O(n) time)
     def helper(self, L, R):
         if L is not None and R is not None:
             if L.val != R.val:
@@ -21,3 +23,22 @@ class Solution:
             return False
         else:
             return True
+
+    # 2nd iterative solution
+    # O(n) time | O(n) time
+    def helper(self, L, R):
+        stack = [L, R]
+        while len(stack) > 0:
+            curStack = []
+            for i in range(len(stack) // 2):
+                left = stack[2 * i]
+                right = stack[2 * i + 1]
+                if left is None and right is None:
+                    continue
+                elif left is None or right is None or left.val != right.val:
+                    return False
+                elif left.val == right.val:
+                    temp = [left.left, right.right, left.right, right.left]
+                    curStack.extend(temp)
+            stack = curStack
+        return True
