f87278d8bc135fad2fb087bc572aee60bbbf94d4
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-28 13:15:17 -0500

    Complement

--- 0036_ValidSudoku.py
+++ 0036_ValidSudoku.py
@@ -4,6 +4,34 @@ class Solution:
         rowDic = [{} for _ in range(9)]
         columnDic = [{} for _ in range(9)]
         boxDic = [[{}, {}, {}] for _ in range(3)]
+        # for i in range(9):
+        #     for j in range(9):
+        #         elem = board[i][j]
+        #         if board[i][j] == '.':
+        #             continue
+        #         # check row
+        #         if elem in rowDic[i]:
+        #             return False
+        #         else:
+        #             rowDic[i][elem] = 1
+        #
+        #         # check column
+        #         if elem in columnDic[j]:
+        #             return False
+        #         else:
+        #             columnDic[j][elem] = 1
+        #
+        #         # check sub-box
+        #         x, y = i // 3, j // 3
+        #         if elem in boxDic[x][y]:
+        #             return False
+        #         else:
+        #             boxDic[x][y][elem] = 1
+        # return True
+
+        # Each iteration does one thing
+        # There is no difference in time complexity, and space complexity.
+        # As a software engineer, you should write clean code which is easy to understand.
         for i in range(9):
             for j in range(9):
                 elem = board[i][j]
@@ -15,11 +43,23 @@ class Solution:
                 else:
                     rowDic[i][elem] = 1
 
+        for i in range(9):
+            for j in range(9):
+                elem = board[i][j]
+                if board[i][j] == '.':
+                    continue
+        
                 # check column
                 if elem in columnDic[j]:
                     return False
                 else:
                     columnDic[j][elem] = 1
+
+        for i in range(9):
+            for j in range(9):
+                elem = board[i][j]
+                if board[i][j] == '.':
+                    continue
 
                 # check sub-box
                 x, y = i // 3, j // 3
