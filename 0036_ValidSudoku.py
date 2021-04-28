cc9f6da66b9897fd1a2f6782920750a9416571c2
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-28 13:06:01 -0500

    Solution for Q36.

---
+++ 0036_ValidSudoku.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowDic = [{} for _ in range(9)]
+        columnDic = [{} for _ in range(9)]
+        boxDic = [[{}, {}, {}] for _ in range(3)]
+        for i in range(9):
+            for j in range(9):
+                elem = board[i][j]
+                if board[i][j] == '.':
+                    continue
+                # check row
+                if elem in rowDic[i]:
+                    return False
+                else:
+                    rowDic[i][elem] = 1
+
+                # check column
+                if elem in columnDic[j]:
+                    return False
+                else:
+                    columnDic[j][elem] = 1
+
+                # check sub-box
+                x, y = i // 3, j // 3
+                if elem in boxDic[x][y]:
+                    return False
+                else:
+                    boxDic[x][y][elem] = 1
+        return True
