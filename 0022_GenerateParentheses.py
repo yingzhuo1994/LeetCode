3fd62ff8a8c4376aa5882a34306cbcd77e958690
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-28 19:14:45 -0500

    Simplify the solution.

--- 0022_GenerateParentheses.py
+++ 0022_GenerateParentheses.py
@@ -3,11 +3,8 @@ class Solution:
         def helper(left, right):
             if left > right or left < 0:
                 return []
-            elif left == right:
-                return ['(' + paren for paren in helper(left - 1, right)]
-            elif right == 1:
+            if left == 0 and right == 1:
                 return [')']
-            lst = []
             addLeft = ["(" + paren for paren in helper(left - 1, right)]
             addRight = [")" + paren for paren in helper(left, right - 1)]
             return addLeft + addRight
