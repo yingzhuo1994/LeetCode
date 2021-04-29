695fb2a653276c982caaa2a335d5a2206b110ade
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-04-29 12:35:18 -0500

    Solution for Q38

---
+++ 0038_CountAndSay.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        s = self.countAndSay(n - 1)
+        lst = []
+        count = 0
+        ch = s[0]
+        for elem in s:
+            if elem == ch:
+                count += 1
+            else:
+                lst.append(str(count))
+                lst.append(ch)
+                ch = elem
+                count = 1
+        lst.append(str(count))
+        lst.append(ch)
+        return ''.join(lst)
