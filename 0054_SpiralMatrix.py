df1e00f79356ca971cf2ea80d2eea1656d40cb50
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-05-04 12:46:21 -0500

    Solution for Q54.

---
+++ 0054_SpiralMatrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # O(n) time | O(n) space
+        lst = []
+        startRow, endRow = 0, len(matrix) - 1
+        startColumn, endColumn = 0, len(matrix[0]) - 1
+        place = (0, 0)
+        while startRow <= endRow and startColumn <= endColumn:
+            if place == (0, 0):
+                for j in range(startColumn, endColumn + 1):
+                    lst.append(matrix[startRow][j])
+                startRow += 1
+                place = (0, -1)
+            elif place == (0, -1):
+                for i in range(startRow, endRow + 1):
+                    lst.append(matrix[i][endColumn])
+                endColumn -= 1
+                place = (-1, -1)
+            elif place == (-1, -1):
+                for j in reversed(range(startColumn, endColumn + 1)):
+                    lst.append(matrix[endRow][j])
+                endRow -= 1
+                place = (-1, 0)
+            elif place == (-1, 0):
+                for i in reversed(range(startRow, endRow + 1)):
+                    lst.append(matrix[i][startColumn])
+                startColumn += 1
+                place = (0, 0)
+        return lst
