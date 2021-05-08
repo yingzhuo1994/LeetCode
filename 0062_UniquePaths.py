10ceb0097e6b9e185a884cd177a1c7374605c4b2
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-07 21:21:46 -0500

    Add 1st solution for Q62

--- 0062_UniquePaths.py
+++ 0062_UniquePaths.py
@@ -1,5 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # 1st recursive solution
         if m == 1 or n == 1:
             return 1
-        
+        toRight = self.uniquePaths(m, n - 1)
+        toBottom = self.uniquePaths(m - 1, n)
+        return toRight + toBottom
