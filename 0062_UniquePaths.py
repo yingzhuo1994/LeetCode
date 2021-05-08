5f738bf8ecc668c1abe437d65ee4325b276c2bce
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-07 21:59:47 -0500

    Add complexity analysis.

--- 0062_UniquePaths.py
+++ 0062_UniquePaths.py
@@ -1,6 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         # 1st recursive solution
+        # O(mn) time | O(m + n) space
         if m == 1 or n == 1:
             return 1
         toRight = self.uniquePaths(m, n - 1)
@@ -8,6 +9,7 @@ class Solution:
         return toRight + toBottom
 
         # 2nd iterative solution
+        # O(mn) time | O(n) space 
         ways = [1 for _ in range(n)]
         for i in range(m - 1):
             for j in reversed(range(n - 1)):
