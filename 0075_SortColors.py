ebcaf5fc69f3953387f752d44a540e51904561e6
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-09 09:34:25 -0500

    Created 2nd one-pass solution.

--- 0075_SortColors.py
+++ 0075_SortColors.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # 1st two-pass solution
+        # O(2n) time | O(1) space
         zero = 0
         for i in range(len(nums)):
             if nums[i] == 0:
@@ -14,4 +16,24 @@ class Solution:
             if nums[i] == 2:
                 nums[i], nums[two] = nums[two], nums[i]
                 two -= 1
+        
+        # 2nd one-pass solution
+        # O(n) time | O(1) space
+        """ Since all numbers befor zero index have been checked, we could move forward directly.
+            But for numbers after two index, after we change the numbers, we need to check current number again.
+        """ 
+        zero = 0
+        two = len(nums) - 1
+        i = 0
+        while i <= two:
+            if nums[i] == 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[two] = nums[two], nums[i]
+                two -= 1
+            elif nums[i] == 1:
+                i += 1
+            
         
