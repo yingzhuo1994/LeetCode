d7475fe6b0d0f314fe8bdfc0f11575da0574efad
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-10 09:34:39 -0500

    Add 2nd solution and complexity analysis.

--- 0078_Subsets.py
+++ 0078_Subsets.py
@@ -1,5 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # 1st recursive solution
+        # O(n * 2^n) time | O(n * 2^n) space
         if not nums:
             return [[]]
         if len(nums) == 1:
@@ -7,3 +9,10 @@ class Solution:
         last = self.subsets(nums[1:])
         lst = [[nums[0]] + elem for elem in last] + last
         return lst
+
+        # 2nd iterative solution
+        # O(n * 2^n) time | O(n * 2^n) space
+        output = [[]]
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        return output
