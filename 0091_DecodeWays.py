07b5cf5436ec9e6e5d2f2cc572d4d3046345fe18
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-12 13:52:36 -0500

    2nd solution for Q91.

--- 0091_DecodeWays.py
+++ 0091_DecodeWays.py
@@ -18,3 +18,23 @@ class Solution:
             else:
                 return 0
         return lst[-1] 
+
+        # 2nd solution
+        # O(n) time | O(1) space
+        front = 1
+        if int(s[0]) > 0:
+            back = 1
+        else:
+            return 0
+        for i in range(1, len(s)):
+            single, double = 0, 0
+            if int(s[i]) > 0:
+                single = 1
+            if 10 <= int(s[i-1:i+1]) <= 26:
+                double = 1
+            if single or double:
+                cur = back * single + front * double
+                front, back = back, cur
+            else:
+                return 0
+        return back
