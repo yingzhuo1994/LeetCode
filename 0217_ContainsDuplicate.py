7037dd87b85982c9c38e7cc9ab1a659df39ec450
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-12 21:25:53 -0500

    Add 2nd solution

--- 0217_ContainsDuplicate.py
+++ 0217_ContainsDuplicate.py
@@ -1,4 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # 1st solution
         # O(n) time | O(n) space
         return len(nums) > len(set(nums))
+
+        # 2nd sorting solution
+        # O(nlogn) time | O(1) space
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
