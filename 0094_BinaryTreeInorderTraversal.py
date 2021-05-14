e345511ec9758a79c1994be9be398af6eb595aa9
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-14 16:17:43 -0500

    Simplify the solution

--- 0094_BinaryTreeInorderTraversal.py
+++ 0094_BinaryTreeInorderTraversal.py
@@ -8,11 +8,8 @@ class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         # 1st recursive solution
         # O(n) time | O(logn) space
-        p = root
-        if not p:
+        if not root:
             return []
-        elif not p.left and not p.right:
-            return [p.val]
-        leftTree = self.inorderTraversal(p.left)
-        rightTree = self.inorderTraversal(p.right)
-        return leftTree + [p.val] + rightTree
+        leftTree = self.inorderTraversal(root.left)
+        rightTree = self.inorderTraversal(root.right)
+        return leftTree + [root.val] + rightTree
