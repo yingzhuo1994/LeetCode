72c23bfc28c141a9333f779598faefa7a3d4db3f
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-18 10:01:25 -0500

    Two solutions for Q116.

---
+++ 0116_PopulatingNextRightPointersInEachNode.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        # 1st stack solution
+        # O(n) time | O(n) space
+        level = [root] if root else []
+        while level:
+            curLevel = []
+            for i in range(len(level)):
+                if i < len(level) - 1:
+                    level[i].next = level[i + 1]
+                if level[i].left:
+                    curLevel.append(level[i].left)
+                if level[i].right:
+                    curLevel.append(level[i].right)
+            level = curLevel
+        return root
+
+        # 2nd solution
+        # O(n) time | O(1) space
+        head = root
+        while root and root.left:
+            next = root.left 
+            while root:
+                root.left.next = root.right 
+                root.right.next = root.next and root.next.left 
+                root = root.next
+            root = next
+        return head
