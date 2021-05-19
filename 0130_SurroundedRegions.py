5da1d700b484c10ac0f9d45284a9c72e34e02443
yingzhuo1994 <zhuoying1994@gmail.com>
2021-05-19 01:27:53 -0500

    1st solution for Q130

---
+++ 0130_SurroundedRegions.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def checkConnection(board, lst, i, j):
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == "O":
+                if [i, j] not in lst:
+                    lst.append([i, j])
+                    checkConnection(board, lst, i + 1, j)
+                    checkConnection(board, lst, i - 1, j)
+                    checkConnection(board, lst, i, j + 1)
+                    checkConnection(board, lst, i, j - 1)
+
+
+        nearBorder = []
+        rowEnd = len(board) - 1
+        colEnd = len(board[0]) - 1
+        for i in range(len(board)):
+            if board[i][0] == "O":
+                nearBorder.append([i, 0])
+            if board[i][colEnd] == "O":
+                nearBorder.append([i, colEnd])
+        for j in range(1, colEnd):
+            if board[0][j] == "O":
+                nearBorder.append([0, j])
+            if board[rowEnd][j] == "O":
+                nearBorder.append([rowEnd, j])
+        oLst = []
+        for pair in nearBorder:
+            checkConnection(board, oLst, pair[0], pair[1])
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if [i, j] not in oLst:
+                    board[i][j] = "X"
+                
+        
+        
