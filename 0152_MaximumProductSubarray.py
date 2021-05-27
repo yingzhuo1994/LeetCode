1c8d93da816e16ab56086970ae00c8f128309f8a
Ying Zhuo <zhuoying1994@gmail.com>
2021-05-27 09:56:06 -0500

    2nd solution for Q152.

--- 0152_MaximumProductSubarray.py
+++ 0152_MaximumProductSubarray.py
@@ -9,3 +9,24 @@ class Solution:
                 largest = max(largest, curProduct)
         return largest
 
+        # 2nd solution
+        r = nums[0]
+        imax = r
+        imin = r
+        # imax/imin stores the max/min product of
+        # subarray that ends with the current number A[i]
+        for i in range(1, len(nums)):
+            # multiplied by a negative makes big number smaller, small number bigger
+            # so we redefine the extremums by swapping them
+            if nums[i] < 0:
+                imax, imin = imin, imax
+
+            # max/min product for the current number is either the current number itself
+            # or the max/min by the previous number times the current one
+            imax = max(nums[i], imax * nums[i])
+            imin = min(nums[i], imin * nums[i])
+
+            # the newly computed max value is a candidate for our global result
+            r = max(r, imax)
+    
+        return r
