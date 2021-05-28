4d6511d9ff3f2db2b59556ae132092a86900f020
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-05-28 17:04:17 -0500

    2nd solution for Q134

--- 0134_GasStation.py
+++ 0134_GasStation.py
@@ -1,5 +1,7 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # 1st solution
+        # O(n^2) | O(1) space
         start = 0
         while start < len(gas):
             i = start
@@ -15,3 +17,23 @@ class Solution:
                     return i
             start += 1
         return -1
+
+        # 2nd solution
+        # O(n) time | O(1) space
+        # If sum of gas is less than sum of cost, then there is no way to get through all stations.
+        # So while we loop through the stations we sum up, so that at the end we can check the sum.
+        # Otherwise, there must be one unique solution, so the first one I find is the right one. 
+        # If the tank becomes negative, we restart because that can't happen.
+        if (sum(gas) - sum(cost) < 0):
+            return -1
+        
+        gas_tank, start_index = 0, 0
+        
+        for i in range(len(gas)):
+            gas_tank += gas[i] - cost[i]
+            
+            if gas_tank < 0:
+                start_index = i+1
+                gas_tank = 0
+            
+        return start_index
