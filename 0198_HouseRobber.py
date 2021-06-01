9cd3516f72722976bf97eb787157a2eb983dafee
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-01 10:26:15 -0500

    Added the 2nd soluton.

--- 0198_HouseRobber.py
+++ 0198_HouseRobber.py
@@ -10,3 +10,13 @@ class Solution:
         for i in range(2, len(nums)):
             lst[i] = max(lst[i-1], nums[i] + lst[i-2])
         return lst[-1]
+
+        # 2nd solution
+        # O(n) time | O(1) sapce
+        if len(nums) == 1:
+            return nums[0]
+        front = nums[0]
+        back = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            front, back = back, max(back, nums[i] + front)
+        return back
