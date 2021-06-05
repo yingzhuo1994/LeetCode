cecdcd289b8050b48ec388a3c0532584c155babc
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-06-05 10:45:47 -0500

    Add 2 solutions for Q215.

--- 0215_KthLargestElementInAnArray.py
+++ 0215_KthLargestElementInAnArray.py
@@ -4,3 +4,22 @@ class Solution:
         # O(nlogn) time | O(1) space
         nums.sort()
         return nums[-k]
+    
+        # 2nd solution, bubble sort idea
+        # O(nk) time | O(1) space
+        for i in range(k):
+            for j in range(len(nums)-i-1):
+                if nums[j] > nums[j+1]:
+                    # exchange elements, time consuming
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums[-k]
+    
+        # 3rd solution, selection sort idea
+        # O(nk) time | o(1) space
+        for i in range(len(nums), len(nums)-k, -1):
+            tmp = 0
+            for j in range(i):
+                if nums[j] > nums[tmp]:
+                    tmp = j
+            nums[tmp], nums[i-1] = nums[i-1], nums[tmp]
+        return nums[-k]
