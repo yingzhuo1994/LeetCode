ce76ae0f9cff2838bc493ad3643dc68fd0a9e4c8
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-06-05 10:55:08 -0500

    Add the 4th quick select solution

--- 0215_KthLargestElementInAnArray.py
+++ 0215_KthLargestElementInAnArray.py
@@ -23,3 +23,20 @@ class Solution:
                     tmp = j
             nums[tmp], nums[i-1] = nums[i-1], nums[tmp]
         return nums[-k]
+
+        # 4th solution, quick select
+        # O(n) time | O(n) space
+        if not nums: return
+        pivot = random.choice(nums)
+        left =  [x for x in nums if x > pivot]
+        mid  =  [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+        
+        L, M = len(left), len(mid)
+        
+        if k <= L:
+            return self.findKthLargest(left, k)
+        elif k > L + M:
+            return self.findKthLargest(right, k - L - M)
+        else:
+            return mid[0]
