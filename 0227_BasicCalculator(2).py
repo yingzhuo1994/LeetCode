a5a809a5be549a2ee21703faa4c9ab87cba89ef9
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-06-06 11:53:32 -0500

    Add the 2nd solution

--- 0227_BasicCalculator(2).py
+++ 0227_BasicCalculator(2).py
@@ -10,7 +10,6 @@ class Solution:
         operation = '+'
         for i in range(n):
             ch = s[i]
-            print(ch, curNum, operation, stack)
             if ch.isdigit():
                 curNum = (curNum * 10) + int(ch)
             if ch in '+-*/' or i == n-1:
@@ -27,3 +26,29 @@ class Solution:
                 operation = ch
                 curNum = 0
         return sum(stack)     
+
+        # 2nd solution
+        # O(n) time | O(1) space
+        n = len(s)
+        if n == 0:
+            return 0
+        front, back = 0, 0
+        curNum = 0
+        operation = '+'
+        for i in range(n):
+            ch = s[i]
+            print(ch, curNum, operation, front, back)
+            if ch.isdigit():
+                curNum = (curNum * 10) + int(ch)
+            if ch in '+-*/' or i == n-1:
+                if operation == '-':
+                    front, back = front + back, -curNum
+                elif operation == '+':
+                    front, back = front + back, curNum
+                elif operation == '*':     
+                    back = back * curNum
+                elif operation == '/':
+                    back = int(back / curNum)
+                operation = ch
+                curNum = 0
+        return front + back
