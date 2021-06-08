84f11f4d6e079c521860106a5c9f1cfefb46f5a4
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-08 11:43:08 -0500

    Add 2nd solution

--- 0236_LowestCommonAncestorOfABinaryTree.py
+++ 0236_LowestCommonAncestorOfABinaryTree.py
@@ -36,3 +36,39 @@ class Solution:
         # Traverse the tree
         recurse_tree(root)
         return self.ans
+
+    # 2nd Iterative using parent pointers
+    # O(n) time | O(n) space
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Stack for tree traversal
+        stack = [root]
+
+        # Dictionary for parent pointers
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        while p not in parent or q not in parent:
+
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers.
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p.
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers.
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        while q not in ancestors:
+            q = parent[q]
+        return q
