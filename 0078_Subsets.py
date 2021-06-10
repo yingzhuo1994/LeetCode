775105b95f904221d3dbf9009656e0afa4b6ba73
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-10 17:42:45 -0500

    Add the 3rd solution

--- 0078_Subsets.py
+++ 0078_Subsets.py
@@ -10,7 +10,23 @@ class Solution:
 
         # 2nd iterative solution
         # O(n * 2^n) time | O(n * 2^n) space
+     def subsets(self, nums: List[int]) -> List[List[int]]:
         output = [[]]
         for num in nums:
             output += [curr + [num] for curr in output]
         return output
+
+        # 3rd recursive solution
+        # O(n * 2^n) time | O(n * 2^n) space
+    def subsets(self, nums: List[int], idx = None) -> List[List[int]]:
+        if idx is None:
+            idx = len(nums) - 1
+        if idx < 0:
+            return [[]]
+        elem = nums[idx]
+        # avoid using list slicing array[:idx-1]
+        subSets = self.subsets(nums, idx - 1)
+        for i in range(len(subSets)):
+            currentSubset = subSets[i]
+            subSets.append(currentSubset + [elem])
+        return subSets
