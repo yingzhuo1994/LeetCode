f04dc0021df9e68767bdaa4d33c0ad4815dfaea0
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-15 15:02:10 -0500

    Add the 2nd solution

--- 0279_PerfectSquares.py
+++ 0279_PerfectSquares.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 1st solution
     def numSquares(self, n: int) -> int:
         if int(sqrt(n))**2 == n: return 1
         for j in range(int(sqrt(n)) + 1):
@@ -8,4 +9,28 @@ class Solution:
             n >>= 2
         if n % 8 == 7: return 4
         return 3
-        
+
+    # 2nd solution
+    def numSquares(self, n: int) -> int:
+        if n < 2:
+            return n
+        lst = []
+        i = 1
+        while i * i <= n:
+            lst.append( i * i )
+            i += 1
+        cnt = 0
+        toCheck = {n}
+        while toCheck:
+            cnt += 1
+            temp = set()
+            for x in toCheck:
+                for y in lst:
+                    if x == y:
+                        return cnt
+                    if x < y:
+                        break
+                    temp.add(x-y)
+            toCheck = temp
+
+        return cnt
