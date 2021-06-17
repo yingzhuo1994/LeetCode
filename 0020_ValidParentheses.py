1641cc258ca79589d67109897bf17070f99d884d
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-17 17:00:59 -0500

    Simplify the solution

--- 0020_ValidParentheses.py
+++ 0020_ValidParentheses.py
@@ -6,19 +6,9 @@ class Solution(object):
         """
         dic ={'(': ')', '[': ']', '{': '}'}
         n = len(s)
-        # if n == 2:
-        #     if s[0] in dic and dic[s[0]] == s[1]:
-        #         return True
-        #     else:
-        #         return False
+
         if n % 2 == 1:
             return False
-        # for i in range(n):
-        #     if s[i] not in dic:
-        #         if i == 0 or dic[s[i-1]] != s[i]:
-        #             return False
-        #         else:
-        #             return self.isValid(s[:i - 1] + s[i + 1:])
 
         stack = []
         for paren in s:
