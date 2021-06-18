aa36ed208ad43e238027d8349d98d26e540436e6
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-18 09:26:56 -0500

    Simplify the solution

--- 0328_OddEvenLinkedList.py
+++ 0328_OddEvenLinkedList.py
@@ -7,12 +7,12 @@ class Solution:
     # 1st solution
     # O(n) time | O(1) space
     def oddEvenList(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
+        if not head:
             return head
         oddNode = head
         evenHead = head.next
         evenNode = evenHead
-        while oddNode and oddNode.next and evenNode and evenNode.next:
+        while evenNode and evenNode.next:
             oddNode.next = oddNode.next.next
             evenNode.next = evenNode.next.next 
             oddNode = oddNode.next
