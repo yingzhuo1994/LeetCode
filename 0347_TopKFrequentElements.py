ee5068513232320797593442b64e659b28f20533
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-21 10:55:05 -0500

    Two solutions for Q347.

---
+++ 0347_TopKFrequentElements.py
@@ -0,0 +1,28 @@
+class Solution:
+    # 1st solution
+    # O(nlogk) time | O(n+k) space
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # O(1) time 
+        if k == len(nums):
+            return nums
+        
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)   
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key=count.get) 
+
+    # 2nd solution
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frq = defaultdict(list)
+        for key, cnt in Counter(nums).items():
+            frq[cnt].append(key)
+
+        res = []
+        for times in reversed(range(len(nums) + 1)):
+            res.extend(frq[times])
+            if len(res) >= k: return res[:k]
+
+        return res[:k]
