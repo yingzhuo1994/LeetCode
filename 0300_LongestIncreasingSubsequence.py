01003c2e6d79cbd22387396582da35620bc59037
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-22 17:35:36 -0500

    Add the second solution

--- 0300_LongestIncreasingSubsequence.py
+++ 0300_LongestIncreasingSubsequence.py
@@ -1,5 +1,5 @@
 class Solution:
-    # 1st solution
+    # 1st solution, dynamic programming
     # O(n^2) time | O(n) space
     def lengthOfLIS(self, nums: List[int]) -> int:
         countLst = [1 for _ in range(len(nums))]
@@ -9,4 +9,19 @@ class Solution:
                     countLst[i] = max(countLst[j] + 1, countLst[i])
         return max(countLst)
 
-        
+    # 2nd solution, Intelligently Build a Subsequence
+    # O(n^2) time | O(n) space
+    def lengthOfLIS(self, nums: List[int]) -> int:
+    sub = [nums[0]]
+    
+    for num in nums[1:]:
+        if num > sub[-1]:
+            sub.append(num)
+        else:
+            # Find the first element in sub that is greater than num
+            i = 0
+            while num > sub[i]:
+                i += 1
+            sub[i] = num
+
+    return len(sub)
