58bfb6ebe461f440a55a51e040ccf0adc9718d24
Ying Zhuo <zhuoying1994@gmail.com>
2021-06-24 11:02:37 -0500

    Add two solutions.

--- 0380_InsertDeleteGetRandomO(1).py
+++ 0380_InsertDeleteGetRandomO(1).py
@@ -1,17 +1,19 @@
 class RandomizedSet:
 
+    # 1st solution
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.stack = []
+        self.stack, self.pos = [], {}
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val not in self.stack:
+        if val not in self.pos:
             self.stack.append(val)
+            self.pos[val] = len(self.stack) - 1
             return True
         return False
 
@@ -19,11 +21,14 @@ class RandomizedSet:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val not in self.stack:
-            return False
-        self.stack.remove(val)
-        return True
-        
+        if val in self.pos:
+            idx, last = self.pos[val], self.stack[-1]
+            self.stack[idx], self.pos[last] = last, idx
+            self.stack.pop()
+            self.pos.pop(val, 0)
+            return True
+        return False
+    
     def getRandom(self) -> int:
         """
         Get a random element from the set.
@@ -31,8 +36,34 @@ class RandomizedSet:
         return random.choice(self.stack)
 
 
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+    # 2nd solution
+    def __init__(self):
+        self.dic_direct = {}
+        self.dic_invert = {}
+        self.num_elem = 0
+        
+    def insert(self, val: int) -> bool:
+        if val in self.dic_invert:
+            return False
+        else:
+            self.dic_invert[val] = self.num_elem
+            self.dic_direct[self.num_elem] = val
+            self.num_elem += 1
+            return True
+        
+    def remove(self, val):
+        if val not in self.dic_invert:
+            return False
+        else:
+            ind = self.dic_invert.pop(val)
+            self.dic_direct.pop(ind)
+            if ind != self.num_elem - 1:
+                self.dic_direct[ind] = self.dic_direct[self.num_elem - 1]
+                self.dic_invert[self.dic_direct[self.num_elem - 1]] = ind
+                self.dic_direct.pop(self.num_elem - 1)
+            self.num_elem -= 1
+            return True
+        
+    def getRandom(self):
+        index = floor(random.random()*self.num_elem)
+        return self.dic_direct[index]
