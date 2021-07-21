42ac26e024caae534ceb1243f40c5924a3cf3075
Ying Zhuo <zhuoying1994@gmail.com>
2021-07-21 10:25:50 -0500

    Add two solutions.

--- 0010_RegularExpressionMatching.py
+++ 0010_RegularExpressionMatching.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 1st solution
     def isMatch(self, s: str, p: str) -> bool:
         if not p:
             return not s
@@ -11,6 +12,38 @@ class Solution:
                     first_match and self.isMatch(s[1:], p))
         else:
             return first_match and self.isMatch(s[1:], p[1:])
-                
-            
-            
+
+    # 2nd solution, Top-Down
+    # O(TP) time | O(TP) space
+    # where T, P are the lengths of the s and p respectively.
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p):
+                    ans = i == len(s)
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j + 1 < len(p) and p[j + 1] == '*':
+                        ans = dp(i, j + 2) or first_match and dp(i + 1, j)
+                    else:
+                        ans = first_match and dp(i + 1, j + 1)
+                memo[i, j]= ans
+            return memo[i, j]
+        return dp(0, 0)
+
+    # 3rd solution, Bottom-Up
+    # O(TP) time | O(TP) space
+    # where T, P are the lengths of the s and p respectively.
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+
+        dp[-1][-1] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    dp[i][j] = dp[i][j + 2] or first_match and dp[i + 1][j]
+                else:
+                    dp[i][j] = first_match and dp[i + 1][j + 1]
+        return dp[0][0]
