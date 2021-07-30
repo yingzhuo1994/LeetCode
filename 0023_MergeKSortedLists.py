97d86d06ad08684b450e8ed04f9aacc640f8ee8a
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-07-29 23:03:23 -0500

    Add the first solution.

--- 0023_MergeKSortedLists.py
+++ 0023_MergeKSortedLists.py
@@ -5,4 +5,18 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        
+        sentinel = ListNode(0)
+        sentinel.next = lists[0]
+        for i in range(1, len(lists)):
+            p = sentinel
+            q = lists[i]
+            while p.next and q:
+                if p.next.val > q.val:
+                    temp = q
+                    q = q.next
+                    temp.next = p.next
+                    p.next = temp
+                p = p.next
+            if q:
+                p.next = q
+        return sentinel.next
