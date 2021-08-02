e5b913afdfc3966cf245d74fe3719b5cff51499f
Ying Zhuo <zhuoying1994@gmail.com>
2021-08-02 11:27:04 -0500

    Add the 3rd solution

--- 0378_KthSmallestElementInASortedMatrix.py
+++ 0378_KthSmallestElementInASortedMatrix.py
@@ -72,3 +72,25 @@ class Solution:
         start = max(k - n*n + n-1, 0)
         k -= n*n - (n - start)**2
         return biselect(range(start, min(n, start+k)), k, k)[0]
+
+    # 3rd solution
+    # O(nlog(A)) time | O(n) space
+    # where A is the difference between the maximum and minimum in the matrix.
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n, beg, end = len(matrix), matrix[0][0], matrix[-1][-1]
+        
+        def check(m):
+            i, j, cnt = 0, n - 1, 0
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > m: j -= 1
+                cnt += (j + 1)
+            return cnt
+         
+        while beg < end:
+            mid = (beg + end)//2
+            if check(mid) < k:
+                beg = mid + 1
+            else:
+                end = mid
+                
+        return beg
