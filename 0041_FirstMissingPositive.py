37396629b888313ec696ff41e8dcb9f23098ed85
Ying Zhuo <zhuoying1994@gmail.com>
2021-08-03 09:55:05 -0500

    Simplify the 2nd solution

--- 0041_FirstMissingPositive.py
+++ 0041_FirstMissingPositive.py
@@ -19,12 +19,12 @@ class Solution:
         """
         nums.append(0)
         n = len(nums)
-        for i in range(len(nums)): #delete those useless elements
+        for i in range(n): #delete those useless elements
             if nums[i] < 0 or nums[i] >= n:
                 nums[i] = 0
-        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
+        for i in range(n): #use the index as the hash to record the frequency of each number
             nums[nums[i] % n] += n
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             if nums[i] // n == 0:
                 return i
         return n
