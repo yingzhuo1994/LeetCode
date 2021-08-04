1ae6b96ec56c74671c364648fa86882a7a142454
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-03 22:40:47 -0500

    Add the 2nd solution

--- 0042_TrappingRainWater.py
+++ 0042_TrappingRainWater.py
@@ -11,5 +11,27 @@ class Solution:
             for idx in range(i + 1, len(height)):
                 rightMax = max(rightMax, height[idx])
             minHeight = min(leftMax, rightMax)
-            totalWater += minHeight - height[i] if minHeight - height[i] < 0 else 0
+            totalWater += minHeight - height[i] if minHeight - height[i] > 0 else 0
         return totalWater
+
+    # 2nd solution, dynamic programming
+    # O(n) time | O(n) space
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        ans = 0
+        size = len(height)
+        left_max = [0 for _ in range(size)] 
+        right_max = [0 for _ in range(size)]
+        left_max[0] = height[0]
+        for i in range(1, size):
+            left_max[i] = max(height[i], left_max[i - 1])
+        
+        right_max[size - 1] = height[size - 1]
+        for i in reversed(range(size - 1)):
+            right_max[i] = max(height[i], right_max[i + 1])
+
+        for i in range(1, size - 1):
+            ans += min(left_max[i], right_max[i]) - height[i]
+
+        return ans
