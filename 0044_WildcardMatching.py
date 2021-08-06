6caddaabe56747600a4d9ab472b22050093af4ef
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-06 13:41:26 -0500

    Add the first solution

---
+++ 0044_WildcardMatching.py
@@ -0,0 +1,20 @@
+class Solution:
+    # 1st solution
+    # O(mn) time | O(mn) space
+    # where m, n are the length of s and p, separately.
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p)+1)] for i in range(len(s)+1)]
+        dp[0][0] = True
+        for j in range(1, len(p)+1):
+            if p[j-1] != '*':
+                break
+            dp[0][j] = True
+                
+        for i in range(1, len(s)+1):
+            for j in range(1, len(p)+1):
+                if p[j-1] in {s[i-1], '?'}:
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+        return dp[-1][-1]
+
