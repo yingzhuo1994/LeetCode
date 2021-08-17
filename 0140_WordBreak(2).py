7595f24eabf6878fd9fe4fee58789307fe3ea57f
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-17 11:55:28 -0500

    1st solution for Q140

--- 0140_WordBreak(2).py
+++ 0140_WordBreak(2).py
@@ -1,3 +1,24 @@
 class Solution:
+    # 1st solution
+    # O(n^2m) time | O(n^2) space
+    # where m is average length of word
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        n = len(s)
+        dp_solution = [[] for _ in range(n)] + [[""]]
+        dp = [0] * n + [1]
         
+        for k in range(n):
+            for j in range(k,-1,-1):
+                if s[j: k + 1] in wordSet:
+                    dp[k] = max(dp[k], dp[j-1])
+
+        if dp[-2] == 0: return []
+
+        for k in range(n):
+            for j in range(k,-1,-1):
+                if s[j: k + 1] in wordSet:
+                    for sol in dp_solution[j-1]:
+                        dp_solution[k].append(sol + " " + s[j: k + 1])
+                        
+        return [s[1:] for s in dp_solution[-2]]
