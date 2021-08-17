e2b1e9e84a7bc7e9d47daa2a29490cc6de7fb26a
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-17 11:36:18 -0500

    Add complexity analysis

--- 0127_WordLadder.py
+++ 0127_WordLadder.py
@@ -1,4 +1,7 @@
 class Solution:
+    # 1st solution
+    # O(n*26^k) time | O(n) space
+    # where k is the length of beginword, and n is the length of wordList
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         arr = set(wordList) #avoid TLE
         q = collections.deque([(beginWord, 1)])
@@ -15,4 +18,24 @@ class Solution:
                     if new_word in arr and new_word not in visted:
                         q.append((new_word, length + 1))
                         visted.add(new_word)
+        return 0
+
+    # 2nd solution
+    # O(n*26^k) time | O(n) space
+    # where k is the length of beginword, and n is the length of wordList
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        word_set = set(wordList) #avoid TLE
+        q = collections.deque([(beginWord, 1)])
+        alpha = string.ascii_lowercase  #'abcd...z'
+        while q:
+            word, length = q.popleft()
+            if word == endWord:
+                return length
+            
+            for i in range(len(word)):
+                for ch in alpha:
+                    new_word = word[:i] + ch + word[i + 1:]
+                    if new_word in  word_set:
+                        q.append((new_word, length + 1))
+                        word_set.remove(new_word)
         return 0
