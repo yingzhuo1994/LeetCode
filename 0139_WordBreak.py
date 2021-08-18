988b0d4250102f12338574ea4ca8f972322d3e9b
Ying Zhuo <zhuoying1994@gmail.com>
2021-08-18 08:36:52 -0500

    Add annotation

--- 0139_WordBreak.py
+++ 0139_WordBreak.py
@@ -5,7 +5,9 @@ class Solution:
             ok += any(ok[j] and s[j:i] in wordDict for j in range(i)),
         return ok[-1]
 
-        # O(n^2) time | O(n) space
+    # 2nd solution
+    # O(n^2) time | O(n) space
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         check = [False] * (len(s) + 1)
         check[0] = True
         for i in range(1, len(s) + 1):
