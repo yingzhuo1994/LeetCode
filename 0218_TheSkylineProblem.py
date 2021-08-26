1c420224f732033d27c2071c6bf86d04cacde618
Ying Zhuo <zhuoying1994@gmail.com>
2021-08-26 10:39:13 -0500

    Add a solution with explanation.

--- 0218_TheSkylineProblem.py
+++ 0218_TheSkylineProblem.py
@@ -11,5 +11,43 @@ class Solution:
                 res += [x, -hp[0][0]],
         return res[1:]
 
+from heapq import heappush, heappop
 
+class Solution(object):
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        # 不难发现这些关键点的特征是：竖直线上轮廓升高或者降低的终点
+        # 所以核心思路是：从左至右遍历建筑物，记录当前的最高轮廓，如果产生变化则记录一个关键点
+        
+        # 首先记录构造一个建筑物的两种关键事件
+        # 第一种是轮廓升高事件(L, -H)、第二种是轮廓降低事件(R, 0)
+        # 轮廓升高事件(L, -H, R)中的R用于后面的最小堆
+        events = [(L, -H, R) for L, R, H in buildings]
+        events += list({(R, 0, 0) for _, R, _ in buildings})
+
+        # 先根据L从小到大排序、再根据H从大到小排序(记录为-H的原因)
+        # 这是因为我们要维护一个堆保存当前最高的轮廓
+        events.sort()
+
+        # 保存返回结果
+        res = [[0, 0]]
+        
+        # 最小堆，保存当前最高的轮廓(-H, R)，用-H转换为最大堆，R的作用是记录该轮廓的有效长度
+        live = [(0, float("inf"))]
+
+        # 从左至右遍历关键事件
+        for L, negH, R in events:
+            
+            # 如果是轮廓升高事件，记录到最小堆中
+            if negH: heappush(live, (negH, R))
+            
+            # 获取当前最高轮廓
+            # 根据当前遍历的位置L，判断最高轮廓是否有效
+            # 如果无效则剔除，让次高的轮廓浮到堆顶，继续判断
+            while live[0][1] <= L: 
+                heappop(live)
+            
+            # 如果当前的最高轮廓发生了变化，则记录一个关键点
+            if res[-1][1] != -live[0][0]:
+                res += [ [L, -live[0][0]] ]
+        return res[1:]
         
