d7db20b696d0faa398b41d762c89b8ee9525a498
yingzhuo <yingzhuo1994@gmail.com>
2021-08-31 10:33:10 -0500

    Add the 2nd solution

--- 0239_SlidingWindowMaximum.py
+++ 0239_SlidingWindowMaximum.py
@@ -2,6 +2,7 @@ from typing import Collection
 
 
 class Solution:
+    # 1st solution
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
         hp = []
@@ -14,3 +15,19 @@ class Solution:
                 heapq.heappop(hp)
             res.append(-hp[0][0])
         return res
+
+    # 2nd solution
+    # O(n) time | O(k) space
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        deq, n, ans = deque([0]), len(nums), []
+
+        for i in range(n):
+            while deq and deq[0] <= i - k:
+                deq.popleft()
+            while deq and nums[i] >= nums[deq[-1]]:
+                deq.pop()
+            deq.append(i)
+            
+            ans.append(nums[deq[0]])
+            
+        return ans[k-1:]
