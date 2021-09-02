db05ba34039652e19bf40b3f8c1c1cc1b1dab2a6
yingzhuo <yingzhuo1994@gmail.com>
2021-09-01 23:28:44 -0500

    Add a solution

--- 0140_WordBreak(2).py
+++ 0140_WordBreak(2).py
@@ -22,3 +22,26 @@ class Solution:
                         dp_solution[k].append(sol + " " + s[j: k + 1])
                         
         return [seq[1:] for seq in dp_solution[-2]]
+
+    # 2nd solution
+    # O(n^2m) time | O(n^2) space
+    # where m is average length of word
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        dp = [False for _ in range(len(s) + 1)]
+        dp[-1] = True
+        for i in range(len(s)):
+            for j in range(i + 1):
+                if dp[j - 1] and s[j: i + 1] in wordSet:
+                    dp[i] = True
+
+        if not dp[-2]:
+            return []
+        res = [[] for _ in range(len(s) + 1)]
+        res[-1] = ['']
+        for i in range(len(s)):
+            for j in range(i+1):
+                if s[j: i + 1] in wordSet:
+                    res[i].extend([w + ' ' + s[j: i + 1] for w in res[j - 1]])
+        return [w[1:] for w in res[-2]]
+                    
