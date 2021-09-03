43d6fd0eabdafeeebab2b33668e41d028ef8f38d
yingzhuo <yingzhuo1994@gmail.com>
2021-09-03 09:51:48 -0500

    Add the 2nd solution

--- 0315_CountOfSmallerNumberAfterSelf.py
+++ 0315_CountOfSmallerNumberAfterSelf.py
@@ -8,3 +8,21 @@ class Solution:
                 if nums[j] < nums[i]:
                     counts[i] += 1
         return counts
+
+    # 2nd solution
+    # O(nlogn) time | O(n) space
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        def sort(enum):
+            half = len(enum) // 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                for i in range(len(enum))[::-1]:
+                    if not right or left and left[-1][1] > right[-1][1]:
+                        smaller[left[-1][0]] += len(right)
+                        enum[i] = left.pop()
+                    else:
+                        enum[i] = right.pop()
+            return enum
+        smaller = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return smaller
