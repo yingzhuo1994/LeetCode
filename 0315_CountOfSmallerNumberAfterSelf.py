044506c2329d13a50ecf640c7167029a35e0b5e9
yingzhuo <yingzhuo1994@gmail.com>
2021-09-04 11:13:46 -0500

    Simplify the 2nd solution

--- 0315_CountOfSmallerNumberAfterSelf.py
+++ 0315_CountOfSmallerNumberAfterSelf.py
@@ -16,7 +16,7 @@ class Solution:
             half = len(enum) // 2
             if half:
                 left, right = sort(enum[:half]), sort(enum[half:])
-                for i in range(len(enum))[::-1]:
+                for i in reversed(range(len(enum))):
                     if not right or left and left[-1][1] > right[-1][1]:
                         smaller[left[-1][0]] += len(right)
                         enum[i] = left.pop()
