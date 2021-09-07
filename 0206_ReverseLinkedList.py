88b7e204ef0e41021885a16f4cda623de4bb72e4
yingzhuo <yingzhuo1994@gmail.com>
2021-09-07 10:49:29 -0500

    Add two solutions

--- 0206_ReverseLinkedList.py
+++ 0206_ReverseLinkedList.py
@@ -4,8 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # 1st solution
+    # O(n) time | O(1) space
     def reverseList(self, head: ListNode) -> ListNode:
-        # O(n) time | O(1) space
         prev = None
         while head:
             curr = head
@@ -13,3 +14,20 @@ class Solution:
             curr.next = prev
             prev = curr
         return prev
+
+    # 2nd solution
+    # O(n) time | O(1) space
+    def reverseList(self, head: ListNode) -> ListNode:
+        new = None
+        while head:
+            new = ListNode(head.val, new)
+            head = head.next
+        return new
+    
+    # 3rd solution
+    # O(n) time | O(1) space
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next: 
+            return head
+        node, head.next.next, head.next = self.reverseList(head.next), head, None
+        return node
