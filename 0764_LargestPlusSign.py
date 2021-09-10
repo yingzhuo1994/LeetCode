bf452c38a3ec5d863bbfcc4655daddcde67b933f
yingzhuo <yingzhuo1994@gmail.com>
2021-09-09 21:15:30 -0500

    2nd solution

--- 0764_LargestPlusSign.py
+++ 0764_LargestPlusSign.py
@@ -2,36 +2,47 @@ class Solution:
     # 1st solution, brute-force method, TLE
     # O(n^3) time | O(1) space
     def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
-        maxOrder = n // 2 + 1
-        while maxOrder > 0:
-            for i in range(n):
-                for j in range(n):
-                    if self.check(n, mines, maxOrder, i, j):
-                        return maxOrder
-            maxOrder -= 1
-        return 0
+        banned = {tuple(mine) for mine in mines}
+        ans = 0
+        for r in range(n):
+            for c in range(n):
+                k = 0
+                while (k <= r < n-k and k <= c < n-k and
+                        (r-k, c) not in banned and
+                        (r+k, c) not in banned and
+                        (r, c-k) not in banned and
+                        (r, c+k) not in banned):
+                    k += 1
+                ans = max(ans, k)
+        return ans
 
-    def check(self, n, mines, order, x, y):
-        if self.checkIndex(n, x, y):
-            left = x - order + 1
-            right = x + order - 1
-            top = y - order + 1
-            bottom = y + order - 1
-            if self.checkIndex(n, left, right) and self.checkIndex(n, top, bottom):
-                for i in range(left, right + 1):
-                    if [i, y] in mines:
-                        return False
-                for j in range(top, bottom + 1):
-                    if [x, j] in mines:
-                        return False
-                return True
-            else:
-                return False
-        else:
-            return False
-    
-
-    def checkIndex(self, n, i, j):
-        if i < 0 or i >= n or j < 0 or j >= n:
-            return False
-        return True
+    # 2nd solution
+    def orderOfLargestPlusSign(self, n: int, mines: List[List[int]]) -> int:
+        baned = {tuple(mine) for mine in mines}
+        dp = [[0] * n for _ in range(n)]
+        ans = 0
+        
+        for r in range(n):
+            count = 0
+            for c in range(n):
+                count = 0 if (r,c) in baned else count+1
+                dp[r][c] = count
+            
+            count = 0
+            for c in range(n-1, -1, -1):
+                count = 0 if (r,c) in baned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+        
+        for c in range(n):
+            count = 0
+            for r in range(n):
+                count = 0 if (r,c) in baned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+            
+            count = 0
+            for r in range(n-1, -1, -1):
+                count = 0 if (r, c) in baned else count+1
+                if count < dp[r][c]: dp[r][c] = count
+                if dp[r][c] > ans: ans = dp[r][c]
+        
+        return ans
