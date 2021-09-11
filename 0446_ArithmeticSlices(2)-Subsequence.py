6caa22d61e2cdb836c2931d4a1e58098bff0ddaf
yingzhuo <yingzhuo1994@gmail.com>
2021-09-10 22:51:36 -0500

    Add brute force solution

--- 0446_ArithmeticSlices(2)-Subsequence.py
+++ 0446_ArithmeticSlices(2)-Subsequence.py
@@ -1,4 +1,28 @@
 class Solution:
+    # 1st solution
+    # O(2^n) time | O(n) space
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        def dfs(dep, A, cur):
+            if dep == n:
+                if len(cur) < 3:
+                    return
+                diff = cur[1] - cur[0]
+                for i in range(1, len(cur)):              
+                    if cur[i] - cur[i - 1] != diff:
+                        return
+                self.ans += 1
+                return
+            dfs(dep + 1, A, cur)
+            cur.append(A[dep])
+            dfs(dep + 1, A, cur)
+            cur.remove(A[dep])
+
+        n = len(nums)
+        self.ans = 0
+        cur = []
+        dfs(0, nums, cur)
+        return self.ans   
+
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         total, n = 0, len(nums)
         dp = [Counter() for item in nums]
