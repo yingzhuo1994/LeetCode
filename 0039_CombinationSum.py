9b41017c19659bd7da61f947870a712dd87c7a72
yingzhuo <yingzhuo1994@gmail.com>
2021-09-12 23:19:26 -0500

    Modify the solution

--- 0039_CombinationSum.py
+++ 0039_CombinationSum.py
@@ -1,36 +1,5 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # Sorting is really helpful, se we can avoid over counting easily
-        candidates.sort()                      
-        result = []
-        self.combine_sum_2(candidates, 0, [], result, target)
-        return result
-    
-    def combine_sum_2(self, nums, start, path, result, target):
-        # Base case: if the sum of the path satisfies the target, we will consider 
-        # it as a solution, and stop there
-        if not target:
-            result.append(path)
-            return
-        
-        for i in range(start, len(nums)):
-            # Very important here! We don't use `i > 0` because we always want 
-            # to count the first element in this recursive step even if it is the same 
-            # as one before. To avoid overcounting, we just ignore the duplicates
-            # after the first element.
-            if i > start and nums[i] == nums[i - 1]:
-                continue
-
-            # If the current element is bigger than the assigned target, there is 
-            # no need to keep searching, since all the numbers are positive
-            if nums[i] > target:
-                break
-
-            # We change the start to `i + 1` because one element only could
-            # be used once
-            self.combine_sum_2(nums, i + 1, path + [nums[i]], result, target - nums[i])
-
-    # 2nd solution
+    # 1st solution
     # O(n^(m/c + 1)) time | O(m/c) space
     # where n = len(candidateds), m = target, c = min(candidates)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
@@ -46,3 +15,20 @@ class Solution:
             return 
         for i in range(len(nums)):
             self.dfs(nums[i:], target - nums[i], path + [nums[i]], ret)
+
+    # 2nd solution
+    # O(n^(m/c + 1)) time | O(m/c) space
+    # where n = len(candidateds), m = target, c = min(candidates)
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ret = []
+        self.dfs(candidates, 0, target, [], ret)
+        return ret
+    
+    def dfs(self, nums, start, target, path, ret):
+        if target < 0:
+            return 
+        if target == 0:
+            ret.append(path)
+            return 
+        for i in range(start, len(nums)):
+            self.dfs(nums, i, target - nums[i], path + [nums[i]], ret)
