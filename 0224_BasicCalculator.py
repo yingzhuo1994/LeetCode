d06f5f587757959ba758f17e098c4ca6224bbe1f
yingzhuo <yingzhuo1994@gmail.com>
2021-09-16 22:52:23 -0500

    Add the 2nd solution

--- 0224_BasicCalculator.py
+++ 0224_BasicCalculator.py
@@ -20,4 +20,29 @@ class Solution:
                 num = 0
         return res + num * sign
 
-
+    # 2nd solution
+    # O(n) time | O(n) space
+    def calculate(self, s: str) -> int:
+        def update(op, v):
+            if op == "+": stack.append(v)
+            if op == "-": stack.append(-v)
+            if op == "*": stack.append(stack.pop() * v)           
+            if op == "/": stack.append(int(stack.pop() / v))      
+    
+        it, num, stack, sign = 0, 0, [], "+"
+        
+        while it < len(s):
+            if s[it].isdigit():
+                num = num * 10 + int(s[it])
+            elif s[it] in "+-*/":
+                update(sign, num)
+                num, sign = 0, s[it]
+            elif s[it] == "(":                 
+                num, j = self.calculate(s[it + 1:])
+                it = it + j
+            elif s[it] == ")":             
+                update(sign, num)
+                return sum(stack), it + 1
+            it += 1
+        update(sign, num)
+        return sum(stack)
