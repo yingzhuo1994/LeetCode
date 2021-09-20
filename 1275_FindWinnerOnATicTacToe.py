d3b6f903827b9d668a1aadd86780f69ffb0ffdb4
yingzhuo <yingzhuo1994@gmail.com>
2021-09-20 09:44:54 -0500

    2nd solution

--- 1275_FindWinnerOnATicTacToe.py
+++ 1275_FindWinnerOnATicTacToe.py
@@ -3,8 +3,8 @@ class Solution:
     # O(1) time | O(1) space
     def tictactoe(self, moves: List[List[int]]) -> str:
         grid = [[0 for j in range(3)] for i in range(3)]
-        for i, movie in enumerate(moves):
-            x, y = movie
+        for i, move in enumerate(moves):
+            x, y = move
             if i & 1 == 0:
                 grid[x][y] = 1
             else:
@@ -47,3 +47,44 @@ class Solution:
             return "B"
         else:
             return None
+
+    # 2nd solution
+    # O(m) time | O(n) space
+    def tictactoe(self, moves: List[List[int]]) -> str:
+
+        # n stands for the size of the board, n = 3 for the current game.
+        n = 3
+
+        # use rows and cols to record the value on each row and each column.
+        # diag1 and diag2 to record value on diagonal or anti-diagonal.
+        rows, cols = [0] * n, [0] * n
+        diag = anti_diag = 0
+        
+        # Two players having value of 1 and -1, player_1 with value = 1 places first.
+        player = 1
+        
+        for row, col in moves:
+            
+            # Update the row value and column value.
+            rows[row] += player
+            cols[col] += player
+            
+            # If this move is placed on diagonal or anti-diagonal, 
+            # we shall update the relative value as well.
+            if row == col:            
+                diag += player
+            if row + col == n - 1:
+                anti_diag += player
+                
+            # check if this move meets any of the winning conditions.
+            if any(abs(line) == n for line in (rows[row], cols[col], diag, anti_diag)):
+                return "A" if player == 1 else "B"
+        
+            # If no one wins so far, change to the other player alternatively. 
+            # That is from 1 to -1, from -1 to 1.
+            player *= -1
+            
+        # If all moves are completed and there is still no result, we shall check if 
+        # the grid is full or not. If so, the game ends with draw, otherwise pending.
+        return "Draw" if len(moves) == n * n else "Pending"   
+        
