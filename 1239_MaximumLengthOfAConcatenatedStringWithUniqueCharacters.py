aa4e87a6037e4c1138a82af0230c449dcacbeaf1
yingzhuo <yingzhuo1994@gmail.com>
2021-09-22 09:11:09 -0500

    1st solution

--- 1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters.py
+++ 1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters.py
@@ -4,18 +4,18 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
         ans = [0]
         path = ''
-        self.dfs(arr, path, ans)
+        self.dfs(arr, 0, path, ans)
         return ans[0]
     
-    def dfs(self, arr, path, ans):
+    def dfs(self, arr, idx, path, ans):
         dic = Counter(path)
         for k, v in dic.items():
             if v > 1:
                 return 
         ans[0] = max(ans[0], len(path))
         
-        if ans[0] == 26 or len(arr) == 0:
+        if ans[0] == 26 or idx == len(arr):
             return 
         
-        for i in range(len(arr)):
-            self.dfs(arr[i + 1:], path + arr[i], ans)
+        for i in range(idx, len(arr)):
+            self.dfs(arr, i + 1, path + arr[i], ans)
