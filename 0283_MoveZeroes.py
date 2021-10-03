9502dfa7c2dbf25b8c7209e1ee2ac7355e2235b9
yingzhuo <yingzhuo1994@gmail.com>
2021-10-02 23:05:04 -0500

    Renew

--- 0283_MoveZeroes.py
+++ 0283_MoveZeroes.py
@@ -1,9 +1,10 @@
 class Solution:
+    # 1st solution
+    # O(n) time | O(1) space
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # O(n) time | O(1) space
         zero = 0  # records the position of "0"
         for i in range(len(nums)):
             if nums[i] != 0:
