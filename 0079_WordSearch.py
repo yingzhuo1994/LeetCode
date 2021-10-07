d1b270a5e72c44d296d5a662446751ee4c30b1bc
yingzhuo <yingzhuo1994@gmail.com>
2021-10-06 23:20:42 -0500

    Simplify the solution

--- 0079_WordSearch.py
+++ 0079_WordSearch.py
@@ -43,27 +43,26 @@ class Solution:
                     return True
         return False
     
-        # 3rd solution
-        def lookNeighbor(board, word, i, j, k):
-            if k > len(word) - 1:
-                return True
-            result = False
-            letter = None
-            if 0 <= i < len(board) and 0 <= j < len(board[0]) and (i, j) not in lst and board[i][j] == word[k]:
-                letter = board[i][j]
-                board[i][j] = '**'
-                result = lookNeighbor(board, word, i - 1, j, k + 1) \
-                      or lookNeighbor(board, word, i + 1, j, k + 1) \
-                      or lookNeighbor(board, word, i, j - 1, k + 1) \
-                      or lookNeighbor(board, word, i, j + 1, k + 1)
-            if letter:
-                board[i][j] = letter
-            return result
-
+    # 3rd solution
+    def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
-                isContained = lookNeighbor(board, word, i, j, 0)
-                if isContained:
+                if self.dfs(board, i, j, word, 0):
                     return True
+        return False
+    
+    def dfs(self, board, i, j, word, k):
+        if board[i][j] == word[k] and k == len(word) - 1:
+            return True
+        if board[i][j] != word[k]:
+            return False
+        temp = board[i][j]
+        board[i][j] = "#"
+        for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            row, col = i + x, j + y
+            if 0 <= row < len(board) and 0 <= col < len(board[0]):
+                if self.dfs(board, row, col, word, k + 1):
+                    return True
+        board[i][j] = temp
         return False
 
