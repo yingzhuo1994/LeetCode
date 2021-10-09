e1bbb440aedf2e2da6345ed7990af2ce31bd9ef1
yingzhuo <yingzhuo1994@gmail.com>
2021-10-09 00:21:24 -0500

    1st solution

---
+++ 0542_01Matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat and mat[0])
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    mat[i][j] = float("inf")
+                    if i > 0 and mat[i - 1][j] + 1 < mat[i][j]:
+                        mat[i][j] = mat[i - 1][j] + 1
+                    if j > 0 and mat[i][j - 1] + 1 < mat[i][j]:
+                        mat[i][j] = mat[i][j - 1] + 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if mat[i][j] != 0:
+                    if i + 1 < m and mat[i + 1][j] + 1 < mat[i][j]:
+                        mat[i][j] = mat[i + 1][j] + 1
+                    if j + 1 < n and mat[i][j + 1] + 1 < mat[i][j]:
+                        mat[i][j] = mat[i][j + 1] + 1
+        return mat
