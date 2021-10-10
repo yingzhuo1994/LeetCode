7b980c9e7205205f91fbc411294dc4e0ab6f8611
yingzhuo <yingzhuo1994@gmail.com>
2021-10-10 11:18:32 -0500

    2ns solution

--- 0542_01Matrix.py
+++ 0542_01Matrix.py
@@ -1,5 +1,5 @@
 class Solution:
-    # 1st solution
+    # 1st solution, dp
     # O(r * c) time | O(1) space
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat and mat[0])
@@ -18,4 +18,22 @@ class Solution:
                         mat[i][j] = mat[i + 1][j] + 1
                     if j + 1 < n and mat[i][j + 1] + 1 < mat[i][j]:
                         mat[i][j] = mat[i][j + 1] + 1
+        return mat
+
+    # 2nd solution, back tracking
+    # O(r * c) time | O(r * c) space
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        q, m, n = [], len(mat), len(mat[0])
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    mat[i][j] = 0x7fffffff
+                else:
+                    q.append((i, j))
+        for i, j in q:
+            for r, c in ((i, 1+j), (i, j-1), (i+1, j), (i-1, j)):
+                z = mat[i][j] + 1
+                if 0 <= r < m and 0 <= c < n and mat[r][c] > z:
+                    mat[r][c] = z
+                    q.append((r, c))
         return mat
