7eaea64e7fc9cb87b51268d63c1ef2801717d22e
yingzhuo <yingzhuo1994@gmail.com>
2021-10-13 22:08:36 -0500

    3rd solution

--- 0136_SingleNumber.py
+++ 0136_SingleNumber.py
@@ -1,11 +1,17 @@
 class Solution:
+    # 1st hash table
+    # O(n) time | O(n) space
     def singleNumber(self, nums: List[int]) -> int:
-        # 1st hash table
-        # O(n) time | O(n) space
-        # for i in range(len(nums)):
-        #     if nums[i] not in nums[:i] and nums[i] not in nums[i+1:]:
-        #         return nums[i]
+        for i in range(len(nums)):
+            if nums[i] not in nums[:i] and nums[i] not in nums[i+1:]:
+                return nums[i]
 
-        # using set
-        # O(n) time | O(n) space
+    # 2nd solution, using set
+    # O(n) time | O(n) space
+    def singleNumber(self, nums: List[int]) -> int:
         return 2 * sum(set(nums)) - sum(nums)
+
+    # 3rd solution, XOR
+    # O(n) time | O(1) space
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x^y, nums, 0)
