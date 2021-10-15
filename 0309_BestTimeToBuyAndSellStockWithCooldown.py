1a61354cba411bf3dc9eaf451e11615d06c69ab1
yingzhuo <yingzhuo1994@gmail.com>
2021-10-14 22:50:35 -0500

    1st solution

---
+++ 0309_BestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,13 @@
+class Solution:
+    # O(n) time | O(n) space
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n <= 1: return 0
+        
+        diff = [prices[i+1] - prices[i] for i in range(n-1)]
+        dp, dp_max = [0]*(n + 1), [0]*(n + 1)
+        for i in range(n-1):
+            dp[i] = diff[i] + max(dp_max[i-3], dp[i-1])
+            dp_max[i] = max(dp_max[i-1], dp[i])
+            
+        return dp_max[-3]
