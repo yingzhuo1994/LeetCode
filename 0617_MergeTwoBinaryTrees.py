4a7cc5d2437e355373903a8c7f5adbbd238e4cb9
yingzhuo <yingzhuo1994@gmail.com>
2021-10-16 11:16:19 -0500

    2nd solution

--- 0617_MergeTwoBinaryTrees.py
+++ 0617_MergeTwoBinaryTrees.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # 1st solution
+    # 1st solution, recursion
     # O(n) time | O(log(n)) space
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root1 or not root2:
@@ -15,3 +15,27 @@ class Solution:
         newTree.left = self.mergeTrees(root1.left, root2.left)
         newTree.right = self.mergeTrees(root1.right, root2.right)
         return newTree
+
+    # 2nd solution, iteration
+    # O(n) time | O(n) space
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1 or not root2:
+            return root1 or root2
+        stack = []
+        stack.append((root1, root2))
+        while stack:
+            t = stack.pop();
+            if not t[0] or not t[1]:
+                continue
+            t[0].val += t[1].val
+        
+            if not t[0].left:
+                t[0].left = t[1].left
+            else:
+                stack.append((t[0].left, t[1].left))
+            
+            if not t[0].right:
+                t[0].right = t[1].right
+            else:
+                stack.append((t[0].right, t[1].right))
+        return root1
