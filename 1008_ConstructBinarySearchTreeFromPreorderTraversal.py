853ae59956d6e9861512b14d95479e51e23cc32e
yingzhuo <yingzhuo1994@gmail.com>
2021-10-16 23:47:16 -0500

    2nd solution

--- 1008_ConstructBinarySearchTreeFromPreorderTraversal.py
+++ 1008_ConstructBinarySearchTreeFromPreorderTraversal.py
@@ -21,3 +21,18 @@ class Solution:
         node.left = self.bstFromPreorder(preorder[1:breakIndex + 1])
         node.right = self.bstFromPreorder(preorder[breakIndex + 1:])
         return node
+
+    # 2nd solution
+    # O(n) time | O(n) space
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        def helper(down, up):
+            if self.idx >= len(preorder): return None
+            if not down < preorder[self.idx] < up: return None
+            root = TreeNode(preorder[self.idx])
+            self.idx += 1
+            root.left = helper(down, root.val)
+            root.right = helper(root.val, up)
+            return root
+        
+        self.idx = 0
+        return helper(-float("inf"), float("inf"))
