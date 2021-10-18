7013aae2819cf7312028c9f1f9a45409810eb773
yingzhuo <yingzhuo1994@gmail.com>
2021-10-17 23:33:13 -0500

    1st solution

---
+++ 0993_CousinsInBinaryTree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        nodes = collections.defaultdict(list)
+        queue = deque([(root,0,0)])
+        findX = False
+        findY = False
+        while queue:
+            node,level,parent = queue.popleft()
+            nodes[node.val] = [level,parent]
+            if node.val == x:
+                findX = True
+            if node.val == y:
+                findY = True
+            if findX and findY:
+                break
+            if node.left:
+                queue.append((node.left,level+1,node.val))
+            if node.right:
+                queue.append((node.right,level+1,node.val))
+
+        if nodes[x][0] == nodes[y][0] and nodes[x][1] != nodes[y][1]:
+            return True
+
+        return False
+
