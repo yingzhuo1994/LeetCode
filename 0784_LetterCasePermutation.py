23d81ae3679925de6b9fec300a2e68eb090cf306
yingzhuo <yingzhuo1994@gmail.com>
2021-10-18 10:22:00 -0500

    2nd solution

--- 0784_LetterCasePermutation.py
+++ 0784_LetterCasePermutation.py
@@ -20,3 +20,18 @@ class Solution:
                 new.extend([lst[:i] + small + lst[i + 1:] for lst in result])
                 result = new
         return result
+
+    # 2nd solution
+    def letterCasePermutation(self, s: str) -> List[str]:
+        result = [s]
+        for i in range(len(s)):
+            if s[i].isalpha():
+                for k in range(len(result)):
+                    string = result[k]
+                    lower = string[:i] + s[i].lower() + string[i+1:]
+                    upper = string[:i] + s[i].upper() + string[i+1:]
+                    if string[i].isupper():
+                        result.append(lower)
+                    if string[i].islower():
+                        result.append(upper)
+        return result
