d17c67296aaea66472521766696349ebcb31aa6a
yingzhuo <yingzhuo1994@gmail.com>
2021-10-18 10:45:03 -0500

    Simplify the solution

--- 0994_RottingOranges.py
+++ 0994_RottingOranges.py
@@ -1,13 +1,11 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        visited = set()
         rotten = []
         m, n = len(grid), len(grid[0])
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     rotten.append((i, j))
-                    visited.add((i, j))
         if self.isAllRotten(grid):
             return 0
         steps = 0
@@ -17,9 +15,8 @@ class Solution:
             for i, j in rotten:
                 for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                     row, col = i + x, j + y
-                    if 0 <= row < m and 0 <= col < n and (row, col) not in visited and grid[row][col] == 1:
+                    if 0 <= row < m and 0 <= col < n and grid[row][col] == 1:
                         grid[row][col] = 2
-                        visited.add((row, col))
                         curRotten.append((row, col))
             rotten = curRotten
         
