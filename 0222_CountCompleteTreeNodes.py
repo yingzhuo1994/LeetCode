776a5633d25b66015f214b6dc8f8e239666ea5c4
yingzhuo <yingzhuo1994@gmail.com>
2021-10-24 00:06:46 -0500

    2nd solution

--- 0222_CountCompleteTreeNodes.py
+++ 0222_CountCompleteTreeNodes.py
@@ -24,3 +24,21 @@ class Solution:
             level += 1
             stack = newStack
         return 2**(level - 1) - 1 + count
+
+
+    # 2nd solution
+    # O(log(n) * log(n)) time | O(log(n)) space
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        leftDepth = self.getDepth(root.left)
+        rightDepth = self.getDepth(root.right)
+        if leftDepth == rightDepth:
+            return pow(2, leftDepth) + self.countNodes(root.right)
+        else:
+            return pow(2, rightDepth) + self.countNodes(root.left)
+
+    def getDepth(self, root):
+        if not root:
+            return 0
+        return 1 + self.getDepth(root.left)
