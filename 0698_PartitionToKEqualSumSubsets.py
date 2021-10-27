66889ee40bcf729ca0fe55271583b04495326f58
yingzhuo <yingzhuo1994@gmail.com>
2021-10-27 11:46:06 -0500

    2nd solution

--- 0698_PartitionToKEqualSumSubsets.py
+++ 0698_PartitionToKEqualSumSubsets.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 1st solution
     # O(k*2^n) time | O(n) space
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
@@ -26,7 +27,23 @@ class Solution:
         
         return can_partition(k)
 
+    # 2nd solution
+    # O(2^n*n) time | O(2^n) space
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        N = len(nums)
+        nums.sort(reverse = True)
 
+        basket, rem = divmod(sum(nums), k)
+        if rem or nums[0] > basket: return False
 
+        dp = [-1] * (1<<N) 
+        dp[0] = 0
+        for mask in range(1<<N):
+            for j in range(N):
+                neib = dp[mask ^ (1<<j)]
+                if mask & (1<<j) and neib >= 0 and neib + nums[j] <= basket:
+                    dp[mask] = (neib + nums[j]) % basket
+                    break
 
+        return dp[-1] == 0
 
