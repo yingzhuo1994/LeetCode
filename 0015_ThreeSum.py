f36f1cce4faf24f6f16098988eb05c6236ee4d89
yingzhuo <yingzhuo1994@gmail.com>
2021-10-27 23:46:53 -0500

    2nd solution

--- 0015_ThreeSum.py
+++ 0015_ThreeSum.py
@@ -24,3 +24,27 @@ class Solution:
                     p1 += 1
                     p2 -= 1
         return lst
+
+    # 2nd solution
+    # O(n^2) time | O(n) space
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        target = 0
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            curTarget = target - nums[i]
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] > curTarget:
+                    right -= 1
+                elif nums[left] + nums[right] < curTarget:
+                    left += 1
+                else:
+                    curLst = [nums[i], nums[left], nums[right]]
+                    if not result or curLst != result[-1]:
+                        result.append(curLst)
+                    left += 1
+                    right -= 1
+        return result
