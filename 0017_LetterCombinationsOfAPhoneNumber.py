c22234f22f9262228047f778136dbf2d5924287d
yingzhuo <yingzhuo1994@gmail.com>
2021-10-30 18:11:05 -0500

    modify

--- 0017_LetterCombinationsOfAPhoneNumber.py
+++ 0017_LetterCombinationsOfAPhoneNumber.py
@@ -1,6 +1,6 @@
 class Solution:
+    # O(4^n) time | O(4^n) space
     def letterCombinations(self, digits: str) -> List[str]:
-        # O(4^n) time | O(4^n) space
         dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
                '6': 'mno', '7': 'pqrs', '8': 'tuv', "9": 'wxyz'}
         n = len(digits)
