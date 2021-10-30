c1db21deb37bc33f3328df09a08e98c2d4361bcb
yingzhuo <yingzhuo1994@gmail.com>
2021-10-29 23:27:28 -0500

    add two methods

--- 1044_LongestDuplicateSubstring.py
+++ 1044_LongestDuplicateSubstring.py
@@ -1,5 +1,5 @@
+# 1st solution, TLE
 class Solution:
-    # 1st solution, TLE
     def longestDupSubstring(self, s: str) -> str:
         length = len(s)
         while length > 0:
@@ -11,3 +11,68 @@ class Solution:
                 stringSet.add(string)
             length -= 1
         return ""
+
+# 2nd solution, MLE
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        trie = Trie(s)
+        print(trie.dic)
+        result = ""
+        for i in range(1, len(s)):
+            string = s[i:]
+            cur = trie.add(string)
+            if len(cur) > len(result):
+                result = cur
+        return result
+    
+
+class Trie:
+    def __init__(self, s):
+        self.dic = {}
+        dic = self.dic
+        for ch in s:
+            dic = dic.setdefault(ch, {})
+
+    def add(self, s):
+        dic = self.dic
+        path = ""
+        for ch in s:
+            if ch in dic:
+                path += ch
+            dic = dic.setdefault(ch, {})
+        return path
+
+# 3rd solution
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        beg, end = 0, len(S)
+        q = (1<<31) - 1 
+        Found = ""
+        while beg + 1 < end:
+            mid = (beg + end)//2
+            isFound, candidate = self.RabinKarp(S, mid, q)
+            if isFound:
+                beg, Found = mid, candidate
+            else:
+                end = mid
+
+        return Found
+
+    def RabinKarp(self,text, M, q):
+        if M == 0: return True
+        h, t, d = (1<<(8*M-8))%q, 0, 256
+
+        dic = defaultdict(list)
+
+        for i in range(M): 
+            t = (d * t + ord(text[i]))% q
+
+        dic[t].append(i-M+1)
+
+        for i in range(len(text) - M):
+            t = (d*(t-ord(text[i])*h) + ord(text[i + M]))% q
+            for j in dic[t]:
+                if text[i+1:i+M+1] == text[j:j+M]:
+                    return (True, text[j:j+M])
+            dic[t].append(i+1)
+        return (False, "")
