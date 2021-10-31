43df7df10124cacec1c65e9794499e76693fab3d
yingzhuo <yingzhuo1994@gmail.com>
2021-10-31 16:55:17 -0500

    simplify the solution

--- 0015_ThreeSum.py
+++ 0015_ThreeSum.py
@@ -2,32 +2,6 @@ class Solution:
     # 1st solution
     # O(n^2) time | O(n) space
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        if n < 3:
-            return []
-        nums.sort()
-        lst = []
-        for i in range(n - 2):
-            p1 = i + 1
-            p2 = n - 1
-            curGoal = 0 - nums[i]
-            while p1 < p2:
-                curSum = nums[p1] + nums[p2]
-                if curSum > curGoal:
-                    p2 -= 1
-                elif curSum < curGoal:
-                    p1 += 1
-                else:
-                    curLst = [nums[i], nums[p1], nums[p2]]
-                    if curLst not in lst:
-                        lst.append(curLst)
-                    p1 += 1
-                    p2 -= 1
-        return lst
-
-    # 2nd solution
-    # O(n^2) time | O(n) space
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
         target = 0
