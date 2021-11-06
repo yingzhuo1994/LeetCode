c363f84c619569aa7edf33b9d1270b82fd1c1da2
yingzhuo <yingzhuo1994@gmail.com>
2021-11-05 23:11:35 -0500

    explanation

--- 0260_SingleNumber(3).py
+++ 0260_SingleNumber(3).py
@@ -2,6 +2,8 @@ class Solution:
     # O(n) time | O(1) space
     def singleNumber(self, nums: List[int]) -> List[int]:
         s = reduce(xor, nums)
+        # nz is non-zero bit
+        # s & s -1 is used to remove last 1
         nz = s & (s-1) ^ s
         num1 = reduce(xor, filter(lambda n: n & nz, nums))
         return(num1, s ^ num1)
