918a05a827b9380946e4f5d8b094903670c4a66f
yingzhuo <yingzhuo1994@gmail.com>
2021-11-11 11:35:56 -0600

    renew the solution

--- 1413_MinimumValueToGetPositiveStepByStepSum.py
+++ 1413_MinimumValueToGetPositiveStepByStepSum.py
@@ -1,16 +1,12 @@
 # 1st solution
+# O(n) time | O(1) space
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
         startValue = 1
-        while True:
-            cur = startValue
-            isTrue = True
-            for num in nums:
-                cur += num
-                if cur < 1:
-                    isTrue = False
-                    break
-            if isTrue:
-                return startValue
-            startValue += 1
-            
+        curSum = 1
+        for num in nums:
+            curSum += num
+            if curSum < 1:
+                startValue += 1 - curSum
+                curSum = 1
+        return startValue
