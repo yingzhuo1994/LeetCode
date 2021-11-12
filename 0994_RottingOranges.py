c13edbb58c99a3d9fb9404e351c8cbbea0f48346
yingzhuo <yingzhuo1994@gmail.com>
2021-11-12 09:53:18 -0600

    2nd solution

--- 0994_RottingOranges.py
+++ 0994_RottingOranges.py
@@ -1,5 +1,6 @@
+# 1st solution
+# O(mn) time | O(1) space
 class Solution:
-    # O(mn) time | O(1) space
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
@@ -38,3 +39,26 @@ class Solution:
                 if grid[i][j] == 2:
                     rotten.append((i, j))
         return rotten
+
+# 2nd solution
+# O(mn) time | O(1) space
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n, queue, fresh = len(grid), len(grid[0]), deque(), 0
+        for i,j in product(range(m), range(n)):
+            if grid[i][j] == 2: queue.append((i,j))
+            if grid[i][j] == 1: fresh += 1
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+        levels = 0
+        
+        while queue:
+            levels += 1
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in dirs:
+                    if 0<=x+dx<m and 0<=y+dy<n and grid[x+dx][y+dy] == 1:
+                        fresh -= 1
+                        grid[x+dx][y+dy] = 2
+                        queue.append((x+dx, y+dy))
+                        
+        return -1 if fresh != 0 else max(levels-1, 0)
