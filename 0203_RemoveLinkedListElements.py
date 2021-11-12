de92a62abc0e21a3a2fde073ba5354ed4c7fe99b
yingzhuo <yingzhuo1994@gmail.com>
2021-11-11 19:28:22 -0600

    2nd solution

--- 0203_RemoveLinkedListElements.py
+++ 0203_RemoveLinkedListElements.py
@@ -15,3 +15,18 @@ class Solution:
                 p = p.next
             head = head.next
         return sentinel.next
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        sentinel = ListNode(0)
+        p = sentinel
+        while head:
+            if head.val != val:
+                p.next = head
+                p = p.next
+            temp = head
+            head = head.next
+            temp.next = None
+        return sentinel.next
