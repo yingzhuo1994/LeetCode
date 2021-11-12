1c2bf063ecce9659c5080ff2bc51b0da80c624a3
yingzhuo <yingzhuo1994@gmail.com>
2021-11-11 19:24:37 -0600

    1st solution

---
+++ 0203_RemoveLinkedListElements.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        sentinel = ListNode(0)
+        p = sentinel
+        while head:
+            if head.val != val:
+                p.next = ListNode(head.val)
+                p = p.next
+            head = head.next
+        return sentinel.next
