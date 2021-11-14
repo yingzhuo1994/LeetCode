f63e1cfa7a48bc9df81b6adcafe4c6171975304d
yingzhuo <yingzhuo1994@gmail.com>
2021-11-14 10:20:08 -0600

    2nd solution

--- 0739_DailyTemperatures.py
+++ 0739_DailyTemperatures.py
@@ -11,3 +11,25 @@ class Solution:
                 result[lastIdx] = i - lastIdx
             stack.append(i)
         return result
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        hottest = 0
+        answer = [0] * n
+        
+        for curr_day in range(n - 1, -1, -1):
+            current_temp = temperatures[curr_day]
+            if current_temp >= hottest:
+                hottest = current_temp
+                continue
+            
+            days = 1
+            while temperatures[curr_day + days] <= current_temp:
+                # Use information from answer to search for the next warmer day
+                days += answer[curr_day + days]
+            answer[curr_day] = days
+
+        return answer
