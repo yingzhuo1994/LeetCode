e3b94af232ec7bfedfcd9bb63501dd7a2f011a44
yingzhuo <yingzhuo1994@gmail.com>
2021-11-15 09:17:32 -0600

    modify

--- 0123_BestTimeToBuyAndSellStock(3).py
+++ 0123_BestTimeToBuyAndSellStock(3).py
@@ -1,20 +1,22 @@
 class Solution:
     # O(n) time | O(n) space
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) <= 1: return 0
+        if len(prices) <= 1: 
+            return 0
         n, k = len(prices), 2
 
-        B = [prices[i+1] - prices[i] for i in range(len(prices) - 1)]
-        if k > len(prices)//2: return sum(x for x in B if x > 0)
+        profits = [prices[i + 1] - prices[i] for i in range(len(prices) - 1)]
+        if k > len(prices) // 2:
+            return sum(x for x in profits if x > 0)
         
-        dp = [[0]*(k+1) for _ in range(n-1)] 
-        mp = [[0]*(k+1) for _ in range(n-1)] 
+        dp = [[0] * (k + 1) for _ in range(n - 1)] 
+        mp = [[0] * (k + 1) for _ in range(n - 1)] 
 
-        dp[0][1], mp[0][1] = B[0], B[0]
+        dp[0][1], mp[0][1] = profits[0], profits[0]
 
-        for i in range(1, n-1):
-            for j in range(1, k+1):
-                dp[i][j] = max(mp[i-1][j-1], dp[i-1][j]) + B[i]
-                mp[i][j] = max(dp[i][j], mp[i-1][j])
+        for i in range(1, n - 1):
+            for j in range(1, k + 1):
+                dp[i][j] = max(mp[i - 1][j - 1], dp[i - 1][j]) + profits[i]
+                mp[i][j] = max(dp[i][j], mp[i - 1][j])
 
         return max(mp[-1])
