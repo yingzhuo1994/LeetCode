43f6f06e58e50b51280ad877dd05391ec7b7a56e
yingzhuo <yingzhuo1994@gmail.com>
2021-11-14 23:43:44 -0600

    1st solution

---
+++ 0787_CheapestFlightsWithinKStops.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = defaultdict(list)
+        dist = [[float("inf")] * (k+2) for _ in range(n)] 
+        dist[src][0] = 0
+
+        SList = SortedList([[0, 0, src]])
+        
+        for i, j, w in flights: 
+            graph[i].append([j, w])
+        
+        while SList:
+            price, visited, city = SList.pop(0)
+
+            if city == dst: return price
+            
+            if visited <= k:
+                for neibh, price_neibh in graph[city]: 
+                    candidate = dist[city][visited] + price_neibh
+                    if candidate <= dist[neibh][visited + 1]:
+                        dist[neibh][visited + 1] = candidate
+                        SList.add([price + price_neibh, visited + 1, neibh])
+                        
+        return -1
