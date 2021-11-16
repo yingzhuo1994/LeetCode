b61b2a8f8633252cafb7a4cf3713f62ad6b8a85f
yingzhuo <yingzhuo1994@gmail.com>
2021-11-16 11:36:07 -0600

    1st solution

--- 0787_CheapestFlightsWithinKStops.py
+++ 0787_CheapestFlightsWithinKStops.py
@@ -2,25 +2,18 @@
 # O(E+(vk)*log(vk)) time
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
-        dist = [[float("inf")] * (k+2) for _ in range(n)] 
-        dist[src][0] = 0
-
-        SList = SortedList([[0, 0, src]])
-        
-        for i, j, w in flights: 
-            graph[i].append([j, w])
-        
-        while SList:
-            price, visited, city = SList.pop(0)
-
-            if city == dst: return price
-            
-            if visited <= k:
-                for neibh, price_neibh in graph[city]: 
-                    candidate = dist[city][visited] + price_neibh
-                    if candidate <= dist[neibh][visited + 1]:
-                        dist[neibh][visited + 1] = candidate
-                        SList.add([price + price_neibh, visited + 1, neibh])
-                        
+        graph = collections.defaultdict(dict)
+        for s, d, w in flights:
+            graph[s][d] = w
+        pq = [(0, src, k + 1)]
+        visited = [0] * n
+        while pq:
+            w, city, stops = heapq.heappop(pq)
+            if city == dst:
+                return w
+            if visited[city] >= stops:
+                continue
+            visited[city] = stops
+            for neibh, dw in graph[city].items():
+                heapq.heappush(pq, (w + dw, neibh, stops - 1))
         return -1
