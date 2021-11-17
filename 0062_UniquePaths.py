aaf9e898821c55f3486a4f6d8759aa1ef5e50772
yingzhuo <yingzhuo1994@gmail.com>
2021-11-16 20:53:07 -0600

    modify

--- 0062_UniquePaths.py
+++ 0062_UniquePaths.py
@@ -1,23 +1,27 @@
+# 1st recursive solution
+# O(mn) time | O(m + n) space
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # 1st recursive solution
-        # O(mn) time | O(m + n) space
         if m == 1 or n == 1:
             return 1
         toRight = self.uniquePaths(m, n - 1)
         toBottom = self.uniquePaths(m - 1, n)
         return toRight + toBottom
 
-        # 2nd iterative solution
-        # O(mn) time | O(n) space 
+# 2nd iterative solution
+# O(mn) time | O(n) space
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int: 
         ways = [1 for _ in range(n)]
-        for i in range(m - 1):
-            for j in reversed(range(n - 1)):
-                ways[j] = ways[j] + ways[j + 1]
-        return ways[0]
+        for i in range(1, m):
+            for j in range(1, n):
+                ways[j] = ways[j] + ways[j - 1]
+        return ways[-1]
 
-        # 3nd math solution
-        # O(mn) time | O(1) space
+# 3nd math solution
+# O(mn) time | O(1) space
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         def factorial(n):
             result = 1
             for i in range(2, n + 1):
