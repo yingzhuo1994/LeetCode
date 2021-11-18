fd0efe955d119c204f91c24155698a43c37ebd90
yingzhuo <yingzhuo1994@gmail.com>
2021-11-17 23:54:18 -0600

    2nd solution

--- 0787_CheapestFlightsWithinKStops.py
+++ 0787_CheapestFlightsWithinKStops.py
@@ -17,3 +17,20 @@ class Solution:
             for neibh, dw in graph[city].items():
                 heapq.heappush(pq, (w + dw, neibh, stops - 1))
         return -1
+
+# 2nd solution
+# Bellman-Ford Algorithm
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        dist = [float("inf") for i in range(n)]
+        dist[src] = 0;
+        
+        # Run only K+1 times since we want shortest distance in K hops
+        for i in range(k + 1):
+            tmp = dist[:]
+            for flight in flights:
+                if dist[flight[0]] != float("inf"):
+                    tmp[flight[1]] = min(tmp[flight[1]], dist[flight[0]] + flight[2])
+            dist = tmp
+
+        return -1 if dist[dst] == float("inf") else dist[dst]
