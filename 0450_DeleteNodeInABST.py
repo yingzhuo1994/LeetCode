a17d68c67b9bebe3bf53bb00963775c983baac3d
yingzhuo <yingzhuo1994@gmail.com>
2021-11-21 23:50:04 -0600

    1st solution

---
+++ 0450_DeleteNodeInABST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 1st solution
+# O(h) time| O(h) space
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root: return None
+        
+        if root.val == key:
+            if not root.left or not root.right:
+                return root.left or root.right
+            
+            if root.left and root.right:
+                temp = root.right
+                while temp.left: temp = temp.left
+                root.val = temp.val
+                root.right = self.deleteNode(root.right, root.val)
+
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
+            
+        return root
