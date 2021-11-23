c1eb8bf939a57179d2957eb741c7fcc96bf013d1
yingzhuo <yingzhuo1994@gmail.com>
2021-11-23 09:39:55 -0600

    2nd solution

--- 0450_DeleteNodeInABST.py
+++ 0450_DeleteNodeInABST.py
@@ -15,15 +15,41 @@ class Solution:
             if not root.left or not root.right:
                 return root.left or root.right
             
-            if root.left and root.right:
-                temp = root.right
-                while temp.left: temp = temp.left
-                root.val = temp.val
-                root.right = self.deleteNode(root.right, root.val)
+            node = root.right
+            while node.left:
+                node = node.left
+            root.val = node.val
+            root.right = self.deleteNode(root.right, root.val)
 
         elif root.val > key:
             root.left = self.deleteNode(root.left, key)
         else:
             root.right = self.deleteNode(root.right, key)
             
+        return root
+
+# 2nd solution, moer efficient
+# O(h) time | O(h) space
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        if root.val == key:
+            if not root.left or not root.right:
+                return root.left or root.right
+            node = root.right
+            parent = root
+            while node.left:
+                node, parent = node.left, node
+            if node != root.right:
+                root.val = node.val
+                parent.left = node.right
+            else:
+                root.val = node.val
+                root.right = node.right
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
         return root
