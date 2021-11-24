308968f4a00c7bf9f4c59a941b53f02beb11be8d
yingzhuo <yingzhuo1994@gmail.com>
2021-11-23 20:19:39 -0600

    2nd simplified solution

--- 0986_IntervalListIntersections.py
+++ 0986_IntervalListIntersections.py
@@ -1,3 +1,4 @@
+# 1st solution
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         if not firstList or not secondList:
@@ -28,3 +29,27 @@ class Solution:
             elif listTwo[0] >= listOne[1]:
                 first += 1
         return result
+
+# 2nd solution
+# O(m + n) time | O(m + n) space
+class Solution:
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        ans = []
+        i = j = 0
+
+        while i < len(A) and j < len(B):
+            # Let's check if A[i] intersects B[j].
+            # lo - the startpoint of the intersection
+            # hi - the endpoint of the intersection
+            lo = max(A[i][0], B[j][0])
+            hi = min(A[i][1], B[j][1])
+            if lo <= hi:
+                ans.append([lo, hi])
+
+            # Remove the interval with the smallest endpoint
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+
+        return ans
