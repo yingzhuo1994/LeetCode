31e3e2aa7864e05b8bba8d38a052e0a92bcb9aad
yingzhuo <yingzhuo1994@gmail.com>
2021-11-24 23:13:02 -0600

    modify

--- 0053_MaximumSubarray.py
+++ 0053_MaximumSubarray.py
@@ -1,7 +1,7 @@
+# Kadane's algorithm
+# O(n) time | O(1) space
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # Kadane's algorithm
-        # O(n) time | O(1) space
         for i in range(len(nums) - 1):
             if nums[i] > 0:
                 nums[i + 1] += nums[i]
