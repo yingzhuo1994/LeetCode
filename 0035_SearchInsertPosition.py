1d43505d6f9dfb2ed7ce011e9441e840b6ea9511
yingzhuo <yingzhuo1994@gmail.com>
2021-11-25 23:27:10 -0600

    modify

--- 0035_SearchInsertPosition.py
+++ 0035_SearchInsertPosition.py
@@ -1,11 +1,12 @@
+# 1st solution
+# O(log(n)) time | O(1) space
 class Solution:
-    # 1st solution
-    # O(log(n)) time | O(1) space
     def searchInsert(self, nums: List[int], target: int) -> int:
         return bisect.bisect_left(nums, target)
 
-    # 2nd solution
-    # O(log(n)) time | O(1) space
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         beg, end = 0, len(nums)
         while beg < end:
