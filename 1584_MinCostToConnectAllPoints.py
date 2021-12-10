f64f5b8128510f611be5f3f57af92264896d8aa6
yingzhuo <yingzhuo1994@gmail.com>
2021-12-10 10:37:58 -0600

    rectify complexity analysis

--- 1584_MinCostToConnectAllPoints.py
+++ 1584_MinCostToConnectAllPoints.py
@@ -1,5 +1,5 @@
 # 1st solution, Kruskal's algorithm
-# O(n^2) time | O(n^2) space
+# O(n^2 * log(n)) time | O(n^2) space
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
@@ -53,17 +53,17 @@ class DisjointSet:
         return self.parent[a]
 
 # 2nd solution, Prim's algorithm
-# O(n^2) time | O(n^2) space
+# O(n^2 * log(n)) time | O(n^2) space
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         manhattan = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        n, c = len(points), collections.defaultdict(list)
+        n, edges = len(points), collections.defaultdict(list)
         for i in range(n):
             for j in range(i + 1, n):
                 dist = manhattan(points[i], points[j])
-                c[i].append((dist, j))
-                c[j].append((dist, i))
-        cnt, ans, visited, heap = 1, 0, [0] * n, c[0]
+                edges[i].append((dist, j))
+                edges[j].append((dist, i))
+        cnt, ans, visited, heap = 1, 0, [0] * n, edges[0]
         visited[0] = 1
         heapq.heapify(heap)
         while heap:
@@ -71,7 +71,7 @@ class Solution:
             if not visited[j]:
                 visited[j], cnt= 1, cnt + 1
                 ans += dist
-                for record in c[j]: 
+                for record in edges[j]: 
                     heapq.heappush(heap, record)
             if cnt >= n: break        
         return ans
