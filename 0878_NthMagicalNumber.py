8ed7ddaa5fbc487aa9ed0875f7ef859ab9dbadd9
yingzhuo <yingzhuo1994@gmail.com>
2021-12-10 23:21:51 -0600

    2nd solution

--- 0878_NthMagicalNumber.py
+++ 0878_NthMagicalNumber.py
@@ -1,3 +1,4 @@
+# 1st solution
 class Solution:
     def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
         g = self.gcd(a, b)
@@ -23,6 +24,33 @@ class Solution:
         else:
             ans = k * lst[-1] + lst[r - 1]
         return ans % mod
+    
+    
+    def gcd(self, a, b):
+        while a:
+            a, b = b % a, a
+        return b
+
+# 2nd solution, binary search
+# O(log(N*min(a, b))) time | O(1) space
+class Solution:
+    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
+        mod = 10 **9 + 7
+        g = self.gcd(a, b)
+        largest = a // g * b
+
+        def largeEnough(x):
+            return x // a + x // b - x // largest >= n
+        
+        left, right = 0, n * min(a, b)
+        while left < right:
+            middle = left + (right - left) // 2
+            if largeEnough(middle):
+                right = middle
+            else:
+                left = middle + 1
+
+        return left % mod
     
     
     def gcd(self, a, b):
