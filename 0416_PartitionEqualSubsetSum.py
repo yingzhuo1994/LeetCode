04d3ae2c81c7d01abbaf3e5bc6b39f1677231778
yingzhuo <yingzhuo1994@gmail.com>
2021-12-12 16:28:00 -0600

    3rd solution

--- 0416_PartitionEqualSubsetSum.py
+++ 0416_PartitionEqualSubsetSum.py
@@ -31,3 +31,20 @@ class Solution:
             dp = [dp[s] or (s >= x and dp[s-x]) for s in range(target+1)]
             if dp[target]: return True
         return False
+
+# 3rd solution
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        s, n, memo = sum(nums), len(nums), {0: True}
+        if s & 1: return False
+        nums.sort(reverse=True)
+        def dfs(i, x):
+            if x not in memo:
+                memo[x] = False
+                if x > 0:
+                    for j in range(i, n):
+                        if dfs(j+1, x-nums[j]):
+                            memo[x] = True
+                            break
+            return memo[x]
+        return dfs(0, s >> 1)
