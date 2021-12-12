36c0af67c633ba49dc5eb7c5e9f14e5c91971ab3
yingzhuo <yingzhuo1994@gmail.com>
2021-12-11 23:06:57 -0600

    2nd solution

--- 0416_PartitionEqualSubsetSum.py
+++ 0416_PartitionEqualSubsetSum.py
@@ -1,6 +1,8 @@
 # 1st solution, TLE
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
         total = sum(nums)
         if total % 2 == 1:
             return False
@@ -16,4 +18,16 @@ class Solution:
             if nums[i] < goal:
                 if self.dfs(nums, i + 1, goal - nums[i]):
                     return True
+        return False
+
+# 2nd solution
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        target, n = sum(nums), len(nums)
+        if target & 1: return False
+        target >>= 1
+        dp = [True] + [False]*target
+        for x in nums:
+            dp = [dp[s] or (s >= x and dp[s-x]) for s in range(target+1)]
+            if dp[target]: return True
         return False
