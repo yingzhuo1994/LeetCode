6c9b57eebb09808a42d5283786dc8b9fca73f5a8
yingzhuo <yingzhuo1994@gmail.com>
2021-12-13 19:09:54 -0600

    2nd solution

--- 1235_MaximumProfitInJobScheduling.py
+++ 1235_MaximumProfitInJobScheduling.py
@@ -11,3 +11,17 @@ class Solution:
                 if table[j][1] <= table[i][0] and dp[j] + table[i][2] > dp[i]:
                     dp[i] = dp[j] + table[i][2]
         return max(dp)
+
+# 2nd solution
+# O(n*log(n)) time | O(n) space
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[1])
+        dpEndTime = [0]
+        dpProfit = [0]
+        for start, end, profit in jobs:
+            i = bisect.bisect_right(dpEndTime, start) - 1
+            if dpProfit[i] + profit > dpProfit[-1]:
+                dpEndTime.append(end)
+                dpProfit.append(dpProfit[i] + profit)
+        return dpProfit[-1]
