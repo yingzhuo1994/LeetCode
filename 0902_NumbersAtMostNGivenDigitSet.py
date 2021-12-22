af0ee1e2705e07d1c5e8701a7ac2c94964abed74
yingzhuo <yingzhuo1994@gmail.com>
2021-12-22 10:11:12 -0600

    simplify

--- 0902_NumbersAtMostNGivenDigitSet.py
+++ 0902_NumbersAtMostNGivenDigitSet.py
@@ -1,44 +1,4 @@
-class Solution:
-    def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
-        if n == 0:
-            return 0
-
-        length = 0
-        while 10**length <= n:
-            length += 1
-        
-        k = len(digits)
-        
-        if k != 1:
-            rest = k * (1 - k**(length - 1)) // (1 - k)
-        else:
-            rest = length - 1
-
-        digitSet = set([int(digit) for digit in digits])
-
-        front = self.fullDigit(digitSet, n, k, length)
-        
-        return front + rest
-
-    def fullDigit(self, digitSet, n, k, length):
-        if length == 1:
-            count = 0
-            for i in range(1, n + 1):
-                if i in digitSet:
-                    count += 1
-            return count
-        count = 0
-        firstDigit = n // 10**(length - 1)
-        for i in range(1, firstDigit):
-            if i in digitSet:
-                count += 1
-
-        if firstDigit in digitSet:
-            return self.fullDigit(digitSet, n % 10**(length - 1), k, length - 1) + count * k**(length - 1)
-        else:
-            return count * k**(length - 1)
-
-# 2nd solution
+# 1st solution
 #  O(log(n)) time | O(log(n)) space
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
