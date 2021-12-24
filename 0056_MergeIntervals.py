54254bb7f515708cd2e2782557d5f63c3f5791d9
yingzhuo <yingzhuo1994@gmail.com>
2021-12-23 20:58:16 -0600

    renew

--- 0056_MergeIntervals.py
+++ 0056_MergeIntervals.py
@@ -1,6 +1,6 @@
+# O(nlogn) time | O(n) space
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # O(nlogn) time | O(n) space
         intervals.sort()
         lst = [intervals[0]]
         for i in range(1, len(intervals)):
