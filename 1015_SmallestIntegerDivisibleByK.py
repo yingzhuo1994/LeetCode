be547bfc7d19c5519118acb624f4068d6c22af0d
yingzhuo <yingzhuo1994@gmail.com>
2021-12-29 19:25:08 -0600

    1st solution

---
+++ 1015_SmallestIntegerDivisibleByK.py
@@ -0,0 +1,10 @@
+# 1st solution
+# O(k) time | O(1) space
+class Solution:
+    def smallestRepunitDivByK(self, K: int) -> int:
+        remainder = 0
+        for length_N in range(1, K+1):
+            remainder = (remainder * 10 + 1) % K
+            if remainder == 0:
+                return length_N
+        return -1
