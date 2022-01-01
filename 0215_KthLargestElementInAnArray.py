5e98bc9a0663957f614e3a0fe63f85668c91e81e
yingzhuo <yingzhuo1994@gmail.com>
2022-01-01 16:19:59 -0600

    add a new solution

--- 0215_KthLargestElementInAnArray.py
+++ 0215_KthLargestElementInAnArray.py
@@ -46,3 +46,33 @@ class Solution:
             return self.findKthLargest(right, k - L - M)
         else:
             return mid[0]
+
+# 5th solution, quick select in place
+# O(n) time | O(1) space
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quickSelect(nums, 0, len(nums) - 1, k)
+    
+    def quickSelect(self, nums, left, right, k):
+        pivot = nums[left]
+        L, R = left, right
+        idx = L
+        while idx <= R:
+            if nums[idx] > pivot:
+                self.swap(nums, L, idx)
+                L += 1
+                idx += 1
+            elif nums[idx] < pivot:
+                self.swap(nums, idx, R)
+                R -= 1
+            else:
+                idx += 1
+        if k <= L:
+            return self.quickSelect(nums, left, L - 1, k)
+        elif k > idx:
+            return self.quickSelect(nums, idx, right, k)
+        else:
+            return pivot
+    
+    def swap(self, nums, i, j):
+        nums[i], nums[j] = nums[j], nums[i]
