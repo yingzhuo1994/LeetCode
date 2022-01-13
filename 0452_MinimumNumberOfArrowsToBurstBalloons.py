6260f6c481eeeb8937b8cc83d8b2513e27119203
yingzhuo <yingzhuo1994@gmail.com>
2022-01-12 19:37:26 -0600

    Simplify the solution

--- 0452_MinimumNumberOfArrowsToBurstBalloons.py
+++ 0452_MinimumNumberOfArrowsToBurstBalloons.py
@@ -1,24 +1,13 @@
 # 1st solution
-# O(n*log(n)) time | O(n) space
+# O(n*log(n)) time | O(1) space
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort(key = lambda v: v[1])
-        self.table = [False for _ in points]
-        self.ans = 0
-        self.dfs(points, 0)
+        count = 1
+        cur = points[0]
 
-        return self.ans
-    
-    def dfs(self, points, start):
-        self.table[start] = True
-        end = points[start][1]
-        self.ans += 1
-
-        for i in range(start + 1, len(points)):
-            if self.table[i]:
-                continue
-            if points[i][0] <= end <= points[i][1]:
-                self.table[i] = True
-            else:
-                self.dfs(points, i,)
-                break
+        for i in range(len(points)):
+            if cur[1] < points[i][0]:
+                count += 1
+                cur = points[i]
+        return count
