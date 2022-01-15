7712952477784699b4dc839a2490ef8f23eda8e6
yingzhuo <yingzhuo1994@gmail.com>
2022-01-14 23:44:07 -0600

    1st solution

--- 1345_JumpGameIV.py
+++ 1345_JumpGameIV.py
@@ -1,4 +1,5 @@
-# 1st solution, TLE
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         dic = collections.defaultdict(list)
@@ -8,19 +9,22 @@ class Solution:
         visited = set([0])
         level = set([0])
         step = 0
-        while len(visited) < len(arr):
+        while level:
             newLevel = set()
             for k in level:
                 if k == len(arr) - 1:
                     return step
                 if k - 1 >= 0 and k - 1 not in visited:
                     newLevel.add(k - 1)
+                    visited.add(k - 1)
                 if k + 1 < len(arr) and k + 1 not in visited:
                     newLevel.add(k + 1)
+                    visited.add(k + 1)
                 for idx in dic[arr[k]]:
                     if idx not in visited:
                         newLevel.add(idx)
+                        visited.add(idx)
+                dic[arr[k]].clear()
             level = newLevel
-            visited = visited.union(level)
             step += 1
         return step
