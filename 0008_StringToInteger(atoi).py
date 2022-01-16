947b9cc74dd8060da4a457ad3c3657e0793ae558
yingzhuo <yingzhuo1994@gmail.com>
2022-01-16 10:39:26 -0600

    simplify the solution

--- 0008_StringToInteger(atoi).py
+++ 0008_StringToInteger(atoi).py
@@ -5,26 +5,18 @@ class Solution:
         sign = 1
         k = 0
         num = 0
-        while k < len(s):
-            if s[k] == ' ':
-                k += 1
-            else:
-                break
-
-        if k < len(s):
-            if s[k] == '+':
-                sign = 1
-                k += 1
-            elif s[k] == '-':
-                sign = -1
-                k += 1
-
-        while k < len(s):
-            if s[k].isdigit():
-                num = 10 * num + int(s[k])
-            else:
-                break
+        while k < len(s) and s[k] == " ":
             k += 1
+
+        if k < len(s) and s[k] in "+-":
+            if s[k] == '-':
+                sign = -1
+            k += 1
+
+        while k < len(s) and s[k].isdigit():
+            num = 10 * num + int(s[k])
+            k += 1
+        
         if sign > 0:
             return min(num, 2**31 - 1)
         else:
