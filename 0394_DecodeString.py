64c37f327b447064ff410cd2bfb01601325135d1
yingzhuo <yingzhuo1994@gmail.com>
2022-01-17 09:51:35 -0600

    1st solution

--- 0394_DecodeString.py
+++ 0394_DecodeString.py
@@ -2,20 +2,19 @@
 # O(n) time | O(n) space
 class Solution:
     def decodeString(self, s: str) -> str:
-        it, num, stack = 0, 0, [""]
-        while it < len(s):
-            if s[it].isdigit():
-                num = num * 10 + int(s[it])
-            elif s[it] == "[":
+        num, stack = 0, [""]
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == "[":
                 stack.append(num)
                 num = 0
                 stack.append("")
-            elif s[it] == "]":
+            elif ch == "]":
                 str1 = stack.pop()
                 rep = stack.pop()
                 str2 = stack.pop()
                 stack.append(str2 + str1 * rep)
             else:
-                stack[-1] += s[it]              
-            it += 1           
+                stack[-1] += ch                         
         return "".join(stack)
