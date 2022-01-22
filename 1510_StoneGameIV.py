a0d696a4fd94109903a971a5d0adff4cb5d97f92
yingzhuo <yingzhuo1994@gmail.com>
2022-01-21 19:06:38 -0600

    2nd solution

--- 1510_StoneGameIV.py
+++ 1510_StoneGameIV.py
@@ -23,4 +23,18 @@ class Solution:
             k += 1
         self.dic[n] = False
         return opponent
-        
+
+# 2nd solution
+# O(n*n^0.5) time | O(n) space
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False]*(n+1)
+        for i in range(n+1):
+            if dp[i]:
+                continue
+            for k in range(1, int(n**0.5)+1):
+                if i+k*k <= n:
+                    dp[i+k*k] = True
+                else:
+                    break
+        return dp[n]
