e0b18b3b1a52c6d1c6733619b5ceb2d004bb6f07
yingzhuo <yingzhuo1994@gmail.com>
2022-01-22 23:37:05 -0600

    2nd solution

--- 1291_SequentialDigits.py
+++ 1291_SequentialDigits.py
@@ -14,3 +14,18 @@ class Solution:
                     continue
                 ans.append(num)
         return ans
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def sequentialDigits(self, low, high):
+        out = []
+        queue = deque(range(1,10))
+        while queue:
+            elem = queue.popleft()
+            if low <= elem <= high:
+                out.append(elem)
+            last = elem % 10
+            if last < 9: queue.append(elem*10 + last + 1)
+                    
+        return out
