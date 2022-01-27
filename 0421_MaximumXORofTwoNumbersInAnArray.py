acb6b0edb672fbc9acca4508f8881bc166d10da3
yingzhuo <yingzhuo1994@gmail.com>
2022-01-27 10:44:45 -0600

    3rd solution

--- 0421_MaximumXORofTwoNumbersInAnArray.py
+++ 0421_MaximumXORofTwoNumbersInAnArray.py
@@ -9,7 +9,7 @@ class Solution:
                 largest = max(largest, x)
         return largest
 
-# 2nd solution, brute-force
+# 2nd solution, mask
 # O(32n) time | O(n) space
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
@@ -25,3 +25,46 @@ class Solution:
                     break
          
         return ans
+
+# 3rd solution, Trie
+# O(32n) time | O(n) space
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        root = TrieNode()
+        for num in nums:
+            node = root
+            for j in reversed(range(32)):
+                tmp = num & 1 << j
+                if tmp:
+                    if not node.one:
+                        node.one = TrieNode()
+                    node = node.one
+                else:
+                    if not node.zero:
+                        node.zero = TrieNode()
+                    node = node.zero
+                    
+        ans = 0
+        for num in nums:
+            node = root
+            tmp_val = 0
+            for j in reversed(range(32)):
+                tmp = num & 1 << j
+                if node.one and node.zero:
+                    if tmp:
+                        node = node.zero
+                    else:
+                        node = node.one
+                    tmp_val += 1 << j
+                else:
+                    if (node.zero and tmp) or (node.one and not tmp):
+                        tmp_val += 1 << j
+                    node = node.one or node.zero
+            ans = max(ans, tmp_val)
+                                                
+        return ans
+
+class TrieNode():
+    def __init__(self):
+        self.one = None
+        self.zero = None
