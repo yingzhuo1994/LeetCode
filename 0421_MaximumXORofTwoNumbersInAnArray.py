3ccf44f4fc2b50ea0f5a164be94f9c97469d1bed
yingzhuo <yingzhuo1994@gmail.com>
2022-01-26 19:46:10 -0600

    2nd solution

--- 0421_MaximumXORofTwoNumbersInAnArray.py
+++ 0421_MaximumXORofTwoNumbersInAnArray.py
@@ -8,3 +8,20 @@ class Solution:
                 x = nums[i] ^ nums[j]
                 largest = max(largest, x)
         return largest
+
+# 2nd solution, brute-force
+# O(32n) time | O(n) space
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        ans, mask = 0, 0
+        for i in reversed(range(32)):
+            mask |= 1<<i
+            found = set([num & mask for num in nums])
+                
+            start = ans | 1<<i
+            for pref in found:
+                if start^pref in found:
+                    ans = start
+                    break
+         
+        return ans
