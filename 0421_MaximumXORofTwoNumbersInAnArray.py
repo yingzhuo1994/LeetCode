86c3b3366b5e908d6bf9c60c17e22173049fdfbe
yingzhuo <yingzhuo1994@gmail.com>
2022-02-02 11:02:19 -0600

    add explanation

--- 0421_MaximumXORofTwoNumbersInAnArray.py
+++ 0421_MaximumXORofTwoNumbersInAnArray.py
@@ -13,17 +13,29 @@ class Solution:
 # O(32n) time | O(n) space
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
-        ans, mask = 0, 0
-        for i in reversed(range(32)):
-            mask |= 1<<i
-            found = set([num & mask for num in nums])
-                
-            start = ans | 1<<i
-            for pref in found:
-                if start^pref in found:
-                    ans = start
+        ans = 0
+        for i in range(31,-1,-1):
+            prefixs = set([num>>i for num in nums])
+            #to get the first i digits of the num. In the given example [3, 10, 5, 25, 2, 8], 
+            #since the largest number 25 is 11001 in binary, thus while i>=5, the prefix = {0}, ignoring all preceding '0'
+            #prefix ={0,1} when i ==4, 
+            #prefix ={00,01,11} when i ==3, etc.
+            
+            ans <<=1 # 0 -> 00  1 ->10
+            candidate = ans+1 # 0->01 1->11
+            #so ans and candidate basically gives two options (0 or 1) on every new digit added
+            #and candidate is the largest possible number given the length of the digit. It's sort of a greedy algorithm here.
+            #if previous ans == 1, then the new ans and candidate will be 10 and 11
+            #depands on if we could get 1 on the new digit, we choose if we move forward with ans(10) or candidate(11)
+            
+            for pre in prefixs:
+                if candidate ^ pre in prefixs:
+                    ans = candidate
                     break
-         
+            # the if condition pre^candidate in prefix is based on the fact a^b^a = b
+            # assume a, b in prefix could generate the largest possilbe xor resuglt "candidate". then if we use candidate^a, the result will be b
+            # so if we do find pre^candidate in prefix, candidate will move forward(ans = candidate), meaning we get 1 in the new digit
+            
         return ans
 
 # 3rd solution, Trie
