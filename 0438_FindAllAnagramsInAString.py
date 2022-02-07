087f1f58036a98fc184b4a71a934035016cdec9c
yingzhuo <yingzhuo1994@gmail.com>
2022-02-06 18:26:07 -0600

    simplify

--- 0438_FindAllAnagramsInAString.py
+++ 0438_FindAllAnagramsInAString.py
@@ -3,25 +3,20 @@
 # where n is the length of s
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        goalDic = {}
-        for ch in p:
-            goalDic[ch] = goalDic.get(ch, 0) + 1
-        validLength = len(p)
-        curLength = 0
-        ans = []
+        targetDic = Counter(p)
         dic = {}
-        for i in reversed(range(len(s))):
-            ch = s[i]
-            if i + validLength < len(s):
-                lastCh = s[i + validLength]
-                if lastCh in dic and lastCh in goalDic:
-                    dic[lastCh] -= 1
-                    if dic[lastCh] < goalDic[lastCh]:
-                        curLength -= 1
-            dic[ch] = dic.get(ch, 0) + 1
-            if ch in goalDic and dic[ch] <= goalDic[ch]:
-                curLength += 1
-            if curLength == validLength:
-                ans.append(i)
-
-        return ans[::-1]
+        count = 0
+        goalLength = len(p)
+        ans = []
+        for i in range(len(s)):
+            front = i - goalLength
+            if front >= 0:
+                dic[s[front]] -= 1
+                if s[front] in targetDic and dic[s[front]] < targetDic[s[front]]:
+                    count -= 1
+            dic[s[i]] = dic.get(s[i], 0) + 1
+            if s[i] in targetDic and dic[s[i]] <= targetDic[s[i]]:
+                count += 1
+            if count == goalLength:
+                ans.append(front + 1)
+        return ans
