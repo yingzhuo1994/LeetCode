31553b202c4e9fbc6dd7843c0544446499d14c90
yingzhuo <yingzhuo1994@gmail.com>
2022-02-06 19:06:38 -0600

    modify

--- 0051_N-Queens.py
+++ 0051_N-Queens.py
@@ -2,36 +2,44 @@
 # O(n!) time | O(n) space
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        visitedCols = set()
-        visitedDigs = set()
-        visitedAnti = set()
+        blockedColumns = set()
+        blockedDownDiagonals = set()
+        blockedUpDiagonals = set()
         ans = []
-        def helper(x, y, lst):
-            if len(lst) == n:
-                if not ans or ans[-1] != lst:
-                    ans.append(lst)
-                return
-            if y in visitedCols or x - y in visitedDigs or x + y in visitedAnti:
-                return
-
-            visitedCols.add(y)
-            visitedDigs.add(x - y)
-            visitedAnti.add(x + y)
-            cur = ""
-            for k in range(n):
-                if k == y:
-                    cur += "Q"
-                else:
-                    cur += "."
-            for j in range(n):
-                helper(x + 1, j, lst + [cur])
-            visitedCols.remove(y)
-            visitedDigs.remove(x - y)
-            visitedAnti.remove(x + y)
-
-        for j in range(n):
-            helper(0, j, [])
-
+        self.getSolutions(0, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, n, ans, [])
         return ans
-                    
-                    
+
+    def getSolutions(self, row, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize, ans, lst):
+        if row == boardSize:
+            ans.append(lst)
+            return
+        for col in range(boardSize):
+            if self.isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+                self.placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)
+                cur = ""
+                for k in range(boardSize):
+                    if k == col:
+                        cur += "Q"
+                    else:
+                        cur += "."
+                self.getSolutions(row + 1, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize, ans, lst + [cur])
+                self.removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)
+
+    def isNonAttackingPlacement(self, row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+        if col in blockedColumns:
+            return False
+        if row + col in blockedUpDiagonals:
+            return False
+        if row - col in blockedDownDiagonals:
+            return False
+        return True
+
+    def placeQueen(self, row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+        blockedColumns.add(col)
+        blockedUpDiagonals.add(row + col)
+        blockedDownDiagonals.add(row - col)
+
+    def removeQueen(self, row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals):
+        blockedColumns.remove(col)
+        blockedUpDiagonals.remove(row + col)
+        blockedDownDiagonals.remove(row - col)
