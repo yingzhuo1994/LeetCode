a3d99989b288ef846b0ec99010fb4b246e9e2baa
yingzhuo <yingzhuo1994@gmail.com>
2022-02-19 19:25:31 -0600

    renew

--- 1288_RemoveCoveredIntervals.py
+++ 1288_RemoveCoveredIntervals.py
@@ -2,23 +2,11 @@
 # O(nlog(n)) time | O(n) space
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda v: v[0])
-        stack = []
-        for i in range(len(intervals)):
-            isCoveredResult = False
-            for j in range(len(stack)):
-                if self.isCovered(stack[j], intervals[i]):
-                    isCoveredResult = True
-                    break
-                elif self.isCovered(intervals[i], stack[j]):
-                    stack[j] = intervals[i]
-                    isCoveredResult = True
-                    break
-            if isCoveredResult:
-                continue
+        intervals.sort(key = lambda x: (x[0], -x[1]))
+        right, rem, n = -1, 0, len(intervals)
+        for _, end in intervals:
+            if end <= right:
+                rem += 1
             else:
-                stack.append(intervals[i])
-        return len(stack)
-   
-    def isCovered(self, one, two):
-        return one[0] <= two[0] and two[1] <= one[1]
+                right = end
+        return n - rem
