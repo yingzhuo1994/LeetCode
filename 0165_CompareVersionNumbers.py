142da65ae2b896525843c292bb17fe5b1665049f
yingzhuo <yingzhuo1994@gmail.com>
2022-02-24 19:36:58 -0600

    1st solution

---
+++ 0165_CompareVersionNumbers.py
@@ -0,0 +1,25 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        stackOne = self.getStack(version1)
+        stackTwo = self.getStack(version2)
+        for i in range(min(len(stackOne), len(stackTwo))):
+            if stackOne[i] < stackTwo[i]:
+                return -1
+            elif stackOne[i] > stackTwo[i]:
+                return 1
+        if len(stackOne) > len(stackTwo):
+            return 1
+        elif len(stackOne) < len(stackTwo):
+            return -1
+        else:
+            return 0
+    
+    def getStack(self, version):
+        stack = version.split(".")
+        for i in range(len(stack)):
+            stack[i] = int(stack[i])
+        while len(stack) > 1 and stack[-1] == 0:
+            stack.pop()
+        return stack
