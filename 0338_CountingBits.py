bc7f12c7e1953a855d0eb4950576c6295f2cb27c
yingzhuo <yingzhuo1994@gmail.com>
2022-02-28 23:37:10 -0600

    2nd solution

--- 0338_CountingBits.py
+++ 0338_CountingBits.py
@@ -12,3 +12,20 @@ class Solution:
                 num >>= 1
             ans[i] = count
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+        ans = [0] * (n + 1)
+        stack = deque([[1, 1]])
+        while stack:
+            num, count = stack.popleft()
+            ans[num] = count
+            if num << 1 <= n:
+                stack.append([num << 1, count])
+            if num << 1 | 1 <= n:
+                stack.append([num << 1 | 1, count + 1])
+        return ans
