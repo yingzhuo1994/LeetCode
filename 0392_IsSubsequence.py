8efd09d08654af44ab6c3add85b9787394015924
yingzhuo <yingzhuo1994@gmail.com>
2022-03-01 18:25:41 -0600

    1st solution

---
+++ 0392_IsSubsequence.py
@@ -0,0 +1,10 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
