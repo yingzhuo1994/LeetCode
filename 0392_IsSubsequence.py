0977f63b016631413628e38564a11f92e3b5f24f
yingzhuo <yingzhuo1994@gmail.com>
2022-03-01 18:32:27 -0600

    2nd solution

--- 0392_IsSubsequence.py
+++ 0392_IsSubsequence.py
@@ -8,3 +8,17 @@ class Solution:
                 i += 1
             j += 1
         return i == len(s)
+
+# 2nd solution, for follow up
+# O(n) time | O(1) space
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        idx = defaultdict(list)
+        for i, c in enumerate(t):
+            idx[c].append(i)
+        prev = 0
+        for i, c in enumerate(s):            
+            j = bisect.bisect_left(idx[c], prev)
+            if j == len(idx[c]): return False
+            prev = idx[c][j] + 1
+        return True  
