f8326a09f5ea4548a7ca0c95ea956b7fad876672
yingzhuo <yingzhuo1994@gmail.com>
2022-03-04 19:44:40 -0600

    2nd solution

--- 0740_DeleteAndEarn.py
+++ 0740_DeleteAndEarn.py
@@ -14,3 +14,21 @@ class Solution:
             dp[k] = dp[k - 1]
             dp[k] = max(dp[k], dp[k-2] + dic.get(k, 0))
         return dp[sortedKeys[-1]]
+
+# 2nd solution
+# O(n*log(n)) time | O(n) space
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + num
+        sortedKeys = sorted(dic.keys())
+        frontTwo = 0
+        frontOne = 0
+        ans = 0
+        for i in range(len(sortedKeys)):
+            k = sortedKeys[i]
+            frontTwo = frontOne if k - 1 in dic else ans
+            frontOne = ans
+            ans = max(frontOne, frontTwo + dic[k])
+        return ans
