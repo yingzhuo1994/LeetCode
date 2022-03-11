3008fb58ff1fa81ce852602407eed9932c1882c9
yingzhuo <yingzhuo1994@gmail.com>
2022-03-10 18:18:18 -0600

    simplify

--- 0061_RotateList.py
+++ 0061_RotateList.py
@@ -8,29 +8,28 @@
 # O(n) time | O(1) space
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        listLength = 0
-        node = head
-        tail = head
-        while node is not None:
-            listLength += 1
-            tail = node
-            node = node.next
-        if listLength <= 1:
+        if not head or not head.next:
             return head
+        listLength = 1
+        tail = head
+        while tail.next:
+            listLength += 1
+            tail = tail.next
+
         k = k % listLength
         if k == 0:
             return head
         moveStep = listLength - k
+
         node = head
-        prevNode = None
-        while moveStep > 0:
-            prevNode = node
+        while moveStep > 1:
             node = node.next
             moveStep -= 1
-        newHead = node
-        if prevNode:
-            prevNode.next = None
+        
+        newHead = node.next
+        node.next = None
         tail.next = head
+
         return newHead
         
         
