4ff98867cc1ee281fff947c358586456da11d82c
yingzhuo <yingzhuo1994@gmail.com>
2022-03-21 10:04:24 -0500

    simplify the solution

--- 1288_RemoveCoveredIntervals.py
+++ 1288_RemoveCoveredIntervals.py
@@ -3,10 +3,9 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda x: (x[0], -x[1]))
-        right, rem, n = -1, 0, len(intervals)
+        right, count = -1, 0
         for _, end in intervals:
-            if end <= right:
-                rem += 1
-            else:
+            if end > right:
+                count += 1
                 right = end
-        return n - rem
+        return count
