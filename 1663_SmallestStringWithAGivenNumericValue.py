0fbc955a89d44626950d821415ebd3d9d0257431
yingzhuo <yingzhuo1994@gmail.com>
2022-03-21 20:56:06 -0500

    2nd solution

--- 1663_SmallestStringWithAGivenNumericValue.py
+++ 1663_SmallestStringWithAGivenNumericValue.py
@@ -8,3 +8,18 @@ class Solution:
             return self.getSmallestString(n - 1, k - 26) + "z"
         else:
             return "a" * (n - 1) + chr(ord("a") + k - n)
+
+# 2nd solution, iteration
+# O(n) time | O(n) space
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        if (k - n) % 25 == 0:
+            zCount = (k - n) // 25
+            aCount = n - zCount
+            xCh = ""
+        else:
+            zCount = (k - n) // 25
+            x = (k - n) % 25 + 1
+            xCh = chr(ord("a") + x - 1)
+            aCount = n - zCount - 1
+        return "a" * aCount + xCh + "z" * zCount 
