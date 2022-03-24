c83f14af6b41766bba26ecfde1253e94e147a569
yingzhuo <yingzhuo1994@gmail.com>
2022-03-23 21:09:16 -0500

    1st solution

---
+++ 0881_BoatsToSavePeople.py
@@ -0,0 +1,13 @@
+# 1st solution
+# O(n*log(n)) time | O(1) space
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        count = 0
+        left, right = 0, len(people) - 1
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            count += 1
+            right -= 1
+        return count
