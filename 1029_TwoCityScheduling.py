ff337a447b5c966a5577f884acab8fc8f91bcea4
yingzhuo <yingzhuo1994@gmail.com>
2022-03-24 21:29:58 -0500

    1st solution

---
+++ 1029_TwoCityScheduling.py
@@ -0,0 +1,33 @@
+# 1st solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        stack = []
+        for i, cost in enumerate(costs):
+            stack.append([cost[1] - cost[0], i])
+        stack.sort(key = lambda v: v[0])
+        leftIdx, rightIdx = 0, len(costs) - 1
+        leftCount, rightCount = 0, 0
+        totalCost = 0
+        while leftIdx <= rightIdx:
+            leftDiff = abs(stack[leftIdx][0])
+            rightDiff = abs(stack[rightIdx][0])
+
+            if leftCount == len(costs) // 2:
+                totalCost += costs[stack[leftIdx][1]][1]
+                leftIdx += 1
+                continue
+            elif rightCount == len(costs) // 2:
+                totalCost += costs[stack[leftIdx][1]][0]
+                leftIdx += 1
+                continue
+
+            if leftDiff >= rightDiff:
+                totalCost += costs[stack[leftIdx][1]][1]
+                rightCount += 1
+                leftIdx += 1
+            else:
+                totalCost += costs[stack[rightIdx][1]][0]
+                leftCount += 1
+                rightIdx -= 1
+        return totalCost
