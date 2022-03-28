4ff169299710e3e4856f06b3f77b18d8a6e63b45
yingzhuo <yingzhuo1994@gmail.com>
2022-03-27 21:15:32 -0500

    1st solution

---
+++ 0081_SearchInRotatedSortedArray(2).py
@@ -0,0 +1,21 @@
+# 1st solution
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        def dfs(beg, end):
+            if end - beg <= 1: return target in nums[beg: end+1]
+            
+            mid = (beg + end)//2
+            if nums[mid] > nums[end]:   # eg. 3,4,5,6,7,1,2
+                if nums[end] < target <= nums[mid]:
+                    return dfs(beg, mid)
+                else:
+                    return dfs(mid + 1, end)
+            elif nums[mid] < nums[end]: # eg. 6,7,1,2,3,4,5
+                if nums[mid] < target <= nums[end]:
+                    return dfs(mid + 1, end)
+                else:
+                    return dfs(beg, mid)
+            else:
+                return dfs(mid+1, end) or dfs(beg, mid)
+    
+        return dfs(0, len(nums)-1)
