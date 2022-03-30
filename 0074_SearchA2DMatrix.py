d450397be9fc42d3f03cdf5a06cb452cc38baf98
yingzhuo <yingzhuo1994@gmail.com>
2022-03-29 20:38:56 -0500

    2nd solution

--- 0074_SearchA2DMatrix.py
+++ 0074_SearchA2DMatrix.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(log(m) + log(n)) time | O(1) space
+# O(log(mn)) time | O(1) space
 # where m is the length of matrix, and n is the length of matrix[0]
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
@@ -13,6 +13,7 @@ class Solution:
             else:
                 return True
         row = startRow
+
         startCol, endCol = 0, len(matrix[0]) - 1
         while startCol < endCol:
             middleCol = startCol + (endCol - startCol) // 2
@@ -23,4 +24,24 @@ class Solution:
             else:
                 return True
         col = startCol
+
         return matrix[row][col] == target
+
+# 2nd solution
+# O(log(mn)) time | O(1) space
+# where m is the length of matrix, and n is the length of matrix[0]
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:       
+        m, n = len(matrix[0]), len(matrix)
+        beg, end = 0, m * n - 1
+        while beg < end:
+            mid = (beg + end) // 2
+            row = mid // m
+            col = mid % m
+            if matrix[row][col] < target:
+                beg = mid + 1
+            elif matrix[row][col] > target:
+                end = mid
+            else:
+                return True
+        return matrix[beg // m][beg % m] == target
