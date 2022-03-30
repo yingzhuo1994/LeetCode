0f50272984a05edd3204e881cfd133c9a993cc37
yingzhuo <yingzhuo1994@gmail.com>
2022-03-30 11:17:10 -0500

    3rd solution

--- 1178_NumberOfValidWordsForEachPuzzle.py
+++ 1178_NumberOfValidWordsForEachPuzzle.py
@@ -55,3 +55,41 @@ class Solution:
                 submask = (submask - 1) & mask
             result.append(count)
         return result
+
+# 3rd solution
+# O(n(N*log(N) + M)+ m*M) time | O(k*M!) space
+# where n, m are the length of words and puzzles, respectively,
+# and N, M are the average length of words[i] and puzzles[i] respectively.
+# k = 26
+class Solution:
+    def findNumOfValidWords(self, words: List[str], puzzles: List[str]) -> List[int]:
+        SIZE = 26  # 26 letters in the alphabet
+        trie = [[0] * SIZE]  # we use list to mimic the trie tree
+        count = [0]  # the number of words ending at node i
+        for word in words:
+            word = sorted(set(word))
+            if len(word) <= 7:  # longer words are never valid
+                # insert into trie
+                node = 0
+                for letter in word:
+                    i = ord(letter) - ord('a')
+                    if trie[node][i] == 0:  # push empty node
+                        trie.append([0] * SIZE)
+                        count.append(0)
+                        trie[node][i] = len(trie) - 1
+                    node = trie[node][i]
+                count[node] += 1
+
+        # search for valid words
+        def dfs(node, has_first):
+            total = count[node] if has_first else 0
+            for letter in puzzle:  # catch puzzle from outside environment
+                i = ord(letter) - ord('a')
+                if trie[node][i]:
+                    total += dfs(trie[node][i], has_first or letter == puzzle[0])
+            return total
+
+        result = []
+        for puzzle in puzzles:
+            result.append(dfs(0, False))
+        return result
