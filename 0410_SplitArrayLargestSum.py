89c68ee13ee1129704ec98a012ecdc310cf0ed34
yingzhuo <yingzhuo1994@gmail.com>
2022-03-30 21:39:00 -0500

    3rd solution

--- 0410_SplitArrayLargestSum.py
+++ 0410_SplitArrayLargestSum.py
@@ -74,3 +74,44 @@ class Solution:
                 memo[curr_index][subarray_count] = minimum_largest_split_sum
         
         return memo[0][m]
+
+# 3rd solution, binary search
+# O(n*log(s)) time | O(1) space
+# where n is the length of the array, and s is the sum of intergers in the array
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        
+        def min_subarrays_required(max_sum_allowed: int) -> int:
+            current_sum = 0
+            splits_required = 0
+            
+            for element in nums:
+                # Add element only if the sum doesn't exceed max_sum_allowed
+                if current_sum + element <= max_sum_allowed:
+                    current_sum += element
+                else:
+                    # If the element addition makes sum more than max_sum_allowed
+                    # Increment the splits required and reset sum
+                    current_sum = element
+                    splits_required += 1
+
+            # Return the number of subarrays, which is the number of splits + 1
+            return splits_required + 1
+        
+        # Define the left and right boundary of binary search
+        left = max(nums)
+        right = sum(nums)
+        while left <= right:
+            # Find the mid value
+            max_sum_allowed = (left + right) // 2
+            
+            # Find the minimum splits. If splits_required is less than
+            # or equal to m move towards left i.e., smaller values
+            if min_subarrays_required(max_sum_allowed) <= m:
+                right = max_sum_allowed - 1
+                minimum_largest_split_sum = max_sum_allowed
+            else:
+                # Move towards right if splits_required is more than m
+                left = max_sum_allowed + 1
+        
+        return minimum_largest_split_sum
