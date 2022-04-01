1c1a722b2f75ed0a42769cd12b9dd64ca50a0b51
yingzhuo <yingzhuo1994@gmail.com>
2022-03-31 19:33:25 -0500

    renew

--- 0344_ReverseString.py
+++ 0344_ReverseString.py
@@ -1,9 +1,9 @@
+# O(n) time | O(1) space
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        # O(n) time | O(1) space
         start = 0
         end = len(s) - 1
         while start < end:
