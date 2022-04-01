ed6cf54ee0e7ad3a0d7fe46099a22882831b5699
yingzhuo <yingzhuo1994@gmail.com>
2022-04-01 10:14:51 -0500

    4th solution

--- 1044_LongestDuplicateSubstring.py
+++ 1044_LongestDuplicateSubstring.py
@@ -77,3 +77,30 @@ class Solution:
                     return (True, text[j:j + M])
             dic[t].append(i + 1)
         return (False, "")
+
+# 4th solution
+# O（n*log(n)) | O(n) space
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        A = [ord(c) - ord('a') for c in s]
+        mod = 2**63 - 1
+
+        def test(L):
+            p = pow(26, L, mod)
+            cur = reduce(lambda x, y: (x * 26 + y) % mod, A[:L], 0)
+            seen = {cur}
+            for i in range(L, len(s)):
+                cur = (cur * 26 + A[i] - A[i - L] * p) % mod
+                if cur in seen: return i - L + 1
+                seen.add(cur)
+        res, lo, hi = 0, 0, len(s)
+
+        while lo < hi:
+            mi = (lo + hi + 1) // 2
+            pos = test(mi)
+            if pos:
+                lo = mi
+                res = pos
+            else:
+                hi = mi - 1
+        return s[res:res + lo]
