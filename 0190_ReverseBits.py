1151c5b01c026ba067ad97cb16f1863e6f7c8420
yingzhuo <yingzhuo1994@gmail.com>
2022-04-01 21:37:25 -0500

    renew

--- 0190_ReverseBits.py
+++ 0190_ReverseBits.py
@@ -1,9 +1,9 @@
+# O(1) time | O(1) space
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # O(1) time | O(1) space
         ret, power = 0, 31
-        while n:
-            ret += (n & 1) << power
-            n = n >> 1
+        while n > 0:
+            ret |= (n & 1) << power
+            n >>= 1
             power -= 1
         return ret
