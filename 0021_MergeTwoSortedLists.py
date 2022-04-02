4c793c39b0a1804056c6b84d9b2d6990e5209120
yingzhuo <yingzhuo1994@gmail.com>
2022-04-01 20:40:47 -0500

    renew

--- 0021_MergeTwoSortedLists.py
+++ 0021_MergeTwoSortedLists.py
@@ -3,42 +3,14 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        # 1st solution
-        # result = ListNode(0)
-        # result_tail = result
-        # p, q = l1, l2
-        # while p or q:
-        #     x = p.val if p else None
-        #     y = q.val if q else None
-        #     if p and q:
-        #         if x < y:
-        #             result_tail.next = ListNode(x)
-        #             p = p.next
-        #         else:
-        #             result_tail.next = ListNode(y)
-        #             q = q.next
-        #     elif p and not q:
-        #         result_tail.next = ListNode(x)
-        #         p = p.next
-        #     elif not p and q:
-        #         result_tail.next = ListNode(y)
-        #         q = q.next
-        #     result_tail = result_tail.next
-        # return result.next
 
-        # 2nd solution iteratively
-        # O(n) time | O(1) space
+# O(n) time | O(1) space
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if None in (l1, l2):
             return l1 or l2
-        result = ListNode(0)
-        p = result
+        sentinel = ListNode(0)
+        p = sentinel
         while l1 and l2:
             if l1.val < l2.val:
                 p.next = l1
@@ -48,4 +20,4 @@ class Solution(object):
                 l2 = l2.next
             p = p.next
         p.next = l1 or l2
-        return result.next
+        return sentinel.next
