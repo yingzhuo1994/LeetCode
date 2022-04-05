76b733554b3ee82695735e5c10d46fe19784ad24
yingzhuo <yingzhuo1994@gmail.com>
2022-04-04 19:13:54 -0500

    renew

--- 0011_ContainerWithMostWater.py
+++ 0011_ContainerWithMostWater.py
@@ -1,6 +1,6 @@
+# 1st brute-force solution
+# O(n^2) time | O(1) space 
 class Solution:
-    # 1st brute-force solution
-    # O(n^2) time | O(1) space 
     def maxArea(self, height: List[int]) -> int:
         area_largest = 0
         for left in range(len(height) - 1):
@@ -10,15 +10,20 @@ class Solution:
                     area_largest = area
         return area_largest
 
-    # 2nd solution
-    # O(n) time | O(1) space
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
     def maxArea(self, height: List[int]) -> int:
         area = 0
         left, right = 0, len(height) - 1
         while left < right:
-            area = max(area, (right - left) * min(height[left], height[right]))
+            w = right - left
             if height[left] < height[right]:
+                h = height[left]
                 left += 1
             else:
+                h = height[right]
                 right -= 1
+            curArea = w * h
+            area = max(area, curArea)
         return area
