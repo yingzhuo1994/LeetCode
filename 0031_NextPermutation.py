ed7aa392ea040f8e193cf740856bd4a9589f019d
yingzhuo <yingzhuo1994@gmail.com>
2022-04-07 09:55:37 -0500

    renew

--- 0031_NextPermutation.py
+++ 0031_NextPermutation.py
@@ -5,21 +5,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        firstDecreseIndex = len(nums) - 1
+        n = len(nums)
+        firstDecreseIndex = n - 1
         while firstDecreseIndex > 0:
             if nums[firstDecreseIndex] <= nums[firstDecreseIndex - 1]:
                 firstDecreseIndex -= 1
             else:
                 break
-        if firstDecreseIndex == len(nums) - 1:
+        if firstDecreseIndex == n - 1:
             self.swap(nums, firstDecreseIndex, firstDecreseIndex - 1)
         elif firstDecreseIndex == 0:
-            self.reverseNums(nums, firstDecreseIndex, len(nums) - 1)
+            self.reverseNums(nums, firstDecreseIndex, n - 1)
         else:
             frontIndex = firstDecreseIndex - 1
             value = nums[frontIndex]
-            self.reverseNums(nums, firstDecreseIndex, len(nums) - 1)
-            nextLargerIndex = self.searchLargerValue(nums, value, firstDecreseIndex, len(nums) - 1)
+            self.reverseNums(nums, firstDecreseIndex, n - 1)
+            nextLargerIndex = self.searchLargerValue(nums, value, firstDecreseIndex, n - 1)
             self.swap(nums, frontIndex, nextLargerIndex)
 
     def swap(self, nums, i, j):
@@ -39,5 +40,3 @@ class Solution:
             else:
                 right = mid
         return left
-
-        
