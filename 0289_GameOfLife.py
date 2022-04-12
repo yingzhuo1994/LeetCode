31e20ec07b7592d4bd11844d1a38ee9085716220
yingzhuo <yingzhuo1994@gmail.com>
2022-04-11 20:00:31 -0500

    renew

--- 0289_GameOfLife.py
+++ 0289_GameOfLife.py
@@ -1,3 +1,4 @@
+# O(mn) time | O(1) space
 class Solution:
     # 0,2 are "dead", and "dead->live"
     # 1,3 are "live", and "live->dead"
@@ -8,22 +9,27 @@ class Solution:
         m, n = len(board), len(board[0])
         for i in range(m):
             for j in range(n):
+                liveCount = self.countNeighborsLives(board, i, j)
                 if board[i][j] == 0 or board[i][j] == 2:
-                    if self.nnb(board, i, j) == 3:
+                    if liveCount == 3:
                         board[i][j] = 2
                 else:
-                    if self.nnb(board, i, j) < 2 or self.nnb(board,i,j) > 3:
+                    if liveCount < 2 or liveCount > 3:
                         board[i][j] = 3
         for i in range(m):
             for j in range(n):
                 if board[i][j] == 2: board[i][j] = 1
                 if board[i][j] == 3: board[i][j] = 0
 
-    def nnb(self, board, i, j):
-        neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+    def countNeighborsLives(self, board, i, j):
         count = 0
         m, n = len(board), len(board[0])
-        for d in neighbors:
-            if 0 <= i + d[0] < m and 0 <= j + d[1] < n:
-                count += board[i + d[0]][j + d[1]] % 2 
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                x = i + dx
+                y = j + dy
+                if x == i and y == j:
+                    continue
+                if 0 <= x < m and 0 <= y < n:
+                    count += board[x][y] % 2 
         return count
