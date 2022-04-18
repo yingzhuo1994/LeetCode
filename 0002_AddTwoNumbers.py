1ff5f405c02c204181b414f723e3fbbe2ad90244
yingzhuo <yingzhuo1994@gmail.com>
2022-04-17 20:07:22 -0500

    renew

--- 0002_AddTwoNumbers.py
+++ 0002_AddTwoNumbers.py
@@ -3,24 +3,19 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(orject):
-    def addTwoNumrers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        result = ListNode(0)
-        currentNode = result
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        sentinel = ListNode(0)
+        currentNode = sentinel
         p, q = l1, l2
         carry = 0
         while p or q or carry:
-            x = p.val if p else 0
-            y = q.val if q else 0
-            currentSum = x + y + carry
+            v1 = p.val if p else 0
+            v2 = q.val if q else 0
+            currentSum = v1 + v2 + carry
             carry, r = currentSum // 10, currentSum % 10
             currentNode.next = ListNode(r)
             currentNode = currentNode.next
             p = p.next if p else None
             q = q.next if q else None
-        return result.next
+        return sentinel.next
