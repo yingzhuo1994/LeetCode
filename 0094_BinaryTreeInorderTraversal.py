6c3be3bb122a50bedac90a506a7561aa98d3617b
yingzhuo <yingzhuo1994@gmail.com>
2022-04-18 21:48:12 -0500

    renew

--- 0094_BinaryTreeInorderTraversal.py
+++ 0094_BinaryTreeInorderTraversal.py
@@ -4,21 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from typing import Counter
 
-
+# 1st recursive solution
+# O(n) time | O(log(n)) space
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        # 1st recursive solution
-        # O(n) time | O(logn) space
         if not root:
             return []
         leftTree = self.inorderTraversal(root.left)
         rightTree = self.inorderTraversal(root.right)
         return leftTree + [root.val] + rightTree
 
-        # 2nd Iterative solution
-        # O(n) time | O(n) space
+# 2nd Iterative solution
+# O(n) time | O(n) space
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
         stack = []
         cur = root
