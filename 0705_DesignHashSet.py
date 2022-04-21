18bdd4af6973fd36dee488ec4f4bdfacbd0d4c5c
yingzhuo <yingzhuo1994@gmail.com>
2022-04-20 20:19:24 -0500

    2nd solution

--- 0705_DesignHashSet.py
+++ 0705_DesignHashSet.py
@@ -31,8 +31,53 @@ class MyHashSet:
                 return mid
         return left
         
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+# 2nd solution
+class MyHashSet:
+    def __init__(self):
+        self.root = TreeNode(0)
+        self.digit = 19
+
+    def add(self, key: int) -> None:
+        node = self.findKey(key)
+        node.isValid = True  
+            
+    def remove(self, key: int) -> None:
+        node = self.findKey(key)
+        node.isValid = False       
+
+    def contains(self, key: int) -> bool:
+        node = self.findKey(key)
+        return node.isValid
+    
+    # O(1) time | O(1) space
+    def findKey(self, key):
+        node = self.root
+        k = self.digit
+        stack = []
+        while k >= 0:
+            d = (key >> k) & 1
+            stack.append(d)
+            if d & 1:
+                if not node.right:
+                    node.right = TreeNode(1)
+                node = node.right
+            else:
+                if not node.left:
+                    node.left = TreeNode(0)
+                node = node.left
+            k -= 1
+        return node
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.isValid = False
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.isValid = False
