7bbc1565da59942b5cdca1ea3b173c03817ccfd4
yingzhuo <yingzhuo1994@gmail.com>
2022-04-20 20:27:55 -0500

    3rd solution

--- 0705_DesignHashSet.py
+++ 0705_DesignHashSet.py
@@ -75,9 +75,25 @@ class TreeNode:
         self.right = right
         self.isValid = False
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.isValid = False
+
+# 3rd solution, Multiplicative Hash
+class MyHashSet: 
+    def eval_hash(self, key):
+        return ((key*1031237) & (1<<20) - 1)>>5
+
+    def __init__(self):
+        self.arr = [[] for _ in range(1<<15)]
+
+    def add(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key not in self.arr[t]:
+            self.arr[t].append(key)
+
+    def remove(self, key: int) -> None:
+        t = self.eval_hash(key)
+        if key in self.arr[t]:
+            self.arr[t].remove(key)
+
+    def contains(self, key: int) -> bool:
+        t = self.eval_hash(key)
+        return key in self.arr[t]
