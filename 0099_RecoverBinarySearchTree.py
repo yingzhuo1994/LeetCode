8dfcb067fb1385de3d9d4e47a5a414206c55facf
yingzhuo <yingzhuo1994@gmail.com>
2022-04-21 20:16:14 -0500

    modify

--- 0099_RecoverBinarySearchTree.py
+++ 0099_RecoverBinarySearchTree.py
@@ -5,35 +5,36 @@
 #         self.left = left
 #         self.right = right
 
-# 1st solution
+# 1st solution, Morris Traversal
 # O(n) time | O(1) space
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        cur, node, cands = root, TreeNode(-float("inf")), []
-        while cur:
-            if cur.left:
-                pre = cur.left
-                while pre.right and pre.right != cur:
+        curNode, lastNode = root, TreeNode(-float("inf"))
+        candidates = []
+        while curNode:
+            if not curNode.left:
+                if curNode.val < lastNode.val:
+                    candidates += [lastNode, curNode]
+                lastNode = curNode
+                curNode = curNode.right
+            else:
+                pre = curNode.left
+                while pre.right and pre.right != curNode:
                     pre = pre.right
                 if not pre.right:
-                    pre.right = cur
-                    cur = cur.left
+                    pre.right = curNode
+                    curNode = curNode.left
                 else:
                     pre.right = None
-                    if cur.val < node.val:
-                        cands += [node, cur]
-                    node = cur
-                    cur = cur.right
-            else:
-                if cur.val < node.val:
-                    cands += [node, cur]
-                node = cur
-                cur = cur.right
-        
-        self.swap(cands[0], cands[-1])
+                    if curNode.val < lastNode.val:
+                        candidates += [lastNode, curNode]
+                    lastNode = curNode
+                    curNode = curNode.right
+
+        self.swap(candidates[0], candidates[-1])
     
     def swap(self, node1, node2):
         node1.val, node2.val = node2.val, node1.val
