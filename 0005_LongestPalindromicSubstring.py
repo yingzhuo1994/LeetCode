9373c1daf7b9635e9c3c2ebcc0c990b84aa9abbd
yingzhuo <yingzhuo1994@gmail.com>
2022-04-22 20:52:49 -0500

    renew

--- 0005_LongestPalindromicSubstring.py
+++ 0005_LongestPalindromicSubstring.py
@@ -1,8 +1,8 @@
+# 1st solution
+# O(n^2) time | O(1) space
 class Solution:
-    # 1st solution
-    # O(n^2) time | O(1) space
     def longestPalindrome(self, s: str) -> str:
-        def helper(s, start, end):
+        def extender(s, start, end):
             while start >= 0 and end < len(s) and s[start] == s[end]:
                 start -= 1
                 end += 1
@@ -10,8 +10,8 @@ class Solution:
 
         pair = [0, 0]
         for i in range(len(s) - 1):
-            pair0 = helper(s, i, i)
-            pair1 = helper(s, i, i + 1)
+            pair0 = extender(s, i, i)
+            pair1 = extender(s, i, i + 1)
             if pair0[1] - pair0[0] > pair[1] - pair[0]:
                 pair = pair0
             if pair1[1] - pair1[0] > pair[1] - pair[0]:
