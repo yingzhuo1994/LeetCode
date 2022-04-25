f40318d55c32445a04e814e33970fd12621ae1ab
yingzhuo <yingzhuo1994@gmail.com>
2022-04-24 21:54:51 -0500

    2nd solution

--- 2251_NumberOfFlowersInFullBloom.py
+++ 2251_NumberOfFlowersInFullBloom.py
@@ -1,4 +1,21 @@
+# 1st solution, Binary Search
+# O(n*log(n) + m*log(m)) time | O(n) space
+# where n is the length of flowers, and m is the length of persons
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
         start, end = sorted(a for a,b in flowers), sorted(b for a,b in flowers)
         return [bisect_right(start, t) - bisect_left(end, t) for t in persons]
+
+# 2nd solution
+# O(n*log(n) + m*log(m)) time | O(n) space
+# where n is the length of flowers, and m is the length of persons
+import sortedcontainers
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
+        diff = sortedcontainers.SortedDict({0: 0})
+        for i, j in flowers:
+            diff[i] = diff.get(i, 0) + 1
+            diff[j + 1] = diff.get(j + 1, 0) - 1
+        
+        count = list(accumulate(diff.values()))
+        return [count[diff.bisect(t) - 1] for t in persons]
