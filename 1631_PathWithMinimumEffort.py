4f54f37bd6e0e2acb5197c3ea7abf7d5e2e73557
yingzhuo <yingzhuo1994@gmail.com>
2022-04-27 20:45:43 -0500

    2nd solution

--- 1631_PathWithMinimumEffort.py
+++ 1631_PathWithMinimumEffort.py
@@ -1,4 +1,5 @@
 # 1st solution
+# O(mn*log(mn)) time | O(mn) space
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         efforts = [[float("inf") for _ in range(len(heights[0]))] for _ in range(len(heights))]
@@ -16,3 +17,29 @@ class Solution:
                         efforts[x][y] = nextEffort
                         heappush(minHeap, (nextEffort, x, y))
         return efforts[-1][-1]
+
+# 2nd solution
+# O(mn*log(H)) time | O(mn) space
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        neibs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
+        def dfs(LIMIT, x, y):
+            visited.add((x, y)) 
+            for dx, dy in neibs:
+                if 0<=dx+x<m and 0<=dy+y<n and (dx+x, dy+y) not in visited:
+                    if abs(heights[x][y] - heights[dx+x][dy+y]) <= LIMIT:
+                        dfs(LIMIT, dx+x, dy+y)
+        
+        beg, end = -1, max(max(heights, key=max))
+        while beg + 1 < end:
+            mid = (beg + end)//2
+            visited = set()
+            dfs(mid, 0, 0)
+            if (m - 1, n - 1) in visited:
+                end = mid
+            else:
+                beg = mid
+                
+        return end
