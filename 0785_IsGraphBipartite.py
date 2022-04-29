5c33674ca53f149deb1e36019218fecedaa0f56a
yingzhuo <yingzhuo1994@gmail.com>
2022-04-28 21:13:49 -0500

    simplify the solution

--- 0785_IsGraphBipartite.py
+++ 0785_IsGraphBipartite.py
@@ -7,25 +7,17 @@ class Solution:
         for i in range(n):
             if states[i] == 0:
                 states[i] = 1
-                visited = set()
-                if not self.dfs(graph, states, visited, i):
+                if not self.dfs(graph, states, i):
                     return False
-            else:
-                for j in graph[i]:
-                    if states[j] == states[i]:
-                        return False
-                    else:
-                        states[j] = -states[i]
         return True
     
-    def dfs(self, graph, states, visited, i):
-        if i in visited:
-            return True
-        visited.add(i)
+    def dfs(self, graph, states, i):
         for j in graph[i]:
-            if states[j] == states[i]:
-                return False
-            states[j] = -states[i]
-            if not self.dfs(graph, states, visited, j):
-                return False
+            if states[j] != 0:
+                if states[j] == states[i]:
+                    return False
+            else:
+                states[j] = -states[i]
+                if not self.dfs(graph, states, j):
+                    return False
         return True
