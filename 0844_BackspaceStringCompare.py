8e48edf246cef31a99745602893f9564d82bb7bc
yingzhuo <yingzhuo1994@gmail.com>
2022-04-30 20:20:52 -0500

    2nd solution

--- 0844_BackspaceStringCompare.py
+++ 0844_BackspaceStringCompare.py
@@ -21,3 +21,32 @@ class Solution:
             else:
                 stack.append(ch)
         return stack
+
+# 2nd solution
+# O(s + t) time | O(1) space
+# where s and t are string length
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        i, j = len(s) - 1, len(t) - 1
+        while i >= 0 or j >= 0:
+            i, s_ch = self.getNextIndexAndCh(s, i)
+            j, t_ch = self.getNextIndexAndCh(t, j)
+            if s_ch != t_ch:
+                return False
+        return i == j
+
+    def getNextIndexAndCh(self, s, i):
+        count = 0
+        while i >= 0:
+            if s[i] == "#":
+                count += 1
+                i -= 1
+            elif count > 0:
+                count -= 1
+                i -= 1
+            else:
+                break
+        if i < 0:
+            return -1, ""
+        else:
+            return i - 1, s[i]
