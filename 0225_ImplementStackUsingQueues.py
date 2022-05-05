f6442838550957d1946337e54e49ccb06c1c3a37
yingzhuo <yingzhuo1994@gmail.com>
2022-05-04 20:43:10 -0500

    1st solution

---
+++ 0225_ImplementStackUsingQueues.py
@@ -0,0 +1,21 @@
+# 1st solution
+class MyStack:
+    def __init__(self):
+        self._queue = collections.deque()
+
+    # O(n) time
+    def push(self, x):
+        q = self._queue
+        q.append(x)
+        for _ in range(len(q) - 1):
+            q.append(q.popleft())
+    
+    # O(1) time
+    def pop(self):
+        return self._queue.popleft()
+
+    def top(self):
+        return self._queue[0]
+    
+    def empty(self):
+        return not len(self._queue)
