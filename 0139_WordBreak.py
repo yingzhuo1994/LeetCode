b0878689b7c370b74f5929a59db91fd6fa91eda9
yingzhuo <yingzhuo1994@gmail.com>
2022-05-08 10:48:51 -0500

    3rd solution

--- 0139_WordBreak.py
+++ 0139_WordBreak.py
@@ -1,3 +1,4 @@
+# 1st solution
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         ok = [True]
@@ -5,8 +6,9 @@ class Solution:
             ok += any(ok[j] and s[j:i] in wordDict for j in range(i)),
         return ok[-1]
 
-    # 2nd solution
-    # O(n^2) time | O(n) space
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         check = [False] * (len(s) + 1)
         check[0] = True
@@ -16,3 +18,19 @@ class Solution:
                     check[i] = True
                     continue
         return check[-1]
+
+# 3rd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for start in range(len(dp)):
+            if dp[start]:
+                for end in range(start + 1, len(dp)):
+                    if s[start:end] in wordSet:
+                        dp[end] = True
+                if dp[-1]:
+                    return True
+        return dp[-1]
