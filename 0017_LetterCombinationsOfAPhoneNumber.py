7f61ad16fe53e1535bab1453fd07ccbb8e27ceea
yingzhuo <yingzhuo1994@gmail.com>
2022-05-08 19:14:07 -0500

    renew

--- 0017_LetterCombinationsOfAPhoneNumber.py
+++ 0017_LetterCombinationsOfAPhoneNumber.py
@@ -1,13 +1,13 @@
+# O(4^n) time | O(4^n) space
 class Solution:
-    # O(4^n) time | O(4^n) space
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
-               '6': 'mno', '7': 'pqrs', '8': 'tuv', "9": 'wxyz'}
         n = len(digits)
         if n < 1:
             return []
-        lst = [ch for ch in dic[digits[0]]]
-        for i in range(1, n):
-            new = [ch for ch in dic[digits[i]]]
-            lst = [ch1 + ch2 for ch1 in lst for ch2 in new]
-        return lst
+        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+               '6': 'mno', '7': 'pqrs', '8': 'tuv', "9": 'wxyz'}
+
+        ans = [""]
+        for i in range(n):
+            ans = [ch1 + ch2 for ch1 in ans for ch2 in dic[digits[i]]]
+        return ans
