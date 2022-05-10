707f0069c7152d616efce13a536cc2a5d1a179e7
yingzhuo <yingzhuo1994@gmail.com>
2022-05-09 21:18:31 -0500

    2nd solution

--- 0216_CombinationSum(3).py
+++ 0216_CombinationSum(3).py
@@ -15,4 +15,14 @@ class Solution:
             return
         for value in range(startValue, 10):
             self.dfs(count - 1, target - value, value + 1, curLst + [value], ans)
-            
+
+# 2nd solution, iteration
+# O(9! / ((9-k)! * k!)) time | O(9! / ((9-k)! * k!)) space
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        combs = [[]]
+        for _ in range(k):
+            combs = [[first] + comb
+                     for comb in combs
+                     for first in range(1, comb[0] if comb else 10)]
+        return [c for c in combs if sum(c) == n]
