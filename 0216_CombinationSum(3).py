26251f9688718a7b2a100c21771e5d2ea9abdee6
yingzhuo <yingzhuo1994@gmail.com>
2022-05-09 21:11:55 -0500

    1 st solution

---
+++ 0216_CombinationSum(3).py
@@ -0,0 +1,18 @@
+# 1st solution
+# O(9! / ((9-k)! * k!)) time | O(9! / ((9-k)! * k!)) space
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+        self.dfs(k, n, 1, [], ans)
+        return ans
+
+    def dfs(self, count, target, startValue, curLst, ans):
+        if target <= 0:
+            return 
+        if count == 1:
+            if 1 <= target <= 9 and startValue <= target:
+                ans.append(curLst + [target])
+            return
+        for value in range(startValue, 10):
+            self.dfs(count - 1, target - value, value + 1, curLst + [value], ans)
+            
