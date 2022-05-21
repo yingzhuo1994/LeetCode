4af29055fff0bf06b703cd43ec06b678e868b323
yingzhuo <yingzhuo1994@gmail.com>
2022-05-20 20:07:29 -0500

    simplify the solution

--- 0001_TwoSum.py
+++ 0001_TwoSum.py
@@ -1,3 +1,5 @@
+# 1st solution
+# O(n^2) time | O(1) space
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -14,31 +16,36 @@ class Solution(object):
         >>> twoSum(nums, target)
         [0,1]
         """
-        # O(n^2) time | O(1) space
-        # if len(nums) < 2:
-        #     return 'the length of nums must be larger than 2'
-        # else:
-        #     i = 0
-        #     while i < len(nums) - 1:
-        #         j = i + 1
-        #         while j < len(nums):
-        #             if nums[i] + nums[j] == target:
-        #                 return [i, j]
-        #             j += 1
-        #         i += 1
-        #     return 'no solution exists!'
+        if len(nums) < 2:
+            return 'the length of nums must be larger than 2'
+        else:
+            i = 0
+            while i < len(nums) - 1:
+                j = i + 1
+                while j < len(nums):
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
+                    j += 1
+                i += 1
+            return 'no solution exists!'
 
-        # O(n) time | O(n) space
-        #  dic = {}
-        # for i in range(len(nums)):
-        #     potentialvalue = target - nums[i]
-        #     if potentialvalue in dic:
-        #         return [dic[potentialvalue], i]
-        #     else:
-        #         dic[nums[i]] = i
-        # return []
+# 2nd solution
+# O(n) time | O(n) space
+class Solution(object):
+    def twoSum(self, nums, target):
+        dic = {}
+        for i in range(len(nums)):
+            potentialvalue = target - nums[i]
+            if potentialvalue in dic:
+                return [dic[potentialvalue], i]
+            else:
+                dic[nums[i]] = i
+        return []
 
-        # O(nlog(n)) time | O(1) space
+# 3rd solution
+# O(nlog(n)) time | O(1) space
+class Solution(object):
+    def twoSum(self, nums, target):
         # critical case, nums = [3, 3], target = 6
         # not a good solution, it needs to be improved
         array = nums[:]
