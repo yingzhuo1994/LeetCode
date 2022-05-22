4baabe4259a2bef1dd9dd1db33a9961a3c8efec2
yingzhuo <yingzhuo1994@gmail.com>
2022-05-21 20:05:18 -0500

    1st solution

---
+++ 0647_PalindromicSubstrings.py
@@ -0,0 +1,19 @@
+# 1st solution
+# O(n^2) time | O(1) space
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)):
+            ans += self.countPalindrome(s, i, i)
+            if i + 1 < len(s):
+                ans += self.countPalindrome(s, i, i + 1)
+        return ans  
+
+    def countPalindrome(self, s, left, right):
+        count = 0
+        while 0 <= left and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+            
+        return count
