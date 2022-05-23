542a247115c9873e7582d1046b5026ac857ab213
yingzhuo <yingzhuo1994@gmail.com>
2022-05-22 20:39:54 -0500

    renew

--- 0518_CoinChange(2).py
+++ 0518_CoinChange(2).py
@@ -22,8 +22,8 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
-        for i in coins:
-            for j in range(1, amount + 1):
-                if j >= i:
-                    dp[j] += dp[j - i]
+        for denom in coins:
+            for value in range(1, amount + 1):
+                if value >= denom:
+                    dp[value] += dp[value - denom]
         return dp[amount]
