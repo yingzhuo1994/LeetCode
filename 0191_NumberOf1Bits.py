6802c75d903f9bd1f09db44329400c126e99a710
yingzhuo <yingzhuo1994@gmail.com>
2022-05-25 19:44:54 -0500

    renew

--- 0191_NumberOf1Bits.py
+++ 0191_NumberOf1Bits.py
@@ -1,9 +1,8 @@
+# O(1) time | O(1) space
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # O(1) time | O(1) space
         count = 0
         while n:
-            if (n & 1) == 1:
-                count += 1
-            n = n >> 1
+            count += n & 1
+            n >>= 1
         return count
