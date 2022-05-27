a03d75edcb7ac5e0bf530a549a3314124eba0b61
yingzhuo <yingzhuo1994@gmail.com>
2022-05-26 19:54:06 -0500

    1st solution

---
+++ 1342_NumberOfStepsToReduceANumberToZero.py
@@ -0,0 +1,12 @@
+# 1st solution
+# O(1) time | O(1) space
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+        while num > 0:
+            if num & 1:
+                num -= 1
+            else:
+                num >>= 1
+            steps += 1
+        return steps
