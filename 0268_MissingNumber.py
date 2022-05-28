3422db399e11f71b0a00469108330d54649ecb99
yingzhuo <yingzhuo1994@gmail.com>
2022-05-27 19:29:40 -0500

    renew

--- 0268_MissingNumber.py
+++ 0268_MissingNumber.py
@@ -1,7 +1,7 @@
+# Gauss' Formula
+# O(n) time | O(1) space
+# sum() function costs O(n) time
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # Gauss' Formula
-        # O(n) time | O(1) space
-        # sum() function costs O(n) time
         n = len(nums)
         return (1 + n) * n // 2 - sum(nums)
