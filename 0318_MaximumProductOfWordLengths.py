df5eacc88a94bfd7733a10c7a3ab63780486e947
yingzhuo <yingzhuo1994@gmail.com>
2022-05-28 20:15:23 -0500

    add two methods

--- 0318_MaximumProductOfWordLengths.py
+++ 0318_MaximumProductOfWordLengths.py
@@ -20,3 +20,37 @@ class Solution:
             if ch in s_set:
                 return False
         return True
+
+# 2nd solution, Bitmask
+# O(n^2) time | O(n) space
+class Solution:
+    def maxProduct(self, words):
+        d, ans = defaultdict(int), 0
+        for word in words:
+            for l in word:
+                d[word] |= 1<<(ord(l) - 97)
+                
+        for w1, w2 in combinations(d.keys(), 2):
+            if d[w1] & d[w2] == 0: 
+                ans = max(ans, len(w1)*len(w2))
+                
+        return ans
+
+# 3rd solution, Improved Bitmask
+# O(n^2) time | O(n) space
+class Solution:
+    def maxProduct(self, words):
+        dic = defaultdict(int)
+        ans = 0
+
+        for word in words:
+            keyBit = 0
+            for l in word:
+                keyBit |= 1<<(ord(l) - 97)
+            dic[keyBit] = max(dic.get(keyBit, 0), len(word))
+
+        for k1, k2 in combinations(dic.keys(), 2):
+            if k1 & k2 == 0: 
+                ans = max(ans, dic[k1]*dic[k2])
+                
+        return ans
