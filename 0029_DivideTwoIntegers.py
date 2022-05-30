f361eb270c2bd6a0aeb7ecbff537aa8a1ac4bb97
yingzhuo <yingzhuo1994@gmail.com>
2022-05-29 20:19:41 -0500

    renew

--- 0029_DivideTwoIntegers.py
+++ 0029_DivideTwoIntegers.py
@@ -1,24 +1,28 @@
+# 1st solution
+# O(logN) time | O(logN) space 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        # O(logN) time | O(logN) space 
         sign1 = 1 if dividend >= 0 else -1
         sign2 = 1 if divisor >= 0 else -1
         dividend = abs(dividend)
         divisor = abs(divisor)
-        r = 0
-        sumDivisor = 0
-        divisorLst = [[1, divisor]]
-        while divisor <= dividend:
-            maxDivisor = divisorLst[-1][1] + divisorLst[-1][1]
-            if maxDivisor <= dividend:
-                curR = divisorLst[-1][0] + divisorLst[-1][0]
-                divisorLst.append([curR, maxDivisor])
-            for i in reversed(range(len(divisorLst))):
-                if divisorLst[i][1] <= dividend:
-                    dividend -= divisorLst[i][1]
-                    r += divisorLst[i][0]
-                    break
+
+        divisorLst = [divisor]
+        quotes = [1]
+        while dividend >= divisor:
+            divisor += divisor
+            divisorLst.append(divisor)
+            quotes.append(quotes[-1] + quotes[-1])
+
+        ans = 0
+        while dividend > 0:
+            idx = bisect.bisect_right(divisorLst, dividend)
+            if idx == 0:
+                break
+            ans += quotes[idx - 1]
+            dividend -= divisorLst[idx - 1]
+
         if (sign1 > 0) is (sign2 > 0):
-            return min(r, 2**31-1)
+            return min(ans, 2**31-1)
         else:
-            return -min(r, 2**31)
+            return -min(ans, 2**31)
