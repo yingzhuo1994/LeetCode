21987a6532b5c225c2a7157ba1bd3d24ad50b88a
yingzhuo <yingzhuo1994@gmail.com>
2022-05-29 20:48:36 -0500

    2nd solution

--- 0029_DivideTwoIntegers.py
+++ 0029_DivideTwoIntegers.py
@@ -26,3 +26,15 @@ class Solution:
             return min(ans, 2**31-1)
         else:
             return -min(ans, 2**31)
+
+# 2nd solution
+# O(1) time | O(1) space 
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if (dividend == -2147483648 and divisor == -1): return 2147483647
+        a, b, res = abs(dividend), abs(divisor), 0
+        for x in reversed(range(32)):
+            if (a >> x) - b >= 0:
+                res += 1 << x
+                a -= b << x
+        return res if (dividend > 0) == (divisor > 0) else -res
