6ac99ce0cc990de7f576493b0368bafbae07b3e6
yingzhuo <yingzhuo1994@gmail.com>
2022-06-04 20:31:01 -0500

    renew

--- 0146_LRUCache.py
+++ 0146_LRUCache.py
@@ -1,38 +1,35 @@
 class LRUCache:
-    def __init__(self, MSize):
-        self.size = MSize
+    def __init__(self, capacity: int):
+        self.size = capacity
         self.cache = {}
-        self.next, self.before = {}, {}
+        self.next, self.prev = {}, {}
         self.head, self.tail = '#', '$'
         self.connect(self.head, self.tail)
 
     def connect(self, a, b):
-        self.next[a], self.before[b] = b, a
+        self.next[a], self.prev[b] = b, a
 
     def delete(self, key):
-        self.connect(self.before[key], self.next[key])
-        del self.before[key], self.next[key], self.cache[key]
+        self.connect(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache[key]
 
     def append(self, k, v):
         self.cache[k] = v
-        self.connect(self.before[self.tail], k)
+        self.connect(self.prev[self.tail], k)
         self.connect(k, self.tail)
         if len(self.cache) > self.size:
             self.delete(self.next[self.head])
 
-    def get(self, key):
+    def get(self, key: int) -> int:
         if key not in self.cache: return -1
         val = self.cache[key]
         self.delete(key)
         self.append(key, val)
         return val
 
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         if key in self.cache: self.delete(key)
         self.append(key, value)
-
-        
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
