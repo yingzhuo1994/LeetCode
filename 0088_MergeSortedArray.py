bad8f1c2a08148f2b94d4750c2efdd670a233413
yingzhuo <yingzhuo1994@gmail.com>
2022-06-06 19:39:03 -0500

    renew

--- 0088_MergeSortedArray.py
+++ 0088_MergeSortedArray.py
@@ -1,9 +1,9 @@
+# O(n) time | O(1) space
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # O(n) time | O(1) space
         while n > 0:
             if m <= 0 or nums2[n-1] >= nums1[m-1]:
                 nums1[m+n-1] = nums2[n-1]
