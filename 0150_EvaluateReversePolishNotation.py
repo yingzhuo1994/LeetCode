b76ba89d0f3a64e8c86f198e754f2289bf524a84
yingzhuo <yingzhuo1994@gmail.com>
2022-06-06 20:33:41 -0500

    renew

--- 0150_EvaluateReversePolishNotation.py
+++ 0150_EvaluateReversePolishNotation.py
@@ -1,7 +1,7 @@
+# 1st solution
+# O(n) time | O(1) space
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # 1st solution
-        # O(n) time | O(1) space
         stack = []
         for ch in tokens:
             if ch not in '+-*/':
