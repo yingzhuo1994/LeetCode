0f54ab1de03178e6a6a3661f191ee1fe2c35f6f0
yingzhuo <yingzhuo1994@gmail.com>
2022-06-08 20:25:14 -0500

    renew

--- 0174_DungeonGame.py
+++ 0174_DungeonGame.py
@@ -1,14 +1,12 @@
+# O(mn) time | O(mn) space
 class Solution:
-    # O(mn) time | O(mn) space
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         m, n = len(dungeon), len(dungeon[0])
         dp = [[float("inf")]*(n+1) for _ in range(m+1)]
         dp[m-1][n], dp[m][n-1] = 1, 1
             
-        for i in range(m-1,-1,-1):
-            for j in range(n-1,-1,-1):
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
                 dp[i][j] = max(min(dp[i+1][j],dp[i][j+1])-dungeon[i][j],1)
         
         return dp[0][0]
-
-        
