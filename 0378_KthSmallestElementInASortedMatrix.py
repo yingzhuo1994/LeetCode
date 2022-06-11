dd0f06cf8055dc873912025056810aa95b8c5e76
yingzhuo <yingzhuo1994@gmail.com>
2022-06-10 20:31:44 -0500

    renew

--- 0378_KthSmallestElementInASortedMatrix.py
+++ 0378_KthSmallestElementInASortedMatrix.py
@@ -79,20 +79,22 @@ class Solution:
 # where A is the difference between the maximum and minimum in the matrix.
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n, beg, end = len(matrix), matrix[0][0], matrix[-1][-1]
+        def enough(value):
+            count = 0
+            for row in matrix:
+                idx = bisect.bisect_right(row, value)
+                count += idx
+            return count < k
         
-        def check(m):
-            i, j, cnt = 0, n - 1, 0
-            for i in range(n):
-                while j >= 0 and matrix[i][j] > m: j -= 1
-                cnt += (j + 1)
-            return cnt
-         
-        while beg < end:
-            mid = (beg + end)//2
-            if check(mid) < k:
-                beg = mid + 1
+        smallest = matrix[0][0]
+        largest = matrix[-1][-1]
+        
+        ans = smallest
+        while smallest <= largest:
+            value = smallest + (largest - smallest) // 2
+            if enough(value):
+                smallest = value + 1
             else:
-                end = mid
-                
-        return beg
+                ans = value
+                largest = value - 1
+        return ans
