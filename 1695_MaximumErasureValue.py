14ba87096cd62ac73899917ff442a66b7bd18140
yingzhuo <yingzhuo1994@gmail.com>
2022-06-11 19:25:58 -0500

    2nd solution

--- 1695_MaximumErasureValue.py
+++ 1695_MaximumErasureValue.py
@@ -14,3 +14,21 @@ class Solution:
             curTotal = arraySum[i+1] - arraySum[start]
             ans = max(ans, curTotal)
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        ans = 0
+        dic = {}
+        start = 0
+        curTotal = 0
+        for i, num in enumerate(nums):
+            if num in dic and dic[num] >= start:
+                for j in range(start, dic[num] + 1):
+                    curTotal -= nums[j]
+                start = dic[num] + 1
+            dic[num] = i
+            curTotal += nums[i]
+            ans = max(ans, curTotal)
+        return ans
