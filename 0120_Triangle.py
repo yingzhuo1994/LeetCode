114d8309ced75b5030df38e7750e7ef90d346894
yingzhuo <yingzhuo1994@gmail.com>
2022-06-12 20:01:04 -0500

    renew

--- 0120_Triangle.py
+++ 0120_Triangle.py
@@ -1,23 +1,7 @@
+# 1st solution, bottom-up dp
+# O(n) time | O(n) space
+# where n is the element number of triangle
 class Solution:
-    # 1st solution, brute-force
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        minSum = [float('inf')]
-        self.dfs(triangle, 0, 0, 0, minSum)
-        return minSum[0]
-    
-    def dfs(self, triangle, layer, index, curSum, minSum):
-        if layer == len(triangle) - 1:
-            if index < len(triangle[layer]):
-                curSum += triangle[layer][index]
-                minSum[0] = min(minSum[0], curSum)
-            return 
-        
-        self.dfs(triangle, layer + 1, index, curSum + triangle[layer][index], minSum)
-        self.dfs(triangle, layer + 1, index + 1, curSum + triangle[layer][index], minSum)
-    
-    # 2nd solution, dp
-    # O(n) time | O(n) space
-    # where n is the element number of triangle
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         dp = [[value for value in layer] for layer in triangle]
         for i in reversed(range(len(triangle) - 1)):
