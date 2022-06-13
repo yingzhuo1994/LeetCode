819f5bd20479b6d6d823ef2ab1409346740397a8
yingzhuo <yingzhuo1994@gmail.com>
2022-06-12 20:04:58 -0500

    2nd solution

--- 0120_Triangle.py
+++ 0120_Triangle.py
@@ -8,3 +8,16 @@ class Solution:
             for j in range(len(triangle[i])):
                 dp[i][j] += min(dp[i + 1][j], dp[i + 1][j + 1])
         return dp[0][0]
+
+# 2nd solution, top-down dp
+# O(n) time | O(n) space
+# where n is the element number of triangle
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [[float("inf") for _ in layer] for layer in triangle]
+        dp[0] = triangle[0]
+        for i in range(1, len(triangle)):
+            for k in range(len(triangle[i-1])):
+                dp[i][k] = min(dp[i][k], dp[i-1][k] + triangle[i][k])
+                dp[i][k+1] = min(dp[i][k+1], dp[i-1][k] + triangle[i][k+1])
+        return min(dp[-1])
