4e9230c598aed9115c1ec0804a7a96b23796512d
yingzhuo <yingzhuo1994@gmail.com>
2022-06-19 20:36:48 -0500

    3rd solution

--- 0222_CountCompleteTreeNodes.py
+++ 0222_CountCompleteTreeNodes.py
@@ -44,3 +44,27 @@ class Solution:
         if not root:
             return 0
         return 1 + self.getDepth(root.left)
+
+# 3rd solution, iteration
+# O(log(n) * log(n)) time | O(1) space
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        node = root
+        count = 0
+        while node:
+            left = self.getDepth(node.left)
+            right = self.getDepth(node.right)
+            if left == right:
+                count += 2**left
+                node = node.right
+            else:
+                count += 2**right
+                node = node.left
+        return count
+
+    def getDepth(self, root):
+        depth = 0
+        while root:
+            depth += 1
+            root = root.left
+        return depth
