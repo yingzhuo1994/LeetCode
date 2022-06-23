7e8aec86c11fc7dc3b1c2bf26943321f158e0a64
yingzhuo <yingzhuo1994@gmail.com>
2022-06-22 22:08:21 -0500

    complexity analysis

--- 0240_SearchA2DMatrix(2).py
+++ 0240_SearchA2DMatrix(2).py
@@ -1,15 +1,6 @@
+# 1st solution
+# O(m+n) time | O(1) space
 class Solution:
-    # 1st solution
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        j = -1
-        for row in matrix:
-            while j + len(row) and row[j] > target:
-                j -= 1
-            if row[j] == target:
-                return True
-        return False
-
-    # 2nd solution
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
         r, c = 0, n-1
