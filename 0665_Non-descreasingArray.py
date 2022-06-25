51f40878c0cb8246a239461916ed16c62b16b01d
yingzhuo <yingzhuo1994@gmail.com>
2022-06-24 20:10:06 -0500

    3rd solution

--- 0665_Non-descreasingArray.py
+++ 0665_Non-descreasingArray.py
@@ -42,3 +42,17 @@ class Solution:
             if count > 1:
                 return False
         return True
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        p, n = -1, len(nums)
+
+        for i in range(n - 1):
+            if nums[i] > nums[i + 1]:
+                if p != -1:
+                    return False
+                p = i
+        
+        return p in [-1, 0, n-2] or nums[p-1] <= nums[p+1] or nums[p] <= nums[p+2]
