ee0dc5a6959b36139e72a6e2f3f4a200f644b41e
yingzhuo <yingzhuo1994@gmail.com>
2022-06-28 20:29:10 -0500

    simplify

--- 0406_QueueReconstructionByHeight.py
+++ 0406_QueueReconstructionByHeight.py
@@ -2,51 +2,9 @@
 # O(n*log(n) + n^2) time | O(n) space
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        people.sort()
+        people.sort(key = lambda x: (-x[0], x[1]))
         
-        dic = defaultdict(list)
-        for h, k in people:
-            dic[h].append(k)
-        heights = sorted(dic.keys())
         queue = []
-        for i in reversed(range(len(heights))):
-            h = heights[i]
-            queue = self.insert(dic, h, queue)
-        
-        return queue
-    
-    def insert(self, dic, h, queue):
-        result = []
-        i = 0
-        j = 0
-        while i < len(dic[h]):
-            k = dic[h][i]
-            if k == len(result):
-                result.append([h, k])
-                i += 1
-            else:
-                length = k - len(result)
-                result.extend(queue[j:j+length])
-                j += length
-        result.extend(queue[j:])
-        return result
-
-# 2nd solution
-# O(n*log(n) + n^2) time | O(n) space
-class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        people.sort()
-
-        indices = [i for i in range(len(people))]
-        queue = [None for _ in range(len(people))]
-        dic = defaultdict(list)
         for h, k in people:
-            dic[h].append(k)
-        heights = sorted(dic.keys())
-        for h in heights:
-            for i in range(len(dic[h])):
-                idx = dic[h][i] - i
-                queue[indices[idx]] = [h, dic[h][i]]
-                indices.pop(idx)
-        
+            queue.insert(k, [h, k])
         return queue
