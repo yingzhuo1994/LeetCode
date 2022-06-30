f9e5bd0788ed0135d75fb3bb0b1135350bbb9642
yingzhuo <yingzhuo1994@gmail.com>
2022-06-29 20:30:13 -0500

    2nd solution

--- 0462_MinimumMovesToEqualArrayElements(2).py
+++ 0462_MinimumMovesToEqualArrayElements(2).py
@@ -7,3 +7,10 @@ class Solution:
         median = nums[n // 2]
 
         return sum([abs(num - median) for num in nums])
+
+# 2nd solution
+# O(n*log(n)) time | O(n) space
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        return sum(nums[~i] - nums[i] for i in range(len(nums) / 2))
