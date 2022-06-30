8aed1ec0b020e79e585271475168445f70c0c9bd
yingzhuo <yingzhuo1994@gmail.com>
2022-06-29 20:36:23 -0500

    3rd solution

--- 0462_MinimumMovesToEqualArrayElements(2).py
+++ 0462_MinimumMovesToEqualArrayElements(2).py
@@ -14,3 +14,16 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
         return sum(nums[~i] - nums[i] for i in range(len(nums) / 2))
+
+# 3rd solution
+# O(n*log(n)) time | O(n) space
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        moves = 0
+        left, right = 0, len(nums) - 1
+        while left < right:
+            moves += nums[right] - nums[left]
+            left += 1
+            right -= 1
+        return moves
