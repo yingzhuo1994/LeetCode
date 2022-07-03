7f69abb1d2f8b5aaa8ca06ef46025a822704d848
yingzhuo <yingzhuo1994@gmail.com>
2022-07-02 20:12:51 -0500

    2nd solution

---
+++ 0376_WiggleSubsequence.py
@@ -0,0 +1,44 @@
+# 1st solution
+# O(n^2) time | O(n) space
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [[1, 1] for _ in range(n)]
+
+        ans = 1
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i][0] = max(dp[i][0], dp[j][1] + 1)
+                elif nums[j] > nums[i]:
+                    dp[i][1] = max(dp[i][1], dp[j][0] + 1)
+            ans = max(ans, max(dp[i]))
+        return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        largerState = [1, nums[0]]
+        smallerState = [1, nums[0]]
+
+        for i in range(1, n):
+            newLargetState = largerState[:]
+            newSmallerState = smallerState[:]
+
+            if nums[i] > smallerState[1]:
+                length = smallerState[0] + 1
+                if length > largerState[0] or (length == largerState[0] and nums[i] > largerState[1]):
+                    newLargetState = [length, nums[i]]
+            
+            if nums[i] < largerState[1]:
+                length = largerState[0] + 1
+                if length > smallerState[0] or (length == smallerState[0] and nums[i] < smallerState[1]):
+                    newSmallerState = [length, nums[i]]
+            
+            largerState = newLargetState
+            smallerState = newSmallerState
+        
+        ans = max(largerState[0], smallerState[0])
+        return ans
