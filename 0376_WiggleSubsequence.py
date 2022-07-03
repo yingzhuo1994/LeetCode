bec5e8cc7a2a4963621bbcee227d07e96b6a5869
yingzhuo <yingzhuo1994@gmail.com>
2022-07-02 20:28:43 -0500

    4th solution

--- 0376_WiggleSubsequence.py
+++ 0376_WiggleSubsequence.py
@@ -59,3 +59,22 @@ class Solution:
                 down = up + 1
         
         return max(up, down)
+
+# 4th solution, Greedy Approach
+# O(n) time | O(1) space
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return n
+        
+        prevDiff = nums[1] - nums[0]
+        count = 2 if prevDiff != 0 else 1
+
+        for i in range(2, n):
+            diff = nums[i] - nums[i-1]
+            if (diff > 0 and prevDiff <= 0) or (diff < 0 and prevDiff >= 0):
+                count += 1
+                prevDiff = diff
+        
+        return count
