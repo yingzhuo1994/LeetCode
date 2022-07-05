805916d0ea56d29dd2b8df3ff22ed5bbb646879d
yingzhuo <yingzhuo1994@gmail.com>
2022-07-04 20:13:47 -0500

    2nd solution

--- 0309_BestTimeToBuyAndSellStockWithCooldown.py
+++ 0309_BestTimeToBuyAndSellStockWithCooldown.py
@@ -1,5 +1,6 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
-    # O(n) time | O(n) space
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         if n <= 1: return 0
@@ -11,3 +12,21 @@ class Solution:
             dp_max[i] = max(dp_max[i-1], dp[i])
             
         return dp_max[-3]
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        n = len(prices)
+        dp = [0 for _ in range(n + 2)]
+        
+        for i in range(1, n):
+            dp[i] = dp[i-1]
+            for j in range(i):
+                profit = prices[i] - prices[j]
+                dp[i] = max(dp[i], profit + dp[j - 2])
+
+        return max(dp)
+                
