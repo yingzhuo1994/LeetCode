ef2fc14c76c04deeb57a730f02601f4651abc1b2
yingzhuo <yingzhuo1994@gmail.com>
2022-07-09 21:48:14 -0500

    2nd solution

--- 0746_MinCostClimbingStairs.py
+++ 0746_MinCostClimbingStairs.py
@@ -11,7 +11,22 @@ class Solution:
         dp[0] = cost[0]
         dp[1] = cost[1]
         
-        for i in range(2, n):
+        for i in range(2, n+1):
             dp[i] = min(dp[i-2:i]) + cost[i]
         
         return dp[-1]
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        if n <= 2:
+            return min(cost)
+        cost.append(0)
+        a, b = cost[0], cost[1]
+        
+        for i in range(2, n + 1):
+            a, b = b, min(a, b) + cost[i]
+        
+        return b
