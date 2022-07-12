d5149fe26de42a6922a1a0d883739336c55f6212
yingzhuo <yingzhuo1994@gmail.com>
2022-07-11 20:30:08 -0500

    2nd solution

--- 0473_MatchsticksToSquare.py
+++ 0473_MatchsticksToSquare.py
@@ -1,3 +1,5 @@
+# 1st solution
+# O(2^n*n) time | O(2^n) space
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
         k = 4
@@ -17,4 +19,34 @@ class Solution:
                     break
 
         return dp[-1] == 0
-       
+
+# 2nd solution
+# O(2^n*n) time | O(2^n) space
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        k = 4
+        nums = matchsticks
+        total = sum(nums)
+        if len(nums) < k or total % k != 0:
+            return False
+        goal = total // k
+        
+        nums.sort(reverse=True)
+        visited = [False] * len(nums)
+        
+        def can_partition(rest_k, cur_sum=0, next_index=0):
+            if rest_k == 1:
+                return True
+            
+            if cur_sum == goal:
+                return can_partition(rest_k - 1)
+            
+            for i in range(next_index, len(nums)):
+                if not visited[i] and cur_sum + nums[i] <= goal:
+                    visited[i] = True
+                    if can_partition(rest_k, cur_sum + nums[i], i + 1):
+                        return True
+                    visited[i] = False
+            return False 
+
+        return can_partition(k)
