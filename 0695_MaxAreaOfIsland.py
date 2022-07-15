90b809d917836d0796eafa4a6b83f18c22391639
yingzhuo <yingzhuo1994@gmail.com>
2022-07-14 19:43:18 -0500

    renew

--- 0695_MaxAreaOfIsland.py
+++ 0695_MaxAreaOfIsland.py
@@ -1,5 +1,5 @@
+# O(R * C) time | O(R * C) space
 class Solution:
-    # O(R * C) time | O(R * C) space
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         maxArea = 0
         for i in range(len(grid)):
@@ -8,13 +8,13 @@ class Solution:
                 maxArea = max(maxArea, curArea)
         return maxArea
     
-    def dfs(self, grid, i, j, curLength):
+    def dfs(self, grid, i, j, curArea):
         if grid[i][j] != 1:
             return 0
-        curLength += 1
+        curArea += 1
         grid[i][j] = "#"
         for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             row, col = i + x, j + y
             if 0 <= row < len(grid) and 0 <= col < len(grid[0]):
-                curLength += self.dfs(grid, row, col, 0)
-        return curLength
+                curArea += self.dfs(grid, row, col, 0)
+        return curArea
