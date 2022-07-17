c524ea990baaec1303120f357d9d5fffd1262efc
yingzhuo <yingzhuo1994@gmail.com>
2022-07-16 20:57:13 -0500

    2nd solution

--- 0629_KInversePairsArray.py
+++ 0629_KInversePairsArray.py
@@ -1,5 +1,4 @@
 # 1st solution, TLE
-from math import factorial
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
         def countInverserPairs(left_n, left_k):
@@ -26,3 +25,23 @@ class Solution:
         memo = {}
         countInverserPairs(n, k)
         return memo[(n, k)]
+
+# 2nd solution
+# O(nk) time | O(nk) space
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        dp = [[0 for _ in range(n + 1)] for _ in range(k + 1)]
+        MOD = 10**9 + 7
+        for j in range(1, n + 1):
+            dp[0][j] = 1
+        
+        for j in range(2, n + 1):
+            for i in range(1, k + 1):
+                if i - j >= 0:
+                    last = dp[i - j][j - 1]
+                else:
+                    last = 0
+                dp[i][j] = dp[i-1][j] - last + dp[i][j - 1]
+                dp[i][j] %= MOD
+
+        return dp[-1][-1]
