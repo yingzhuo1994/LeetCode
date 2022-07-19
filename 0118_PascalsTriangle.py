c99ce8ba6324424fcefcad6b9d0935adfc460309
yingzhuo <yingzhuo1994@gmail.com>
2022-07-18 19:56:48 -0500

    add complexity analysis

--- 0118_PascalsTriangle.py
+++ 0118_PascalsTriangle.py
@@ -1,30 +1,34 @@
+# 1st Recursive Solution
+# O(n^2) time | O(n^2) space
 class Solution:
-    # 1st Recursive Solution
-    # def generate(self, numRows: int) -> List[List[int]]:
-    #     def helper(n):
-    #         if n == 1:
-    #             return [1]
-    #         else:
-    #             lst = [1 for _ in range(n)]
-    #             lastRow = helper(n - 1)
-    #             for i in range(1, n - 1):
-    #                 lst[i] = lastRow[i - 1] + lastRow[i]
-    #             return lst
-    #     lst = [helper(r) for r in range(1, numRows + 1)]
-    #     return lst
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        
+        ans = self.generate(numRows - 1)
+        lst = [1 for _ in range(numRows)]
+        lastRow = ans[-1]
+        for i in range(1, numRows - 1):
+            lst[i] = lastRow[i - 1] + lastRow[i]
+        ans.append(lst)
+        return ans
 
-    # 2nd Iterative Solution
-    # def generate(self, numRows: int) -> List[List[int]]:
-    #     lst = [[1]]
-    #     for i in range(2, numRows + 1):
-    #         curRow = [1 for _ in range(i)]
-    #         lastRow = lst[i-2]
-    #         for j in range(1, i - 1):
-    #             curRow[j] = lastRow[j - 1] + lastRow[j]
-    #         lst.append(curRow)
-    #     return lst
+# 2nd Iterative Solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        lst = [[1]]
+        for i in range(2, numRows + 1):
+            curRow = [1 for _ in range(i)]
+            lastRow = lst[-1]
+            for j in range(1, i - 1):
+                curRow[j] = lastRow[j - 1] + lastRow[j]
+            lst.append(curRow)
+        return lst
 
-    # 3rd Solution
+# 3rd Solution
+# O(n^2) time | O(n^2) space
+class Solution:
     def generate(self, numRows):
         res = [[1]]
         for i in range(1, numRows):
