094362937dabe0be1b4e44b8a5d334d36d1c76cd
yingzhuo <yingzhuo1994@gmail.com>
2022-07-25 22:19:19 -0500

    2nd solution

--- 0394_DecodeString.py
+++ 0394_DecodeString.py
@@ -18,3 +18,23 @@ class Solution:
             else:
                 stack[-1] += ch                         
         return "".join(stack)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def decodeString(self, s: str) -> str:
+        num, stack = 0, [[]]
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == "[":
+                stack.append(num)
+                num = 0
+                stack.append([])
+            elif ch == "]":
+                last = stack.pop()
+                rep = stack.pop()
+                stack[-1] += rep * last
+            else:
+                stack[-1].append(ch)                         
+        return "".join(stack[0])
