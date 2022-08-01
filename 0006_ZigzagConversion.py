8bd5637af408506e1c5cd30d5ffb36965f610a92
yingzhuo <yingzhuo1994@gmail.com>
2022-07-31 20:00:15 -0500

    2nd solution

--- 0006_ZigzagConversion.py
+++ 0006_ZigzagConversion.py
@@ -17,3 +17,27 @@ class Solution:
         for i in range(k - 1, len(s), 2 * k - 2):
             stack.append(s[i])
         return "".join(stack)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) <= numRows or numRows == 1:
+            return s
+        stack = [[] for _ in range(numRows)]
+        i = 0
+        while i < len(s):
+            for row in range(numRows):
+                if i >= len(s):
+                    break
+                stack[row].append(s[i])
+                i += 1
+            for row in reversed(range(1, numRows-1)):
+                if i >= len(s):
+                    break
+                stack[row].append(s[i])
+                i += 1
+        ans = []
+        for row in range(numRows):
+            ans.extend(stack[row])
+        return "".join(ans)
