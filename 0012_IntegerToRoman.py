f5267e6fdf8e8b296d49cdd07a2b362853145466
yingzhuo <yingzhuo1994@gmail.com>
2022-08-01 20:45:47 -0500

    2nd solution

--- 0012_IntegerToRoman.py
+++ 0012_IntegerToRoman.py
@@ -1,3 +1,4 @@
+# 1st solution
 # O(1) time | O(1) space
 class Solution:
     def intToRoman(self, num: int) -> str:
@@ -43,3 +44,19 @@ class Solution:
                 num -= 1
                 stack.append("I")
         return "".join(stack)
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        def digit(a,b,c,dig):
+            return ["",a,2*a,3*a,a+b,b,b+a,b+2*a,b+3*a,a+c][dig]
+        
+        l = ["I","V","X","L","C","D","M","!","!"]
+        out, i = "", 0
+
+        while num > 0:
+            num, last = divmod(num, 10)
+            out = digit(l[i], l[i+1], l[i+2], last) + out
+            i += 2
+        return out
