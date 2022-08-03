5257cbd1e404b7db6e32fe087583a4271c53a7ba
yingzhuo <yingzhuo1994@gmail.com>
2022-08-02 21:20:52 -0500

    2nd solution

--- 0729_MyCalendar(1).py
+++ 0729_MyCalendar(1).py
@@ -11,7 +11,23 @@ class MyCalendar:
             self.endTime.insert(idx, end)
             return True
         return False
-    
+
+# 2nd solution
+# O(n*log(n)) time | O(n) space
+from sortedcontainers import SortedList
+class MyCalendar:
+    def __init__(self):
+        self.calendar = SortedList()
+
+    def book(self, start: int, end: int) -> bool:
+        q1 = SortedList.bisect_right(self.calendar, start)
+        q2 = SortedList.bisect_left(self.calendar, end)
+        if q1 == q2 and q1 % 2 == 0:
+            self.calendar.add(start)
+            self.calendar.add(end)
+            return True
+        return False
+
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
