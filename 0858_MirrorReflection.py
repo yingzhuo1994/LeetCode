f6d0517fe3529938bba099d241323406a5805830
yingzhuo <yingzhuo1994@gmail.com>
2022-08-03 20:59:04 -0500

    2nd solution

--- 0858_MirrorReflection.py
+++ 0858_MirrorReflection.py
@@ -3,3 +3,11 @@
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
         return (q//gcd(p,q)%2 - p//gcd(p,q)%2 + 1)
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        while p % 2 == 0 and q % 2 == 0: 
+            p, q = p // 2, q // 2
+        return 1 - p % 2 + q % 2
