d5b0059d78425b5c2da0e6e94bb7c73f725ef330
yingzhuo <yingzhuo1994@gmail.com>
2022-08-05 15:28:39 -0500

    renew

--- 0525_ContiguousArray.py
+++ 0525_ContiguousArray.py
@@ -31,9 +31,9 @@ class Solution:
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         count = 0
-        max_length=0
-        table = {0: 0}
-        for index, num in enumerate(nums, 1):
+        max_length = 0
+        table = {0: -1}
+        for index, num in enumerate(nums):
             if num == 0:
                 count -= 1
             else:
