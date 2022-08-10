a51e02b68cfb702fa7377898062ba84f1d803b42
yingzhuo <yingzhuo1994@gmail.com>
2022-08-09 20:26:26 -0500

    3rd solution

--- 0729_MyCalendar(1).py
+++ 0729_MyCalendar(1).py
@@ -28,6 +28,39 @@ class MyCalendar:
             return True
         return False
 
+# 3rd solution
+# O(n*log(n)) time | O(n) space
+class MyCalendar:
+    def __init__(self):
+        self.root = None
+
+    def book(self, start: int, end: int) -> bool:
+        if self.root is None:
+            self.root = Node(start, end)
+            return True
+        return self.root.insert(Node(start, end))
+
+class Node:
+    __slots__ = 'start', 'end', 'left', 'right'
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = self.right = None
+
+    def insert(self, node):
+        if node.start >= self.end:
+            if not self.right:
+                self.right = node
+                return True
+            return self.right.insert(node)
+        elif node.end <= self.start:
+            if not self.left:
+                self.left = node
+                return True
+            return self.left.insert(node)
+        else:
+            return False
+
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
