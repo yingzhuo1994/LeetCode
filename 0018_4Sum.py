5fe9fbfb1ca0c86593cd0f945464df5b9befd2a8
yingzhuo <yingzhuo1994@gmail.com>
2022-08-09 20:09:54 -0500

    2nd solution

--- 0018_4Sum.py
+++ 0018_4Sum.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n^2) time | O(n^2) space
+# O(n^3) time | O(n^2) space
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
@@ -37,3 +37,32 @@ class Solution:
             if array[i + 1] != array[i]:
                 ans.append(array[i + 1])
         return ans
+
+# 2nd solution
+# O(n^3) time | O(n^2) space
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def KSum(l, r, target, k, result, results):
+            if r-l+1 < k or k < 2 or target < nums[l]*k or target > nums[r]*k:  # early termination
+                return
+            if k == 2: # two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # recursively reduce N
+                for i in range(l, r+1):
+                    if i == l or nums[i-1] != nums[i]:
+                        KSum(i+1, r, target-nums[i], k-1, result+[nums[i]], results)
+
+        nums.sort()
+        results = []
+        KSum(0, len(nums)-1, target, 4, [], results)
+        return results
