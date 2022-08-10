03cbcd1dd4a772649e5f497c02418dfd88933e85
yingzhuo <yingzhuo1994@gmail.com>
2022-08-09 19:24:18 -0500

    2nd solution

--- 0108_ConvertSortedArrayToBinarySearchTree.py
+++ 0108_ConvertSortedArrayToBinarySearchTree.py
@@ -4,9 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 1st solution
+# O(n) time | O(logn) space
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        # O(N) time | O(logN) space
         if not nums:
             return None
 
@@ -15,3 +17,19 @@ class Solution:
         p.left = self.sortedArrayToBST(nums[:mid])
         p.right = self.sortedArrayToBST(nums[mid+1:])
         return p
+
+# 2nd solution
+# O(n) time | O(logn) space
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(start, end):
+            if start > end:
+                return None
+
+            mid = start + (end - start) // 2
+            root = TreeNode(nums[mid])
+            root.left = helper(start, mid - 1)
+            root.right = helper(mid + 1, end)
+            
+            return root
+        return helper(0, len(nums) - 1)
