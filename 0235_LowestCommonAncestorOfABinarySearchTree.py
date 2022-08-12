9b65d2c7fee9bab7506423a602568bbe6d67a4f2
yingzhuo <yingzhuo1994@gmail.com>
2022-08-11 19:50:03 -0500

    2nd solution

--- 0235_LowestCommonAncestorOfABinarySearchTree.py
+++ 0235_LowestCommonAncestorOfABinarySearchTree.py
@@ -15,3 +15,15 @@ class Solution:
         if left != None and right != None: return root
         if left != None: return left
         return right
+
+# 2nd solution
+# O(h) time | O(1) space
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
