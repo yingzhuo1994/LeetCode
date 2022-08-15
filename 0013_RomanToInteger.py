f95d37f3c2986cbfc64f66a679f1b7f419d4cefd
yingzhuo <yingzhuo1994@gmail.com>
2022-08-14 19:30:46 -0500

    renew

--- 0013_RomanToInteger.py
+++ 0013_RomanToInteger.py
@@ -1,44 +1,6 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # def substitute(c):
-            # if c == 'I':
-            #     return 1
-            # elif c == 'V':
-            #     return 5
-            # elif c == 'X':
-            #     return 10
-            # elif c == 'L':
-            #     return 50
-            # elif c == 'C':
-            #     return 100
-            # elif c == 'D':
-            #     return 500
-            # elif c == 'M':
-            #     return 1000
-            # else:
-            #     return None
-
-        # def transfrom(t):
-        #     num = 0
-        #     if len(t) == 1:
-        #         return t[0]
-        #     for i in range(len(t) - 1):
-        #         if t[i] >= t[i+1]:
-        #             num += t[i]
-        #         else:
-        #             num -= t[i]
-        #     num += t[-1]
-        #     return num
-
-        # lst = [0 for _ in range(len(s))]
-        # for i in range(len(s)):
-        #     lst[i] = substitute(s[i])
-        # return transfrom(lst)
-
+# O(n) time | O(1) space
+class Solution:
+    def romanToInt(self, s: str) -> int:
         dic = {
             'I': 1,
             'V': 5,
