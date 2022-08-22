73d57e26e8bd64da79081de65fd5188af0593f42
yingzhuo <yingzhuo1994@gmail.com>
2022-08-21 20:11:22 -0500

    2nd solution

--- 0342_PowerOfFour.py
+++ 0342_PowerOfFour.py
@@ -7,3 +7,9 @@ class Solution:
             x += 1
         
         return 4**x == n
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and n & (n-1) == 0 and 0b1010101010101010101010101010101 & n == n
