ea7cbaf84a235ef3935c60893d99702060d39d2a
yingzhuo <yingzhuo1994@gmail.com>
2022-08-23 19:50:29 -0500

    renew

--- 0326_PowerOfThree.py
+++ 0326_PowerOfThree.py
@@ -1,11 +1,13 @@
+# 1st solution
+# O(1) time | O(1) space
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        # 1st solution
-        # O(1) time | O(1) space
         return n > 0 and 3 ** 19 % n == 0
 
-        # 2nd loop iteration solution
-        # O(logn) time | O(1) space
+# 2nd loop iteration solution
+# O(logn) time | O(1) space
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
         if n < 1:
             return False
         while n % 3 == 0:
