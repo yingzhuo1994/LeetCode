8c82c9b57abafbdaeee6cf7668b8d519075b37cb
yingzhuo <yingzhuo1994@gmail.com>
2022-08-23 20:15:14 -0500

    1st solution

---
+++ 1780_CheckIfNumberIsASumOfPowersOfThree.py
@@ -0,0 +1,27 @@
+# 1st solution
+# O(n) time | O(log(n)) space
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        threeArray = []
+        k = 0
+        while 3**k <= n:
+            threeArray.append(3**k)
+            k += 1
+        threeArray.reverse()
+
+        def dfs(idx, left):
+            if left == 0:
+                return True
+            if idx >= len(threeArray):
+                return False
+            num = threeArray[idx]
+            if left > num:
+                if dfs(idx + 1, left - num):
+                    return True
+            elif left == num:
+                return True
+            if dfs(idx + 1, left):
+                return True
+            return False
+        
+        return dfs(0, n)
