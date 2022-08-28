9198b38c027b4d747d3800432898dac4656e1768
yingzhuo <yingzhuo1994@gmail.com>
2022-08-27 20:12:31 -0500

    2nd solution

--- 1329_SortTheMatrixDiagonally.py
+++ 1329_SortTheMatrixDiagonally.py
@@ -27,3 +27,20 @@ class Solution:
             modifyMatrix(i, 0)
 
         return mat
+
+# 2nd solution
+# O((m + n) * D * log(D)) time | O(mn) space
+# where D = min(m + n)
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n, diagDic = len(mat), len(mat[0]), defaultdict(list)
+        for i in range(m):
+            for j in range(n):
+                diagDic[j - i].append(mat[i][j])
+                
+        for k in diagDic:
+            diagDic[k].sort()
+            for i, num in enumerate(diagDic[k]):
+                mat[i + max(-k, 0)][k + i + max(-k, 0)] = num
+                
+        return mat
