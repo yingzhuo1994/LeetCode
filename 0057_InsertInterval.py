d99d44b591728489a57c3e645203ec858bcb51a7
yingzhuo <yingzhuo1994@gmail.com>
2022-09-05 20:53:35 -0500

    1st solution

---
+++ 0057_InsertInterval.py
@@ -0,0 +1,47 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            intervals.append(newInterval)
+            return intervals
+        def getIndex(idx):
+            left, right = 0, len(intervals) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if intervals[mid][idx] <= newInterval[idx]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+        
+        startIdx = getIndex(0)
+        endIdx = getIndex(1)
+
+        if startIdx == len(intervals):
+            if intervals[-1][1] < newInterval[0]:
+                intervals.append(newInterval)
+            else:
+                intervals[-1][1] = max(intervals[-1][1], newInterval[1])
+        elif endIdx == 0:
+            if intervals[0][0] > newInterval[1]:
+                intervals.insert(0, newInterval)
+            else:
+                intervals[0][0] = min(intervals[0][0], newInterval[0])
+        else:
+            if startIdx > 0 and intervals[startIdx - 1][1] >= newInterval[0]:
+                start = startIdx - 1
+            else:
+                start = startIdx
+            startValue = min(intervals[start][0], newInterval[0])
+            
+            if endIdx < len(intervals) and intervals[endIdx][0] > newInterval[1]:
+                end = endIdx - 1
+            else:
+                end = min(endIdx, len(intervals) - 1)
+            endValue = max(intervals[end][1], newInterval[1])
+        
+            for i in reversed(range(start, end + 1)):
+                intervals.pop(i)
+            intervals.insert(start, [startValue, endValue])
+        return intervals
