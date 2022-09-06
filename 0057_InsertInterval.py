4b8320aeb4f54377add1ce15b7ebaae0bf8bd6a7
yingzhuo <yingzhuo1994@gmail.com>
2022-09-05 21:05:28 -0500

    2nd solution

--- 0057_InsertInterval.py
+++ 0057_InsertInterval.py
@@ -45,3 +45,20 @@ class Solution:
                 intervals.pop(i)
             intervals.insert(start, [startValue, endValue])
         return intervals
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res, i = [], -1
+        for i, (x, y) in enumerate(intervals):
+            if y < newInterval[0]:
+                res.append([x, y])
+            elif newInterval[1] < x:
+                i -= 1
+                break
+            else:
+                newInterval[0] = min(newInterval[0], x)
+                newInterval[1] = max(newInterval[1], y)
+                
+        return res + [newInterval] + intervals[i+1:]
