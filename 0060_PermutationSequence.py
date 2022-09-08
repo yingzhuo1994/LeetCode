2be82ef6f4ec246c2f0250aad2ae5ec368b2b114
yingzhuo <yingzhuo1994@gmail.com>
2022-09-07 19:57:33 -0500

    1st solution

---
+++ 0060_PermutationSequence.py
@@ -0,0 +1,19 @@
+# 1st solution
+# O(n^2) time | O(n) space
+import math
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        lst = [str(i) for i in range(1, n + 1)]
+        ans = []
+        def permutationWithK(lst, ans, k):
+            length = len(lst)
+            if length == 0:
+                return
+            m = math.factorial(length - 1)
+            i = (k - 1) // m
+            r = (k -1 ) % m + 1
+            ans.append(lst.pop(i))
+            permutationWithK(lst, ans, r)
+        
+        permutationWithK(lst, ans, k)
+        return "".join(ans)
