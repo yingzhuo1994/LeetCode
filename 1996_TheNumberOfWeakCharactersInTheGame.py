5c7ef50289d9f179775cfce060e18028c594767c
yingzhuo <yingzhuo1994@gmail.com>
2022-09-08 20:30:29 -0500

    2nd solution

--- 1996_TheNumberOfWeakCharactersInTheGame.py
+++ 1996_TheNumberOfWeakCharactersInTheGame.py
@@ -1,5 +1,6 @@
 # 1st solution, TLE
 # O(n^2) time | O(1) space
+from collections import defaultdict
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         n = len(properties)
@@ -11,3 +12,24 @@ class Solution:
                 if j not in visited and all(properties[i][k] > properties[j][k] for k in range(2)):
                     visited.add(j)
         return len(visited)
+
+# 2nd solution
+# O(nlog(n)) time | O(1) space
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        attackDic = defaultdict(list)
+        for attack, defense in properties:
+            attackDic[attack].append(defense)
+        
+        attackArray = sorted(attackDic.keys())
+        count = 0
+        for attack in attackArray:
+            attackDic[attack].sort()
+            
+        maxDefense = attackDic[attackArray[-1]][-1]
+        for i in reversed(range(0, len(attackArray) - 1)):
+            attack = attackArray[i]
+            count += bisect.bisect_left(attackDic[attack], maxDefense)
+            maxDefense = max(maxDefense, attackDic[attack][-1])
+
+        return count
