606cd5eb9818e6f7f44880f5cc39a52a6a908952
yingzhuo <yingzhuo1994@gmail.com>
2022-09-09 19:29:14 -0500

    2nd solution

--- 0089_GrayCode.py
+++ 0089_GrayCode.py
@@ -1,7 +1,21 @@
-# 1st solution, TLE
+# 1st solution, recursion
 # O(2^n) time | O(n) space
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        if n == 0: return [0]
-        t = self.grayCode(n-1)
-        return t + [i+(1<<(n-1)) for i in t][::-1]
+        if n == 1: return [0, 1]
+        k = n - 1
+        last = self.grayCode(k)
+        new = [num | (1<<(k)) for num in last]
+        new.reverse()
+        return last + new
+
+# 2nd solution, iteration
+# O(2^n) time | O(n) space
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        ans = [0, 1]
+        for k in range(1, n):
+            new = [num | (1<<(k)) for num in ans]
+            new.reverse()
+            ans += new
+        return ans
