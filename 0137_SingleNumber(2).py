9a51460e32f1beec95a603183a073e71eb183a85
yingzhuo <yingzhuo1994@gmail.com>
2022-09-14 20:36:39 -0500

    1st solution

---
+++ 0137_SingleNumber(2).py
@@ -0,0 +1,20 @@
+# 1st solution
+# O(32n) time | O(1) space
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        negCount = 0
+        for num in nums:
+            if num < 0:
+                negCount += 1
+        sign = 1
+        if negCount % 3 != 0:
+            sign = -1
+        ans = 0
+        for k in range(32):
+            count = 0
+            for num in nums:
+                if (abs(num) >> k) & 1:
+                    count += 1
+            if count % 3 > 0:
+                ans |= 1 << k
+        return sign * ans
