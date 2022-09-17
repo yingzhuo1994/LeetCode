49384d645ee47e7cae95eee11e3fd0a1a7b457d0
yingzhuo <yingzhuo1994@gmail.com>
2022-09-16 22:23:21 -0500

    2nd solution

--- 0336_PalindromePairs.py
+++ 0336_PalindromePairs.py
@@ -22,3 +22,78 @@ class Solution:
             i += 1
             j -= 1
         return True
+
+# 2nd solution
+# O(ksn) time | O(ksn) space
+# where n is the length of words, k is the largest word length, and s is the unique word length of words
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        dic = {word: i for i, word in enumerate(words)}
+        sameLengthPalindrome = set()
+        palindromLst = []
+        possibleLength = set()
+        for word in words:
+            possibleLength.add(len(word))
+        if 0 in possibleLength:
+            possibleLength.remove(0)
+        possibleLength = sorted(list(possibleLength))
+        ans = []
+        hasEmpty = False
+        for word in words:
+            i = dic[word]
+            if word == "":
+                hasEmpty = True
+            elif self.isPalindrome(word):
+                palindromLst.append(word)
+            elif word not in sameLengthPalindrome and word[::-1] in dic:
+                j = dic[word[::-1]]
+                ans.append([i, j])
+                ans.append([j, i])
+                sameLengthPalindrome.add(word)
+                sameLengthPalindrome.add(word[::-1])
+            
+            candidates = self.getCandidates(word, possibleLength)
+            for candidate, place in candidates:
+                if candidate in dic:
+                    j = dic[candidate]
+                    if place == "left":
+                        ans.append([i, j])
+                    else:
+                        ans.append([j, i])
+        
+        if hasEmpty:
+            i = dic[""]
+            for word in palindromLst:
+                j = dic[word]
+                ans.append([i, j])
+                ans.append([j, i])
+
+        return ans
+    
+    def isPalindrome(self, word):
+        i, j = 0, len(word) - 1
+        while i < j:
+            if word[i] != word[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+    
+    def getCandidates(self, word, possibleLength):
+        n = len(word)
+        candidates = []
+        idx = bisect.bisect_left(possibleLength, n)
+        # in the left
+        for i in range(idx):
+            L = possibleLength[i]
+            if self.isPalindrome(word[L:]):
+                candidate = word[:L]
+                candidates.append([candidate[::-1], "left"])
+
+        # in the right
+        for i in range(idx):
+            L = possibleLength[i]
+            if self.isPalindrome(word[:-L]):
+                candidate = word[-L:]
+                candidates.append([candidate[::-1], "right"])
+        return candidates
