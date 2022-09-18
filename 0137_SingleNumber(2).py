b6ba608b36c214ccea686858388fc70a3bea3564
yingzhuo <yingzhuo1994@gmail.com>
2022-09-17 20:00:49 -0500

    2nd solution

--- 0137_SingleNumber(2).py
+++ 0137_SingleNumber(2).py
@@ -18,3 +18,20 @@ class Solution:
             if count % 3 > 0:
                 ans |= 1 << k
         return sign * ans
+
+# 2nd solution
+# O(32n) time | O(1) space
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for k in range(32):
+            count = 0
+            for num in nums:
+                if (num >> k) & 1:
+                    count += 1
+            if count % 3 != 0:
+                ans |= 1 << k
+
+        if (ans >> 31) & 1:
+            return ans - (1 << 32)
+        return ans
