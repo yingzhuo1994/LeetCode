54297b81a6755ab1970a6bb0921f71efda33990e
yingzhuo <yingzhuo1994@gmail.com>
2022-09-21 20:02:57 -0500

    2nd solution

--- 0111_MinimumDepthOfBinaryTree.py
+++ 0111_MinimumDepthOfBinaryTree.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 
-# 1st solution
+# 1st solution, BFS
 # O(n) time | O(n) space
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
@@ -22,3 +22,14 @@ class Solution:
                 stack.append([node.left, depth + 1])
             if node.right:
                 stack.append([node.right, depth + 1])
+
+# 2nd solution, DFS
+# O(n) time | O(d) space
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if root.left and root.right:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
