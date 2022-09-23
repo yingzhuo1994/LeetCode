ad69c8c12c2ab7c97c9e6df65cfef11d6fc4fdc6
yingzhuo <yingzhuo1994@gmail.com>
2022-09-22 20:31:57 -0500

    3rd solution

--- 1680_ConcatenationOfConsecutiveBinaryNumbers.py
+++ 1680_ConcatenationOfConsecutiveBinaryNumbers.py
@@ -19,3 +19,34 @@ class Solution:
         for i in range(1, n + 1):
             ans = (ans * (1 << (len(bin(i)) - 2)) + i) % MOD
         return ans
+
+# 3rd solution
+# O((log(n))^2) time | O(log(n)) space
+class Solution:
+    def concatenatedBinary(self, n: int, p: int = 10 ** 9 + 7) -> int:
+        def matmul(X, Y, p):
+            return [[sum(a * b for a, b in zip(X_row, Y_col)) % p for Y_col in zip(*Y)] for X_row in X]
+
+        def mv(X, v, p):
+            return [sum(a * b for a, b in zip(X_row, v)) % p for X_row in X]
+
+        def matpowv(X, n, v, p):
+            while n > 0:
+                if n & 1: v = mv(X, v, p)
+                X, n = matmul(X, X, p), n >> 1
+            return v
+    
+        n += 1
+        v = [0, 1, 1]
+        bit = 1
+        while bit < n:
+            M = bit << 1
+            mat = [
+                [M, 1, 0],
+                [0, 1, 1],
+                [0, 0, 1],
+            ]
+            npow = min(bit, n - bit)
+            v = matpowv(mat, npow, v, p)
+            bit = M
+        return v[0]
