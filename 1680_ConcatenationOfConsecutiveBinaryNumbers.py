09e179dfad15289e3ba415ac0a81e835afd36ab9
yingzhuo <yingzhuo1994@gmail.com>
2022-09-22 19:30:55 -0500

    2nd solution

--- 1680_ConcatenationOfConsecutiveBinaryNumbers.py
+++ 1680_ConcatenationOfConsecutiveBinaryNumbers.py
@@ -9,4 +9,13 @@ class Solution:
         ans = "".join(stack)
         ans = int(ans, 2) % MOD
         return ans
-        
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10**9 + 7
+        ans = 0
+        for i in range(1, n + 1):
+            ans = (ans * (1 << (len(bin(i)) - 2)) + i) % MOD
+        return ans
