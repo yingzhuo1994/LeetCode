29031bcd72b8ec6174365651e59954b0045f3cef
yingzhuo <yingzhuo1994@gmail.com>
2022-09-23 21:12:02 -0500

    renew

--- 0087_ScrambleString.py
+++ 0087_ScrambleString.py
@@ -1,9 +1,9 @@
 # O(3^n) time | O(3^n) space
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
-        def helper(s1, s2, m):
-            if (s1, s2) in m:
-                return m[(s1, s2)]
+        def helper(s1, s2):
+            if (s1, s2) in memo:
+                return memo[(s1, s2)]
 
             if len(s1) == 1:
                 return s1 == s2
@@ -11,11 +11,11 @@ class Solution:
                 return False
 
             for i in range(1, len(s1)):
-                if helper(s1[:i], s2[-i:], memo) and helper(s1[i:], s2[:-i], memo) or \
-                helper(s1[:i], s2[:i], memo) and helper(s1[i:], s2[i:], memo):
+                if helper(s1[:i], s2[-i:]) and helper(s1[i:], s2[:-i]) or \
+                helper(s1[:i], s2[:i]) and helper(s1[i:], s2[i:]):
                     memo[(s1, s2)] = True
                     return True
             memo[(s1, s2)] = False
             return False
         memo = {}
-        return helper(s1, s2, memo)
+        return helper(s1, s2)
