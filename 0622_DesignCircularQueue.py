de4298ddcf25914f0cd5c6470bcfa6109c39fee9
yingzhuo <yingzhuo1994@gmail.com>
2022-09-24 19:47:01 -0500

    2nd solution

--- 0622_DesignCircularQueue.py
+++ 0622_DesignCircularQueue.py
@@ -1,3 +1,4 @@
+# 1st solution
 class MyCircularQueue:
 
     def __init__(self, k: int):
@@ -56,11 +57,44 @@ class Node:
         self.next = next
 
 
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
+# 2nd solution
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.capacity = k
+        self.size = 0
+        self.stack = [0] * k
+        self.head = self.tail = 0
+        
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.stack[self.tail] = value
+        self.tail = (self.tail + 1) % self.capacity
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.stack[self.head] = 0
+        self.head = (self.head + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.stack[self.head]
+       
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.stack[self.tail - 1]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+        
+    def isFull(self) -> bool:
+        return self.size == self.capacity
