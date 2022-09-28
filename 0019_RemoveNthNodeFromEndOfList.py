c5973a0bb1be4815b5174d56eb75e9ab779d9a11
yingzhuo <yingzhuo1994@gmail.com>
2022-09-27 19:23:59 -0500

    renew

--- 0019_RemoveNthNodeFromEndOfList.py
+++ 0019_RemoveNthNodeFromEndOfList.py
@@ -3,9 +3,10 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# 1st solution
+# O(n) time | O(1) space
 class Solution:
-    # 1st solution
-    # O(n) time | O(1) space
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         sentinel = ListNode(0, head)
         p = sentinel.next
