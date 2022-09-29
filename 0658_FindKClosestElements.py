a17b6dcac67dbe1ce0dc660e521ed11a1fc71dbd
yingzhuo <yingzhuo1994@gmail.com>
2022-09-28 20:14:43 -0500

    2nd solution

--- 0658_FindKClosestElements.py
+++ 0658_FindKClosestElements.py
@@ -26,3 +26,16 @@ class Solution:
             return ans + arr[right:right+diff]
         else:
             return arr[left+1-diff:left+1] + ans
+
+# 2nd solution
+# O(log(n - k)) time | O(k) space
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left + k]
