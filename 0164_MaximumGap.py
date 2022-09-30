3e5df834bfc0184b1a3201e96041298bef19481e
yingzhuo <yingzhuo1994@gmail.com>
2022-09-29 21:18:15 -0500

    2nd solution

--- 0164_MaximumGap.py
+++ 0164_MaximumGap.py
@@ -10,3 +10,18 @@ class Solution:
         for i in range(n - 1):
             ans = max(ans, nums[i+1] - nums[i])
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        low, high, n = min(nums), max(nums), len(nums)
+        if n <= 2 or high == low:
+            return high - low
+        B = defaultdict(list)
+        for num in nums:
+            ind = n-2 if num == high else (num - low)*(n-1)//(high-low)
+            B[ind].append(num)
+            
+        cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
+        return max(cands[i+1][0]-cands[i][1] for i in range(len(cands) - 1))
