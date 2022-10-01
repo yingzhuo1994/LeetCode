57a8308dd0850bc97c8fbf867493d3afc85c5027
yingzhuo <yingzhuo1994@gmail.com>
2022-09-30 19:46:01 -0500

    renew

--- 0091_DecodeWays.py
+++ 0091_DecodeWays.py
@@ -1,26 +1,23 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def numDecodings(self, s: str) -> int:    
+        if s[0] == "0":
+            return 0
+        n = len(s)
+        dp = [0] * n + [1]
+        for i in range(n):
+            if s[i] != "0":
+                dp[i] += dp[i-1]
+                if i + 1 < n and int(s[i:i+2]) <= 26:
+                    dp[i+1] += dp[i-1]
+        return dp[-2]
+
+
+# 2nd solution
+# O(n) time | O(1) space
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # 1st solution
-        # O(n) time | O(n) space
-        lst = [1 for _ in range(len(s) + 1)]
-        if int(s[0]) > 0:
-            lst[1] = 1
-        else:
-            return 0
-        for i in range(2, len(s) + 1):
-            single, double = 0, 0
-            if int(s[i-1]) > 0:
-                single = 1
-            if 10 <= int(s[i-2:i]) <= 26:
-                double = 1
-            if single or double:
-                lst[i] = lst[i-1] * single + lst[i-2] * double
-            else:
-                return 0
-        return lst[-1] 
-
-        # 2nd solution
-        # O(n) time | O(1) space
         if s[0] == '0':
             return 0
         front, back = 1, 1
