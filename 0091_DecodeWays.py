c42fcaf7cb51541a0e28e27118365c2b113f7827
yingzhuo <yingzhuo1994@gmail.com>
2022-09-30 19:57:13 -0500

    3rd solution

--- 0091_DecodeWays.py
+++ 0091_DecodeWays.py
@@ -4,14 +4,19 @@ class Solution:
     def numDecodings(self, s: str) -> int:    
         if s[0] == "0":
             return 0
-        n = len(s)
-        dp = [0] * n + [1]
-        for i in range(n):
-            if s[i] != "0":
-                dp[i] += dp[i-1]
-                if i + 1 < n and int(s[i:i+2]) <= 26:
-                    dp[i+1] += dp[i-1]
-        return dp[-2]
+        dp = [1 for _ in range(len(s) + 1)]
+        for i in range(2, len(s) + 1):
+            single, double = 0, 0
+            if int(s[i-1]) > 0:
+                single = 1
+            if 10 <= int(s[i-2:i]) <= 26:
+                double = 1
+            if single or double:
+                dp[i] = dp[i-1] * single + dp[i-2] * double
+            else:
+                return 0
+        return dp[-1] 
+
 
 
 # 2nd solution
@@ -33,3 +38,20 @@ class Solution:
             else:
                 return 0
         return back
+    
+# 3rd solution
+# O(n) time | O(n) space
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+        n = len(s)
+        dp = [0] * n + [1]
+        for i in range(n):
+            if s[i] != "0":
+                dp[i] += dp[i-1]
+                if i + 1 < n and int(s[i:i+2]) <= 26:
+                    dp[i+1] += dp[i-1]
+            if dp[i] == 0  and dp[i-1] == 0:
+                return 0
+        return dp[-2]
