e316429fa01bdb12f8668717b573f67bb38b6001
yingzhuo <yingzhuo1994@gmail.com>
2022-10-01 19:29:37 -0500

    2nd solution

--- 1155_NumberOfDiceRollsWithTargetSum.py
+++ 1155_NumberOfDiceRollsWithTargetSum.py
@@ -18,3 +18,20 @@ class Solution:
             ans %= MOD
             return ans
         return helper(n, target)
+
+# 2nd solution, bottom-up
+# O(k^n) time | O(k^n) space
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        MOD = 10**9 + 7
+        level = Counter([i for i in range(1, k + 1)])
+        for _ in range(n-1):
+            newLevel = Counter()
+            for i in range(1, k + 1):
+                for key, value in level.items():
+                    if key + i > target:
+                        continue
+                    newLevel[key + i] += value
+                    newLevel[key + i] %= MOD
+            level = newLevel
+        return level[target]
