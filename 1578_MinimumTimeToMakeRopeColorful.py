994347156999d83c5ef5d0f95fa62894dadc529a
yingzhuo <yingzhuo1994@gmail.com>
2022-10-02 19:52:31 -0500

    2nd solution

--- 1578_MinimumTimeToMakeRopeColorful.py
+++ 1578_MinimumTimeToMakeRopeColorful.py
@@ -14,4 +14,15 @@ class Solution:
             lastColor = color
         ans += curTime
         return sum(neededTime) - ans
-    
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        res = max_cost = 0
+        for i in range(len(colors)):
+            if i > 0 and colors[i] != colors[i - 1]:
+                max_cost = 0
+            res += min(max_cost, neededTime[i])
+            max_cost = max(max_cost, neededTime[i])
+        return res
