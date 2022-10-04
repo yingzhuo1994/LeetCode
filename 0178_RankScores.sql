0aecde2bb61f3e799e7b76be00895873813e7c0c
yingzhuo <yingzhuo1994@gmail.com>
2022-10-03 19:36:36 -0500

    renew

---
+++ 0178_RankScores.sql
@@ -0,0 +1,16 @@
+Create table If Not Exists Scores (id int, score DECIMAL(3,2))
+Truncate table Scores
+insert into Scores (id, score) values ('1', '3.5')
+insert into Scores (id, score) values ('2', '3.65')
+insert into Scores (id, score) values ('3', '4.0')
+insert into Scores (id, score) values ('4', '3.85')
+insert into Scores (id, score) values ('5', '4.0')
+insert into Scores (id, score) values ('6', '3.65')
+# Write your MySQL query statement below
+SELECT S.Score, COUNT(S2.Score) as `Rank`
+FROM Scores S,
+(SELECT DISTINCT Score FROM Scores) S2
+WHERE S.Score<=S2.Score
+GROUP BY S.Id
+ORDER BY S.Score DESC;
+
