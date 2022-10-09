e72d0c90268910aff07241dcf38ba4011ef711ca
yingzhuo <yingzhuo1994@gmail.com>
2022-10-08 20:49:08 -0500

    3rd solution

--- 0653_TwoSum(4)-InputIsABST.py
+++ 0653_TwoSum(4)-InputIsABST.py
@@ -51,3 +51,40 @@ class Solution:
             else:
                 return True
         return False
+
+# 3rd solution
+# O(n) time | O(h) space
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        def pushLeft(st, root):
+            while root:
+                st.append(root)
+                root = root.left
+
+        def pushRight(st, root):
+            while root:
+                st.append(root)
+                root = root.right
+
+        def nextLeft(st):
+            node = st.pop()
+            pushLeft(st, node.right)
+            return node.val
+
+        def nextRight(st):
+            node = st.pop()
+            pushRight(st, node.left)
+            return node.val
+
+        stLeft, stRight = [], []
+        pushLeft(stLeft, root)
+        pushRight(stRight, root)
+
+        left, right = nextLeft(stLeft), nextRight(stRight)
+        while left < right:
+            if left + right == k: return True
+            if left + right < k:
+                left = nextLeft(stLeft)
+            else:
+                right = nextRight(stRight)
+        return False
