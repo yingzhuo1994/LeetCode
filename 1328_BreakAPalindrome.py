43ab810d8c531c4c29a786a4ee332ef22ea872c4
yingzhuo <yingzhuo1994@gmail.com>
2022-10-09 19:21:37 -0500

    renew

--- 1328_BreakAPalindrome.py
+++ 1328_BreakAPalindrome.py
@@ -1,6 +1,6 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
-    # 1st solution
-    # O(n) time | O(n) space
     def breakPalindrome(self, palindrome: str) -> str:
         if len(palindrome) == 1:
             return ''
