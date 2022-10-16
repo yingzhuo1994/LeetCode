49e12b52d439f99597d9c2db7845ae88ecb9e45d
yingzhuo <yingzhuo1994@gmail.com>
2022-10-15 20:52:21 -0500

    2nd solution

--- 1335_MinimumDifficultyOfAJobSchedule.py
+++ 1335_MinimumDifficultyOfAJobSchedule.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(kn) time | O(kn) space
+# O(kn^2) time | O(kn) space
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
         n = len(jobDifficulty)
@@ -23,3 +23,27 @@ class Solution:
         
         ans = dfs(0, d - 1)
         return ans if ans != float("inf") else -1
+
+# 2nd solution
+# O(kn^2) time | O(kn) space
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+        if n < d:
+            return -1
+        
+        dp = [[float("inf") for _ in range(n)] for _ in range(d)]
+        for k in range(d):
+            for i in range(n - k):
+                if k == 0:
+                    dp[k][i] = max(jobDifficulty[i:])
+                    continue
+
+                diff = -1
+                ans = float("inf")
+                for j in range(i, n - k):
+                    diff = max(diff, jobDifficulty[j])
+                    ans = min(ans, dp[k-1][j+1] + diff)
+                dp[k][i] = ans
+
+        return dp[d-1][0]
