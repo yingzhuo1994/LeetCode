dd6295fcd6d462e626fddfec02188adb0c09d92d
yingzhuo <yingzhuo1994@gmail.com>
2022-10-21 19:55:48 -0500

    1st solution

---
+++ 0455_AssignCookies.py
@@ -0,0 +1,16 @@
+# 1st solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i, j = 0, 0
+        ans = 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+                j += 1
+                ans += 1
+            else:
+                j += 1
+        return ans
