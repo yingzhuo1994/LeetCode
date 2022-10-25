2ee49f65c14d51929e60ac9afb238049f069aacb
yingzhuo <yingzhuo1994@gmail.com>
2022-10-24 19:54:30 -0500

    1st solution

---
+++ 1662_CheckIfTwoStringArraysAreEquivalent.py
@@ -0,0 +1,7 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        s1 = "".join(word1)
+        s2 = "".join(word2)
+        return s1 == s2
