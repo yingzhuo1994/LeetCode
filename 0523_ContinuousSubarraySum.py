4619f73343dcc4a1bd4423c4444a230b8c112bdd
yingzhuo <yingzhuo1994@gmail.com>
2022-10-25 20:25:31 -0500

    2nd solution

--- 0523_ContinuousSubarraySum.py
+++ 0523_ContinuousSubarraySum.py
@@ -17,3 +17,20 @@ class Solution:
                 if diff % k == 0:
                     return True
         return False
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hash_map = {0: 0}
+        s = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            # if the remainder s % k occurs for the first time
+            target = s % k
+            if target not in hash_map:
+                hash_map[target] = i + 1
+            # if the subarray size is at least two
+            elif hash_map[target] < i:
+                return True
+        return False
