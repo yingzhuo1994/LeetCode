774bcdccbd5c2bdc90d6f7c5cc96a38449b17186
yingzhuo <yingzhuo1994@gmail.com>
2022-10-29 20:15:35 -0500

    2nd solution

--- 0478_GenerateRandomPointInACircle.py
+++ 0478_GenerateRandomPointInACircle.py
@@ -15,6 +15,24 @@ class Solution:
             if dx**2 + dy**2 <= self.radius**2:
                 return [self.x_center + dx, self.y_center + dy]
 
+# 2nd solution
+# O(1) time | O(1) space
+import random
+import math
+class Solution:
+
+    def __init__(self, radius: float, x_center: float, y_center: float):
+        self.x_center = x_center
+        self.y_center = y_center
+        self.radius = radius
+        
+    def randPoint(self) -> List[float]:
+        theta = random.uniform(0, 2 * math.pi)
+        r = math.sqrt(random.uniform(0, 1)) * self.radius
+        x = self.x_center + r * math.cos(theta)
+        y = self.y_center + r * math.sin(theta)
+        return [x, y]
+
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(radius, x_center, y_center)
