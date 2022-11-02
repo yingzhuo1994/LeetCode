15d1f6d258e658731357eb3efdc09f60a6264bf6
yingzhuo <yingzhuo1994@gmail.com>
2022-11-01 20:06:56 -0500

    2nd solution

--- 0475_Heaters.py
+++ 0475_Heaters.py
@@ -28,3 +28,19 @@ class Solution:
             else:
                 left = mid + 1
         return ans
+
+# 2nd solution
+# O(n * log(n) + m * log(n)) time | O(n) space
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()
+        r = 0
+        for h in houses:
+            ind = bisect.bisect_left(heaters, h)
+            if ind == len(heaters):
+                r = max(r, h - heaters[-1])
+            elif ind == 0:
+                r = max(r, heaters[0] - h)
+            else:
+                r = max(r, min(heaters[ind] - h, h - heaters[ind - 1]))
+        return r
