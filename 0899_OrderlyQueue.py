dc9703a3490745c522b19374c681bd0c7f2ff74c
yingzhuo <yingzhuo1994@gmail.com>
2022-11-05 20:43:16 -0500

    1st solution

---
+++ 0899_OrderlyQueue.py
@@ -0,0 +1,8 @@
+# 1st solution
+# O(n^2) time | O(n) space
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k == 1:
+            return min(s[i:] + s[:i] for i in range(len(s)))
+        else:
+            return ''.join(sorted(s))
