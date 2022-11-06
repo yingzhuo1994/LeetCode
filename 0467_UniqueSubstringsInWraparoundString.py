0f4484e589e982bb94f98988bef8003c5e091df4
yingzhuo <yingzhuo1994@gmail.com>
2022-11-05 21:42:39 -0500

    2nd solution

--- 0467_UniqueSubstringsInWraparoundString.py
+++ 0467_UniqueSubstringsInWraparoundString.py
@@ -31,3 +31,14 @@ class Solution:
                     end = i
             dfs(start, end, memo)
         return len(memo)
+
+# 2nd solution
+# o(n) time | O(n) space
+class Solution:
+    def findSubstringInWraproundString(self, p: str) -> int:
+        res = {i: 1 for i in p}
+        l = 1
+        for i, j in zip(p, p[1:]):
+            l = l + 1 if (ord(j) - ord(i)) % 26 == 1 else 1
+            res[j] = max(res[j], l)
+        return sum(res.values())
