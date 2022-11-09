34f578e4c2bb1cb9d1ad376be0d2f1527c646386
yingzhuo <yingzhuo1994@gmail.com>
2022-11-08 20:13:07 -0600

    3rd solution

--- 0466_CountTheRepetitions.py
+++ 0466_CountTheRepetitions.py
@@ -54,3 +54,34 @@ class Solution:
             else:
                 start[s2_idx] = (s1_round, s2_round)
         return s2_round // n2
+
+# 3rd solution
+# O(n1 * len(s1)) time | O(len(s1) * len(s2)) space
+class Solution:
+    def getMaxRepetitions(self, s1, n1, s2, n2):
+        count_s1 = Counter(s1)
+        count_s2 = Counter(s2)
+
+        for ch in count_s2:
+            if ch not in count_s1:
+                return 0
+
+        l1, l2 = len(s1), len(s2)
+        i1, i2 = 0, 0
+        dic = {}
+        total = l1 * n1
+
+        while i1 < total:
+            if s1[i1 % l1] == s2[i2 % l2]:
+                if (i1 % l1, i2 % l2) in dic:
+                    prev1, prev2 = dic[(i1 % l1, i2 % l2)]
+                    cir1, cir2 = i1 - prev1, i2 - prev2
+                    count_cir1 = (total - i1) // cir1
+                    i1 += count_cir1 * cir1
+                    i2 += count_cir1 * cir2
+                    if i1 >= total: break
+                else:
+                    dic[(i1 % l1, i2 % l2)] = (i1, i2)
+                i2 += 1
+            i1 += 1
+        return i2 // (l2 * n2)
