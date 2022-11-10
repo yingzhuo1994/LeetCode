480daf92b5757cc8a38b932eee7828ee9c2a1562
yingzhuo <yingzhuo1994@gmail.com>
2022-11-09 18:51:21 -0600

    1st solution

---
+++ 1047_RemoveAllAdjacentDuplicatesInString.py
@@ -0,0 +1,11 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = [""]
+        for ch in s:
+            if stack[-1] == ch:
+                stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
