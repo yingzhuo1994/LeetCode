80afa1fbfa31325e9e8d5ca40febb1ed06209e69
yingzhuo <yingzhuo1994@gmail.com>
2022-11-11 20:14:35 -0600

    2nd solution

--- 0457_CircularArrayLoop.py
+++ 0457_CircularArrayLoop.py
@@ -34,3 +34,39 @@ class Solution:
                     return True
         return False
 
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        def move(i):
+            step = nums[i]
+            return (i + step) % n
+        
+        for i in range(n):
+            if nums[i] > 0:
+                nums[i] %= n
+            else:
+                nums[i] = -(abs(nums[i]) % n)
+
+        for i, num in enumerate(nums):
+            if abs(num) >= n:
+                continue
+            linkLength = 0
+            j = i
+            direction = nums[j]
+            while True:
+                if direction * nums[j] < 0:
+                    break
+                if nums[j] > 0:
+                    nums[j] += n
+                else:
+                    nums[j] -= n
+                nextj = move(j)
+                if nextj == j:
+                    break
+                j = nextj
+                linkLength += 1
+                if linkLength > n:
+                    return True
+        return False
