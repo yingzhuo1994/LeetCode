eb46a2a97b601b0c0d1656c427b1ef6bba8dbf38
yingzhuo <yingzhuo1994@gmail.com>
2022-11-12 19:29:39 -0600

    3rd solution

--- 0151_ReverseWordsInAString.py
+++ 0151_ReverseWordsInAString.py
@@ -23,3 +23,28 @@ class Solution:
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(s.split()[::-1])
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        chars = [ch for ch in s]
+        n = len(chars)
+        slow = 0
+        for i in range(n):
+            if chars[i] != " " or (i > 0 and chars[i] == " " and chars[i-1] != " "):
+                chars[slow] = chars[i]
+                slow += 1
+        if slow == 0:
+            return ""
+        
+        chars = chars[:slow-1] if chars[-1] == " " else chars[:slow]
+        chars.reverse()
+
+        slow, m = 0, len(chars)
+        for i in range(m + 1):
+            if i == m or chars[i] == " ":
+                chars[slow:i] = chars[slow:i][::-1]
+                slow = i + 1
+        
+        return "".join(chars)
