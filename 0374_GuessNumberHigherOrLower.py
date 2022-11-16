976caa323f4afa6341d830726538c5ada849a1e9
yingzhuo <yingzhuo1994@gmail.com>
2022-11-15 19:50:27 -0600

    renew

--- 0374_GuessNumberHigherOrLower.py
+++ 0374_GuessNumberHigherOrLower.py
@@ -3,17 +3,18 @@
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 # def guess(num: int) -> int:
 
+# 1st solution
+# O(n) time | O(1) space
 class Solution:
-    # 1st solution
-    # O(n) time | O(1) space
     def guessNumber(self, n: int) -> int:
         low, high = 1, n
-        while low < high:
+        while low <= high:
             mid = low + (high - low) // 2
-            if guess(mid) > 0:
+            res = guess(mid)
+            if res > 0:
                 low = mid + 1
-            elif guess(mid) < 0:
+            elif res < 0:
                 high = mid - 1
             else:
                 return mid
-        return low
+        return -1
