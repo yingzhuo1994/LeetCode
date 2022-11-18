209e04b55b01ce0ad13b9d0b7dea3febc9d2e548
yingzhuo <yingzhuo1994@gmail.com>
2022-11-17 19:28:10 -0600

    2nd solution

--- 0263_UglyNumber.py
+++ 0263_UglyNumber.py
@@ -13,3 +13,21 @@ class Solution:
         if n % 5 == 0:
             return self.isUgly(n // 5)
         return False
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while True:
+            if n in {1, 2, 3, 5}:
+                return True
+            if n % 2 == 0:
+                n //= 2
+            elif n % 3 == 0:
+                n //= 3
+            elif n % 5 == 0:
+                n //= 5
+            else:
+                return False
