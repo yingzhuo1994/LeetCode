dc9b8f86ddbe1e5e71613ef8ac2f24a364be50ef
yingzhuo <yingzhuo1994@gmail.com>
2022-11-22 20:31:25 -0600

    renew

--- 0036_ValidSudoku.py
+++ 0036_ValidSudoku.py
@@ -1,37 +1,12 @@
+# O(1) time | O(1) space
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # O(1) time | O(1) space
-        rowDic = [{} for _ in range(9)]
-        columnDic = [{} for _ in range(9)]
-        boxDic = [[{}, {}, {}] for _ in range(3)]
-        # for i in range(9):
-        #     for j in range(9):
-        #         elem = board[i][j]
-        #         if board[i][j] == '.':
-        #             continue
-        #         # check row
-        #         if elem in rowDic[i]:
-        #             return False
-        #         else:
-        #             rowDic[i][elem] = 1
-        #
-        #         # check column
-        #         if elem in columnDic[j]:
-        #             return False
-        #         else:
-        #             columnDic[j][elem] = 1
-        #
-        #         # check sub-box
-        #         x, y = i // 3, j // 3
-        #         if elem in boxDic[x][y]:
-        #             return False
-        #         else:
-        #             boxDic[x][y][elem] = 1
-        # return True
-
         # Each iteration does one thing
         # There is no difference in time complexity, and space complexity.
         # As a software engineer, you should write clean code which is easy to understand.
+        rowDic = [set() for _ in range(9)]
+        columnDic = [set() for _ in range(9)]
+        boxDic = [set() for _ in range(9)]
         for i in range(9):
             for j in range(9):
                 elem = board[i][j]
@@ -41,7 +16,7 @@ class Solution:
                 if elem in rowDic[i]:
                     return False
                 else:
-                    rowDic[i][elem] = 1
+                    rowDic[i].add(elem)
 
         for i in range(9):
             for j in range(9):
@@ -53,7 +28,7 @@ class Solution:
                 if elem in columnDic[j]:
                     return False
                 else:
-                    columnDic[j][elem] = 1
+                    columnDic[j].add(elem)
 
         for i in range(9):
             for j in range(9):
@@ -63,8 +38,8 @@ class Solution:
 
                 # check sub-box
                 x, y = i // 3, j // 3
-                if elem in boxDic[x][y]:
+                if elem in boxDic[3*x+y]:
                     return False
                 else:
-                    boxDic[x][y][elem] = 1
+                    boxDic[3*x+y].add(elem)
         return True
