35f1a5bfb3f79bee9cde022f3d512e88a1b3f970
yingzhuo <yingzhuo1994@gmail.com>
2022-11-23 19:37:21 -0600

    3rd solution

--- 0079_WordSearch.py
+++ 0079_WordSearch.py
@@ -1,28 +1,4 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        def lookNeighbor(board, word, i, j, lst = []):
-            if not word:
-                return True
-            result = False
-            if 0 <= i - 1 < len(board) and (i - 1, j) not in lst and board[i - 1][j] == word[0]:
-                result = lookNeighbor(board, word[1:], i - 1, j, lst + [(i - 1, j)])
-            if not result and 0 <= i + 1 < len(board) and (i + 1, j) not in lst and board[i + 1][j] == word[0]:
-                result = lookNeighbor(board, word[1:], i + 1, j, lst + [(i + 1, j)])
-            if not result and 0 <= j - 1 < len(board[0]) and (i, j - 1) not in lst and board[i][j - 1] == word[0]:
-                result = lookNeighbor(board, word[1:], i, j - 1, lst + [(i, j - 1)])
-            if not result and 0 <= j + 1 < len(board[0]) and (i, j + 1) not in lst and board[i][j + 1] == word[0]:
-                result = lookNeighbor(board, word[1:], i, j + 1, lst + [(i, j + 1)])
-            return result
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    isContained = lookNeighbor(board, word[1:], i, j, [(i, j)])
-                    if isContained:
-                        return True
-        return False
-
-# 2nd simplified solution
+# 1st simplified solution
 # O(m*n*4^l) time | O(m * n + l) space
 # where m and n are the row number and column number of board, and l is the length of word
 class Solution:
@@ -45,7 +21,7 @@ class Solution:
                     return True
         return False
     
-# 3rd solution
+# 2nd solution
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
@@ -69,3 +45,44 @@ class Solution:
         board[i][j] = temp
         return False
 
+# 3rd solution
+# O(m*n*3^k) time | O(k) space
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+
+        if len(word) > m * n:
+            return False
+        
+        count = Counter(sum(board, []))
+        
+        # count of a LETTER in word is Greater than count of it being in board
+        for c, countWord in Counter(word).items():
+            if count[c] < countWord:
+                return False
+            
+        # if count of 1st letter of Word(A) is Greater than that of Last One in Board(B). 
+        # Reverse Search the word then search as less case will be searched.
+        if count[word[0]] > count[word[-1]]:
+             word = word[::-1]
+
+        for i in range(m):
+            for j in range(n):
+                if self.dfs(board, i, j, word, 0):
+                    return True
+        return False
+    
+    def dfs(self, board, i, j, word, k):
+        if board[i][j] == word[k] and k == len(word) - 1:
+            return True
+        if board[i][j] != word[k]:
+            return False
+        temp = board[i][j]
+        board[i][j] = "#"
+        for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            row, col = i + x, j + y
+            if 0 <= row < len(board) and 0 <= col < len(board[0]):
+                if self.dfs(board, row, col, word, k + 1):
+                    return True
+        board[i][j] = temp
+        return False
