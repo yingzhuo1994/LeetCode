97a2a9b6c08d331f9963ca92f2f560255bd324d7
yingzhuo <yingzhuo1994@gmail.com>
2022-11-24 20:18:27 -0600

    simplify

--- 0907_SumOfSubarrayMinimums.py
+++ 0907_SumOfSubarrayMinimums.py
@@ -21,24 +21,14 @@ class Solution:
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        stack = []
+        stack = [-1]
+        arr.append(-1)
         sum_of_minimums = 0;
 
-        for i in range(len(arr) + 1):
-            
-            # when i reaches the array length, it is an indication that
-            # all the elements have been processed, and the remaining
-            # elements in the stack should now be popped out.
-
-            while stack and (i == len(arr) or arr[stack[-1]] >= arr[i]):
-
-                # Notice the sign ">=", This ensures that no contribution
-                # is counted twice. right_boundary takes equal or smaller 
-                # elements into account while left_boundary takes only the
-                # strictly smaller elements into account
-
+        for i in range(len(arr) ):
+            while arr[i] < arr[stack[-1]]:
                 mid = stack.pop()
-                left_boundary = -1 if not stack else stack[-1]
+                left_boundary = stack[-1]
                 right_boundary = i
 
                 # count of subarrays where mid is the minimum element
