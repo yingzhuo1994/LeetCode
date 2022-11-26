99f3270ecb481547eae70aca357dd2759675da28
yingzhuo <yingzhuo1994@gmail.com>
2022-11-25 21:26:46 -0600

    3rd solution

--- 0424_LongestRepeatingCharacterReplacement.py
+++ 0424_LongestRepeatingCharacterReplacement.py
@@ -81,3 +81,31 @@ class Solution:
 
     def __is_window_valid(self, start: int, end: int, count: int, k: int):
         return end + 1 - start - count <= k
+
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start = 0
+        frequency_map = {}
+        max_frequency = 0
+        longest_substring_length = 0
+        for end in range(len(s)):
+            frequency_map[s[end]] = frequency_map.get(s[end], 0) + 1
+
+            # the maximum frequency we have seen in any window yet
+            max_frequency = max(max_frequency, frequency_map[s[end]])
+
+            # move the start pointer towards right if the current
+            # window is invalid
+            is_valid = (end + 1 - start - max_frequency <= k)
+            if not is_valid:
+                frequency_map[s[start]] -= 1
+                start += 1
+
+            # the window is valid at this point, store length
+            # size of the window never decreases
+            longest_substring_length = end + 1 - start
+
+        return longest_substring_length
