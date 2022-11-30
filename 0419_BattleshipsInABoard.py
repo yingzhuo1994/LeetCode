c6ea2852531358b5a9a5d6045f1529c998d74c38
yingzhuo <yingzhuo1994@gmail.com>
2022-11-29 20:09:31 -0600

    1st solution

---
+++ 0419_BattleshipsInABoard.py
@@ -0,0 +1,22 @@
+# 1st solution
+# O(mn) time | O(k) space
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        def isBattleship(i, j):
+            if board[i][j] != "X":
+                return False
+            board[i][j] = "."
+            for dx, dy in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                x = i + dx
+                y = j + dy
+                if 0 <= x < m and 0 <= y < n:
+                    isBattleship(x, y)
+            return True
+        
+        m, n = len(board), len(board[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if isBattleship(i, j):
+                    count += 1
+        return count
