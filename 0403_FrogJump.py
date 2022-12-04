0edf8ad73c057d6c9339eb07a5752cd309f6c473
yingzhuo <yingzhuo1994@gmail.com>
2022-12-03 20:33:48 -0600

    3rd solution

--- 0403_FrogJump.py
+++ 0403_FrogJump.py
@@ -49,3 +49,31 @@ class Solution:
                     return True
 
         return False
+
+# 3rd solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1] - stones[0] != 1:
+            return False
+        n = len(stones)
+        if n == 2:
+            return True
+        dp = [set() for _ in range(n)]
+        dp[0].add(1)
+        
+        for i in range(1, n):
+            # check all the previous stones from where the jumps are possible
+            for j in range(i):
+                # store the move value required from jth to ith stone
+                diff = stones[i] - stones[j]
+                # check if that move value is present in jth stone
+                if diff in dp[j]:
+                    # If possible, then add possibilities of move values in the ith stone set
+                    dp[i].add(diff - 1)
+                    dp[i].add(diff)
+                    dp[i].add(diff + 1)
+                    if i == n - 1:
+                        return True
+
+        return False
