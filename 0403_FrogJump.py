7c7b4b036efaf48e140a0f561bc7accdb6349494
yingzhuo <yingzhuo1994@gmail.com>
2022-12-03 20:17:07 -0600

    2nd solution

---
+++ 0403_FrogJump.py
@@ -0,0 +1,51 @@
+# 1st solution
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1] - stones[0] != 1:
+            return False
+        n = len(stones)
+        if n == 2:
+            return True
+        dic = {num: i for i, num in enumerate(stones)}
+        level = deque([[1, 1]])
+        visited = set([(1, 1)])
+        while level:
+            idx, k = level.popleft()
+            for step in [k - 1, k, k + 1]:
+                if step == 0:
+                    continue
+                place = idx + step
+                if place == stones[-1]:
+                    return True
+                if place not in dic or (place, step) in visited:
+                    continue
+                visited.add((place, step))
+                level.append([place, step])
+        return False
+
+# 2nd solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1] - stones[0] != 1:
+            return False
+        n = len(stones)
+        if n == 2:
+            return True
+        dp = [[False for _ in range(n + 1)] for _ in range(n)]
+        dp[0][1] = True
+        
+        for i in range(n):
+            for j in range(i):
+                diff = stones[i] - stones[j]
+                if diff < 0 or diff > i or not dp[j][diff]:
+                    continue
+                dp[i][diff] = True
+                if diff - 1 >= 0:
+                    dp[i][diff - 1] = True
+                if diff + 1 <= n:
+                    dp[i][diff + 1] = True
+                if i == n - 1:
+                    return True
+
+        return False
