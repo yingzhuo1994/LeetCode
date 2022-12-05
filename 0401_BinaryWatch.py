175eb6e3cdb5c17901440b204e7a43af19a483b5
yingzhuo <yingzhuo1994@gmail.com>
2022-12-04 20:22:36 -0600

    2nd solution

--- 0401_BinaryWatch.py
+++ 0401_BinaryWatch.py
@@ -67,4 +67,13 @@ class Solution:
                 for b in minutes:
                     ans.append(a + ":" + b)
         return ans
-            
+
+# 2nd solution
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        times = []
+        for h in range(12):
+            for m in range(60):
+                if (bin(h) + bin(m)).count('1') == turnedOn:
+                    times.append(f'{h}:{m:02d}')
+        return times  
