122fe4acddca85e34cdbb0b774e8caa67197600a
yingzhuo <yingzhuo1994@gmail.com>
2022-12-07 18:40:57 -0600

    3rd solution

--- 0872_Leaf-SimilarTrees.py
+++ 0872_Leaf-SimilarTrees.py
@@ -47,3 +47,14 @@ class Solution:
             if v1 != v2:
                 return False
         return not stack1 and not stack2
+
+# 3rd solution
+# O(n) time | O(log(n)) space
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node: return
+            if not node.left and not node.right: yield node.val
+            for i in dfs(node.left): yield i
+            for i in dfs(node.right): yield i
+        return all(a == b for a, b in itertools.zip_longest(dfs(root1), dfs(root2)))
