a940a8f467c14d5cb56888d4419f65d6bdb6cce7
yingzhuo <yingzhuo1994@gmail.com>
2022-12-10 21:15:50 -0600

    simplify

--- 0388_LongestAbsoluteFilePath.py
+++ 0388_LongestAbsoluteFilePath.py
@@ -1,16 +1,16 @@
+# 1st solution
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
         dict = {}
         longest = 0
         fileList = input.split("\n")
         for i in fileList:
+            key = i.count("\t") #是几级文件夹
             if "." not in i:  #是文件夹
-                key = i.count("\t") #是几级文件夹
-                value = len(i.replace("\t","")) #除去\t后的长度，是实际长度
+                value = len(i) - key
                 dict[key] = value
             else: #是文件。
-                key = i.count("\t")
                 #　文件的长度：所有目录的长度＋文件的长度＋“\”的数量
-                length = sum([dict[j] for j in dict.keys() if j < key]) + len(i.replace("\t","")) + key
-                longest = max(longest,length)
+                length = sum([dict[j] for j in dict.keys() if j < key]) + len(i)
+                longest = max(longest, length)
         return longest
