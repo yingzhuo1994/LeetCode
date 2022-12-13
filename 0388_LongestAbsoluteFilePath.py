7ab6e7a45d9cee113566ff1c9ed7804ef5d30a31
yingzhuo <yingzhuo1994@gmail.com>
2022-12-12 21:25:15 -0600

    3rd solution

--- 0388_LongestAbsoluteFilePath.py
+++ 0388_LongestAbsoluteFilePath.py
@@ -34,3 +34,42 @@ class Solution:
                 length = dic[key-1] + len(i)
                 longest = max(longest, length)
         return longest
+
+# 3rd solution
+# O(nd) time | O(n) space
+# where n is string length, and d is the largerst file level.  
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        longest = 0
+        fileList = input.split("\n")
+        node = Node()
+        root = node
+        for line in fileList:
+            key = line.count("\t")
+            name = line.lstrip('\t')
+            while node.level >= key:
+                node = node.parent
+            newNode = Node(name, key, node)
+            node.children.append(newNode)
+            if "." in line:             
+                longest = max(longest, newNode.length)
+            else:
+                node = newNode
+
+        def dfs(node):
+            if not node:
+                return None
+            print(node.name, node.level, node.length)
+            for child in node.children:
+                dfs(child)
+            return 
+        # dfs(root)
+        return longest
+
+class Node:
+    def __init__(self, name="", level=-1, parent=None):
+        self.name = name
+        self.level = level
+        self.length = len(name) + (parent.length + 1 if parent and parent.length > 0 else 0)
+        self.children = []
+        self.parent = parent
