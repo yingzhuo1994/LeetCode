6baf1cfc7f5329888e1c7bfaef7531a8c9f32aae
yingzhuo <yingzhuo1994@gmail.com>
2022-12-13 19:31:42 -0600

    simplify

--- 0198_HouseRobber.py
+++ 0198_HouseRobber.py
@@ -1,6 +1,6 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
-    # 1st solution
-    # O(n) time | O(n) space
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
@@ -11,13 +11,11 @@ class Solution:
             lst[i] = max(lst[i-1], nums[i] + lst[i-2])
         return lst[-1]
 
-    # 2nd solution
-    # O(n) time | O(1) sapce
+# 2nd solution
+# O(n) time | O(1) sapce
+class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        front = nums[0]
-        back = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            front, back = back, max(back, nums[i] + front)
+        front, back = 0, 0
+        for num in nums:
+            front, back = back, max(back, num + front)
         return back
