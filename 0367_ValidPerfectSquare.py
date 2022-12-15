b3faa69a3c338ff2aabe8b056ab14f77e286f3af
yingzhuo <yingzhuo1994@gmail.com>
2022-12-14 21:01:04 -0600

    3rd solution

--- 0367_ValidPerfectSquare.py
+++ 0367_ValidPerfectSquare.py
@@ -20,3 +20,19 @@ class Solution:
         while r * r > num:
             r = (r + num // r) // 2
         return r * r == num
+
+# 3rd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 1, num
+        while left < right:
+            mid = left + (right - left) // 2
+            sq = mid * mid
+            if sq < num:
+                left = mid + 1
+            elif sq > num:
+                right = mid
+            else:
+                return True
+        return left * left == num
