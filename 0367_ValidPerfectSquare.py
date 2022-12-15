29fbefaca6a9b09abc1ebcfc024d4e516b2de6af
yingzhuo <yingzhuo1994@gmail.com>
2022-12-14 20:57:33 -0600

    2nd solution

--- 0367_ValidPerfectSquare.py
+++ 0367_ValidPerfectSquare.py
@@ -11,3 +11,12 @@ class Solution:
                 k = 1
             k += 1
         return num == 1
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        r = num
+        while r * r > num:
+            r = (r + num // r) // 2
+        return r * r == num
