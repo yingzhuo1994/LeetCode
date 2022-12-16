3a5f5e5ded2050b85915ec92766f4d48386bbc7f
yingzhuo <yingzhuo1994@gmail.com>
2022-12-15 20:08:35 -0600

    renew

--- 0365_WaterAndJugProblem.py
+++ 0365_WaterAndJugProblem.py
@@ -17,34 +17,23 @@ class Solution:
             x, y = stack.popleft()
             if x + y == targetCapacity:
                 return True
-            if x > 0 and (0, y) not in visited:
-                stack.append([0, y])
-                visited.add((0, y))
-            if y > 0 and (x, 0) not in visited:
-                stack.append([x, 0])
-                visited.add((x, 0))
-            if (jug1Capacity, y) not in visited:
-                stack.append([jug1Capacity, y])
-                visited.add((jug1Capacity, y))
-            if (x, jug2Capacity) not in visited:
-                stack.append([x, jug2Capacity])
-                visited.add((x, jug2Capacity))
+            states = []
+            states.append((0, y))
+            states.append((x, 0))
+            states.append((jug1Capacity, y))
+            states.append((x, jug2Capacity))
             if x + y > jug2Capacity:
-                pair = (x + y - jug2Capacity, jug2Capacity)
+                states.append((x + y - jug2Capacity, jug2Capacity))
             else:
-                pair = (0, x + y)
-            if pair not in visited:
-                stack.append(pair)
-                visited.add(pair)
-
+                states.append((0, x + y))
             if x + y > jug1Capacity:
-                pair = (jug1Capacity, x + y - jug1Capacity)
+                states.append((jug1Capacity, x + y - jug1Capacity))
             else:
-                pair = (x + y, 0)
-            if pair not in visited:
-                stack.append(pair)
-                visited.add(pair)
-
+                states.append((x + y, 0))
+            for state in states:
+                if state not in visited:
+                    stack.append(state)
+                    visited.add(state)
         return False
 
 # 2nd solution
