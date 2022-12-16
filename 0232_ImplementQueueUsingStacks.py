293a30c64ad325562103e1887a758ebedac1a95f
yingzhuo <yingzhuo1994@gmail.com>
2022-12-15 19:21:48 -0600

    3rd solution

--- 0232_ImplementQueueUsingStacks.py
+++ 0232_ImplementQueueUsingStacks.py
@@ -40,7 +40,32 @@ class MyQueue:
         
     def empty(self) -> bool:
         return len(self.stack) == self.start
+
+# 3rd solution
+class MyQueue:
+    def __init__(self):
+        self.stackPush = []
+        self.stackPop = []
+        self.size = 0
         
+    def push(self, x: int) -> None:
+        self.stackPush.append(x)
+        self.size += 1
+        
+    def pop(self) -> int:
+        self.peek()
+        self.size -= 1
+        return self.stackPop.pop()
+        
+    def peek(self) -> int:
+        if len(self.stackPop) == 0:
+            while self.stackPush:
+                self.stackPop.append(self.stackPush.pop())
+        return self.stackPop[-1]
+        
+    def empty(self) -> bool:
+        return self.size == 0
+
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
