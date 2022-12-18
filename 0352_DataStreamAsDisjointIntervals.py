3ca9e7c491a5c09c4ad804b2ecb5df634b62d405
yingzhuo <yingzhuo1994@gmail.com>
2022-12-17 20:40:55 -0600

    3rd solution

--- 0352_DataStreamAsDisjointIntervals.py
+++ 0352_DataStreamAsDisjointIntervals.py
@@ -112,6 +112,56 @@ class SummaryRanges:
         self.intervals = stack
         return list(map(lambda x: x[1], stack))
 
+# 3rs solution, Disjoint Set
+class DSU:
+    def __init__(self):
+        self.parents = {}
+        self.intervals = {}
+
+    def exists(self, x): 
+        return x in self.parents
+
+    def make_set(self, x):
+        self.parents[x] = x
+        self.intervals[x] = [x, x]
+        
+    def getParent(self, x):
+        if not self.exists(x): return None
+        
+        if self.parents[x] != x:
+            self.parents[x] = self.getParent(self.parents[x])
+
+        return self.parents[x]
+
+    def union(self, x, y):
+        xr = self.getParent(x)
+        yr = self.getParent(y)
+        
+        if xr is None or yr is None: return
+        
+        self.parents[xr] = yr
+        
+        ## interval adjusting logic
+        x_interval = self.intervals[xr]
+        del self.intervals[xr]
+        
+        self.intervals[yr] = [min(self.intervals[yr][0], x_interval[0]), max(self.intervals[yr][1], x_interval[1])]
+        
+class SummaryRanges:    
+    def __init__(self):
+        self.dsu = DSU()
+
+    def addNum(self, val: int) -> None:
+        if self.dsu.exists(val): return
+            
+        self.dsu.make_set(val)
+        
+        self.dsu.union(val, val-1)
+        self.dsu.union(val, val+1)
+
+    def getIntervals(self) -> List[List[int]]:
+        return sorted(self.dsu.intervals.values())
+
 # Your SummaryRanges object will be instantiated and called as such:
 # obj = SummaryRanges()
 # obj.addNum(value)
