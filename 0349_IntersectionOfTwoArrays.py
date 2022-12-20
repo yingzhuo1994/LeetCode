4b73c546607154844e0b725fd33d5b56228d4037
yingzhuo <yingzhuo1994@gmail.com>
2022-12-19 19:59:41 -0600

    1st solution

---
+++ 0349_IntersectionOfTwoArrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        if len(set1) > len(set2):
+            set1, set2 = set2, set1
+        ans = []
+        for num in set1:
+            if num in set2:
+                ans.append(num)
+        return ans
