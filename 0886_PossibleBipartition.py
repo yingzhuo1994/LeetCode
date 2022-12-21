92eab6866f6857acc7d961701bf27c734f3554a4
yingzhuo <yingzhuo1994@gmail.com>
2022-12-20 20:11:22 -0600

    1st solution

---
+++ 0886_PossibleBipartition.py
@@ -0,0 +1,26 @@
+# 1st solution
+# O(E) time | O(E) space
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        if len(dislikes) == 0:
+            return True
+        graph = [[] for _ in range(n + 1)]
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        def dfs(i, last=0):
+            if i in labels:
+                return labels[i] * last <= 0
+            labels[i] = 1 if last == 0 else -last
+            for j in graph[i]:
+                if not dfs(j, labels[i]):
+                    return False
+            return True
+    
+        labels = {}
+        for i in range(1, n + 1):
+            if i not in labels:
+                if not dfs(i):
+                    return False
+        return True
