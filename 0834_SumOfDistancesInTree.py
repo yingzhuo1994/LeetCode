e26be3c49573c921ed604d91a482c29e34db0e76
yingzhuo <yingzhuo1994@gmail.com>
2022-12-21 22:25:58 -0600

    2nd solution

--- 0834_SumOfDistancesInTree.py
+++ 0834_SumOfDistancesInTree.py
@@ -1,4 +1,5 @@
 # 1st solution, TLE
+# O(n^2) time | O(n^2) space
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         dp = [[-1 for _ in range(n)] for _ in range(n)]
@@ -23,3 +24,33 @@ class Solution:
             dfs(i, i)
         
         return [sum(row) for row in dp]
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(set)
+        for i, j in edges:
+            graph[i].add(j)
+            graph[j].add(i)
+        
+        def dfs(node, parent, depth):
+            ans = 1
+            for neib in graph[node]:
+                if neib != parent:
+                    ans += dfs(neib, node, depth + 1)
+            weights[node] = ans
+            depths[node] = depth
+            return ans
+        
+        def dfs2(node, parent, w):
+            ans[node] = w
+            for neib in graph[node]:
+                if neib != parent:
+                    dfs2(neib, node, w + n - 2*weights[neib])
+        
+        weights, depths, ans = [0]*n, [0]*n, [0]*n
+        dfs(0, -1, 0)
+        dfs2(0, -1, sum(depths))
+        
+        return ans
