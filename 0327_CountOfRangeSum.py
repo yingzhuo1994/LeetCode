b69f11b85e45b6105c900b483262355e8ba94ab3
yingzhuo <yingzhuo1994@gmail.com>
2022-12-25 15:49:45 -0600

    2nd solution

--- 0327_CountOfRangeSum.py
+++ 0327_CountOfRangeSum.py
@@ -14,3 +14,24 @@ class Solution:
                 if lower <= curSum <= upper:
                     ans += 1
         return ans
+
+# 2nd solution, mergesort
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        array = [0]
+        for num in nums:
+            array.append(array[-1] + num)
+        def mergesort(lo, hi):
+            mid = (lo + hi) // 2
+            if mid == lo:
+                return 0
+            count = mergesort(lo, mid) + mergesort(mid, hi)
+            i = j = mid
+            for left in array[lo:mid]:
+                while i < hi and array[i] - left <  lower: i += 1
+                while j < hi and array[j] - left <= upper: j += 1
+                count += j - i
+            array[lo:hi] = sorted(array[lo:hi])
+            return count
+        return mergesort(0, len(array))
