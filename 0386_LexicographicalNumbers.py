dfa2d7bf54ec351b6b311c5602280588f2d73879
yingzhuo <yingzhuo1994@gmail.com>
2022-12-25 16:43:51 -0600

    renew

--- 0386_LexicographicalNumbers.py
+++ 0386_LexicographicalNumbers.py
@@ -2,23 +2,20 @@
 # O(n) time | O(n) space
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        num = 1
-        stack = []
+        stack = [1]
         while len(stack) < n:
-            stack.append(num)
+            num = stack[-1]
             if num * 10 <= n:
                 num *= 10
-            elif num + 1 <= n:
-                num += 1
+            else:
+                if num + 1 <= n:
+                    num += 1
+                else:
+                    num //= 10
+                    num += 1
                 while num % 10 == 0:
                     num //= 10
-            else:
-                num //= 10
-                if num == 0 or num == 9:
-                    break
-                num += 1
-                while num % 10 == 0:
-                    num //= 10                
+            stack.append(num)         
         return stack
 
 # 2nd solution
