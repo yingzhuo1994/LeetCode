c8d8e09dd307660533da9f0532fbdaa6827ede96
yingzhuo <yingzhuo1994@gmail.com>
2022-12-27 17:30:00 -0600

    1st solution

---
+++ 0319_BulbSwitcher.py
@@ -0,0 +1,14 @@
+# 1st solution, TLE
+# # O(n^2) time | O(n) space
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        if n == 0:
+            return 0
+        bulbs = [True for _ in range(n)]
+        for start in range(2, n):
+            for i in range(start - 1, n, start):
+                bulbs[i] = not bulbs[i]
+            # print(start, bulbs)
+        if n > 1:
+            bulbs[-1] = not bulbs[-1]
+        return sum(bulbs)
