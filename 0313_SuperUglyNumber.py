b36107d7032aee0fb38151c49331f4e59420d952
yingzhuo <yingzhuo1994@gmail.com>
2022-12-29 19:54:23 -0600

    2nd solution

--- 0313_SuperUglyNumber.py
+++ 0313_SuperUglyNumber.py
@@ -14,3 +14,21 @@ class Solution:
             count += 1
         
         return heappop(stack)
+
+# 2nd solution
+# O(kn) time | O(k + n) space
+# where k = len(primes)
+class Solution(object):
+    def nthSuperUglyNumber(self, n, primes):
+        size = len(primes)
+        ugly, dp, index, ugly_nums = 1, [1], [0] * size, [1] * size
+        for i in range(1, n):
+            # compute possibly ugly numbers and update index
+            for j in range(0, size):
+                if ugly_nums[j] == ugly:
+                    ugly_nums[j] = dp[index[j]] * primes[j]
+                    index[j] += 1
+            # get the minimum
+            ugly = min(ugly_nums)
+            dp.append(ugly)
+        return dp[-1]
