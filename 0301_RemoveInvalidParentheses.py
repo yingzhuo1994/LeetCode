68b8ee1e453dc5d1a4b0e4383bbf941b66362107
yingzhuo <yingzhuo1994@gmail.com>
2023-01-02 01:30:59 -0600

    2nd solution

--- 0301_RemoveInvalidParentheses.py
+++ 0301_RemoveInvalidParentheses.py
@@ -62,3 +62,26 @@ class Solution:
         results = ["".join(lst) for lst in results]
         results = list(set(results))
         return results
+
+# 2nd solution
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        result = []
+        self.remove(s, result, 0, 0, ('(', ')'))
+        return result
+
+    def remove(self, s, result, last_i, last_j, par):
+        count = 0
+        for i in range(last_i, len(s)):
+            count += (s[i] == par[0]) - (s[i] == par[1])
+            if count >= 0:
+                continue
+            for j in range(last_j, i + 1):
+                if s[j] == par[1] and (j == last_j or s[j - 1] != par[1]):
+                    self.remove(s[:j] + s[j + 1:], result, i, j, par)
+            return
+        reversed_s = s[::-1]
+        if par[0] == '(':
+            self.remove(reversed_s, result, 0, 0, (')', '('))
+        else:
+            result.append(reversed_s)
