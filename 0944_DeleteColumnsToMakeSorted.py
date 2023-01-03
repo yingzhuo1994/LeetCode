6e3854666a44951186801773ee6b1e734eab3f6f
yingzhuo <yingzhuo1994@gmail.com>
2023-01-02 19:37:59 -0600

    2nd solution

--- 0944_DeleteColumnsToMakeSorted.py
+++ 0944_DeleteColumnsToMakeSorted.py
@@ -23,3 +23,19 @@ class Solution:
         #     return "".join(stack)
         
         return len(deletedCols)
+
+# 2nd solution
+# O(mn) time | O(1) space
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        m, n = len(strs), len(strs[0])
+        ans = 0
+        for j in range(n):
+            last = "a"
+            for i in range(m):
+                if strs[i][j] < last:
+                    ans += 1
+                    break
+                last = strs[i][j]
+                
+        return ans
