bd0b9ad1d4b0d358ec4206bf3c24f33614526349
yingzhuo <yingzhuo1994@gmail.com>
2023-01-04 21:10:17 -0600

    2nd solution

--- 1834_Single-ThreadedCPU.py
+++ 1834_Single-ThreadedCPU.py
@@ -18,3 +18,29 @@ class Solution:
             elif i < len(tasks):
                 lastTime = tasks[i][0]
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        arrays = [[start, cost, idx] for idx, (start, cost) in enumerate(tasks)]
+        arrays.sort()
+        lastTime = 0
+        stack = []
+        ans = []
+
+        for start, cost, i in arrays:
+            while stack and lastTime < start:
+                c, idx = heappop(stack)
+                lastTime += c
+                ans.append(idx)
+            if not stack and start >= lastTime:
+                ans.append(i)
+                lastTime = start + cost
+            else:
+                heappush(stack, [cost, i])
+
+        while stack:
+            cost, idx = heappop(stack)
+            ans.append(idx)
+        return ans
