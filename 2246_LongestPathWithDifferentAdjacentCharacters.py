e4c4213686d9be578bd9c8a7a58aa43807dc1598
yingzhuo <yingzhuo1994@gmail.com>
2023-01-12 19:09:18 -0600

    2nd solution

--- 2246_LongestPathWithDifferentAdjacentCharacters.py
+++ 2246_LongestPathWithDifferentAdjacentCharacters.py
@@ -29,3 +29,27 @@ class Solution:
         
         _, ans = dfs(0, -1)
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        children = [[] for i in range(len(s))]
+        for i,j in enumerate(parent):
+            if j >= 0:
+                children[j].append(i)
+        
+        res = [0]
+        def dfs(i):
+            candi = [0]
+            for j in children[i]:
+                cur = dfs(j)
+                if s[i] != s[j]:
+                    candi.append(cur)
+                    
+            candi = nlargest(2, candi)
+            res[0] = max(res[0], sum(candi) + 1)
+            return max(candi) + 1
+        
+        dfs(0)
+        return res[0]
