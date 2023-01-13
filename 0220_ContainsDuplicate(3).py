21f5ccf647d334a13b4ae77a91150659ac6440a2
yingzhuo <yingzhuo1994@gmail.com>
2023-01-13 16:31:11 -0600

    2nd solution

--- 0220_ContainsDuplicate(3).py
+++ 0220_ContainsDuplicate(3).py
@@ -10,4 +10,22 @@ class Solution:
                     return True
         return False
 
-
+# 2nd solution
+# O(n * log(k)) time | O(k) space
+# where n = len(nums), and k = indexDiff
+from sortedcontainers import SortedList
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        SList = SortedList()
+        k = indexDiff
+        t = valueDiff
+        for i in range(len(nums)):
+            if i > k: SList.remove(nums[i-k-1])   
+            pos1 = SortedList.bisect_left(SList, nums[i] - t)
+            pos2 = SortedList.bisect_right(SList, nums[i] + t)
+            
+            if pos1 != pos2 and pos1 != len(SList): return True
+            
+            SList.add(nums[i])
+        
+        return False
