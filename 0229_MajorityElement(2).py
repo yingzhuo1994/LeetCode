97713ec9b04119f8c644027bbaa297165887fa94
yingzhuo <yingzhuo1994@gmail.com>
2023-01-12 19:58:22 -0600

    2nd solution

--- 0229_MajorityElement(2).py
+++ 0229_MajorityElement(2).py
@@ -10,3 +10,24 @@ class Solution:
             if v > freq:
                 ans.append(k)
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        return [n for n in (candidate1, candidate2)
+                        if nums.count(n) > len(nums) // 3]
