fbca57a9cb7168a59873885f30ca4680e1b99ed2
yingzhuo <yingzhuo1994@gmail.com>
2023-01-13 23:42:39 -0600

    3rd solution

--- 0220_ContainsDuplicate(3).py
+++ 0220_ContainsDuplicate(3).py
@@ -29,3 +29,20 @@ class Solution:
             SList.add(nums[i])
         
         return False
+
+# 3rd solution
+# O(n) time | O(k) space
+# where n = len(nums), and d = indexDiff
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        k = indexDiff
+        t = valueDiff
+
+        seen = {}
+        for i, x in enumerate(nums): 
+            bkt = x // (t + 1)
+            if bkt in seen and i - seen[bkt][0] <= k: return True 
+            if bkt-1 in seen and i - seen[bkt-1][0] <= k and abs(x - seen[bkt-1][1]) <= t: return True 
+            if bkt+1 in seen and i - seen[bkt+1][0] <= k and abs(x - seen[bkt+1][1]) <= t: return True 
+            seen[bkt] = (i, x) 
+        return False 
