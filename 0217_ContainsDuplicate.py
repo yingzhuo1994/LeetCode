aa42725d40360782d5a43ddd4ab88d257c190b0f
yingzhuo <yingzhuo1994@gmail.com>
2023-01-13 22:37:57 -0600

    rectify

--- 0217_ContainsDuplicate.py
+++ 0217_ContainsDuplicate.py
@@ -1,11 +1,13 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # 1st solution
-        # O(n) time | O(n) space
         return len(nums) > len(set(nums))
 
-        # 2nd sorting solution
-        # O(nlogn) time | O(1) space
+# 2nd sorting solution
+# O(nlogn) time | O(n) space
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
         for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:
