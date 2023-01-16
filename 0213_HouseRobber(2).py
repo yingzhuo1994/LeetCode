3fb4ee41bd1a8f7d9ee510f25b5ba05c98be0868
yingzhuo <yingzhuo1994@gmail.com>
2023-01-15 22:32:05 -0600

    2nd solution

--- 0213_HouseRobber(2).py
+++ 0213_HouseRobber(2).py
@@ -33,3 +33,22 @@ class Solution:
                     dp[i] = dp[i-1]
             return max(dp)
         return max(helper(nums), helper(nums[::-1]))
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        elif n <= 3:
+            return max(nums)
+        
+        def rob_helper(nums, lo, hi):
+            dp1, dp2 = 0, 0
+            for i in range(lo, hi+1):
+                num = nums[i]
+                dp1, dp2 = dp2, max(dp1 + num, dp2)          
+            return dp2
+    
+        return max(rob_helper(nums, 0, n-2), rob_helper(nums, 1, n-1))
