b98a49179eb8662f1485302c85e22a04416e3a4a
yingzhuo <yingzhuo1994@gmail.com>
2023-01-16 21:01:58 -0600

    4th solution

--- 0926_FlipStringToMonotoneIncreasing.py
+++ 0926_FlipStringToMonotoneIncreasing.py
@@ -57,3 +57,16 @@ class Solution:
             else:
                 zero += 1
         return ans
+
+# 4th solution
+# O(n) time | O(1) space
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        ans = 0
+        num = 0
+        for ch in s:
+            if ch == '0':
+                ans = min(num, ans + 1)
+            else:
+                num += 1
+        return ans
