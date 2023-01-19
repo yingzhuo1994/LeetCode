0d2e44fde9bda0b7cac89ee845de43d2e0c60e86
yingzhuo <yingzhuo1994@gmail.com>
2023-01-18 20:57:53 -0600

    2nd solution

--- 0193_ValidPhoneNumbers.sh
+++ 0193_ValidPhoneNumbers.sh
@@ -1,2 +1,4 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
 grep -e "^[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}$" -e "^([0-9]\{3\}) [0-9]\{3\}\-[0-9]\{4\}$" file.txt
+
+grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
