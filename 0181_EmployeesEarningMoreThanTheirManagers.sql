c95171186e55ff1219f158d7a3d233a098b14217
yingzhuo <yingzhuo1994@gmail.com>
2023-01-18 19:49:54 -0600

    1st solution

---
+++ 0181_EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,11 @@
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None')
+
+# Write your MySQL query statement below
+Select E1.name as "Employee"
+FROM Employee E1, Employee E2
+WHERE E1.salary > E2.salary and E1.managerId = E2.id
