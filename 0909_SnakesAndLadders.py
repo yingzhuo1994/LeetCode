80bcbf50b74a709e420fb5cb142762e76c0a11b2
yingzhuo <yingzhuo1994@gmail.com>
2023-01-23 20:04:10 -0600

    2nd solution

---
+++ 0909_SnakesAndLadders.py
@@ -0,0 +1,75 @@
+# 1st solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        def getLocation(num):
+            k, r = divmod(num - 1, n)
+            if k & 1:
+                y = n - 1 - r
+            else:
+                y = r
+            x = n - 1 - k
+            return x, y
+
+        level = [1]
+        visited = set([1])
+        step = 0
+        while level:
+            newLevel = []
+            step += 1
+            for num in level:
+                for k in range(num + 1, min(num + 6, n**2) + 1):
+                    if k in visited:
+                        continue
+                    visited.add(k)
+                    x, y = getLocation(k)
+                    if board[x][y] != -1:
+                        new = board[x][y]
+                    else:
+                        new = k
+
+                    if new == n**2:
+                        return step
+
+                    newLevel.append(new)
+            level = newLevel
+
+        return -1
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        def getLocation(num):
+            k, r = divmod(num - 1, n)
+            if k & 1:
+                y = n - 1 - r
+            else:
+                y = r
+            x = n - 1 - k
+            return x, y
+
+        level = [1]
+        step = 0
+        while level:
+            newLevel = []
+            step += 1
+            for num in level:
+                for k in range(num + 1, min(num + 6, n**2) + 1):
+                    x, y = getLocation(k)
+                    if board[x][y] == 0:
+                        continue
+                    elif board[x][y] == -1:
+                        new = k
+                    else:
+                        new = board[x][y]
+                    
+                    board[x][y] = 0
+                    if new == n**2:
+                        return step
+                    
+                    newLevel.append(new)
+            level = newLevel
+        return -1
