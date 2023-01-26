f1e041218f15200cdd7d4b7cbe90c3dab2ebabe8
yingzhuo <yingzhuo1994@gmail.com>
2023-01-25 23:37:44 -0600

    3rd solution

--- 0214_ShortestPalindrome.py
+++ 0214_ShortestPalindrome.py
@@ -29,3 +29,23 @@ class Solution:
         remain = s[i:]
         remain_rev = remain[::-1]
         return remain_rev + self.shortestPalindrome(s[:i]) + remain
+
+# 3rd solution, KMP
+# O(n) time | O(n) space
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        n = len(s)
+        rev = s[::-1]
+        s_new = s + "#" + rev
+        n_new = len(s_new)
+        f = [0] * n_new
+        for i in range(1, n_new):
+            t = f[i - 1]
+            while t > 0 and s_new[i] != s_new[t]:
+                t = f[t - 1]
+
+            if s_new[i] == s_new[t]:
+                t += 1
+            f[i] = t
+
+        return rev[0:n - f[n_new - 1]] + s
