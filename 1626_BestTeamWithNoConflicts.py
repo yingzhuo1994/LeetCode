6838e48819092fcf67c4ac8acf0778e6ed4c68ab
yingzhuo <yingzhuo1994@gmail.com>
2023-01-30 20:20:20 -0600

    2nd solution

--- 1626_BestTeamWithNoConflicts.py
+++ 1626_BestTeamWithNoConflicts.py
@@ -12,3 +12,18 @@ class Solution:
                 if players[j][1] <= players[i][1]:
                     dp[i] = max(dp[i], dp[j] + players[i][1])
         return max(dp)
+
+# 2nd solution
+# O(n*log(n) + kn) time | O(k) space
+# where n = len(scores) and k = max(ages)
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        players = [[score, age] for age, score in zip(ages, scores)]
+        players.sort()
+        k = max(ages)
+        dp = [0] * (k + 1)
+        
+        for score, age in players:
+            dp[age] = score + max(dp[:age+1])
+        
+        return max(dp)
