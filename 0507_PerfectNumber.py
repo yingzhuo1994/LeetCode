1131285b3c50d153ec72acc9cd9364596ecc4925
yingzhuo <yingzhuo1994@gmail.com>
2023-02-04 18:49:44 -0600

    renew

--- 0507_PerfectNumber.py
+++ 0507_PerfectNumber.py
@@ -7,5 +7,8 @@ class Solution:
         ans = 1
         for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
-                ans += i + (num // i)
+                if i * i != num:
+                    ans += i + (num // i)
+                else:
+                    ans += i
         return ans == num
