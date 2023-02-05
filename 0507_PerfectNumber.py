f3591f33e6d04fb772f4ae0d7dfccc5801b0fbe4
yingzhuo <yingzhuo1994@gmail.com>
2023-02-04 18:43:36 -0600

    1st solution

---
+++ 0507_PerfectNumber.py
@@ -0,0 +1,11 @@
+# 1st solution
+# O(sqrt(n)) time | O(1) space
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        ans = 1
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                ans += i + (num // i)
+        return ans == num
