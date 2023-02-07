1a2eab11dd297a47c742c8588cda2709e4bdd12a
yingzhuo <yingzhuo1994@gmail.com>
2023-02-06 20:32:29 -0600

    2nd solution

--- 0904_FruitIntoBaskets.py
+++ 0904_FruitIntoBaskets.py
@@ -41,3 +41,37 @@ class Solution:
 
         return ans
 
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
+       
+        ans = 1
+        cur_fruits = {fruits[0]: [1, 1, 0]}
+
+        for i in range(1, n):
+            fruit = fruits[i]
+            if fruit in cur_fruits:
+                cur_fruits[fruit][0] += 1
+                if cur_fruits[fruit][2] == i - 1:
+                    cur_fruits[fruit][1] += 1
+                else:
+                    cur_fruits[fruit][1] = 1
+                cur_fruits[fruit][2] = i
+            else:
+                if len(cur_fruits) == 2:
+                    lst = list(cur_fruits.keys())
+                    for key in lst:
+                        if cur_fruits[key][2] != i - 1:
+                            cur_fruits.pop(key)
+                        else:
+                            cur_fruits[key][0] = cur_fruits[key][1]
+                cur_fruits[fruit] = [1, 1, i]
+
+            curSum = 0
+            for key in cur_fruits:
+                curSum += cur_fruits[key][0]
+            ans = max(ans, curSum)
+
+        return ans
