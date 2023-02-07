1bd4d8137510d92c5bc04cf5d2f1f156ac2ae429
yingzhuo <yingzhuo1994@gmail.com>
2023-02-06 20:40:56 -0600

    3rd solution

--- 0904_FruitIntoBaskets.py
+++ 0904_FruitIntoBaskets.py
@@ -75,3 +75,18 @@ class Solution:
             ans = max(ans, curSum)
 
         return ans
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l, nums, res = 0, collections.Counter(), 0
+        for r in range(len(fruits)):
+            nums[fruits[r]] += 1
+            while len(nums) > 2:
+                nums[fruits[l]] -= 1 
+                if not nums[fruits[l]]:
+                    nums.pop(fruits[l])
+                l += 1
+            res = max(res, r - l + 1)
+        return res
