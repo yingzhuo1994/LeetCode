d7d8405887a6f35fe22d1c315c98e91137ab20b9
yingzhuo <yingzhuo1994@gmail.com>
2023-02-12 23:15:43 -0600

    1st solution

---
+++ 1523_CountOddNumbersInAnIntervalRange.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if (low & 1) or (high & 1):
+            return (high - low) // 2 + 1
+        else:
+            return (high - low) // 2
