13808c49aa2796a5cf86ce546af4b382364a5ca9
yingzhuo <yingzhuo1994@gmail.com>
2023-02-13 19:38:31 -0600

    2nd solution

--- 0513_FindBottomLeftTreeValue.py
+++ 0513_FindBottomLeftTreeValue.py
@@ -21,3 +21,28 @@ class Solution:
                 return stack[0].val
             stack = newStack
         return None
+
+# 2nd soluton, DFS
+# O(n) time | O(h) space
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        self.maxDepth = 0
+        self.getValue = False
+        self.ans = None
+        def dfs(node, depth=0):
+            if not node:
+                return
+            self.maxDepth = max(self.maxDepth, depth)
+            if node.left:
+                dfs(node.left, depth + 1)
+            
+            if node.right:
+                dfs(node.right, depth + 1)
+            
+            if self.getValue and self.maxDepth == depth:
+                self.ans = node.val
+                self.getValue = False
+        dfs(root)
+        self.getValue = True
+        dfs(root)
+        return self.ans
