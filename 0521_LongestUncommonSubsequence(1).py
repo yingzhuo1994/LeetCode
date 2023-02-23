eb2d6f3de2aac8900b958823322abe263bd4bc73
yingzhuo <yingzhuo1994@gmail.com>
2023-02-22 19:58:07 -0600

    1st solution

---
+++ 0521_LongestUncommonSubsequence(1).py
@@ -0,0 +1,7 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a), len(b))
