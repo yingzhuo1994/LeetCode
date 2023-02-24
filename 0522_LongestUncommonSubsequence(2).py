828fd5f3c22e3a1bbdcaf1f84ac43004a4ee131a
yingzhuo <yingzhuo1994@gmail.com>
2023-02-23 19:49:51 -0600

    2nd and 3rd solutions

--- 0522_LongestUncommonSubsequence(2).py
+++ 0522_LongestUncommonSubsequence(2).py
@@ -18,3 +18,36 @@ class Solution:
                     newVisited.add(s[:i] + s[i+1:])
             visited = newVisited
         return -1
+
+# 2nd solution
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        def isSubsequence(s, t):
+            t = iter(t)
+            return all(c in t for c in s)
+ 
+        strs.sort(key = lambda x: -len(x))
+        for i, word in enumerate(strs):
+            if all(not isSubsequence(word, strs[j]) for j in range(len(strs)) if j != i): 
+                return len(word)
+        
+        return -1
+
+# 3rd solution
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        def isSub(w1, w2, count):
+            for i in w2:
+                if i == w1[count]:
+                     count += 1
+                if count == len(w1): 
+                    return True 
+            return count == len(w2)
+        
+        d = collections.Counter(strs)
+        strs.sort(key = len, reverse = True)
+        for i in range(len(strs)):
+            if d[strs[i]] == 1:
+                res = any(isSub(strs[i], strs[j], 0) for j in range(i))        
+                if res == False: return len(strs[i])
+        return -1
