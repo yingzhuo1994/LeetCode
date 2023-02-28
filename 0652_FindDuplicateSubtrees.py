3834dd5c08d7276f85ba8b84e4d07f09febf63f8
yingzhuo <yingzhuo1994@gmail.com>
2023-02-27 19:45:18 -0600

    1st solution

---
+++ 0652_FindDuplicateSubtrees.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 1st solution
+# O(n^2) time | O(n) space
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        dic = defaultdict(list)
+        isAdded = set()
+        ans = []
+        def getDepth(node, depth=0):
+            if not node:
+                return 0
+            return max(getDepth(node.left), getDepth(node.right)) + 1
+
+        def isSameTree(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            return isSameTree(node1.left, node2.left) and isSameTree(node1.right, node2.right)
+
+        def hasSameTree(node):
+            findSame = False
+            depth = getDepth(node)
+            for curNode in dic[(node.val, depth)]:
+                if isSameTree(curNode, node):
+                    return curNode
+
+                if findSame:
+                    break
+            if not findSame:
+                dic[(node.val, depth)].append(node)
+            return None
+        
+        def appendNode(node):
+            if not node:
+                return
+            if node not in isAdded:
+                ans.append(node)
+                isAdded.add(node)
+                # appendNode(node.left)
+                # appendNode(node.right)
+        
+        def dfs(node):
+            if not node:
+                return
+            oldNode =  hasSameTree(node)
+            if oldNode is not None:
+                appendNode(oldNode)
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return ans
