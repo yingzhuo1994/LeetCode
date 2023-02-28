0cde4b4af1dcc7c3fe90690833c7fcc7002c6fee
yingzhuo <yingzhuo1994@gmail.com>
2023-02-27 19:48:08 -0600

    2nd solution

--- 0652_FindDuplicateSubtrees.py
+++ 0652_FindDuplicateSubtrees.py
@@ -58,3 +58,17 @@ class Solution:
         
         dfs(root)
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        def trv(root):
+            if not root: return "null"
+            struct = "%s,%s,%s" % (str(root.val), trv(root.left), trv(root.right))
+            nodes[struct].append(root)
+            return struct
+        
+        nodes = collections.defaultdict(list)
+        trv(root)
+        return [nodes[struct][0] for struct in nodes if len(nodes[struct]) > 1]
