abcbba831022dbff9232efb9a9cdd7c648e1981d
yingzhuo <yingzhuo1994@gmail.com>
2023-03-07 20:20:30 -0600

    1st solution

--- 0539_MinimumTimeDifference.py
+++ 0539_MinimumTimeDifference.py
@@ -6,17 +6,13 @@ class Solution:
             h, m = time_str.split(":")
             return int(h) * 60 + int(m)
         
-        lst1 = [transform_time(time_str) for time_str in timePoints]
-        count = collections.Counter(lst1)
-        lst2 = []
-        for time in count:
-            if count[time] > 1:
-                return 0
-            lst2.append(time + 24 * 60)
-        lst = list(count.keys()) + lst2
+        lst = [transform_time(time_str) for time_str in timePoints]
         lst.sort()
         ans = 24 * 60
-        for i in range(len(lst) - 1):
-            diff = lst[i + 1] - lst[i]
+        for i in range(len(lst)):
+            if i == len(lst) - 1:
+                diff = lst[0] + 24 * 60 - lst[i]
+            else:
+                diff = lst[i + 1] - lst[i]
             ans = min(ans, diff)
         return ans
