4398d5048d71d60a697abdfc80e3786d1c8ab8b9
yingzhuo <yingzhuo1994@gmail.com>
2023-03-07 19:53:40 -0600

    renew

--- 0875_KokoEatingBananas.py
+++ 0875_KokoEatingBananas.py
@@ -1,12 +1,14 @@
 # 1st solution
-# O(n*log(n)) time | O(1) space
+# O(n * log(n)) time | O(1) space
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        total = sum(piles)
-        smallest = total // h
-        if smallest == 0:
-            return 1
+        n = len(piles)
         largest = max(piles)
+        if n == h:
+            return max(piles)
+
+        smallest = 1
+
         while smallest < largest:
             middle = smallest + (largest - smallest) // 2
             if self.isEnough(piles, h, middle):
@@ -18,9 +20,5 @@ class Solution:
     def isEnough(self, piles, h, k):
         count = 0
         for pile in piles:
-            q, r = pile // k, pile % k
-            if r == 0:
-                count += q
-            else:
-                count += q + 1
+            count += (pile + k - 1) // k
         return count <= h
