ba400267d2fd5341babd0b105a274cdd223eb5ed
yingzhuo <yingzhuo1994@gmail.com>
2023-03-08 19:44:49 -0600

    1st solution

---
+++ 0541_ReverseString(2).py
@@ -0,0 +1,11 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        if len(s) <= k:
+            return s[::-1]
+
+        if len(s) <= 2 * k:
+            return s[:k][::-1] + s[k:]
+        else:
+            return s[:k][::-1] + s[k:2 * k] + self.reverseStr(s[2 * k:], k)
