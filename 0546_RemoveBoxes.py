5563822222fcecb3d126d2b5ecf32ce94f2ea168
yingzhuo <yingzhuo1994@gmail.com>
2023-03-09 21:00:10 -0600

    2nd solution

--- 0546_RemoveBoxes.py
+++ 0546_RemoveBoxes.py
@@ -21,3 +21,18 @@ class Solution:
             memo[key] = ans
             return ans
         return dfs(boxes)
+
+# 2nd solution
+# O(^4) time | O(n^3) space
+class Solution:
+    def removeBoxes(self, boxes: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i, j, k):
+            # dp(i,j,k) means the max points you can earn between boxes "i" and "j", with "k" boxes before i that has the same color as "i".
+            if i > j: 
+                return 0
+            indx = [m for m in range(i+1, j+1) if boxes[m] == boxes[i]]
+            ans = (k+1)**2 + dp(i+1, j, 0)
+            return max([ans] + [dp(i+1, m-1, 0) + dp(m, j, k+1) for m in indx])   
+            
+        return dp(0, len(boxes)-1, 0)
