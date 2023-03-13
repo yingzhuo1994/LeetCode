71b764397217c59cb0ddfb223b1d4ee42435980a
yingzhuo <yingzhuo1994@gmail.com>
2023-03-12 20:43:15 -0500

    2nd solution

--- 0546_RemoveBoxes.py
+++ 0546_RemoveBoxes.py
@@ -45,3 +45,29 @@ class Solution:
             return ans
              
         return dp(0, len(boxes)-1, 0)
+
+# 3rd solution, Bottom-Up
+# O(^4) time | O(n^3) space
+class Solution:
+    def removeBoxes(self, boxes: List[int]) -> int:
+        n = len(boxes)
+        dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(n)]
+
+        for j in range(n):
+            for k in range(j + 1):
+                dp[j][j][k] = (k + 1) * (k + 1)
+        
+        for l in range(1, n):
+            for j in range(l, n):
+                i = j - l
+
+                for k in range(i + 1):
+                    ans = (k + 1) * (k + 1) + dp[i + 1][j][0]
+
+                    for m in range(i + 1, j + 1):
+                        if boxes[m] == boxes[i]:
+                            ans = max(ans, dp[i+1][m-1][0] + dp[m][j][k+1])
+                    
+                    dp[i][j][k] = ans
+        
+        return dp[0][n-1][0]
