cbe94a5381448262d0aecbe85a3c1fc012cf18c9
yingzhuo <yingzhuo1994@gmail.com>
2023-03-21 20:07:46 -0500

    2nd solution

--- 2492_MinimumScoreOfAPathBetweenTwoCities.py
+++ 2492_MinimumScoreOfAPathBetweenTwoCities.py
@@ -7,7 +7,7 @@ class Solution:
             graph[a].append([b, dist])
             graph[b].append([a, dist])
         
-        visited =set()
+        visited = set()
         self.score = float("inf")
         def dfs(node):
             if node in visited:
@@ -19,3 +19,27 @@ class Solution:
         
         dfs(1)
         return self.score
+
+# 2nd solution, bfs
+# O(n) time | O(n) space
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = [[] for _ in range(n  +1)]
+        for a, b, dist in roads:
+            graph[a].append([b, dist])
+            graph[b].append([a, dist])
+        
+        visited = set([1])
+        score = float("inf")
+        level = [1]
+        while level:
+            newLevel = []
+            for node in level:
+                for neigh, dist in graph[node]:
+                    score = min (score, dist)
+                    if neigh not in visited:
+                        newLevel.append(neigh)
+                        visited.add(neigh)
+            level = newLevel
+        
+        return score
