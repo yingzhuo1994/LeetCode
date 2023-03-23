0a93b8b4b22aca679aa9ca165b2377d0e104d0fd
yingzhuo <yingzhuo1994@gmail.com>
2023-03-22 20:23:53 -0500

    2nd solution

--- 1319_NumberOfOperationsToMakeNetworkConnected.py
+++ 1319_NumberOfOperationsToMakeNetworkConnected.py
@@ -1,5 +1,6 @@
 # 1st soluiton, disojint set
-# O(n) time | O(n) space
+# O(k) time | O(n) space
+# k = len(connections)
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         DS = DisjointSet(n)
@@ -41,3 +42,28 @@ class DisjointSet:
             return ans
         else:
             return -1
+
+# 2nd soluiton, dfs
+# O(k) time | O(n) space
+# k = len(connections)
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+        
+        graph = [set() for _ in range(n)]
+        for a, b in connections:
+            graph[a].add(b)
+            graph[b].add(a)
+        
+        seen = [0] * n
+
+        def dfs(i):
+            if seen[i]:
+                return 0
+            seen[i] = 1
+            for j in graph[i]:
+                dfs(j)
+            return 1
+        
+        return sum(dfs(i) for i in range(n)) - 1
