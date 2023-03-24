8b9bcdd716172f67755c5d859be77dbba8f30aef
yingzhuo <yingzhuo1994@gmail.com>
2023-03-23 19:46:11 -0500

    2nd solution

--- 1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
+++ 1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -30,3 +30,32 @@ class Solution:
             level = newLevel
         
         return ans
+
+# 2nd solution, DFS
+# O(n) time | O(n) space
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graphForward = [[] for _ in range(n)]
+        graphBackward = [[] for _ in range(n)]
+
+        for a, b in connections:
+            graphForward[a].append(b)
+            graphBackward[b].append(a)
+        
+        self.ans = 0
+        visited = [0 for _ in range(n)]
+
+        def dfs(start):
+            visited[start] = 1
+            for neib in graphForward[start]:
+                if visited[neib] == 0:
+                    self.ans += 1
+                dfs(neib)
+            
+            for neib in graphBackward[start]:
+                if visited[neib] == 0:
+                    dfs(neib)
+            
+        dfs(0)
+
+        return self.ans
