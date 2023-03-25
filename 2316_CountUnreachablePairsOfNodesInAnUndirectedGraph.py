a375b96a87f11b0c1c8370ff936129ef63570216
yingzhuo <yingzhuo1994@gmail.com>
2023-03-24 19:33:43 -0500

    2nd solution

--- 2316_CountUnreachablePairsOfNodesInAnUndirectedGraph.py
+++ 2316_CountUnreachablePairsOfNodesInAnUndirectedGraph.py
@@ -31,3 +31,49 @@ class Solution:
                 ans += lst[i] * lst[j]
         
         return ans
+
+# 2nd solution, Disjoint Set
+# O(n) time | O(n) space
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        DS = DisjointSet(n)
+        for a, b in edges:
+            DS.merge(a, b)
+        
+        countDic = DS.getCountDic()
+        ans = 0
+        values = list(countDic.values())
+        count = 0
+        for value in values:
+            count += value
+            ans += value * (n - count)
+        return ans
+
+class DisjointSet:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.size = [1 for _ in range(n)]
+    
+    def merge(self, a, b):
+        pa = self.getParent(a)
+        pb = self.getParent(b)
+        if pa == pb:
+            return
+        if self.size[pa] < self.size[pb]:
+            pa, pb = pb, pa
+        
+        self.parents[pb] = pa
+        self.size[pa] += self.size[pb]
+    
+    def getParent(self, a):
+        if self.parents[a] != a:
+            self.parents[a] = self.getParent(self.parents[a])
+        return self.parents[a]
+    
+    def getCountDic(self):
+        dic = {}
+        for node in range(len(self.parents)):
+            parent = self.getParent(node)
+            if parent not in dic:
+                dic[parent] = self.size[parent]
+        return dic
