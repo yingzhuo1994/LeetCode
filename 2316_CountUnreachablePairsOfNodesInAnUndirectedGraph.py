0afd6db252f169498e147b576cc33666cc6af8ae
yingzhuo <yingzhuo1994@gmail.com>
2023-03-24 19:34:45 -0500

    rectify

--- 2316_CountUnreachablePairsOfNodesInAnUndirectedGraph.py
+++ 2316_CountUnreachablePairsOfNodesInAnUndirectedGraph.py
@@ -1,4 +1,4 @@
-# 1st solution, dfs, TLE
+# 1st solution, dfs
 # O(n) time | O(n) space
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
@@ -18,18 +18,18 @@ class Solution:
                 count += dfs(neig)
             return count
 
-        lst = []
+        values = []
         for i in range(n):
             if visited[i]:
                 continue
             count = dfs(i)
-            lst.append(count)
+            values.append(count)
         
         ans = 0
-        for i in range(len(lst)):
-            for j in range(i + 1, len(lst)):
-                ans += lst[i] * lst[j]
-        
+        count = 0
+        for value in values:
+            count += value
+            ans += value * (n - count)
         return ans
 
 # 2nd solution, Disjoint Set
