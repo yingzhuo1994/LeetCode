14f639c49c195ce57148dc2b974169526a8879ad
yingzhuo <yingzhuo1994@gmail.com>
2023-03-25 20:55:50 -0500

    2nd solution

--- 2360_LongestCycleInAGraph.py
+++ 2360_LongestCycleInAGraph.py
@@ -28,3 +28,28 @@ class Solution:
                     slow = edges[slow]
                 ans = max(ans, count)
         return ans
+
+# 2nd solution
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        n = len(edges)
+        self.max_length = -1
+        seen = [False] * n
+        visiting = {}
+        stack = []
+            
+        def dfs(node):
+            if not seen[node]:
+                if node in visiting:
+                    self.max_length = max(self.max_length, len(stack) - visiting[node])
+                elif edges[node] != -1: 
+                    visiting[node] = len(stack) 
+                    stack.append(node)
+                    dfs(edges[node])
+                    stack.pop()
+                    visiting.pop(node)
+                seen[node] = True
+    
+        for i in range(n):            
+            dfs(i)
+        return self.max_length
