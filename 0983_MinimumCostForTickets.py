a3e9f23f2da823e1a33d61193a3442d580ad18dc
yingzhuo <yingzhuo1994@gmail.com>
2023-03-27 20:37:00 -0500

    2nd solution

--- 0983_MinimumCostForTickets.py
+++ 0983_MinimumCostForTickets.py
@@ -15,5 +15,21 @@ class Solution:
                 cur_cost = cost + dfs(start, days[start] + day)
                 total = min(total, cur_cost)
             return total
-        
+
         return dfs(0, -1)
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [0 for i in range(days[-1] + 1)]
+        travelSet = set(days)
+        for i in range(days[-1] + 1):
+            if i not in travelSet:
+                dp[i] = dp[i - 1]
+            else:
+                dp[i] = min(dp[max(0, i - 7)] + costs[1],
+                            dp[max(0, i - 1)] + costs[0],
+                            dp[max(0, i - 30)] + costs[2])
+        return dp[-1]
