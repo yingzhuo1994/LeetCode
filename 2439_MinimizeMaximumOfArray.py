31c4ff71c7756c2ecad1624fd456112101094490
yingzhuo <yingzhuo1994@gmail.com>
2023-04-04 19:46:35 -0500

    2nd solution

--- 2439_MinimizeMaximumOfArray.py
+++ 2439_MinimizeMaximumOfArray.py
@@ -14,3 +14,13 @@ class Solution:
             else:
                 total -= nums[i]
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+            ans = max(ans, (nums[i] + i) // (i + 1))
+        return ans
