35ca1abfd2378a3a111e46578d8f2395f30f7940
yingzhuo <yingzhuo1994@gmail.com>
2023-04-07 20:08:27 -0500

    2nd solution

--- 0565_ArrayNesting.py
+++ 0565_ArrayNesting.py
@@ -13,3 +13,20 @@ class Solution:
             length = dfs(i, visited)
             ans = max(ans, length)
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        n = len(nums)
+        def dfs(idx, depth=0):
+            idx %= n
+            if nums[idx] >= n:
+                return depth
+            nums[idx] += n
+            return dfs(nums[idx], depth + 1)
+        ans = 0
+        for i in range(len(nums)):
+            length = dfs(i)
+            ans = max(ans, length)
+        return ans
