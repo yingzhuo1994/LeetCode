8a55c70e483a8b32ecc124cecde94c6781983d38
yingzhuo <yingzhuo1994@gmail.com>
2023-04-09 19:45:02 -0500

    1st solution

---
+++ 0550_GamePlayAnalysis(4).sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
+FROM
+(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 LEFT JOIN Activity t2
+ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
