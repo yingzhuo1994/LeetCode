639cf669233b5ebc55f63514ccdf41b31fd9529b
yingzhuo <yingzhuo1994@gmail.com>
2023-04-14 20:12:18 -0500

    2nd solution

--- 2218_MaximumValueOfKCoinsFromPiles.py
+++ 2218_MaximumValueOfKCoinsFromPiles.py
@@ -23,3 +23,23 @@ class Solution:
         for key in level:
             ans = max(ans, key[-1])
         return ans
+
+# 2nd solution
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        n = len(piles)
+        @cache
+        def dfs(idx, left):
+            if idx == n:
+                if left == 0:
+                    return 0
+                if left > 0:
+                    return float("-inf")
+            ans = dfs(idx + 1, left)
+            curSum = 0
+            for i in range(min(left, len(piles[idx]))):
+                curSum += piles[idx][i]
+                ans = max(ans, curSum + dfs(idx + 1, left - i - 1))
+            return ans
+        
+        return dfs(0, k)
