3fe276a4117b5c69a8a68ee7b5fe497886f4abf2
yingzhuo <yingzhuo1994@gmail.com>
2023-04-18 20:25:22 -0500

    2nd solution

--- 1372_LongestZigZigPathInABinaryTree.py
+++ 1372_LongestZigZigPathInABinaryTree.py
@@ -21,3 +21,19 @@ class Solution:
             return left, right
         dfs(root)
         return self.ans
+
+# 2nd solution
+# O(n) time | O(h) space
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return -1, -1, 0
+            leftLeft, leftRight, leftMax = dfs(node.left)
+            rightLeft, rightRight, rightMax = dfs(node.right)
+            left = leftRight + 1
+            right = rightLeft + 1
+            ans = max(left, right, leftMax, rightMax)
+            return left, right, ans
+        ans = max(dfs(root))
+        return ans
