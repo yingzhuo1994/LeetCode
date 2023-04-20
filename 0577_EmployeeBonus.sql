15d7c462c0e41daef6eca119d018cb91cbb82786
yingzhuo <yingzhuo1994@gmail.com>
2023-04-19 19:42:31 -0500

    simplify

--- 0577_EmployeeBonus.sql
+++ 0577_EmployeeBonus.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 
 # 1st solution
-SELECT T.name, T.bonus from (
 SELECT E.name as "name", B.bonus as "bonus" FROM Employee E left join Bonus B
-ON E.empId = B.empId
-) T
-WHERE T.bonus IS NULL OR T.bonus < 1000 
+ON E.empId = B.empId 
+WHERE bonus IS NULL OR bonus < 1000
