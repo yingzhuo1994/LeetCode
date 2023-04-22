0cd09203a06cf2ec1008f81db4403253414b5ec6
yingzhuo <yingzhuo1994@gmail.com>
2023-04-21 19:55:19 -0500

    3rd solution

--- 1312_MinimumInsertionStepsToMakeAStringPalindrome.py
+++ 1312_MinimumInsertionStepsToMakeAStringPalindrome.py
@@ -37,3 +37,15 @@ class Solution:
                         newStack.append([newT, count + 1])
             stack = newStack
         return ans
+
+# 3rd solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * (n + 1) for i in range(n + 1)]
+        for i in range(n):
+            for j in range(n):
+                # ~j = -j - 1
+                dp[i + 1][j + 1] = dp[i][j] + 1 if s[i] == s[~j] else max(dp[i][j + 1], dp[i + 1][j])
+        return n - dp[n][n]
