2d428498dbe97c35544c5a9487bc99bfc68191e5
yingzhuo <yingzhuo1994@gmail.com>
2023-04-24 20:17:59 -0500

    2nd solution

--- 2336_SmallestNumberInInfiniteSet.py
+++ 2336_SmallestNumberInInfiniteSet.py
@@ -14,7 +14,28 @@ class SmallestInfiniteSet:
     def addBack(self, num: int) -> None:
         if num in self.numSet:
             self.numSet.remove(num)
-        
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class SmallestInfiniteSet:
+    def __init__(self):
+        self.numSet = set()
+        self.minNum = 1
+
+    def popSmallest(self) -> int:
+        while self.minNum in self.numSet:
+            self.minNum += 1
+        self.numSet.add(self.minNum)
+        num = self.minNum
+        self.minNum += 1
+        return num
+
+    def addBack(self, num: int) -> None:
+        if num in self.numSet:
+            if num < self.minNum:
+                self.minNum = num
+            self.numSet.remove(num)
+
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
