1b3b94f3247a9c1dbeafb6aa2f671534f9b72617
yingzhuo <yingzhuo1994@gmail.com>
2023-04-26 19:50:39 -0500

    renew

--- 0586_CustomerPlacingTheLargestNumberOfOrders.sql
+++ 0586_CustomerPlacingTheLargestNumberOfOrders.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 
 # 1st solution
-select t1.customer_number from
-(select customer_number, count(order_number) as "order_count" from Orders group by customer_number) t1
-order by t1.order_count desc
-limit 1
+select customer_number from orders
+group by customer_number
+order by count(*) desc limit 1;
