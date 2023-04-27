5208c44c5b2b6c9069f0b3e3db13d08b2da49963
yingzhuo <yingzhuo1994@gmail.com>
2023-04-26 19:53:01 -0500

    2nd solution

--- 0586_CustomerPlacingTheLargestNumberOfOrders.sql
+++ 0586_CustomerPlacingTheLargestNumberOfOrders.sql
@@ -3,4 +3,18 @@
 # 1st solution
 select customer_number from orders
 group by customer_number
-order by count(*) desc limit 1;
+order by count(*) desc
+limit 1;
+
+# 2nd solution
+# follow up
+SELECT customer_number
+FROM orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+	SELECT COUNT(order_number) cnt
+	FROM orders
+	GROUP BY customer_number
+	ORDER BY cnt DESC
+	LIMIT 1
+)
