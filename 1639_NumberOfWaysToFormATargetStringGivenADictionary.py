f3a04c9f8f33c3640b4c32bec9b93eb41834aea5
yingzhuo <yingzhuo1994@gmail.com>
2023-04-30 21:05:47 -0500

    renew

--- 1639_NumberOfWaysToFormATargetStringGivenADictionary.py
+++ 1639_NumberOfWaysToFormATargetStringGivenADictionary.py
@@ -6,18 +6,16 @@ class Solution:
         MOD = 10**9 + 7
         m = len(target)
         n = len(words[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[0][0] = 1
 
         countDic = [Counter([words[j][k] for j in range(len(words))]) for k in range(n)]
 
-        for j in range(n):
-            dp[0][j] = countDic[j][target[0]]
-        
-        for i in range(1, m):
+        for i in range(1, m  + 1):
             curSum = 0
-            for j in range(i, n):
-                count = countDic[j][target[i]]
+            for j in range(i, n + 1):
                 curSum += dp[i - 1][j - 1]
+                count = countDic[j - 1][target[i - 1]]
                 dp[i][j] = curSum * count % MOD
         
         ans = sum(dp[-1]) % MOD
