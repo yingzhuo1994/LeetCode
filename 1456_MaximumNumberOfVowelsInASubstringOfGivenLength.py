473a824c597b595db9e8070e64a69ab58f52b8ce
yingzhuo <yingzhuo1994@gmail.com>
2023-05-04 20:56:08 -0500

    2nd solution

--- 1456_MaximumNumberOfVowelsInASubstringOfGivenLength.py
+++ 1456_MaximumNumberOfVowelsInASubstringOfGivenLength.py
@@ -13,3 +13,20 @@ class Solution:
             ans = max(ans, len(stack))
         return ans
 
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = set(["a", "e", "i", "o", "u"])
+        ans = 0
+        start = 0
+        count = 0
+        for i in range(len(s)):
+            if s[i] in vowels:
+                count += 1
+            while start < i - k + 1:
+                if s[start] in vowels:
+                    count -= 1
+                start += 1
+            ans = max(ans, count)
+        return ans
