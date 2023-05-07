c3f076d668199ee344cafcf1d2fc0ac99db61706
yingzhuo <yingzhuo1994@gmail.com>
2023-05-06 20:47:22 -0500

    2nd solution

--- 0649_Dota2Senate.py
+++ 0649_Dota2Senate.py
@@ -1,3 +1,5 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         n = len(senate)
@@ -45,3 +47,22 @@ class Solution:
         if len(R_list) > 0:
             return "Radiant"
         return "Dire"
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+
+        r_arr = [i for i in range(len(senate)) if senate[i]=='R']
+        d_arr = [j for j in range(len(senate)) if senate[j]=='D']
+        
+        while r_arr and d_arr:
+            r = r_arr.pop(0)
+            d = d_arr.pop(0)
+            if r < d:
+                r_arr.append(n + r)
+            else:
+                d_arr.append(n + d)
+                
+        return 'Radiant' if r_arr else 'Dire'
