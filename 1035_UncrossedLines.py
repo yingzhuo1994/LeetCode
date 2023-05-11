bbc66307ed0bb611c1ea049c2793283e4a8c7e41
yingzhuo <yingzhuo1994@gmail.com>
2023-05-10 20:37:28 -0500

    2nd solution

--- 1035_UncrossedLines.py
+++ 1035_UncrossedLines.py
@@ -22,5 +22,14 @@ class Solution:
 
             return ans
 
-
         return dfs(0, 0)
+
+# 2nd solution, DP
+# O(mn) time | O(mn) space
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp, m, n = collections.defaultdict(int), len(nums1), len(nums2)
+        for i in range(m):
+            for j in range(n):
+                dp[i, j] = max(dp[i - 1, j - 1] + (nums1[i] == nums2[j]), dp[i - 1, j], dp[i, j - 1])
+        return dp[m - 1, n - 1]
