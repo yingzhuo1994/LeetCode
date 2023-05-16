402fe1fd197fd9f97b49f82a4b1b487bf6c2931f
yingzhuo <yingzhuo1994@gmail.com>
2023-05-15 21:16:16 -0500

    update

--- 0564_FindTheClosestPalindrome.py
+++ 0564_FindTheClosestPalindrome.py
@@ -1,11 +1,11 @@
 class Solution:
     def nearestPalindromic(self, n: str) -> str:
-        k = len(n)
-        candidates = [str(10**k + d) for k in (k-1, k) for d in (-1, 1)]
-        prefix = n[:(k+1)//2]
+        m = len(n)
+        candidates = [str(10**k + d) for k in (m-1, m) for d in (-1, 1)]
+        prefix = n[:(m+1)//2]
         p = int(prefix)
         for start in map(str, (p-1, p, p+1)):
-            candidates.append(start + (start[:-1] if k%2 else start)[::-1])
+            candidates.append(start + (start[:-1] if m & 1 else start)[::-1])
         
         def delta(x):
             return abs(int(n) - int(x))
