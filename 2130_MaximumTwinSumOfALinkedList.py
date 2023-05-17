ab0d2308ef88f4b1289fe160b69a041e0b3d144d
yingzhuo <yingzhuo1994@gmail.com>
2023-05-16 20:22:52 -0500

    2nd solution

--- 2130_MaximumTwinSumOfALinkedList.py
+++ 2130_MaximumTwinSumOfALinkedList.py
@@ -21,3 +21,25 @@ class Solution:
             end -= 1
         
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        node = head
+        fastNode = head
+        prev = None
+        while fastNode and fastNode.next:
+            fastNode = fastNode.next.next
+            nextNode = node.next
+            node.next = prev
+            prev = node
+            node = nextNode
+
+        ans = 0
+        while node:
+            ans = max(ans, prev.val + node.val)
+            node = node.next
+            prev = prev.next
+
+        return ans
