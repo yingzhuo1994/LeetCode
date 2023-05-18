c9a0cb61cd90813f0b31067c607c28d2113e0b63
yingzhuo <yingzhuo1994@gmail.com>
2023-05-17 20:13:23 -0500

    1st solution

---
+++ 1557_MinimumNumberOfVerticesToReachAllNodes.py
@@ -0,0 +1,9 @@
+# 1st solution
+# O(E) time | O(n) space
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        inCount = [0 for _ in range(n)]
+        for a, b in edges:
+            inCount[b] += 1
+        
+        return [node for node in range(n) if inCount[node] == 0]
