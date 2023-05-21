6ff8e2f4f13e30f15e2ff65790d872a8a63611ee
yingzhuo <yingzhuo1994@gmail.com>
2023-05-20 19:33:39 -0500

    3rd solution

--- 0934_ShortestBridge.py
+++ 0934_ShortestBridge.py
@@ -80,3 +80,57 @@ class Solution:
                 ans = min(ans, dist)
         
         return ans
+
+# 3rd solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        neighbors = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        def dfs(i, j, lst):
+            if grid[i][j] == 1:
+                lst.append([i, j])
+                grid[i][j] = -1
+                for dx, dy in neighbors:
+                    x = i + dx
+                    y = j + dy
+                    if 0 <= x < n and 0 <= y < n:
+                        dfs(x, y, lst)
+        
+        def getBorders(lst, borders):
+            for i, j in lst:
+                for dx, dy in neighbors:
+                    x = i + dx
+                    y = j + dy
+                    if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
+                        borders.add((i, j))
+            return borders
+
+        
+        lst1 = []
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    if len(lst1) == 0:
+                        dfs(i, j, lst1)
+
+        level = set()
+        getBorders(lst1, level)
+
+        ans = 0
+        while level:
+            newLevel = set()
+            for i, j in level:
+                for dx, dy in neighbors:
+                    x = i + dx
+                    y = j + dy
+                    if 0 <= x < n and 0 <= y < n:
+                        if grid[x][y] == 0:
+                            grid[x][y] = -2
+                            newLevel.add((x, y))
+                        elif grid[x][y] == 1:
+                            return ans
+            level = newLevel
+            ans += 1
+        
+        return -1
