38465ed05163202018bb9c78a24a114e3db879fd
yingzhuo <yingzhuo1994@gmail.com>
2023-05-23 19:06:04 -0500

    2nd solution

--- 1438_LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.py
+++ 1438_LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.py
@@ -30,3 +30,26 @@ class Solution:
             else:
                 end = length - 1
         return start - 1
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        minStack = []
+        maxStack = []
+        start = 0
+        ans = 0
+        for i in range(n):
+            heappush(minStack, [nums[i], i])
+            heappush(maxStack, [-nums[i], i])
+
+            while -maxStack[0][0] - minStack[0][0] > limit:
+                start += 1
+                while minStack and minStack[0][1] < start:
+                    heappop(minStack)
+                while maxStack and maxStack[0][1] < start:
+                    heappop(maxStack)
+            
+            ans = max(ans, i - start + 1)
+        return ans
