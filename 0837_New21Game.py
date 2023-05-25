56ee20be6c9568c8408ea24cc9de5361c38e3043
yingzhuo <yingzhuo1994@gmail.com>
2023-05-24 20:59:50 -0500

    2nd solution

--- 0837_New21Game.py
+++ 0837_New21Game.py
@@ -12,3 +12,20 @@ class Solution:
                 dp[i + j] += dp[i] * 1 / maxPts
 
         return sum(dp[k:n+1])
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        if k == 0 or n >= k + maxPts: 
+            return 1
+        dp = [1.0] + [0.0] * n
+        Wsum = 1.0
+        for i in range(1, n + 1):
+            dp[i] = Wsum / maxPts
+            if i < k: 
+                Wsum += dp[i]
+            if i - maxPts >= 0: 
+                Wsum -= dp[i - maxPts]
+        
+        return sum(dp[k:])
