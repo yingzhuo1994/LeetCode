84cdef5a6c29bee169cfdbe599b37ff7d285f1dd
yingzhuo <yingzhuo1994@gmail.com>
2023-05-27 23:54:48 -0500

    3rd solution

--- 1547_MinimumCostToCustAStick.py
+++ 1547_MinimumCostToCustAStick.py
@@ -40,3 +40,16 @@ class Solution:
         cuts.sort()
 
         return dfs(0, len(cuts) - 1)
+
+# 3rd solution, bottom up
+# O(k^3) time | O(k^2) space
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts = cuts + [0, n]
+        k = len(cuts)
+        dp = [[0 for _ in range(k)] for _ in range(k)]
+        cuts.sort()
+        for d in range(2, k):
+            for i in range(k - d):
+                dp[i][i+d] = min(dp[i][m] + dp[m][i + d] for m in range(i + 1, i + d)) + cuts[i + d] - cuts[i]
+        return dp[0][k - 1]
