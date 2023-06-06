48edad930bf6ca1800e459f2efc97f2acdcfb3e4
yingzhuo <yingzhuo1994@gmail.com>
2023-06-05 19:59:53 -0500

    1st solution

---
+++ 1502_CanMakeArithmeticProgressionFromSequence.py
@@ -0,0 +1,10 @@
+# 1st solution
+# O(n*log(n)) time | O(n) space
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i-1] != diff:
+                return False
+        return True
