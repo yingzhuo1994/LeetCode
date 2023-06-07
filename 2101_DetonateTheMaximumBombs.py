a44ffe55290879b9850644068bfc205ef8e8cec6
yingzhuo <yingzhuo1994@gmail.com>
2023-06-06 20:15:02 -0500

    improve

--- 2101_DetonateTheMaximumBombs.py
+++ 2101_DetonateTheMaximumBombs.py
@@ -28,27 +28,38 @@ class Solution:
         return ans
 
 # 2nd solution, bfs
+# O(n^2) time | O(n^2) space
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         n = len(bombs)
-        
-        def bfs(node, visited):
+        graph = [[] for _ in range(n)]
 
-            stack = [node]
-            visited.add(node)
-            while stack:
-                idx = stack.pop()
-                x1, y1, r1 = bombs[idx]
-                for i in range(n):
-                    if i in visited:
-                        continue
-                    x2, y2, r2 = bombs[i]
-                    if r1**2 >= (x2 - x1)**2 + (y2 - y1)**2:
-                        visited.add(i)
-                        stack.append(i)
-            return len(visited)
-        ans = 1
         for i in range(n):
-            visited = set()
-            ans = max(ans, bfs(i, visited))
+            for j in range(n):
+                if i == j:
+                    continue
+                x1, y1, r1 = bombs[i]
+                x2, y2, r2 = bombs[j]
+                if r1 * r1 >= (x2 - x1)**2 + (y2 - y1)**2:
+                    graph[i].append(j)
+        
+        def bfs(idx):
+            level = [idx]
+            visited = set([idx])
+            while level:
+                newLevel = []
+                for node in level:
+                    for child in graph[node]:
+                        if child in visited:
+                            continue
+                        visited.add(child)
+                        newLevel.append(child)
+                level = newLevel
+            
+            return len(visited)
+
+        ans = 0
+        for i in range(n):
+            ans = max(ans, bfs(i))
+        
         return ans
