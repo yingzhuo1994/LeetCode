b3eeeb4da9340981e6c7f1c12849bce18b0c4705
yingzhuo <yingzhuo1994@gmail.com>
2023-06-08 19:29:40 -0500

    1st solution

---
+++ 0744_FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,13 @@
+# 1st solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        n = len(letters)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if letters[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return letters[left] if left < n else letters[0]
