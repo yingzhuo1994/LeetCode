c019888a2215db51293763002511edcb7e724344
yingzhuo <yingzhuo1994@gmail.com>
2023-06-13 20:48:33 -0500

    2nd solution

--- 0601_HumanTrafficOfStadium.sql
+++ 0601_HumanTrafficOfStadium.sql
@@ -8,3 +8,13 @@ where S.id in (select id from Stadium where people >= 100) and
 or ((S.id + 1) in (select id from Stadium where people >= 100) and (S.id + 2) in (select id from Stadium where people >= 100))
 or ((S.id + 1) in (select id from Stadium where people >= 100) and (S.id - 1) in (select id from Stadium where people >= 100))
 )
+
+# 2nd solution
+with t as ( select t1.id , t1.visit_date , t1.people , 
+            id - row_number() OVER(ORDER BY id) as grp
+       from stadium t1
+       where people >= 100 
+          )
+select t.id, t.visit_date ,t.people
+from t 
+where grp in ( select grp from t group by grp having count(*) >=3 )
