32e9f84a9cb3e3c52baacccc2cfc3fcb304db926
yingzhuo <yingzhuo1994@gmail.com>
2023-06-14 20:02:56 -0500

    2nd solution

--- 1161_MaximumLevelSumOfABinaryTree.py
+++ 1161_MaximumLevelSumOfABinaryTree.py
@@ -30,3 +30,26 @@ class Solution:
             stack = newStack
         return level
 
+# 2nd solution, dfs
+# O(n) time | O(n) space
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        dic = {}
+
+        def dfs(node, depth):
+            if not node:
+                return
+            dic[depth] = dic.get(depth, 0) + node.val
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        
+        dfs(root, 1)
+        depth = 1
+        level = 0
+        value = float("-inf")
+        while depth in dic:
+            if dic[depth] > value:
+                value = dic[depth]
+                level = depth
+            depth += 1
+        return level
