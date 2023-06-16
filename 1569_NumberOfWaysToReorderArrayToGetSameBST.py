64f1f7c294b2979f5f5d469744e75ef54f91b37c
yingzhuo <yingzhuo1994@gmail.com>
2023-06-15 19:50:52 -0500

    1st solution

---
+++ 1569_NumberOfWaysToReorderArrayToGetSameBST.py
@@ -0,0 +1,33 @@
+# 1st solution, TLE
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        @cache
+        def helper(m, n):
+            if n <= 0 or m <= 0:
+                return 1
+            if m == 1:
+                return n + 1
+            if m > n:
+                m, n = n, m
+            ans = 0
+            for i in range(n + 1):
+                ans += helper(m - 1, n - i)
+
+            return ans
+
+        def dfs(array):
+            if len(array) <= 1:
+                return 1
+            
+            value = array[0]
+            left = [num for num in array if num < value]
+            right = [num for num in array if num > value]
+            leftCount = dfs(left)
+            rightCount = dfs(right)
+            count = leftCount * rightCount * helper(len(left), len(right)) % MOD
+            return count
+        
+        
+        return dfs(nums) - 1
