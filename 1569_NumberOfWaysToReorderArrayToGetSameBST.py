9f611ccba3ecbc693f28b6f2a758683fe6942e35
yingzhuo <yingzhuo1994@gmail.com>
2023-06-15 20:10:38 -0500

    3rd solution

--- 1569_NumberOfWaysToReorderArrayToGetSameBST.py
+++ 1569_NumberOfWaysToReorderArrayToGetSameBST.py
@@ -40,14 +40,14 @@ class Solution:
         MOD = 10**9 + 7
 
         @cache
-        def helper(m, n):
+        def f(m, n):
             if n <= 0 or m <= 0:
                 return 1
             if m == 1:
                 return n + 1
             if m > n:
                 m, n = n, m
-            return helper(m - 1, n) + helper(m, n - 1)
+            return f(m - 1, n) + f(m, n - 1)
 
         def dfs(array):
             if len(array) <= 1:
@@ -58,7 +58,33 @@ class Solution:
             leftCount = dfs(left)
             rightCount = dfs(right)
 
-            count = leftCount * rightCount * helper(len(left), len(right)) % MOD
+            count = leftCount * rightCount * f(len(left), len(right)) % MOD
+            return count
+        
+        return dfs(nums) - 1
+
+# 3rd solution
+# O(n^2 time) | O(n^2) space
+# n = len(nums)
+import math
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        def f(m, n):
+            x = m + n
+            return math.factorial(x) // (math.factorial(m) * math.factorial(n))
+
+        def dfs(array):
+            if len(array) <= 1:
+                return 1
+            value = array[0]
+            left = [num for num in array if num < value]
+            right = [num for num in array if num > value]
+            leftCount = dfs(left)
+            rightCount = dfs(right)
+
+            count = leftCount * rightCount * f(len(left), len(right)) % MOD
             return count
         
         return dfs(nums) - 1
