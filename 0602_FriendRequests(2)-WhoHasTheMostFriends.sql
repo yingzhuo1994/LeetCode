82b00c832ff3f0b6de1a9af4adc4f997c025c09a
yingzhuo <yingzhuo1994@gmail.com>
2023-06-19 19:44:30 -0500

    2nd solution

--- 0602_FriendRequests(2)-WhoHasTheMostFriends.sql
+++ 0602_FriendRequests(2)-WhoHasTheMostFriends.sql
@@ -9,3 +9,13 @@ select id, count(*) as "num" from t
 group by id
 order by count(*) desc
 limit 1
+
+# 2nd solution
+# Write your MySQL query statement below
+select id, count(*) num from 
+(
+      (select requester_id id from RequestAccepted) 
+      union all 
+      (select accepter_id id from RequestAccepted)
+) tb 
+group by id order by num desc limit 1
