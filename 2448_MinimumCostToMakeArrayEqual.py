04f2cb657295245bae993066fd50b09b99f1d078
yingzhuo <yingzhuo1994@gmail.com>
2023-06-20 20:18:18 -0500

    2nd solution

--- 2448_MinimumCostToMakeArrayEqual.py
+++ 2448_MinimumCostToMakeArrayEqual.py
@@ -31,3 +31,22 @@ class Solution:
             else:
                 break
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(1) space
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        def f(x):
+            return sum(abs(a - x) * c for a,c in zip(nums, cost))
+
+        l, r = min(nums), max(nums)
+        res = f(l)
+        while l < r:
+            x = (l + r) // 2
+            y1, y2 = f(x), f(x + 1)
+            res = min(y1, y2)
+            if y1 < y2:
+                r = x
+            else:
+                l = x + 1
+        return res
