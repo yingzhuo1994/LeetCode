2171526f635f87700417bbbb7f9a9aa565277bd7
yingzhuo <yingzhuo1994@gmail.com>
2023-06-20 20:04:59 -0500

    1st solution

---
+++ 2448_MinimumCostToMakeArrayEqual.py
@@ -0,0 +1,33 @@
+# 1st solution
+# O(n * log(n)) time | O(1) space
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        def calculate_cost(target):
+            left = [0, 0]
+            right = [0, 0]
+            for i in range(len(nums)):
+                if nums[i] < target:
+                    left[0] += (target - nums[i]) * cost[i]
+                    left[1] += cost[i]
+                elif nums[i] > target:
+                    right[0] += (nums[i] - target) * cost[i]
+                    right[1] += cost[i]
+            
+            return left, right
+
+        start = min(nums)
+        end = max(nums)
+
+        ans = float("inf")
+        while start <= end:
+            mid = start + (end - start) // 2
+            left, right = calculate_cost(mid)
+            totalCost = left[0] + right[0]
+            ans = min(ans, totalCost)
+            if left[1] > right[1]:
+                end = mid - 1
+            elif left[1] < right[1]:
+                start = mid + 1
+            else:
+                break
+        return ans
