f8199c003b6f3d30fc8123301e85969bc7a17986
yingzhuo <yingzhuo1994@gmail.com>
2023-06-21 20:13:04 -0500

    3rd solution

--- 0714_BestTimeToBuyAndSellStockWithTransactionFee.py
+++ 0714_BestTimeToBuyAndSellStockWithTransactionFee.py
@@ -38,3 +38,26 @@ class Solution:
             sell[i] = max(sell[i - 1], buy[i - 1] + prices[i] - fee)
         
         return sell[n - 1]
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n <= 1:
+            return 0
+
+        buy = [0, 0]
+        sell = [0, 0]
+        buy[0]= -prices[0]
+
+        for i in range(1, n):
+            # keep the same as day i-1, or buy from sell status at day i-1
+            buy[1] = max(buy[0], sell[0] - prices[i])
+            # keep the same as day i-1, or sell from buy status at day i-1
+            sell[1] = max(sell[0], buy[0] + prices[i] - fee)
+            
+            buy[0] = buy[1]
+            sell[0] = sell[1]
+        
+        return sell[1]
