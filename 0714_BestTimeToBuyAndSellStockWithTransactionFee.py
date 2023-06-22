b511782c6b569e8160b3d57bbe17416163af2141
yingzhuo <yingzhuo1994@gmail.com>
2023-06-21 20:10:53 -0500

    2nd solution

--- 0714_BestTimeToBuyAndSellStockWithTransactionFee.py
+++ 0714_BestTimeToBuyAndSellStockWithTransactionFee.py
@@ -10,15 +10,31 @@ class Solution:
             if idx >= n:
                 return 0
             ans = 0
-            for i in range(idx + 1, n):
-                profit = prices[i] - prices[idx] - fee
-                new = max([dfs(j) for j in range(i + 1, n)] + [0])
-                ans = max(ans, profit + new)
+            new = max([dfs(i) for i in range(idx + 1, n)] + [prices[i] - prices[idx] - fee + dfs(i) for i in range(idx + 1, n)] + [0])
+            ans = max(ans, new)
             memo[idx] = ans
             return ans
         
-        ans = 0
-        for i in range(n):
-            ans = max(ans, dfs(i))
+        ans = dfs(0)
         print(sorted(list(memo.items())))
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n <= 1:
+            return 0
+
+        buy = [0] * n
+        sell = [0] * n
+        buy[0]= -prices[0]
+
+        for i in range(1, n):
+            # keep the same as day i-1, or buy from sell status at day i-1
+            buy[i] = max(buy[i - 1], sell[i - 1] - prices[i])
+            # keep the same as day i-1, or sell from buy status at day i-1
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i] - fee)
+        
+        return sell[n - 1]
