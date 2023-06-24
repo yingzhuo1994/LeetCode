39c1f2c1e1a4ef572a687daea1ef0e7476620223
yingzhuo <yingzhuo1994@gmail.com>
2023-06-23 21:03:57 -0500

    2nd solution

--- 0956_TallestBillboard.py
+++ 0956_TallestBillboard.py
@@ -45,3 +45,13 @@ class Solution:
                         # print(lst2, sum(lst2))
                         return value
         return 0
+
+# 2nd solution
+class Solution:
+    def tallestBillboard(self, rods: List[int]) -> int:        
+        dp = {0: 0}
+        for x in rods:
+            for d, y in list(dp.items()):
+                dp[d + x] = max(dp.get(x + d, 0), y)
+                dp[abs(d - x)] = max(dp.get(abs(d - x), 0), y + min(d, x))
+        return dp[0]
