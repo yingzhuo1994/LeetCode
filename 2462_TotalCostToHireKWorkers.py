fa845e8a8cae6a65fb2670357bd32e4beb1c121e
yingzhuo <yingzhuo1994@gmail.com>
2023-06-25 20:07:15 -0500

    2nd solution

--- 2462_TotalCostToHireKWorkers.py
+++ 2462_TotalCostToHireKWorkers.py
@@ -21,3 +21,53 @@ class Solution:
             k -= 1
             costs = costs[:idx] + costs[idx+1:]
         return ans
+
+# 2nd solution
+# O((k + m)* log(m) + n) time | O(n) space
+# n = len(costs), m = candidates
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        frontHeap = []
+        backHeap = []
+        n = len(costs)
+        visited = [False] * n
+        
+        for i in range(candidates):
+            heappush(frontHeap, [costs[i], i])
+            heappush(backHeap, [costs[n-1-i], n-1-i])
+
+        left = candidates
+        right = n - 1 - candidates
+        ans = 0
+
+        while k > 0:
+            if frontHeap[0][0] < backHeap[0][0]:
+                value, idx = frontHeap[0]
+            elif frontHeap[0][0] > backHeap[0][0]:
+                value, idx = backHeap[0]
+            else:
+                if frontHeap[0][1] <= backHeap[0][1]:
+                    value, idx = frontHeap[0]
+                else:
+                    value, idx = backHeap[0]
+            ans += value
+            visited[idx] = True
+            while frontHeap and visited[frontHeap[0][1]]:
+                heappop(frontHeap)
+            while backHeap and visited[backHeap[0][1]]:
+                heappop(backHeap)
+            if idx < left:
+                while left < n and visited[left]:
+                    left += 1
+                if left < n:
+                    heappush(frontHeap, [costs[left], left])
+                    left += 1
+            if idx > right:
+                while right >= 0 and visited[right]:
+                    right -= 1
+                if right >= 0:
+                    heappush(backHeap, [costs[right], right])
+                    right -= 1
+            k -= 1
+        
+        return ans
