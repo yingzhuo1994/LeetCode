dface0b78b072db4086b89645ac44ed5255f6866
yingzhuo <yingzhuo1994@gmail.com>
2023-07-06 20:02:00 -0500

    3rd solution

--- 2024_MaximizeTheConfusionOfAnExam.py
+++ 2024_MaximizeTheConfusionOfAnExam.py
@@ -45,7 +45,7 @@ class Solution:
         return max(T, F)
 
 # 2nd solution
-# O(n) time | O(n) space
+# O(n) time | O(1) space
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         s = answerKey
@@ -59,3 +59,18 @@ class Solution:
             else:
                 count[s[i - res]] -= 1
         return res
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        s = answerKey
+        maxf = i = 0
+        count = collections.Counter()
+        for j in range(len(s)):
+            count[s[j]] += 1
+            maxf = max(maxf, count[s[j]])
+            if j - i + 1 > maxf + k:
+                count[s[i]] -= 1
+                i += 1
+        return len(s) - i
