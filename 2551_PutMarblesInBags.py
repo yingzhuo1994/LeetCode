eb2f5bb9ac41494de45d0e7cc4a708a878371ee3
yingzhuo <yingzhuo1994@gmail.com>
2023-07-07 19:52:24 -0500

    2nd solution

--- 2551_PutMarblesInBags.py
+++ 2551_PutMarblesInBags.py
@@ -22,3 +22,22 @@ class Solution:
         
         minCost, maxCost = dfs(0, n - 1, k - 1)
         return maxCost - minCost
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        # We collect and sort the value of all n - 1 pairs.
+        n = len(weights)
+        pair_weights = [0] * (n - 1)
+        for i in range(n - 1):
+            pair_weights[i] = weights[i] + weights[i + 1]
+        pair_weights.sort()
+        
+        # Get the difference between the largest k - 1 values and the 
+        # smallest k - 1 values.
+        answer = 0
+        for i in range(k - 1):
+            answer += pair_weights[n - 2 - i] - pair_weights[i]
+            
+        return answer
