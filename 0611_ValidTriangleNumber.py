4f909cafd1cbfba7f5a1b0360f8d7c3ddc2aec2a
yingzhuo <yingzhuo1994@gmail.com>
2023-07-09 20:19:09 -0500

    3rd solution

--- 0611_ValidTriangleNumber.py
+++ 0611_ValidTriangleNumber.py
@@ -48,3 +48,20 @@ class Solution:
                 ans += val
         
         return ans
+
+# 3rd solution
+# O(n^2) time, O(n) space
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        nums.sort()
+        for i in range(2, n):
+            left, right = 0, i-1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += (right - left)
+                    right -= 1
+                else:
+                    left += 1
+        return count
