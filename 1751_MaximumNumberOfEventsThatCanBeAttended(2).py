508c58ef316e61f826ec4118e3367d66e36bc135
yingzhuo <yingzhuo1994@gmail.com>
2023-07-14 19:46:28 -0500

    2nd solution

--- 1751_MaximumNumberOfEventsThatCanBeAttended(2).py
+++ 1751_MaximumNumberOfEventsThatCanBeAttended(2).py
@@ -17,3 +17,25 @@ class Solution:
                 ans = max(ans, dp[run][end])
 
         return ans
+
+# 2nd solution
+# O(kn * log(n)) time | O(n) space
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort(key=lambda v: [v[1], -v[2], v[0]])
+        ends = set([v[1] for v in events])
+        ends = [0] + sorted(list(ends))
+        ans = 0
+
+        dp = {end: 0 for end in ends}
+        for run in range(1, k + 1):
+            newDp = {end: 0 for end in ends}
+            for i, (start, end, value) in enumerate(events):
+                idx = bisect.bisect_left(ends, start)
+                newDp[end] = max(newDp[end], dp[ends[idx - 1]] + value)
+                if i > 0:
+                    newDp[end] = max(newDp[end], newDp[events[i-1][1]])
+                ans = max(ans, newDp[end])
+            dp = newDp
+
+        return ans
