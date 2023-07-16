c9002af348f92cfc8133951b52f47cee892fe021
yingzhuo <yingzhuo1994@gmail.com>
2023-07-15 20:36:45 -0500

    2nd solution

--- 1125_SmallestSufficientTeam.py
+++ 1125_SmallestSufficientTeam.py
@@ -38,5 +38,22 @@ class Solution:
                 left = mid + 1
         return [new_people[idx][0] for idx in ans]
         
-
-
+# 2nd solution
+# O(m * 2^n) time | O(2^n) space
+# where n = len(req_skills), m = len(people)
+class Solution:
+    def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
+        n, m = len(req_skills), len(people)
+        skill_index = {v: i for i, v in enumerate(req_skills)}
+        dp = {0: []}
+        for i, p in enumerate(people):
+            cur_skill = 0
+            for skill in p:
+                if skill in skill_index:
+                    cur_skill |= 1 << skill_index[skill]
+            for prev, need in dict(dp).items():
+                comb = prev | cur_skill
+                if comb == prev: continue
+                if comb not in dp or len(dp[comb]) > len(need) + 1:
+                    dp[comb] = need + [i]
+        return dp[(1 << n) - 1]
