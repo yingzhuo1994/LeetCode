9aebcef57858c11e88f26a0fc994cd0d1849e26d
yingzhuo <yingzhuo1994@gmail.com>
2023-07-17 21:07:07 -0500

    1st solution

---
+++ 0621_TaskScheduler.py
@@ -0,0 +1,8 @@
+# 1st solution
+# O(k) time | O(1) space
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = Counter(tasks)
+        Most_freq = freq.most_common()[0][1]
+        Found_most = sum([freq[key] == Most_freq for key in freq])
+        return max(len(tasks), (Most_freq - 1) * (n + 1) + Found_most)
