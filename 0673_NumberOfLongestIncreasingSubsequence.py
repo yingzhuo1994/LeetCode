452f9b9247d48a84142a342b35d1cc0b26c58afb
yingzhuo <yingzhuo1994@gmail.com>
2023-07-20 20:04:13 -0500

    2nd solution

--- 0673_NumberOfLongestIncreasingSubsequence.py
+++ 0673_NumberOfLongestIncreasingSubsequence.py
@@ -19,3 +19,29 @@ class Solution:
         
         maxLnegth = max(dic.keys())
         return dic[maxLnegth]
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space 
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+    
+        decks, ends_decks, paths = [], [], []
+        for num in nums:
+            deck_idx = bisect.bisect_left(ends_decks, num)
+            n_paths = 1
+            if deck_idx > 0:
+                l = bisect.bisect(decks[deck_idx-1], -num)
+                n_paths = paths[deck_idx-1][-1] - paths[deck_idx-1][l]
+                
+            if deck_idx == len(decks):
+                decks.append([-num])
+                ends_decks.append(num)
+                paths.append([0, n_paths])
+            else:
+                decks[deck_idx].append(-num)
+                ends_decks[deck_idx] = num
+                paths[deck_idx].append(n_paths + paths[deck_idx][-1])
+              
+        return paths[-1][-1]
