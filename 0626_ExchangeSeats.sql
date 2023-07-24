7db0fb6a0924cdd56b8729fe7c52d8e580dc165f
yingzhuo <yingzhuo1994@gmail.com>
2023-07-23 20:02:30 -0500

    2nd solution

--- 0626_ExchangeSeats.sql
+++ 0626_ExchangeSeats.sql
@@ -7,3 +7,13 @@ end as newid, student from Seat)
 
 select ROW_NUMBER() OVER() as id, student from t
 order by newid
+
+# 2nd solution
+select  
+      case 
+        when id % 2 = 0 then id - 1
+        when id % 2 = 1 and id < (select count(*) from seat)then id + 1
+        else id
+      end as id, 
+    student from seat
+    order by id;
