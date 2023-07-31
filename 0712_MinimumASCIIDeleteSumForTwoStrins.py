62926bee65be3c28db15301c3784acadbccce9ce
yingzhuo <yingzhuo1994@gmail.com>
2023-07-30 20:04:32 -0500

    2nd solution

--- 0712_MinimumASCIIDeleteSumForTwoStrins.py
+++ 0712_MinimumASCIIDeleteSumForTwoStrins.py
@@ -14,3 +14,24 @@ class Solution:
             ans = min(ans, self.minimumDeleteSum(s1[1:], s2[1:]))
         
         return ans
+
+# 2nd solution
+# O(n1 * n2) time | O(n1 * n2) space
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        @cache
+        def helper(i, j):
+            if i == len(s1) and j == len(s2):
+                return 0
+            elif i == len(s1):
+                return sum(ord(s2[k]) for k in range(j, len(s2)))
+            elif j == len(s2):
+                return sum(ord(s1[k]) for k in range(i, len(s1)))
+            else:
+                if s1[i] == s2[j]:
+                    return helper(i + 1, j + 1)
+                else:
+                    left = ord(s1[i]) + helper(i + 1, j)
+                    right = ord(s2[j]) + helper(i, j + 1)
+                    return min(left, right)
+        return helper(0, 0)
