11873f2869f236676bdabdbdb6ebfd2f5544f212
yingzhuo <yingzhuo1994@gmail.com>
2023-08-01 20:04:42 -0500

    update

--- 0046_Permutations.py
+++ 0046_Permutations.py
@@ -5,12 +5,12 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums) == 1:
             return [nums]
-        lst = []
+        ans = []
         for i in range(len(nums)):
             newNums = nums[:i] + nums[i+1:]
-            curLst = [[nums[i]] + k for k in self.permute(newNums)]
-            lst.extend(curLst)
-        return lst
+            curLst = [[nums[i]] + lst for lst in self.permute(newNums)]
+            ans.extend(curLst)
+        return ans
 
 # 2nd time
 # O(n*n!) time | O(n*n!) space
