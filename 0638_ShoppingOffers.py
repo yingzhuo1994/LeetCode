d21f632333bdbac98f64b0e7cf91589c603b30ab
yingzhuo1994 <zhuoying1994@gmail.com>
2023-08-03 18:20:23 -0500

    2nd solution

--- 0638_ShoppingOffers.py
+++ 0638_ShoppingOffers.py
@@ -22,4 +22,26 @@ class Solution:
         
         ans = dfs(tuple(needs))
 
-        return ans 
+        return ans
+
+# 2nd solution
+# O(10^n) time | O(10^n) space
+class Solution:
+    def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
+        n = len(price)
+
+        @cache
+        def dfs(keys):
+            if all(key == 0 for key in keys):
+                return 0
+            ans = float("inf")
+            for lst in special:
+                p = lst[-1]
+                if all(keys[i] >= lst[i] for i in range(n)):
+                    ans = min(ans, p + dfs(tuple([keys[i] - lst[i] for i in range(n)])))
+            ans = min(ans, sum([price[i] * keys[i] for i in range(n)]))
+            return ans
+        
+        ans = dfs(tuple(needs))
+
+        return ans
