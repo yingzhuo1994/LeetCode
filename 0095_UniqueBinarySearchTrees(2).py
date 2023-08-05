0c49f36f521a85373609dd1a4fcca57902068f2e
yingzhuo1994 <zhuoying1994@gmail.com>
2023-08-05 09:34:01 -0500

    2nd solution

--- 0095_UniqueBinarySearchTrees(2).py
+++ 0095_UniqueBinarySearchTrees(2).py
@@ -33,3 +33,16 @@ class Solution:
         return buildTree(1, n)
 
 # 2nd solution
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        @cache
+        def getTreeList(nodes):
+            if not nodes:
+                return [None]
+            res = []
+            for i, val in enumerate(nodes):
+                for leftTree, rightTree in product(getTreeList(nodes[:i]), getTreeList(nodes[i+1:])):
+                    res += [ TreeNode(val, left = leftTree, right = rightTree) ]
+            return res
+        
+        return getTreeList(tuple(range(1, n+1)))
