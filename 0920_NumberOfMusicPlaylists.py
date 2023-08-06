7d3315e7297baf3aa892a6aaed05741984dd6678
yingzhuo1994 <zhuoying1994@gmail.com>
2023-08-05 19:33:02 -0500

    update

--- 0920_NumberOfMusicPlaylists.py
+++ 0920_NumberOfMusicPlaylists.py
@@ -2,6 +2,7 @@
 # O((L-K)(N-K)) time | O(LN) space
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        MOD = 10**9 + 7
         dp = [[0 for i in range(goal + 1)] for j in range(n + 1)]
 
         for i in range(k + 1, n + 1):
@@ -11,4 +12,4 @@ class Solution:
                 else:
                     dp[i][j] = dp[i - 1][j - 1] * i + dp[i][j - 1] * (i - k)
         
-        return dp[n][goal] % (10**9 + 7)
+        return dp[n][goal] % MOD
