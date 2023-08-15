ae6b46ff1792749b0e0299767f1c297158e10056
yingzhuo <yingzhuo1994@gmail.com>
2023-08-14 21:08:37 -0500

    2nd solution

--- 0671_SecondMinimumNodeInABinaryTree.py
+++ 0671_SecondMinimumNodeInABinaryTree.py
@@ -22,3 +22,23 @@ class Solution:
         if self.ans == float("inf"):
             return -1
         return self.ans
+
+# 2nd solution
+# O(n) time | O(h) space
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        self.ans = float("inf")
+        minVal = root.val
+        def dfs(node):
+            if not node:
+                return
+            if node.val > self.ans:
+                return
+            if node.val != minVal:
+                self.ans = min(self.ans, node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        if self.ans == float("inf"):
+            return -1
+        return self.ans
