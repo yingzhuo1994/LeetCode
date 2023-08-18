429ad177c78d11fce06870e11c9cd5032f5a3b69
yingzhuo1994 <zhuoying1994@gmail.com>
2023-08-17 19:48:53 -0500

    2nd solution

--- 1615_MaximalNetworkRank.py
+++ 1615_MaximalNetworkRank.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n^2) time | O(n^2) space
+# O(n^2) time | O(n + len(roads)) space
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
         graph = [[] for _ in range(n)]
@@ -17,3 +17,23 @@ class Solution:
                 ans = max(ans, len(neig1) + len(neig2))
         
         return ans
+    
+# 2nd solution
+# O(n^2) time | O(n + len(roads)) space
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        degree = [0] * n
+        for a, b in roads:
+            degree[a] += 1
+            degree[b] += 1
+        
+        road_set = set(tuple(road) for road in roads)
+        max_rank = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                rank = degree[i] + degree[j]
+                if (i, j) in road_set or (j, i) in road_set:
+                    rank -= 1
+                max_rank = max(max_rank, rank)
+        
+        return max_rank
