fcdfc90ad5bda9d5f08ede5ab97fb6a1a8e02507
yingzhuo <yingzhuo1994@gmail.com>
2023-08-23 21:09:07 -0500

    2nd solution

--- 0646_MaximumLengthOfPairChain.py
+++ 0646_MaximumLengthOfPairChain.py
@@ -3,7 +3,7 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         n = len(pairs)
-        pairs.sort(key = lambda p: [p[1], [0]])
+        pairs.sort(key = lambda p: [p[1], p[0]])
         ans = [[None, 1] for _ in range(n)]
         for i in range(1, n):
             for j in range(i):
@@ -11,3 +11,14 @@ class Solution:
                     ans[i] = [j, ans[j][1] + 1]
         
         return max([v[1] for v in ans])
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort(key=lambda x: x[1])
+        cur, res = float('-inf'), 0
+        for p in pairs:
+            if cur < p[0]: 
+                cur, res = p[1], res + 1
+        return res
