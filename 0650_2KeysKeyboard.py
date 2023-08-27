5024016814d0461aec439a346debaa1ec172d5e6
yingzhuo <yingzhuo1994@gmail.com>
2023-08-26 21:04:11 -0500

    2nd solution

--- 0650_2KeysKeyboard.py
+++ 0650_2KeysKeyboard.py
@@ -15,3 +15,23 @@ class Solution:
                     newStack.append([left, n - left])
             ans += 1
             stack = newStack
+
+# 2nd solution
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        @cache
+        def dfs(left, k):
+            if left == 0:
+                return 0
+            elif left == k:
+                return 1
+            elif left < k:
+                return float("inf")
+            ans = dfs(left - k, k)
+            if n - left > k:
+                ans = min(ans, dfs(left, n - left))
+            return ans + 1
+        
+        return dfs(n, 1)
