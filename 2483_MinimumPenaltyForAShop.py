ee2534aafc4cdaa30ea8dd5cf57e04539b99461d
yingzhuo <yingzhuo1994@gmail.com>
2023-08-28 20:43:02 -0500

    3rd solution

--- 2483_MinimumPenaltyForAShop.py
+++ 2483_MinimumPenaltyForAShop.py
@@ -43,3 +43,15 @@ class Solution:
                 count += 1
         
         return close
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        h = m = s = 0
+        for i, ch in enumerate(customers):  # [1] compute running profit where
+            s += (ch == "Y") * 2 - 1        #     we add +1 for Y, -1 for N
+            if s > m:                       # [2] keep track of the maximal 
+                m, h = s, i+1               #     profit and its hour
+        
+        return h                            # [3] this is the hour with minimal penalty
