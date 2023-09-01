4306ed6a6d7896e971260189bbec5c7e57f5ee3a
yingzhuo <yingzhuo1994@gmail.com>
2023-08-31 20:00:18 -0500

    4th solution

--- 0338_CountingBits.py
+++ 0338_CountingBits.py
@@ -42,3 +42,12 @@ class Solution:
             result[index] = result[index - offset] + 1
         
         return result
+
+# 4th solution
+# O(n) time | O(n) space
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0 for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + i % 2
+        return ans
