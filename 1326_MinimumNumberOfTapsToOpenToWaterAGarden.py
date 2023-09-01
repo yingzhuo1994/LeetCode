ced77eafa6c8bb4973db31d6d148d0f6e4f40742
yingzhuo <yingzhuo1994@gmail.com>
2023-08-31 20:53:08 -0500

    3rd solution

--- 1326_MinimumNumberOfTapsToOpenToWaterAGarden.py
+++ 1326_MinimumNumberOfTapsToOpenToWaterAGarden.py
@@ -33,3 +33,24 @@ class Solution:
                 dp[j] = min(dp[j], dp[max(0, i - x)] + 1)
 
         return dp[n] if dp[n] < n + 2 else -1
+
+# 3rd solution
+# O(n) time | O(n) space
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        max_range = [0] * (n + 1)
+        
+        for i, r in enumerate(ranges):
+            left, right = max(0, i - r), min(n, i + r)
+            max_range[left] = max(max_range[left], right - left)
+        
+		# it's a jump game now
+        start = end = step = 0
+        
+        while end < n:
+            step += 1
+            start, end = end, max(i + max_range[i] for i in range(start, end + 1))
+            if start == end:
+                return -1
+            
+        return step
