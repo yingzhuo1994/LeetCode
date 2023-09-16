5fbda40c19ce0ba7440d8cdb0e2b9e847bfe69c8
yingzhuo <yingzhuo1994@gmail.com>
2023-09-15 20:48:03 -0500

    modify

--- 1631_PathWithMinimumEffort.py
+++ 1631_PathWithMinimumEffort.py
@@ -28,19 +28,22 @@ class Solution:
         m, n = len(heights), len(heights[0])
         neibs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         
-        def dfs(LIMIT, x, y):
+        def dfs(LIMIT, x, y, visited):
             visited.add((x, y)) 
             for dx, dy in neibs:
-                if 0<=dx+x<m and 0<=dy+y<n and (dx+x, dy+y) not in visited:
-                    if abs(heights[x][y] - heights[dx+x][dy+y]) <= LIMIT:
-                        dfs(LIMIT, dx+x, dy+y)
+                i = x + dx
+                j = y + dy
+                if 0 <= i < m and 0 <= j < n and (i, j) not in visited:
+                    if abs(heights[x][y] - heights[i][j]) <= LIMIT:
+                        if dfs(LIMIT, i, j, visited):
+                            return True
+            return (m - 1, n - 1) in visited
         
         beg, end = -1, max(max(heights, key=max))
         while beg + 1 < end:
-            mid = (beg + end)//2
+            mid = (beg + end) // 2
             visited = set()
-            dfs(mid, 0, 0)
-            if (m - 1, n - 1) in visited:
+            if dfs(mid, 0, 0, visited):
                 end = mid
             else:
                 beg = mid
