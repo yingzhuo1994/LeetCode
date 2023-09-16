6443de16c2ce4e9621f2e45fabc474f1e47aace8
yingzhuo <yingzhuo1994@gmail.com>
2023-09-15 20:55:21 -0500

    3rd solution

--- 1631_PathWithMinimumEffort.py
+++ 1631_PathWithMinimumEffort.py
@@ -49,3 +49,28 @@ class Solution:
                 beg = mid
                 
         return end
+
+# 3rd solution, Dijikstra
+# O(ElogV) = O(mn * log(mn)) time | O(mn) space
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        dist = [[math.inf] * n for _ in range(m)]
+        dist[0][0] = 0
+        minHeap = [(0, 0, 0)] # distance, row, col
+        DIR = [0, 1, 0, -1, 0]
+
+        while minHeap:
+            d, r, c = heappop(minHeap)
+            if d > dist[r][c]: 
+                continue  # this is an outdated version -> skip it
+            if r == m - 1 and c == n - 1:
+                return d  # Reach to bottom right
+            
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i+1]
+                if 0 <= nr < m and 0 <= nc < n:
+                    newDist = max(d, abs(heights[nr][nc] - heights[r][c]))
+                    if dist[nr][nc] > newDist:
+                        dist[nr][nc] = newDist
+                        heappush(minHeap, (dist[nr][nc], nr, nc))
