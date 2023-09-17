3fa9d8dfde877b07d3eb8923e2adff88116631ca
yingzhuo <yingzhuo1994@gmail.com>
2023-09-16 20:00:59 -0500

    modify

--- 0847_ShortestPathVisitingAllNodes.py
+++ 0847_ShortestPathVisitingAllNodes.py
@@ -30,10 +30,11 @@ class Solution:
 # O(2^n * n^2) time | O(2^n * n) space
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        if len(graph) == 1:
+        n = len(graph)
+
+        if n == 1:
             return 0
         
-        n = len(graph)
         ending_mask = (1 << n) - 1
         queue = [(node, 1 << node) for node in range(n)]
         seen = set(queue)
