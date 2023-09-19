1d7ccc1d29c9be16a74569d6d0d2eb3290c6afb0
yingzhuo <yingzhuo1994@gmail.com>
2023-09-18 22:12:54 -0500

    2nd solution

--- 2560_HouseRobber(4).py
+++ 2560_HouseRobber(4).py
@@ -14,3 +14,19 @@ class Solution:
 
 
         return min(dp[k][1+2*(k-1):-1])
+
+# 2nd solution
+# O(kn) time | O(kn) space
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp = [[float("inf") for _ in range(n + 2)] for _ in range(k + 1)]
+        dp[0] = [0 for _ in range(n + 2)]
+
+        for i in range(1, k + 1):
+            prev = float("inf")
+            for j in range(1 + 2 * (i - 1), n + 1):
+                dp[i][j] = min(max(dp[i-1][j-2], nums[j - 1]), prev)
+                prev = dp[i][j]
+        
+        return dp[k][n]
