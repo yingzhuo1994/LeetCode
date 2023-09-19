b4ef8339d0014067f9b8e26323ff8ccf09979bf7
yingzhuo <yingzhuo1994@gmail.com>
2023-09-18 21:24:07 -0500

    1st solution

--- 0287_FindTheDuplicateNumber.py
+++ 0287_FindTheDuplicateNumber.py
@@ -79,3 +79,14 @@ class Solution:
                 duplicate |= mask
                 
         return duplicate
+
+# 6th solution
+# O(n) time | O(1) space
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        mask = 0
+        for num in nums:
+            key = 1 << num
+            if mask & key:
+                return num
+            mask |= key
