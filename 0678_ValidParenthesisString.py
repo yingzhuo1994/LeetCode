b6372a5f0a9368682213e6504298169a4c5a981e
yingzhuo <yingzhuo1994@gmail.com>
2023-09-24 20:02:04 -0500

    2nd solution

--- 0678_ValidParenthesisString.py
+++ 0678_ValidParenthesisString.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(3^n) time | O(3^n) space
+# O(n^#) time | O(n^2) space
 class Solution:
     def checkValidString(self, s: str) -> bool:
         left = s.count("(")
@@ -22,3 +22,24 @@ class Solution:
                 return dfs(idx + 1, total + 1) or dfs(idx + 1, total - 1) or dfs(idx + 1, total)
         
         return dfs(0, 0)
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        cmin = cmax = 0
+        for ch in s:
+            if ch == "(":
+                cmin += 1
+                cmax += 1
+            elif ch == ")":
+                cmax -= 1
+                cmin = max(cmin - 1, 0)
+            else:
+                cmax += 1
+                cmin = max(cmin - 1, 0)
+            
+            if cmax < 0:
+                return False
+        
+        return cmin == 0
