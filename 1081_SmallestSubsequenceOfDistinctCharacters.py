4c4229e2ce56023c6f9616d934b0354c9e2b68c9
yingzhuo <yingzhuo1994@gmail.com>
2023-09-25 21:09:05 -0500

    1st solution

---
+++ 1081_SmallestSubsequenceOfDistinctCharacters.py
@@ -0,0 +1,17 @@
+# 1st solution
+# O(n) time | O(26) space
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        last_occ = {c: i for i, c in enumerate(s)}
+        stack = ["!"]
+        Visited = set()
+        
+        for i, symbol in enumerate(s):
+            if symbol in Visited: continue
+            
+            while (symbol < stack[-1] and last_occ[stack[-1]] > i):
+                Visited.remove(stack.pop())
+           
+            stack.append(symbol)
+            Visited.add(symbol)        
+        return "".join(stack)[1:]
