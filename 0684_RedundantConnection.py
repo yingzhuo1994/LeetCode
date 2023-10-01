ab552b7bf4bc9a5f737ee9b2ba9ddb3e7af30f95
yingzhuo <yingzhuo1994@gmail.com>
2023-09-30 20:11:42 -0500

    2nd solution

--- 0684_RedundantConnection.py
+++ 0684_RedundantConnection.py
@@ -41,4 +41,35 @@ class Solution:
         
         return edges[ans]
 
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = [-1] * (len(edges) + 1)
+        rank = [0] * (len(edges) + 1)
 
+        def find(x):
+            if parent[x] == -1:
+                return x
+            parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            root_x = find(x)
+            root_y = find(y)
+            if root_x == root_y:
+                return False
+            elif rank[root_x] < rank[root_y]:
+                parent[root_x] = root_y
+                rank[root_y] += 1
+                return True
+            else:
+                parent[root_y] = root_x
+                rank[root_x] += 1
+                return True
+
+        for x, y in edges:
+            if not union(x, y): 
+                return [x, y]
+        
+        raise ValueError("Illegal input.")
