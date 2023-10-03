e25d32a67d3f2386473f6d514fe76b3834cbe0ca
yingzhuo <yingzhuo1994@gmail.com>
2023-10-02 20:00:27 -0500

    2nd solution

--- 0675_CutOffTreesForGolfEvent.py
+++ 0675_CutOffTreesForGolfEvent.py
@@ -1,4 +1,4 @@
-# 1st solution
+# 1st solution, BFS
 # O((mn)^2) time | O((mn)^2) space
 class Solution:
     def cutOffTree(self, forest: List[List[int]]) -> int:
@@ -43,3 +43,33 @@ class Solution:
             last = [i, j]
         
         return cost
+
+# 2nd solution, A star
+# O((mn)^2) time | O((mn)^2) space
+class Solution:
+    def cutOffTree(self, forest: List[List[int]]) -> int:
+        def astar(forest, sr, sc, tr, tc):
+            R, C = len(forest), len(forest[0])
+            heap = [(0, 0, sr, sc)]
+            cost = {(sr, sc): 0}
+            while heap:
+                f, g, r, c = heapq.heappop(heap)
+                if r == tr and c == tc: 
+                    return g
+                for nr, nc in ((r-1,c), (r+1,c), (r,c-1), (r,c+1)):
+                    if 0 <= nr < R and 0 <= nc < C and forest[nr][nc]:
+                        ncost = g + 1 + abs(nr - tr) + abs(nc - tc)
+                        if ncost < cost.get((nr, nc), 9999):
+                            cost[nr, nc] = ncost
+                            heapq.heappush(heap, (ncost, g+1, nr, nc))
+            return -1
+        
+        trees = sorted((v, r, c) for r, row in enumerate(forest)
+                       for c, v in enumerate(row) if v > 1)
+        sr = sc = ans = 0
+        for _, tr, tc in trees:
+            d = astar(forest, sr, sc, tr, tc)
+            if d < 0: return -1
+            ans += d
+            sr, sc = tr, tc
+        return ans
