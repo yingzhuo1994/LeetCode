d88ec99c59c58487e06477e003fca40944d7a25a
yingzhuo <yingzhuo1994@gmail.com>
2023-10-04 19:56:04 -0500

    update

--- 0229_MajorityElement(2).py
+++ 0229_MajorityElement(2).py
@@ -29,5 +29,5 @@ class Solution:
                 candidate2, count2 = n, 1
             else:
                 count1, count2 = count1 - 1, count2 - 1
-        return [n for n in (candidate1, candidate2)
-                        if nums.count(n) > len(nums) // 3]
+        return [num for num in (candidate1, candidate2)
+                        if nums.count(num) > len(nums) // 3]
