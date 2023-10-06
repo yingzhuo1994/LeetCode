adfc273e8115e14917300bfd3a967d2b1d999d0b
yingzhuo <yingzhuo1994@gmail.com>
2023-10-05 20:10:33 -0500

    2nd solution

--- 0685_RedundantConnection(2).py
+++ 0685_RedundantConnection(2).py
@@ -58,3 +58,44 @@ class Solution:
         for a, b in reversed(edges):
             if b == ansNode:
                 return [a, b]
+
+# 2nd solution
+class Solution:
+    def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
+        def find(ds, i):
+            if ds[i] == 0:
+                return i
+            else:
+                ds[i] = find(ds, ds[i])
+                return ds[i]
+
+        n = len(edges)
+        parent = [-1 for _ in range(n + 1)]
+        ds = [0 for _ in range(n + 1)]
+
+        first = -1
+        second = -1
+        last = -1
+
+        for i in range(n):
+            p, c = edges[i]
+            if parent[c] != -1:
+                first = parent[c]
+                second = i
+                continue
+
+            parent[c] = i
+            
+            p1 = find(ds, p)
+
+            if p1 == c:
+                last = i
+            else:
+                ds[c] = p1
+
+        if last == -1:
+            return edges[second] # no cycle found by removing second
+        if second == -1:
+            return edges[last] # no edge removed
+    
+        return edges[first]
