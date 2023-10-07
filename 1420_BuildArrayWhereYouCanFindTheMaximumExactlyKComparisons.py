bce9838f4a33c05bfcbb5c1f0c1c57ae08cf3c00
yingzhuo <yingzhuo1994@gmail.com>
2023-10-06 20:20:16 -0500

    2nd solution

--- 1420_BuildArrayWhereYouCanFindTheMaximumExactlyKComparisons.py
+++ 1420_BuildArrayWhereYouCanFindTheMaximumExactlyKComparisons.py
@@ -1,5 +1,5 @@
-# 1st solution
-# O(mn) time | O(mn) space
+# 1st solution, Top-Down DP
+# O(mnk) time | O(mnk) space
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
         MOD = 10**9 + 7
@@ -22,3 +22,23 @@ class Solution:
             ans += dp(n, num, k)
         
         return ans % MOD
+
+# 2nd solution, Bottom-Up DP
+# O(mnk) time | O(mnk) space
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        if k == 0 or k > m:
+            return 0
+        MOD = 10**9 + 7
+        dp = [[[0 for _ in range(m + 1)] for _ in range(k + 1)] for _ in range(n + 1)]
+        # the array has length of 1, and 1 jump, only 1 way to do that, for any k
+        for maxV in range(1, m + 1):
+            dp[1][1][maxV] = 1
+        
+        for i in range(1, n + 1):
+            for j in range(1, k + 1):
+                for maxV in range(m + 1):
+                    dp[i][j][maxV] += dp[i - 1][j][maxV] * maxV
+                    dp[i][j][maxV] += sum(dp[i - 1][j - 1][1:maxV])
+        
+        return sum(dp[n][k][1:]) % MOD
