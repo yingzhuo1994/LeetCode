3f3789b5f3b7e64adb7a2610de69dc0439aa934f
yingzhuo <yingzhuo1994@gmail.com>
2023-10-07 22:01:55 -0500

    2nd solution

--- 1458_MaxDotProductOfTwoSubsequences.py
+++ 1458_MaxDotProductOfTwoSubsequences.py
@@ -48,3 +48,17 @@ class Solution:
             ans = max(ans, value)
 
         return ans
+
+# 2nd solution
+# O(mn) time | O(mn) space
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        n, m = len(nums1), len(nums2)
+        dp = [[0] * (m) for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                dp[i][j] = nums1[i] * nums2[j]
+                if i and j: dp[i][j] += max(dp[i - 1][j - 1], 0)
+                if i: dp[i][j] = max(dp[i][j], dp[i - 1][j])
+                if j: dp[i][j] = max(dp[i][j], dp[i][j - 1])
+        return dp[-1][-1]
