a6477f03b87b9d471cbb8d9702b53be065ec61e7
yingzhuo <yingzhuo1994@gmail.com>
2023-10-08 20:28:23 -0500

    2nd solution

--- 2578_SplitWithMinimumSum.py
+++ 2578_SplitWithMinimumSum.py
@@ -10,3 +10,27 @@ class Solution:
         num1 = int(num1)
         num2 = int(num2)
         return  num1 + num2
+
+# 2nd solution
+# O(n) time | O(n) sapce
+class Solution:
+    def splitNum(self, num: int) -> int:
+        count = [0 for _ in range(11)]
+        while num > 0:
+            d = num % 10
+            num //= 10
+            count[d] += 1
+        
+        n = sum(count)
+        for i in range(1, 10):
+            count[i] += count[i - 1]
+        stack = [0 for _ in range(n)]
+        for i in reversed(range(10)):
+            stack[count[i - 1]:count[i]] = [str(i)] * (count[i] - count[i - 1])
+
+        num1 = "".join(stack[0::2])
+        num2 = "".join(stack[1::2])
+
+        num1 = int(num1)
+        num2 = int(num2)
+        return  num1 + num2
