65c0f42d2be6326251d081f3899b4e49a600caa3
yingzhuo <yingzhuo1994@gmail.com>
2023-10-09 20:28:16 -0500

    2nd solution

--- 2009_MinimumNumberOfOperationsToMakeArrayContinuous.py
+++ 2009_MinimumNumberOfOperationsToMakeArrayContinuous.py
@@ -18,3 +18,35 @@ class Solution:
                 if idx == n:
                     break
             return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        minVal, maxVal = min(nums), max(nums)
+        diff = maxVal - minVal
+        if diff <= n - 1:
+            dic = Counter(nums)
+            return n - len(dic)
+        else:
+            nums.sort()
+            ans = n
+            dic = {}
+            last = 0
+            for i, num in enumerate(nums):
+                idx = bisect.bisect_left(nums, num + n)
+                if last < idx:
+                    for j in range(last, idx):
+                        dic[nums[j]] = dic.get(nums[j], 0) + 1
+                if i > 0:
+                    dic[nums[i - 1]] -= 1
+                    if dic[nums[i - 1]] == 0:
+                        dic.pop(nums[i - 1])
+
+                valid = len(dic)
+                ans = min(ans, n - valid)
+                last = idx
+                if idx == n:
+                    break
+            return ans
