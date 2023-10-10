ab46d37cd4af98b144ba55a3d9cab95660447ea8
yingzhuo <yingzhuo1994@gmail.com>
2023-10-09 20:32:03 -0500

    3rd solution

--- 2009_MinimumNumberOfOperationsToMakeArrayContinuous.py
+++ 2009_MinimumNumberOfOperationsToMakeArrayContinuous.py
@@ -50,3 +50,18 @@ class Solution:
                 if idx == n:
                     break
             return ans
+
+# 3rd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = sorted(set(nums))  # Make `nums` as unique numbers and sort `nums`.
+
+        ans = n
+        for i, num in enumerate(nums):
+            end = num + n - 1  # We expect elements of continuous array must in range [start..end]
+            idx = bisect_right(nums, end)  # Find right insert position
+            uniqueLen = idx - i
+            ans = min(ans, n - uniqueLen)
+        return ans
