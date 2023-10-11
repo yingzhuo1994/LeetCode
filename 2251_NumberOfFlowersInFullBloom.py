e501a404789a5b297a0f5f70c757274977eef66b
yingzhuo <yingzhuo1994@gmail.com>
2023-10-10 20:31:05 -0500

    renew

--- 2251_NumberOfFlowersInFullBloom.py
+++ 2251_NumberOfFlowersInFullBloom.py
@@ -2,20 +2,20 @@
 # O(n*log(n) + m*log(m)) time | O(n) space
 # where n is the length of flowers, and m is the length of persons
 class Solution:
-    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
-        start, end = sorted(a for a,b in flowers), sorted(b for a,b in flowers)
-        return [bisect_right(start, t) - bisect_left(end, t) for t in persons]
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        start, end = sorted(a for a, b in flowers), sorted(b for a, b in flowers)
+        return [bisect_right(start, t) - bisect_left(end, t) for t in people]
 
 # 2nd solution
 # O(n*log(n) + m*log(m)) time | O(n) space
 # where n is the length of flowers, and m is the length of persons
 import sortedcontainers
 class Solution:
-    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
         diff = sortedcontainers.SortedDict({0: 0})
         for i, j in flowers:
             diff[i] = diff.get(i, 0) + 1
             diff[j + 1] = diff.get(j + 1, 0) - 1
         
         count = list(accumulate(diff.values()))
-        return [count[diff.bisect(t) - 1] for t in persons]
+        return [count[diff.bisect(t) - 1] for t in people]
