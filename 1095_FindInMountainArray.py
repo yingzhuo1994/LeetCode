4dcd7df61971d08c4996022764bdc4199974a077
yingzhuo <yingzhuo1994@gmail.com>
2023-10-11 20:03:22 -0500

    2nd solution

--- 1095_FindInMountainArray.py
+++ 1095_FindInMountainArray.py
@@ -58,9 +58,40 @@ class Solution:
         helper(start, end)
         return self.ans if self.ans != float("inf") else -1
         
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        n = mountain_arr.length()
+        # find index of peak
+        l, r = 0, n - 1
+        while l < r:
+            m = (l + r) // 2
+            if mountain_arr.get(m) < mountain_arr.get(m + 1):
+                l = peak = m + 1
+            else:
+                r = m
         
-
-
-
-
-
+        # find target in the left of peak
+        l, r = 0, peak
+        while l <= r:
+            m = (l + r) // 2
+            if mountain_arr.get(m) < target:
+                l = m + 1
+            elif mountain_arr.get(m) > target:
+                r = m - 1
+            else:
+                return m
+        
+        # find target in the right of peak
+        l, r = peak, n - 1
+        while l <= r:
+            m = (l + r) // 2
+            if mountain_arr.get(m) > target:
+                l = m + 1
+            elif mountain_arr.get(m) < target:
+                r = m - 1
+            else:
+                return m
+        
+        return -1
