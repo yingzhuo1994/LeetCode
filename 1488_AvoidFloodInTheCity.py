e3d0974771b79601e08a13b4e45cf9cb049ce7f6
yingzhuo <yingzhuo1994@gmail.com>
2023-10-12 22:14:37 -0500

    3rd solution

--- 1488_AvoidFloodInTheCity.py
+++ 1488_AvoidFloodInTheCity.py
@@ -55,3 +55,33 @@ class Solution:
                     st.discard(st[it])
                 mp[rain] = i
         return ans
+    
+
+# 3rd solution
+# O(n) time | O(n) space
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        nxt = list(range(len(rains) + 1))
+        pre = {}
+        res = [1] * len(rains)
+
+        def find(i):
+            j = i if nxt[i] == i else find(nxt[i])
+            nxt[i] = j + 1
+            return j
+
+        for i, a in enumerate(rains):
+            if a == 0:
+                continue
+
+            nxt[i] = i + 1
+            if a in pre:
+                j = find(pre[a])
+                if j > i:
+                    return []
+                res[j] = a
+            
+            res[i] = -1
+            pre[a] = i
+        
+        return res
