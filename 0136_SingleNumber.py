1d9f05ada36e7031c185faece528fe90ceb7686d
yingzhuo <yingzhuo1994@gmail.com>
2023-10-13 21:52:34 -0500

    update

--- 0136_SingleNumber.py
+++ 0136_SingleNumber.py
@@ -1,17 +1,19 @@
+# 1st hash table
+# O(n) time | O(n) space
 class Solution:
-    # 1st hash table
-    # O(n) time | O(n) space
     def singleNumber(self, nums: List[int]) -> int:
         for i in range(len(nums)):
             if nums[i] not in nums[:i] and nums[i] not in nums[i+1:]:
                 return nums[i]
 
-    # 2nd solution, using set
-    # O(n) time | O(n) space
+# 2nd solution, using set
+# O(n) time | O(n) space
+class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return 2 * sum(set(nums)) - sum(nums)
 
-    # 3rd solution, XOR
-    # O(n) time | O(1) space
+# 3rd solution, XOR
+# O(n) time | O(1) space
+class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return reduce(lambda x, y: x^y, nums, 0)
