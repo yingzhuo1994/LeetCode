1edf6f8236a562988c78310923e29e6f5e50d0b3
yingzhuo <yingzhuo1994@gmail.com>
2023-10-16 20:38:22 -0500

    2nd solution

--- 2652_SumMultiples.py
+++ 2652_SumMultiples.py
@@ -7,3 +7,11 @@ class Solution:
             if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
                 ans += num
         return ans
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        def s(m: int) -> int:
+            return n // m * (n // m + 1) // 2 * m
+        return s(3) + s(5) + s(7) - s(15) - s(21) - s(35) + s(105)
