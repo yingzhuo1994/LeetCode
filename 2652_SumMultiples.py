4b259b4ff7b8c2a2837994eee8cf405cec3a5098
yingzhuo <yingzhuo1994@gmail.com>
2023-10-16 20:36:15 -0500

    1st solution

---
+++ 2652_SumMultiples.py
@@ -0,0 +1,9 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        ans = 0
+        for num in range(1, n + 1):
+            if num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
+                ans += num
+        return ans
