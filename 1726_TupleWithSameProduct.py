83f751398855ab1d37bb670c1b9cbfe88e5c36b2
yingzhuo <yingzhuo1994@gmail.com>
2023-10-18 19:58:44 -0500

    2nd solution

--- 1726_TupleWithSameProduct.py
+++ 1726_TupleWithSameProduct.py
@@ -1,5 +1,5 @@
 # 1st solution, TLE
-# O(n^3) time | O(n) space
+# O(n^3 * log(n)) time | O(n) space
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         nums.sort()
@@ -20,4 +20,23 @@ class Solution:
                     if target in numSet:
                         ans += 8
                     # print(ans)
+        return ans
+
+# 2nd solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        ans = 0
+
+        dic = {}
+        for i in range(n):
+            for j in range(i + 1, n):
+                product = nums[i] * nums[j]
+                dic[product] = dic.get(product, 0) + 1
+        
+        for _, k in dic.items():
+            ans += 4 * k * (k - 1)
+
         return ans
