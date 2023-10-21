292c1ac6f2eaf9328e1ab7552afb0ce294f1c14e
yingzhuo <yingzhuo1994@gmail.com>
2023-10-20 20:26:03 -0500

    2nd solution

--- 1425_ConstrainedSubsequenceSum.py
+++ 1425_ConstrainedSubsequenceSum.py
@@ -10,3 +10,20 @@ class Solution:
                 value = max(value, dp[j])
             dp[i] = value + nums[i]
         return max(dp[:-k])
+
+# 2nd solution
+# O(nlog(n)) time | O(n) space
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        minHeap = []
+        ans = float("-inf")
+        for i, num in enumerate(nums):
+            while minHeap and i - minHeap[0][1] > k:
+                heappop(minHeap)
+            value = num
+            if minHeap and minHeap[0][0] < 0:
+                value = -minHeap[0][0] + num
+
+            heappush(minHeap, [-value, i])
+            ans = max(ans, value)
+        return ans
