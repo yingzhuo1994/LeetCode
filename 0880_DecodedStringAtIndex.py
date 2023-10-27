9651933de8d194dfde8f0ea084e92533f8d2ef2c
yingzhuo <yingzhuo1994@gmail.com>
2023-10-26 20:41:41 -0500

    update

--- 0880_DecodedStringAtIndex.py
+++ 0880_DecodedStringAtIndex.py
@@ -1,22 +1,3 @@
-# 1st solution, MLE
-class Solution:
-    def decodeAtIndex(self, s: str, k: int) -> str:
-        stack = []
-        for ch in s:
-            if ch.isdigit():
-                d = int(ch)
-                newStack = []
-                for _ in range(d):
-                    newStack.extend(stack)
-                stack = newStack
-            else:
-                stack.append(ch)
-
-            if len(stack) >= k:
-                return stack[k - 1]
-
-# 2nd solution
-# O(n) time | o(n) space
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
         stack = [[], 0]
@@ -35,19 +16,16 @@ class Solution:
             last, n = stack
             left = 0
             m = None
-            for i in reversed(range(len(last))):
-                ch = last[i]
-                if type(ch) is int:
-                    m = ch
-                    break
-                left += 1
+            if len(last) > 1 and type(last[1]) is int:
+                m = last[1]
+            left = len(last) - 2
             if m is None:
                 return last[k- 1]
             repeat = (n - left) // m
             r = k - repeat * m
             if r > 0:
-                return last[i + r]
+                return last[1 + r]
             else:
-                return dfs(last[:i+1], (k - 1) % m + 1)
+                return dfs(last[:2], (k - 1) % m + 1)
 
         return dfs(stack, k)
