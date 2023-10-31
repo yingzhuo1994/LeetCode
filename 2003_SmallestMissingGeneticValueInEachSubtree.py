9930e768ef04c7a4e3c108880ec6db12927962f7
yingzhuo <yingzhuo1994@gmail.com>
2023-10-30 20:40:10 -0500

    2nd solution

--- 2003_SmallestMissingGeneticValueInEachSubtree.py
+++ 2003_SmallestMissingGeneticValueInEachSubtree.py
@@ -72,3 +72,33 @@ class Solution:
             
         dfs(0)
         return ans
+
+# 2nd solution
+class Solution:
+    def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        n = len(parents)
+        ans = [1] * n
+        if 1 not in nums:  # 不存在基因值为 1 的点
+            return ans
+
+        # 建树
+        g = [[] for _ in range(n)]
+        for i in range(1, n):
+            g[parents[i]].append(i)
+
+        vis = set()
+        def dfs(x: int) -> None:
+            vis.add(nums[x])  # 标记基因值
+            for son in g[x]:
+                if nums[son] not in vis:
+                    dfs(son)
+
+        mex = 2  # 缺失的最小基因值
+        node = nums.index(1)  # 出发点
+        while node >= 0:
+            dfs(node)
+            while mex in vis:  # node 子树包含这个基因值
+                mex += 1
+            ans[node] = mex  # 缺失的最小基因值
+            node = parents[node]  # 往上走
+        return ans
