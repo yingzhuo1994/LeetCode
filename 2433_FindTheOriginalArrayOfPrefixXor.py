cd7b127a047a2289ad69c93224d3776e13c5691b
yingzhuo <yingzhuo1994@gmail.com>
2023-10-30 19:18:06 -0500

    2nd solution

--- 2433_FindTheOriginalArrayOfPrefixXor.py
+++ 2433_FindTheOriginalArrayOfPrefixXor.py
@@ -7,3 +7,12 @@ class Solution:
         for i in range(1, n):
             arr[i] = pref[i - 1] ^ pref[i]
         return arr
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        n = len(pref)
+        for i in reversed(range(1, n)):
+            pref[i] ^= pref[i - 1]
+        return pref
