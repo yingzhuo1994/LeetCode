5ba2b30be5f1d8d4fb84eee3d921fe70249751ee
yingzhuo <yingzhuo1994@gmail.com>
2023-11-01 20:33:53 -0500

    2nd solution

--- 0880_DecodedStringAtIndex.py
+++ 0880_DecodedStringAtIndex.py
@@ -1,3 +1,5 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
         stack = [[], 0]
@@ -29,3 +31,24 @@ class Solution:
                 return dfs(last[:2], (k - 1) % m + 1)
 
         return dfs(stack, k)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        n = 0
+        for i, c in enumerate(s):
+            n = n * int(c) if c.isdigit() else n + 1
+
+            if k <= n:
+                break
+        
+        for j in range(i, -1, -1):
+            c = s[j]
+            if c.isdigit():
+                n //= int(c)
+                k %= n
+            else:
+                if k == n or k == 0:
+                    return c
+                n -= 1
