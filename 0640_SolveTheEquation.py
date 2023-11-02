77612efa7a0036a176c6f2f024c421f5cfc8278d
yingzhuo <yingzhuo1994@gmail.com>
2023-11-01 21:11:03 -0500

    2nd solution

--- 0640_SolveTheEquation.py
+++ 0640_SolveTheEquation.py
@@ -2,50 +2,80 @@
 # O(n) time | O(n) space
 class Solution:
     def solveEquation(self, equation: str) -> str:
-        leftEquation, rightEquation = equation.split("=")
-
-        def getStack(equation):
-            start = 0
-            stack = []
-            for i in range(len(equation)):
-                if equation[i] in "+-":
-                    num = equation[start:i]
-                    if len(num) == 0:
-                        num = "0"
-                    stack.append(num)
-                    stack.append(equation[i])
-                    start = i + 1
-                elif i == len(equation) - 1:
-                    stack.append(equation[start:i+1])
-            const = 0
-            coef = 0
-            lastOp = "+"
-            i = 0
-            while i <= len(stack):
-                if i == len(stack) or stack[i] in "+-":
-                    if stack[i - 1][-1] == "x":
-                        if len(stack[i -1]) > 1:
-                            num = int(stack[i - 1][:-1])
-                        else:
-                            num = 1
-                        if lastOp == "+":
-                            coef += num
-                        else:
-                            coef -= num
+        def parseEquation(s):
+            coef, const = 0, 0
+            if s[0] in "+-":
+                s = "0+0" + s + "+"
+            else:
+                s = "0+" + s + "+"
+            sign = 1
+            lastSign = 1
+            # print(s)
+            for i in range(2, len(s)):
+                ch = s[i]
+                # print(i, ch)
+                if ch == "x":
+                    # print(lastSign, i, s[lastSign+1:i])
+                    if i > lastSign + 1:
+                        num = int(s[lastSign+1:i])
                     else:
-                        num = int(stack[i - 1])
-                        if lastOp == "+":
-                            const += num
-                        else:
-                            const -= num
-                    if i < len(stack):
-                        lastOp = stack[i]
-                i += 1
-
+                        num = 1
+                    coef += sign * num
+                elif ch == "+":
+                    if "x" not in s[lastSign+1:i]:
+                        num = int(s[lastSign+1:i])
+                        const += sign * num
+                    sign = 1
+                    lastSign = i
+                elif ch == "-":
+                    if "x" not in s[lastSign+1:i]:
+                        num = int(s[lastSign+1:i])
+                        const += sign * num
+                    sign = -1
+                    lastSign = i
             return coef, const
         
-        leftCoef, leftConst = getStack(leftEquation)
-        rightCoef, rightConst = getStack(rightEquation)
+        leftEquation, rightEquation = equation.split("=")
+        leftCoef, leftConst = parseEquation(leftEquation)
+        rightCoef, rightConst = parseEquation(rightEquation)
+
+        coef = leftCoef - rightCoef
+        const = rightConst - leftConst
+        
+        if coef == 0:
+            if const == 0:
+                return "Infinite solutions"
+            else:
+                return "No solution"
+        else:
+            return f"x={const // coef}"
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def solveEquation(self, equation: str) -> str:
+        def parseEquation(s):
+            s = s.replace("-", "+-").replace("-x", "-1x")
+            stack = s.split("+")
+            coef, const = 0, 0
+            for ch in stack:
+                if len(ch) == 0:
+                    continue
+
+                if ch[-1] == "x":
+                    t = ch[:-1]
+                    if len(t) == 0:
+                        coef += 1
+                    else:
+                        coef += int(t)
+                else:
+                    const += int(ch)
+                    
+            return coef, const
+        
+        leftEquation, rightEquation = equation.split("=")
+        leftCoef, leftConst = parseEquation(leftEquation)
+        rightCoef, rightConst = parseEquation(rightEquation)
 
         coef = leftCoef - rightCoef
         const = rightConst - leftConst
