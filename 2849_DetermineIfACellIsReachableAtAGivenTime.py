ff02a41f52125569681e4ddf7de06c70d10d159e
yingzhuo <yingzhuo1994@gmail.com>
2023-11-07 20:22:45 -0600

    update

--- 2849_DetermineIfACellIsReachableAtAGivenTime.py
+++ 2849_DetermineIfACellIsReachableAtAGivenTime.py
@@ -3,8 +3,7 @@
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
         dist = max(abs(fx - sx), abs(fy - sy))
-        if dist > t:
-            return False
-        if dist == 0:
-            return t == 0 or t > 1
-        return True
+        if dist != 0:
+            return dist <= t
+        else:
+            return t != 1
