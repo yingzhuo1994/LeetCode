f2392b37f0304bc8a195a501c676782374acc35d
yingzhuo <yingzhuo1994@gmail.com>
2023-11-14 21:15:21 -0600

    2nd solution

--- 1846_MaximumElementAfterDecreasingAndRearranging.py
+++ 1846_MaximumElementAfterDecreasingAndRearranging.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n * log(n)) time | O(1) space 
+# O(n * log(n)) time | O(n) space 
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         arr.sort()
@@ -7,3 +7,25 @@ class Solution:
         for i in range(1, len(arr)):
             arr[i] = min(arr[i-1] + 1, arr[i])
         return arr[-1]
+
+# 2nd solution
+# O(n) time | O(n) space 
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        n = len(arr)
+        count = [0 for _ in range(n + 1)]
+        for i in range(n):
+            arr[i] = min(arr[i], n)
+            count[arr[i]] += 1
+
+        for j in reversed(range(1, n)):
+            count[j] += count[j + 1]
+
+        ans = n
+
+        for i in range(1, n):
+            if i >= ans:
+                break
+            ans = min(ans, i + count[i + 1])
+        
+        return ans
