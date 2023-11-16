a70af583a22e81af9ce7747a0c5e7972c3120639
yingzhuo <yingzhuo1994@gmail.com>
2023-11-15 19:12:33 -0600

    2nd solution

--- 2760_LongestEvenOddSubarrayWithThreshold.py
+++ 2760_LongestEvenOddSubarrayWithThreshold.py
@@ -19,3 +19,21 @@ class Solution:
                     i += 1
                 start = i + 1
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+        n = len(nums)
+        ans = i = 0
+        while i < n:
+            if nums[i] > threshold or nums[i] & 1:
+                i += 1  # 直接跳过
+                continue
+            start = i  # 记录这一组的开始位置
+            i += 1  # 开始位置已经满足要求，从下一个位置开始判断
+            while i < n and nums[i] <= threshold and nums[i] % 2 != nums[i - 1] % 2:
+                i += 1
+            # 从 start 到 i-1 是满足题目要求的子数组
+            ans = max(ans, i - start)
+        return ans
