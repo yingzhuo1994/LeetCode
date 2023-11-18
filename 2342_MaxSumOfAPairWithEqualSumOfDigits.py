df7275eacfa2d598ae913b685e4ad39ebe0f724c
yingzhuo <yingzhuo1994@gmail.com>
2023-11-17 20:46:06 -0600

    update

--- 2342_MaxSumOfAPairWithEqualSumOfDigits.py
+++ 2342_MaxSumOfAPairWithEqualSumOfDigits.py
@@ -3,20 +3,17 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
         dic = {}
+        ans = -1
         for num in nums:
             value = num
             key = 0
             while value > 0:
                 key += value % 10
                 value //= 10
-            if key not in dic:
-                dic[key] = []
-            dic[key].append(num)
-            if len(dic[key]) > 2:
-                dic[key].sort(reverse=True)
-                dic[key].pop()
-        ans = -1
-        for key in dic:
-            if len(dic[key]) == 2:
-                ans = max(ans, sum(dic[key]))
+            if key in dic:
+                ans = max(ans, dic[key] + num)
+                dic[key] = max(dic[key], num)
+            else:
+                dic[key] = num
+
         return ans
