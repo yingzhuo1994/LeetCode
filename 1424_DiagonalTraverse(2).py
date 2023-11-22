14cd443d1f7330875e1dd6af160e785099228d7e
yingzhuo <yingzhuo1994@gmail.com>
2023-11-21 19:30:07 -0600

    2nd solution

--- 1424_DiagonalTraverse(2).py
+++ 1424_DiagonalTraverse(2).py
@@ -18,3 +18,21 @@ class Solution:
         for j in range(1, n):
             traverse(m - 1, j)
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+# n = total num
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                key = i + j
+                if key not in dic:
+                    dic[key] = []
+                dic[key].append(nums[i][j])
+        maxIdx = max(dic.keys())
+        ans = []
+        for i in range(maxIdx + 1):
+            ans.extend(dic[i][::-1])
+        return ans
