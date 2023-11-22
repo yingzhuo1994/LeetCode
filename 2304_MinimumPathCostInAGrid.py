ab8013af11f37b0cb0782fc0869a992e11f8cca7
yingzhuo <yingzhuo1994@gmail.com>
2023-11-21 21:01:10 -0600

    2nd solution

--- 2304_MinimumPathCostInAGrid.py
+++ 2304_MinimumPathCostInAGrid.py
@@ -14,3 +14,13 @@ class Solution:
                 for y, cost in enumerate(moveCost[grid[i][j]]):
                     dp[i + 1][y] = min(dp[i + 1][y], dp[i][j] + cost + grid[i+1][y])
         return min(dp[-1])
+
+# 2nd solution
+# O(mn^2) time | O(1) space
+class Solution:
+    def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        for i in range(m - 2, -1, -1):
+            for j in range(n):
+                grid[i][j] += min(g + c for g, c in zip(grid[i + 1], moveCost[grid[i][j]]))
+        return min(grid[0])
