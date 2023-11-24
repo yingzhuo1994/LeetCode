9beb3d101b7e7f0ccfab20f59471fc585e3751ad
yingzhuo <yingzhuo1994@gmail.com>
2023-11-24 01:03:18 -0600

    2nd solution

--- 2824_CountPairsWhoseSumIsLessThanTarget.py
+++ 2824_CountPairsWhoseSumIsLessThanTarget.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n^2) time | O(n) space
+# O(n^2) time | O(1) space
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         n = len(nums)
@@ -9,4 +9,17 @@ class Solution:
                 curSum = nums[i] + nums[j]
                 if curSum < target:
                     ans += 1
+        return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in reversed(range(1, n)):
+            curTarget = target - nums[i]
+            idx = bisect.bisect_left(nums, curTarget, hi=i)
+            ans += idx
         return ans
