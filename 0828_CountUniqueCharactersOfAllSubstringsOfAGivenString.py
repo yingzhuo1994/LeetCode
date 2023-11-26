a7d2c2e76e31ccd8e99c55c99b52e2ce0da67a2e
yingzhuo <yingzhuo1994@gmail.com>
2023-11-25 21:28:58 -0600

    1st solution

---
+++ 0828_CountUniqueCharactersOfAllSubstringsOfAGivenString.py
@@ -0,0 +1,29 @@
+# 1st solution
+# O(n^2) time | O(1) space
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        ans = 0
+        for length in range(1, len(s) + 1):
+            dic = Counter(s[:length])
+            uniqueCount = 0
+            for ch in dic:
+                if dic[ch] == 1:
+                    uniqueCount += 1
+            ans += uniqueCount
+            for i in range(length, len(s)):
+                ch = s[i]
+                if dic.get(ch, 0) == 0:
+                    uniqueCount += 1
+                elif dic.get(ch, 0) == 1:
+                    uniqueCount -= 1
+                dic[ch] = dic.get(ch, 0) + 1
+                ch1 = s[i - length]
+                dic[ch1] -= 1
+                if dic[ch1] == 0:
+                    uniqueCount -= 1
+                elif dic[ch1] == 1:
+                    uniqueCount += 1
+                
+                ans += uniqueCount
+        return ans
+
