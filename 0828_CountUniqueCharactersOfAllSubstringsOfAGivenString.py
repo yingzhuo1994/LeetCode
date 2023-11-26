48f8783bb8998cb17c739af221e2fb6ace079f41
yingzhuo <yingzhuo1994@gmail.com>
2023-11-25 22:27:54 -0600

    2nd solution

--- 0828_CountUniqueCharactersOfAllSubstringsOfAGivenString.py
+++ 0828_CountUniqueCharactersOfAllSubstringsOfAGivenString.py
@@ -27,3 +27,17 @@ class Solution:
                 ans += uniqueCount
         return ans
 
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        index = {c: [-1, -1] for c in string.ascii_uppercase}
+        res = 0
+        for i, c in enumerate(s):
+            k, j = index[c]
+            res += (i - j) * (j - k)
+            index[c] = [j, i]
+        for c in index:
+            k, j = index[c]
+            res += (len(s) - j) * (j - k)
+        return res % (10**9 + 7)
