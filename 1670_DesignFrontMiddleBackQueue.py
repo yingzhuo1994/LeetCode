27483b5070d21e7d1dc620299faf0b2618759110
yingzhuo <yingzhuo1994@gmail.com>
2023-11-27 21:12:38 -0600

    2nd solution

--- 1670_DesignFrontMiddleBackQueue.py
+++ 1670_DesignFrontMiddleBackQueue.py
@@ -127,6 +127,57 @@ class Node:
     def __str__(self):
         return f"val: {self.val}, prev: {self.prev.val if self.prev else None}, next: {self.next.val if self.next else None}"
 
+# 2nd solution
+class FrontMiddleBackQueue:
+    __slots__ = 'left', 'right'
+
+    def __init__(self):
+        self.left = deque()
+        self.right = deque()
+
+    # 调整长度，保证 0 <= len(right) - len(left) <= 1
+    # 从而保证可以在正中间插入删除元素
+    def balance(self):
+        if len(self.left) > len(self.right):
+            self.right.appendleft(self.left.pop())
+        elif len(self.right) > len(self.left) + 1:
+            self.left.append(self.right.popleft())
+
+    def pushFront(self, val: int) -> None:
+        self.left.appendleft(val)
+        self.balance()
+
+    def pushMiddle(self, val: int) -> None:
+        if len(self.left) < len(self.right):
+            self.left.append(val)
+        else:
+            self.right.appendleft(val)
+
+    def pushBack(self, val: int) -> None:
+        self.right.append(val)
+        self.balance()
+
+    def popFront(self) -> int:
+        if not self.right:  # 整个队列为空
+            return -1
+        val = self.left.popleft() if self.left else self.right.popleft()
+        self.balance()
+        return val
+
+    def popMiddle(self) -> int:
+        if not self.right:  # 整个队列为空
+            return -1
+        if len(self.left) == len(self.right):
+            return self.left.pop()
+        return self.right.popleft()
+
+    def popBack(self) -> int:
+        if not self.right:  # 整个队列为空
+            return -1
+        val = self.right.pop()
+        self.balance()
+        return val
+
 # Your FrontMiddleBackQueue object will be instantiated and called as such:
 # obj = FrontMiddleBackQueue()
 # obj.pushFront(val)
