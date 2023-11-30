f47c57be9fe4b592e1ced51e7afe954095d76c67
yingzhuo <yingzhuo1994@gmail.com>
2023-11-29 23:40:22 -0600

    2nd solution

--- 1611_MinimumOneBitOperationsToMakeIntegersZero.py
+++ 1611_MinimumOneBitOperationsToMakeIntegersZero.py
@@ -7,3 +7,17 @@ class Solution:
             res = -res - (n ^ (n - 1))
             n &= n - 1
         return abs(res)
+
+# 2nd solution
+# O(log(n)) time | O(log(n)) space
+class Solution:
+    @cache
+    def minimumOneBitOperations(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 3
+        d = len(bin(n)) - 2
+        return 2**d - 1 -  self.minimumOneBitOperations(n - 2**d)
