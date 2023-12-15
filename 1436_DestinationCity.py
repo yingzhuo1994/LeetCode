66e0406bf5612f23cc5f06724ed55c76110db91f
yingzhuo <yingzhuo1994@gmail.com>
2023-12-14 19:22:29 -0600

    1st solution

---
+++ 1436_DestinationCity.py
@@ -0,0 +1,13 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        inCount = {}
+        outCount = {}
+        for a, b in paths:
+            inCount[b] = inCount.get(b, 0) + 1
+            outCount[a] = outCount.get(a, 0) + 1
+        for city in inCount:
+            if city not in outCount:
+                return city
+        return ""
