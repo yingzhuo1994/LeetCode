ed90a89c1b79db796566e650b98612254d3ff8fb
yingzhuo <yingzhuo1994@gmail.com>
2023-12-15 19:37:32 -0600

    1st solution

---
+++ 2276_CountIntegersInIntervals.py
@@ -0,0 +1,55 @@
+class CountIntervals:
+
+    def __init__(self):
+        self.intervals = [[float("inf"), float("inf")]]
+        self.number = 0
+
+    def add(self, left: int, right: int) -> None:
+        idx = self.binarySearch(left)
+        if idx > 0 and self.intervals[idx - 1][1] + 1 >= left:
+            left = self.intervals[idx - 1][1] + 1
+        if left > right:
+            return
+
+        i = idx
+        while i < len(self.intervals):
+            if self.intervals[i][1] <= right:
+                self.number -= self.intervals[i][1] - self.intervals[i][0] + 1
+            else:
+                break
+            i += 1
+        self.number += right - left + 1
+        self.intervals[idx:i] = [[left, right]]
+
+        if idx > 0 and self.intervals[idx - 1][1] + 1 == left:
+            self.intervals[idx - 1][1] = right
+            self.intervals.pop(idx)
+            idx -= 1
+        
+        if idx + 1 < len(self.intervals) and self.intervals[idx + 1][0] - 1 <= self.intervals[idx][1]:
+            diff = max(0, self.intervals[idx][1] - self.intervals[idx + 1][0] + 1)
+            self.number -= diff
+            self.intervals[idx][1] = self.intervals[idx + 1][1]
+            self.intervals.pop(idx + 1)
+
+            
+    def count(self) -> int:
+        return self.number
+        
+    def binarySearch(self, val):
+        left, right = 0, len(self.intervals) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if self.intervals[mid][0] > val:
+                right = mid - 1
+            elif self.intervals[mid][0] < val:
+                left = mid + 1
+            else:
+                return mid
+        return left
+            
+
+# Your CountIntervals object will be instantiated and called as such:
+# obj = CountIntervals()
+# obj.add(left,right)
+# param_2 = obj.count()
