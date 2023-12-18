f05be4fd0ba416c84607c5eeee2efa8539793da2
yingzhuo <yingzhuo1994@gmail.com>
2023-12-17 18:42:13 -0600

    1st solution

---
+++ 1913_MaximumProductDifferenceBetweenTwoPairs.py
@@ -0,0 +1,6 @@
+# 1st solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[-1] * nums[-2] - (nums[0] * nums[1])
