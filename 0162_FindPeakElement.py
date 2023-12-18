c5b2a46283bb26af986527888bcfe932e93422d4
yingzhuo <yingzhuo1994@gmail.com>
2023-12-17 18:49:40 -0600

    update

--- 0162_FindPeakElement.py
+++ 0162_FindPeakElement.py
@@ -1,7 +1,7 @@
+# 1st solution
+# O(n) time | O(1) space
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # 1st solution
-        # O(n) time | O(1) space
         if len(nums) == 1 or nums[0] > nums[1]:
             return 0
         for i in range(1, len(nums) - 1):
@@ -10,15 +10,19 @@ class Solution:
         if nums[-1] > nums[-2]:
             return len(nums) - 1
         
-        # 2nd solution
-        # O(n) time | O(1) space
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
                 return i
         return len(nums) - 1
 
-        # 3rd solution
-        # O(logn) time | O(1) space
+# 3rd solution
+# O(logn) time | O(1) space
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
         while left <= right:
