f559c73e3fb76d123178999e54752263e1c41668
yingzhuo <yingzhuo1994@gmail.com>
2023-12-19 19:39:31 -0600

    2nd solution

--- 2828_CheckIfAStringIsAcronymOfWords.py
+++ 2828_CheckIfAStringIsAcronymOfWords.py
@@ -1,5 +1,13 @@
+# 1st solution
+# O(n) time | O(n) space
 class Solution:
     def isAcronym(self, words: List[str], s: str) -> bool:
         if len(words) != len(s):
             return False
         return "".join([word[0] for word in words]) == s
+    
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        return len(words) == len(s) and all(w[0] == c for w, c in zip(words, s))
