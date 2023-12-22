af98755dc548048488fd3add09c30c56fb70a55e
yingzhuo <yingzhuo1994@gmail.com>
2023-12-21 23:25:00 -0600

    2nd solution

--- 1671_MinimumNumberOfRemovalsToMakeMountainArray.py
+++ 1671_MinimumNumberOfRemovalsToMakeMountainArray.py
@@ -34,3 +34,32 @@ class Solution:
             length = leftLength + rightLength + 1
             ans = min(ans, n - length)
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        suf = [0] * n
+        g = []
+        for i in range(n - 1, 0, -1):
+            x = nums[i]
+            j = bisect_left(g, x)
+            if j == len(g):
+                g.append(x)
+            else:
+                g[j] = x
+            suf[i] = j + 1  # 从 nums[i] 开始的最长严格递减子序列的长度
+
+        mx = 0  # 最长山形子序列的长度
+        g = []
+        for i, x in enumerate(nums):
+            j = bisect_left(g, x)
+            if j == len(g):
+                g.append(x)
+            else:
+                g[j] = x
+            pre = j + 1  # 在 nums[i] 结束的最长严格递增子序列的长度
+            if pre >= 2 and suf[i] >= 2:
+                mx = max(mx, pre + suf[i] - 1)  # 减去重复的 nums[i]
+        return n - mx
