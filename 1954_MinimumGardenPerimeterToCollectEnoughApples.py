0915dc1209377a871f8775de8f81392057f7c6a0
yingzhuo <yingzhuo1994@gmail.com>
2023-12-23 19:55:32 -0600

    2nd solution

--- 1954_MinimumGardenPerimeterToCollectEnoughApples.py
+++ 1954_MinimumGardenPerimeterToCollectEnoughApples.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(log(m)) time | O(1) space
+# O(m^(1/3)) time | O(1) space
 # m = neededApples
 class Solution:
     def minimumPerimeter(self, neededApples: int) -> int:
@@ -9,3 +9,19 @@ class Solution:
             n += 1
             s += 12 * n**2
         return 8 * n
+
+# 2nd solution
+# O(log(m)) time | O(1) space
+# m = neededApples
+class Solution:
+    def minimumPerimeter(self, neededApples: int) -> int:
+        left, right, ans = 1, 100000, 0
+        while left <= right:
+            mid = (left + right) // 2
+            s = 2 * mid * (mid + 1) * (mid * 2 + 1)
+            if s >= neededApples:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans * 8
