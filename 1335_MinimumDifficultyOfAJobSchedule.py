86d9756fe0958ea0b5b26db7c2bf572a05ac5106
yingzhuo <yingzhuo1994@gmail.com>
2023-12-28 20:16:35 -0600

    5th solution

--- 1335_MinimumDifficultyOfAJobSchedule.py
+++ 1335_MinimumDifficultyOfAJobSchedule.py
@@ -85,3 +85,22 @@ class Solution:
                     mx = max(mx, a[k])  # 从 a[k] 到 a[j] 的最大值
                     f[i][j] = min(f[i][j], f[i - 1][k - 1] + mx)
         return f[-1][-1]
+
+# 5th solution, Bottom-up DP, space optimized
+# O(dn^2) time | O(n) space
+class Solution:
+    def minDifficulty(self, a: List[int], d: int) -> int:
+        n = len(a)
+        if n < d:
+            return -1
+
+        f = list(accumulate(a, max))
+        for i in range(1, d):
+            for j in range(n - 1, i - 1, -1):
+                f[j] = inf
+                mx = 0
+                for k in range(j, i - 1, -1):
+                    mx = max(mx, a[k])  # 从 a[k] 到 a[j] 的最大值
+                    f[j] = min(f[j], f[k - 1] + mx)
+        
+        return f[-1]
