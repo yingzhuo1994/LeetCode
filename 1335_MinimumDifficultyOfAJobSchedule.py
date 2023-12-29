712139c47925b62bb3f8348d0a8cfaabf4388cfa
yingzhuo <yingzhuo1994@gmail.com>
2023-12-28 20:34:06 -0600

    6th solution

--- 1335_MinimumDifficultyOfAJobSchedule.py
+++ 1335_MinimumDifficultyOfAJobSchedule.py
@@ -104,3 +104,25 @@ class Solution:
                     f[j] = min(f[j], f[k - 1] + mx)
         
         return f[-1]
+
+# 6th solution, Monotonic Stack
+# O(dn) time | O(dn) space
+class Solution:
+    def minDifficulty(self, a: List[int], d: int) -> int:
+        n = len(a)
+        if n < d:
+            return -1
+
+        f = [[inf] * n for _ in range(d)]
+        f[0] = list(accumulate(a, max))
+        for i in range(1, d):
+            st = []  # (下标 j，从 f[i-1][left[j]] 到 f[i-1][j-1] 的最小值)
+            for j in range(i, n):
+                mn = f[i - 1][j - 1]  # 只有 a[j] 一项工作
+                while st and a[st[-1][0]] <= a[j]:  # 向左一直计算到 left[j]
+                    mn = min(mn, st.pop()[1])
+                f[i][j] = mn + a[j]  # 从 a[left[j]+1] 到 a[j] 的最大值是 a[j]
+                if st:  # 如果这一段包含 <=left[j] 的工作，那么这一段的最大值必然不是 a[j]
+                    f[i][j] = min(f[i][j], f[i][st[-1][0]])  # 答案和 f[i][left[j]] 是一样的
+                st.append((j, mn))  # 注意这里保存的不是 f[i][j]
+        return f[-1][-1]
