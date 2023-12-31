ece7fbe40e285ec7fc2fb13825954ba1248547a2
yingzhuo <yingzhuo1994@gmail.com>
2023-12-30 18:51:01 -0600

    1st solution

---
+++ 1154_DayOfTheYear.py
@@ -0,0 +1,16 @@
+# 1st solution
+# O(1) time | O(1) space
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        year, month, day = date.split("-")
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        ans = 0
+        for i in range(month - 1):
+            ans += months[i]
+        ans += day
+        if month > 2 and ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+            ans += 1
+        return ans
