486929cc98f39f159a51f72a8cfeab6f5735d203
yingzhuo <yingzhuo1994@gmail.com>
2023-12-30 18:52:09 -0600

    2nd solution

--- 1154_DayOfTheYear.py
+++ 1154_DayOfTheYear.py
@@ -14,3 +14,12 @@ class Solution:
         if month > 2 and ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
             ans += 1
         return ans
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        y, m, d = map(int, date.split('-'))
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if (y % 400) == 0 or ((y % 4 == 0) and (y % 100 != 0)): days[1] = 29
+        return d + sum(days[:m-1])
