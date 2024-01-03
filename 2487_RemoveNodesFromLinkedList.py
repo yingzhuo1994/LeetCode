bc3a55ce840846a0bf3736b0ca0b6a14b4d7b978
yingzhuo <yingzhuo1994@gmail.com>
2024-01-02 19:14:08 -0600

    2nd solution

--- 2487_RemoveNodesFromLinkedList.py
+++ 2487_RemoveNodesFromLinkedList.py
@@ -19,4 +19,24 @@ class Solution:
             node = node.next
         stack[-1].next = None
         return stack[0]
-            
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre, cur = None, head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
+
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head = self.reverseList(head)
+        while cur.next:
+            if cur.val > cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return self.reverseList(head)
