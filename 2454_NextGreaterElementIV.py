b2206dfd42a8572c1074ad99e9c1abca08fc7563
yingzhuo <yingzhuo1994@gmail.com>
2024-01-08 20:14:23 -0600

    update

--- 2454_NextGreaterElementIV.py
+++ 2454_NextGreaterElementIV.py
@@ -3,15 +3,15 @@
 class Solution:
     def secondGreaterElement(self, nums: List[int]) -> List[int]:
         ans = [-1] * len(nums)
-        s = []
-        t = []
+        stack1 = []
+        stack2 = []
         for i, x in enumerate(nums):
-            while t and nums[t[-1]] < x:
-                ans[t.pop()] = x  # t 栈顶的下下个更大元素是 x
-            j = len(s) - 1
-            while j >= 0 and nums[s[j]] < x:
+            while stack2 and nums[stack2[-1]] < x:
+                ans[stack2.pop()] = x  # t 栈顶的下下个更大元素是 x
+            j = len(stack1) - 1
+            while j >= 0 and nums[stack1[j]] < x:
                 j -= 1  # s 栈顶的下一个更大元素是 x
-            t += s[j + 1:]  # 把从 s 弹出的这一整段元素加到 t
-            del s[j + 1:]  # 弹出一整段元素
-            s.append(i)  # 当前元素（的下标）加到 s 栈顶
+            stack2 += stack1[j + 1:]  # 把从 s 弹出的这一整段元素加到 t
+            del stack1[j + 1:]  # 弹出一整段元素
+            stack1.append(i)  # 当前元素（的下标）加到 s 栈顶
         return ans
