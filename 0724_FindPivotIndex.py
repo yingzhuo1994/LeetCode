3c7a394845242befb1204a89bb3b04f28ce5573b
yingzhuo <yingzhuo1994@gmail.com>
2024-01-08 20:06:19 -0600

    1st solution

---
+++ 0724_FindPivotIndex.py
@@ -0,0 +1,11 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        preSum = 0
+        for i in range(len(nums)):
+            if preSum == total - preSum - nums[i]:
+                return i
+            preSum += nums[i]
+        return -1
