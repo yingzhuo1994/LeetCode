4aee4f6e6603617234bb2eff7d0b853b68ca2987
yingzhuo <yingzhuo1994@gmail.com>
2024-01-09 19:35:28 -0600

    2nd solution

--- 2696_MinimumStringLengthAfterRemovingSubstrings.py
+++ 2696_MinimumStringLengthAfterRemovingSubstrings.py
@@ -5,3 +5,15 @@ class Solution:
         while "AB" in s or "CD" in s:
             s = s.replace("AB", "").replace("CD", "")
         return len(s)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def minLength(self, s: str) -> int:
+        st = []
+        for c in s:
+            if st and (c == 'B' and st[-1] == 'A' or c == 'D' and st[-1] == 'C'):
+                st.pop()
+            else:
+                st.append(c)
+        return len(st)
