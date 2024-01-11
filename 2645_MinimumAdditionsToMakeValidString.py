f2a03d48b2772d63be4ef225bb109a8e36c46fa8
yingzhuo <yingzhuo1994@gmail.com>
2024-01-10 20:06:01 -0600

    2nd solution

--- 2645_MinimumAdditionsToMakeValidString.py
+++ 2645_MinimumAdditionsToMakeValidString.py
@@ -28,4 +28,13 @@ class Solution:
                     ans += 1
             prev = ch
         return ans
-        
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        k, prev = 0, 'z'
+        for c in word:
+            k += c <= prev
+            prev = c
+        return k * 3 - len(word)
