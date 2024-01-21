f21c0575a86521091683906d3934145a4c4e9b94
yingzhuo <yingzhuo1994@gmail.com>
2024-01-20 20:04:38 -0600

    2nd solution

--- 0719_FindK-thSmallestPairDistance.py
+++ 0719_FindK-thSmallestPairDistance.py
@@ -7,3 +7,33 @@ class Solution:
         array.sort()
         return array[k - 1]
 
+# 2nd solution
+# O(n * log(D)) time | O(n) space
+# D is the largest distance among all dist pairs
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        nums.sort()
+
+        minDist, maxDist = 0, nums[-1] - nums[0]
+        while minDist < maxDist:
+            targetDist = minDist + (maxDist - minDist) // 2
+            start = 0
+            count = 0
+            i = 1
+            while i < n:
+                dist = nums[i] - nums[start]
+                if dist <= targetDist:
+                    count += i - start
+                    i += 1
+                else:
+                    start += 1
+                if count > k:
+                    break
+            
+            if count >= k:
+                maxDist = targetDist
+            else:
+                minDist = targetDist + 1
+        
+        return maxDist
