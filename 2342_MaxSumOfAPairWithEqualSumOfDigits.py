8c82be8b2d6d520e4c7e12392d537b7199cf0e6c
yingzhuo <yingzhuo1994@gmail.com>
2024-01-24 20:28:16 -0600

    improve

--- 2342_MaxSumOfAPairWithEqualSumOfDigits.py
+++ 2342_MaxSumOfAPairWithEqualSumOfDigits.py
@@ -5,11 +5,7 @@ class Solution:
         dic = {}
         ans = -1
         for num in nums:
-            value = num
-            key = 0
-            while value > 0:
-                key += value % 10
-                value //= 10
+            key = sum(map(int, str(num)))
             if key in dic:
                 ans = max(ans, dic[key] + num)
                 dic[key] = max(dic[key], num)
