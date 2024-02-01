d8e110414a516630d616048e59b11b46b4fcfb85
yingzhuo <yingzhuo1994@gmail.com>
2024-01-31 20:41:37 -0600

    modify comment

--- LCP_24.py
+++ LCP_24.py
@@ -9,13 +9,13 @@ class Solution:
         left_sum = right_sum = 0
         for i, b in enumerate(nums):
             b -= i
-            if i % 2 == 0:  # 前缀长度是奇数
+            if i % 2 == 0:  # len(left) == len(right)
                 left_sum -= max(-left[0] - b, 0) if left else 0
                 t = -heappushpop(left, -b)
                 right_sum += t
                 heappush(right, t)
                 ans[i] = (right_sum - right[0] - left_sum) % MOD
-            else:  # 前缀长度是偶数
+            else:  # len(left) + 1 == len(right)
                 right_sum += max(b - right[0], 0)
                 t = heappushpop(right, b)
                 left_sum += t
