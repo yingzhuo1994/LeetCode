7c336dffcbd21ef3255d30ba0c3fb4e32b08e207
yingzhuo <yingzhuo1994@gmail.com>
2024-02-02 21:27:34 -0600

    2nd solution

---
+++ 1690_StoneGame(7).py
@@ -0,0 +1,25 @@
+# 1st solution
+# O(n^2) time | O(n^2) space
+class Solution:
+    def stoneGameVII(self, stones: List[int]) -> int:
+        s = list(accumulate(stones, initial=0))  # 前缀和
+        @cache  # 缓存装饰器，避免重复计算 dfs 的结果
+        def dfs(i: int, j: int) -> int:
+            if i == j:  # 递归边界
+                return 0
+            return max(s[j + 1] - s[i + 1] - dfs(i + 1, j), s[j] - s[i] - dfs(i, j - 1))
+        ans = dfs(0, len(stones) - 1)
+        dfs.cache_clear()  # 防止爆内存
+        return ans
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def stoneGameVII(self, stones: List[int]) -> int:
+        n = len(stones)
+        s = list(accumulate(stones, initial=0))
+        f = [0] * n
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                f[j] = max(s[j + 1] - s[i + 1] - f[j], s[j] - s[i] - f[j - 1])
+        return f[-1]
