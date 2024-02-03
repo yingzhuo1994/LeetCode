7bc8cbea2328825f9cfe7e23bb3262fa51a7cdb6
yingzhuo <yingzhuo1994@gmail.com>
2024-02-02 19:50:32 -0600

    2nd solution

--- 1043_PartitionArrayForMaximumSum.py
+++ 1043_PartitionArrayForMaximumSum.py
@@ -1,9 +1,7 @@
 # 1st solution
-# O(n) time | O(n) space
+# O(kn) time | O(n) space
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        if k == 1:
-            return sum(arr)
         @cache
         def dfs(idx):
             if idx == len(arr):
@@ -16,3 +14,16 @@ class Solution:
             return maxSum
 
         return dfs(0)
+
+# 2nd solution
+# O(kn) time | O(n) space
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            curMax = 0
+            for j in range(1, min(k, i) + 1):
+                curMax = max(curMax, arr[i - j])
+                dp[i] = max(dp[i], dp[i - j] + curMax * j)
+        return dp[n]
