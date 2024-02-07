a8f8a3f4b460e7150123d4e373f659afa04897a9
yingzhuo <yingzhuo1994@gmail.com>
2024-02-06 19:11:50 -0600

    2nd solution

--- 2641_CousinsInBinaryTree(2).py
+++ 2641_CousinsInBinaryTree(2).py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 
-# 1st solution
+# 1st solution, DFS
 # O(n) time | O(h) space
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
@@ -38,4 +38,35 @@ class Solution:
         dfs(root, 0)
         dfs_replace(root, 0)
         root.val = 0
+        return root
+
+# 2nd solution, BFS
+# O(n) time | O(n) space
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        root.val = 0
+        level = [root]
+        while level:
+            newLevel = []
+
+            # 计算下一层的节点值之和
+            next_level_sum = 0
+            for node in level:
+                if node.left:
+                    newLevel.append(node.left)
+                    next_level_sum += node.left.val
+                if node.right:
+                    newLevel.append(node.right)
+                    next_level_sum += node.right.val
+
+            # 再次遍历，更新下一层的节点值
+            for node in level:
+                children_sum = (node.left.val if node.left else 0) + \
+                               (node.right.val if node.right else 0)
+                if node.left:
+                    node.left.val = next_level_sum - children_sum
+                if node.right:
+                    node.right.val = next_level_sum - children_sum
+            
+            level = newLevel
         return root
