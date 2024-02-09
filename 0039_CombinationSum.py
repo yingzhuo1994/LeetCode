9871b02a7d1104eadf6f86c6b3d07cb5921c72bb
yingzhuo <yingzhuo1994@gmail.com>
2024-02-09 13:19:36 -0600

    update

--- 0039_CombinationSum.py
+++ 0039_CombinationSum.py
@@ -1,7 +1,7 @@
+# 1st solution
+# O(n^(m/c + 1)) time | O(m/c) space
+# where n = len(candidateds), m = target, c = min(candidates)
 class Solution:
-    # 1st solution
-    # O(n^(m/c + 1)) time | O(m/c) space
-    # where n = len(candidateds), m = target, c = min(candidates)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ret = []
         self.dfs(candidates, target, [], ret)
@@ -16,9 +16,10 @@ class Solution:
         for i in range(len(nums)):
             self.dfs(nums[i:], target - nums[i], path + [nums[i]], ret)
 
-    # 2nd solution
-    # O(n^(m/c + 1)) time | O(m/c) space
-    # where n = len(candidateds), m = target, c = min(candidates)
+# 2nd solution
+# O(n^(m/c + 1)) time | O(m/c) space
+# where n = len(candidateds), m = target, c = min(candidates)
+class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ret = []
         self.dfs(candidates, 0, target, [], ret)
