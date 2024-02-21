4db77506f421d9bcae1e41003b83009c8bbfacf1
yingzhuo <yingzhuo1994@gmail.com>
2024-02-20 19:48:13 -0600

    2nd solution

--- 0749_ContainVirus.py
+++ 0749_ContainVirus.py
@@ -58,3 +58,55 @@ class Solution:
                 break
         
         return ans
+
+# 2nd solution
+# O(mn * (m + n)) time | O(mn) space
+class Solution:
+    def containVirus(self, isInfected: List[List[int]]) -> int:
+        def dfs(i, j):
+            vis[i][j] = True
+            areas[-1].append((i, j))
+            for a, b in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
+                x, y = i + a, j + b
+                if 0 <= x < m and 0 <= y < n:
+                    if isInfected[x][y] == 1 and not vis[x][y]:
+                        dfs(x, y)
+                    elif isInfected[x][y] == 0:
+                        c[-1] += 1
+                        boundaries[-1].add((x, y))
+
+        m, n = len(isInfected), len(isInfected[0])
+        ans = 0
+        while 1:
+            vis = [[False] * n for _ in range(m)]
+            areas = []
+            c = []
+            boundaries = []
+            for i, row in enumerate(isInfected):
+                for j, v in enumerate(row):
+                    # 找到每块连通的病毒区域
+                    if v == 1 and not vis[i][j]:
+                        areas.append([])
+                        boundaries.append(set())
+                        c.append(0)
+                        dfs(i, j)
+            if not areas:
+                break
+
+            # 威胁最大的区域
+            idx = boundaries.index(max(boundaries, key=len))
+            # 累加防火墙
+            ans += c[idx]
+            for k, area in enumerate(areas):
+                if k == idx:
+                    # 此区域已成功堵住
+                    for i, j in area:
+                        isInfected[i][j] = -1
+                else:
+                    # 向外感染一个区域
+                    for i, j in area:
+                        for a, b in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
+                            x, y = i + a, j + b
+                            if 0 <= x < m and 0 <= y < n and isInfected[x][y] == 0:
+                                isInfected[x][y] = 1
+        return ans
