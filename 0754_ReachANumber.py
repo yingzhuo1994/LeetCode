db6ed20a9a1a9b3dcd993b58777834a54ba18449
yingzhuo <yingzhuo1994@gmail.com>
2024-02-25 19:58:59 -0600

    2nd solution

--- 0754_ReachANumber.py
+++ 0754_ReachANumber.py
@@ -12,4 +12,18 @@ class Solution:
             if target in level:
                 return step
             step += 1
-        
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        t = abs(target)
+        x = int(-1 + math.sqrt(1 + 8 * t)) // 2
+
+        while True:
+            diff = (x + 1) * x // 2 - t
+            if diff < 0 or diff & 1:
+                x += 1
+                continue
+            else:
+                return x
