823920ec6d3ff118d2ee8ec7e62e1afe2bdc7728
yingzhuo <yingzhuo1994@gmail.com>
2024-03-04 19:01:18 -0600

    improve

--- 1750_MinimumLengthOfStringAfterDeletingSimilarEnds.py
+++ 1750_MinimumLengthOfStringAfterDeletingSimilarEnds.py
@@ -4,14 +4,9 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         start, end = 0, len(s) - 1
         while start < end and s[start] == s[end]:
-            i = start
-            while i < end and s[i] == s[start]:
-                i += 1
-            j = end
-            while j > start and s[j] == s[end]:
-                j -= 1
-            if i > j:
-                return 0
-            start = i
-            end = j
+            ch = s[start]
+            while start < end and s[start] == ch:
+                start += 1
+            while end >= start and s[end] == ch:
+                end -= 1
         return end - start + 1
