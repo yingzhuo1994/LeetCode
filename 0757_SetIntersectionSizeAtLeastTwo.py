96827eb6c6634c06ed77ea6d136de3eef750b379
yingzhuo <yingzhuo1994@gmail.com>
2024-03-09 21:22:17 -0600

    2nd solution

--- 0757_SetIntersectionSizeAtLeastTwo.py
+++ 0757_SetIntersectionSizeAtLeastTwo.py
@@ -15,3 +15,25 @@ class Solution:
                     vals[p].append(j)
                 j += 1
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        intervals.sort(key = lambda x : (x[0], -x[1]))
+        ans = 2
+        cur, next = intervals[-1][0], intervals[-1][0] + 1
+        for xi, yi in reversed(intervals[:-1]):
+            if yi >= next:
+                continue
+            elif yi < cur:
+                cur = xi
+                next = xi + 1
+                ans += 2
+            else:
+                next = cur
+                cur = xi
+                ans += 1
+        return ans
