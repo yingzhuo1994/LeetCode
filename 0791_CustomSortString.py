d9d3380c919be23fda9fb0dc7fb8e5b411fd280e
yingzhuo <yingzhuo1994@gmail.com>
2024-03-10 19:36:55 -0500

    2nd solution

--- 0791_CustomSortString.py
+++ 0791_CustomSortString.py
@@ -6,3 +6,17 @@ class Solution:
         lst = list(s)
         lst.sort(key=lambda v: [orderDic.get(v, 100), v])
         return "".join(lst)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        counts = Counter(s)
+        lst = []
+        for ch in order:
+            if ch in counts:
+                lst.append(ch * counts[ch])
+        for ch in counts:
+            if ch not in order:
+                lst.append(ch * counts[ch])
+        return "".join(lst)
