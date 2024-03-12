826d212745b1e4f0cc9c6e7b3a5eafae545a5c78
yingzhuo <yingzhuo1994@gmail.com>
2024-03-11 22:57:01 -0500

    1st solution

---
+++ 1261_FindElementsInAContaminatedBinaryTree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.recover(self.root, 0)
+        
+
+    def find(self, target: int) -> bool:
+        path = [target]
+        val = target
+        while val > 0:
+            if val & 1:
+                val = (val - 1) // 2
+            else:
+                val = (val - 2) // 2
+            path.append(val)
+        node = self.root
+        for i in reversed(range(len(path))):
+            val = path[i]
+            if not node or node.val != val:
+                return False
+            if i > 0:
+                if path[i-1] & 1:
+                    node = node.left
+                else:
+                    node = node.right
+        return True
+    
+
+    def recover(self, node, val):
+        if not node:
+            return
+        node.val = val
+        self.recover(node.left, val * 2 + 1)
+        self.recover(node.right, val * 2 + 2)
+
+
+        
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
