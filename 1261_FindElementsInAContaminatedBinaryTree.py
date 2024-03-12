843f38a7d606054a5256da8489d14335b27a40e8
yingzhuo <yingzhuo1994@gmail.com>
2024-03-11 23:06:37 -0500

    2nd solution

--- 1261_FindElementsInAContaminatedBinaryTree.py
+++ 1261_FindElementsInAContaminatedBinaryTree.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 1st solution
 class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
@@ -40,6 +42,36 @@ class FindElements:
         self.recover(node.left, val * 2 + 1)
         self.recover(node.right, val * 2 + 2)
 
+
+# 2nd solution
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.recover(self.root, 0)
+        
+
+    def find(self, target: int) -> bool:
+        binary = bin(target+1)[3:]                  # remove the useless first `1`
+        index = 0
+        root = self.root                                    # use a new pointer `root` to traverse the tree
+        while root and index <= len(binary): # traverse the binary number from left to right
+            if root.val == target:
+                return True
+            if  binary[index] == '0':  # if it's 0, we have to go left
+                root = root.left
+            else:  # if it's 1, we have to go right
+                root = root.right
+            index += 1
+        return False
+    
+
+    def recover(self, node, val):
+        if not node:
+            return
+        node.val = val
+        self.recover(node.left, val * 2 + 1)
+        self.recover(node.right, val * 2 + 2)
 
         
 
