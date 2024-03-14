cfe5830cf5f1cbe19c4f1c916df592c7b937fe96
yingzhuo <yingzhuo1994@gmail.com>
2024-03-13 19:50:55 -0500

    2nd solution

--- 2789_LargestElementInAnArrayAfterMergeOperations.py
+++ 2789_LargestElementInAnArrayAfterMergeOperations.py
@@ -11,3 +11,13 @@ class Solution:
                 nums.pop()
                 ans = max(ans, nums[-1])
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def maxArrayValue(self, nums: List[int]) -> int: 
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i - 1] <= nums[i]:
+                nums[i - 1] = nums[i - 1] + nums[i]
+        
+        return nums[0]
