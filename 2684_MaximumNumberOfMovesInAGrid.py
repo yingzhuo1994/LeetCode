91a947c2160fa48951164d43f889c6682f4ffde7
yingzhuo <yingzhuo1994@gmail.com>
2024-03-15 19:35:37 -0500

    2nd solution

--- 2684_MaximumNumberOfMovesInAGrid.py
+++ 2684_MaximumNumberOfMovesInAGrid.py
@@ -16,3 +16,23 @@ class Solution:
                 if i + 1 < m and grid[i+1][j+1] > grid[i][j]:
                     dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + 1)
         return max(map(max, dp))
+
+
+# 2nd solution
+# O(mn) time | O(m) space
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        level = set([i for i in range(m)])
+        step = 0
+        while step < n - 1:
+            newLevel = set()
+            for i in level:
+                for j in [i-1, i, i+1]:
+                    if 0 <= j < m and grid[j][step+1] > grid[i][step]:
+                        newLevel.add(j)
+            if not newLevel:
+                break
+            level = newLevel
+            step += 1
+        return step
