404b05ad0e00a04228c91d7243ccccc992ec5e32
yingzhuo <yingzhuo1994@gmail.com>
2024-03-17 20:11:01 -0500

    2nd solution

--- 0796_RotateString.py
+++ 0796_RotateString.py
@@ -1,5 +1,38 @@
 # 1st solution
-# O(n) time | O(1) space
+# O(n^2) time | O(n) space
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         return len(s) == len(goal) and s in (goal + goal)
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution(object):
+    def rotateString(self, A, B):
+        MOD = 10**9 + 7
+        P = 113
+        Pinv = pow(P, MOD-2, MOD)
+
+        hb = 0
+        power = 1
+        for x in B:
+            code = ord(x) - 96
+            hb = (hb + power * code) % MOD
+            power = power * P % MOD
+
+        ha = 0
+        power = 1
+        for x in A:
+            code = ord(x) - 96
+            ha = (ha + power * code) % MOD
+            power = power * P % MOD
+
+        if ha == hb and A == B: return True
+        for i, x in enumerate(A):
+            code = ord(x) - 96
+            ha += power * code
+            ha -= code
+            ha *= Pinv
+            ha %= MOD
+            if ha == hb and A[i+1:] + A[:i+1] == B:
+                return True
+        return False
