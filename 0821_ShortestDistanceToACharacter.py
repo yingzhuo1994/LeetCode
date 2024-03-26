10f61d8eb03b6cb366fa08dd9754c3810766e4de
yingzhuo <yingzhuo1994@gmail.com>
2024-03-25 20:41:05 -0500

    2nd solution

--- 0821_ShortestDistanceToACharacter.py
+++ 0821_ShortestDistanceToACharacter.py
@@ -19,4 +19,29 @@ class Solution:
                     dist += 1
                 last = i
         return ans
-            
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        ans = [inf for _ in s]
+        last = 0
+        i = 0
+        while i < len(s):
+            ch = s[i]
+            if ch == c:
+                dist = 0
+                for j in reversed(range(last, i + 1)):
+                    ans[j] = min(ans[j], dist)
+                    dist += 1
+                last = i
+                
+                dist = 1
+                for j in range(i + 1, len(s)):
+                    if s[j] == c:
+                        i = j - 1
+                        break
+                    ans[j] = min(ans[j], dist)
+                    dist += 1
+            i += 1
+        return ans
