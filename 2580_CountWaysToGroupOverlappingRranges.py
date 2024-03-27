3e24363144445fa0954f5c88b7552337eb7b29af
yingzhuo <yingzhuo1994@gmail.com>
2024-03-26 19:22:35 -0500

    1st solution

---
+++ 2580_CountWaysToGroupOverlappingRranges.py
@@ -0,0 +1,15 @@
+# 1st solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def countWays(self, ranges: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        ranges.sort()
+        last = -1
+        count = 0
+        for start, end in ranges:
+            if start > last:
+                count += 1
+                last = end
+            else:
+                last = max(last, end)
+        return pow(2, count) % MOD
