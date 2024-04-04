eb888c954f3373eac667a58da148cdf200ad4eca
yingzhuo <yingzhuo1994@gmail.com>
2024-04-03 21:14:35 -0500

    2nd solution

--- 2192_AllAncestorsOfANodeInADirectedAcyclicGraph.py
+++ 2192_AllAncestorsOfANodeInADirectedAcyclicGraph.py
@@ -25,3 +25,25 @@ class Solution:
             getChildren(node)
         return ans
         
+
+# 2nd solution
+# O(n(n + m)) time | O(n + m) space
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        graph = [[] for _ in range(n)]
+        for x, y in edges:
+            graph[y].append(x)  # 反向建图
+
+        def dfs(x: int) -> None:
+            vis[x] = True  # 避免重复访问
+            for y in graph[x]:
+                if not vis[y]:
+                    dfs(y)  # 只递归没有访问过的点
+
+        ans = [None] * n
+        for node in range(n):
+            vis = [False] * n
+            dfs(node)  # 从 node 开始 DFS
+            vis[node] = False  # ans[i] 不含 i
+            ans[node] = [j for j, b in enumerate(vis) if b]
+        return ans
