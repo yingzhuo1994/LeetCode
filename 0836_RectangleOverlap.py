2e83161d0b23da9809880065f97f302dbefd3090
yingzhuo <yingzhuo1994@gmail.com>
2024-04-04 19:30:48 -0500

    1st solution

---
+++ 0836_RectangleOverlap.py
@@ -0,0 +1,7 @@
+# 1st solution
+# O(1) time | O(1) space
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if rec1[0] >= rec2[2] or rec1[2] <= rec2[0] or rec1[1] >= rec2[3] or rec1[3] <= rec2[1]:
+            return False
+        return True
