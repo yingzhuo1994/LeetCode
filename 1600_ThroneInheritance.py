50415c8759c474471281128b8e0ad37623c59a6a
yingzhuo <yingzhuo1994@gmail.com>
2024-04-06 21:18:48 -0500

    2nd solution

--- 1600_ThroneInheritance.py
+++ 1600_ThroneInheritance.py
@@ -41,7 +41,32 @@ class ThroneInheritance:
             return self.getSuccessor(self.parents[name], self.childIdx[name] + 1)
         return None
         
-        
+# 2nd solution
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        self.edges = defaultdict(list)
+        self.dead = set()
+        self.king = kingName
+
+    def birth(self, parentName: str, childName: str) -> None:
+        self.edges[parentName].append(childName)
+
+    def death(self, name: str) -> None:
+        self.dead.add(name)
+
+    def getInheritanceOrder(self) -> List[str]:
+        ans = list()
+
+        def preorder(name: str) -> None:
+            if name not in self.dead:
+                ans.append(name)
+            if name in self.edges:
+                for childName in self.edges[name]:
+                    preorder(childName)
+
+        preorder(self.king)
+        return ans
 
 
 
