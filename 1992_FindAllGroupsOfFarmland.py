26b5b2f1754f3a101005e58fe40a5b17373ecdc3
yingzhuo <yingzhuo1994@gmail.com>
2024-04-19 21:51:37 -0500

    2nd solution

--- 1992_FindAllGroupsOfFarmland.py
+++ 1992_FindAllGroupsOfFarmland.py
@@ -28,3 +28,27 @@ class Solution:
                     dfs(i, j, loc)
                     ans.append(loc)
         return ans
+
+
+# 2nd solution
+# O(mn) time | O(mn) space
+class Solution:
+    def findFarmland(self, land: "List[List[int]]") -> "List[List[int]]":
+        m, n, res = len(land), len(land[0]), []
+        for i in range(m):
+            for j in range(n):
+                if land[i][j] == 1:
+                    r, c = i, j
+                    ## 题目确保农场相互独立
+                    while c < n - 1 and land[i][c + 1] == 1:
+                        c += 1
+                    while r < m - 1 and land[r + 1][j] == 1:
+                        r += 1
+                    res.append([i, j, r, c])
+
+                    ## 退耕还林
+                    for n1 in range(i, r + 1):
+                        for n2 in range(j, c + 1):
+                            land[n1][n2] = 0
+                    
+        return res
