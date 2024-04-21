e35949c09bf7436ab565f0f67967e0bc85e979a5
yingzhuo <yingzhuo1994@gmail.com>
2024-04-20 20:33:47 -0500

    2nd solution

--- 0777_SwapAdjacentInLRString.py
+++ 0777_SwapAdjacentInLRString.py
@@ -31,3 +31,24 @@ class Solution:
             return False
         
         return dfs(start, end)
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def canTransform(self, start: str, target: str) -> bool:
+        if start.replace('X', '') != target.replace('X', ''): 
+            return False
+        j = 0
+        for i, c in enumerate(start):
+            if c == 'X':
+                continue
+            
+            while target[j] == 'X':
+                j += 1
+            
+            if i != j and (c == 'L') != (i > j):
+                return False
+            
+            j += 1
+        return True
