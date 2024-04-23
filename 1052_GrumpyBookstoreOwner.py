1d7880822c2221a49ff1d8c3b48849d204fb4e91
yingzhuo <yingzhuo1994@gmail.com>
2024-04-22 20:20:41 -0500

    1st solution

--- 1052_GrumpyBookstoreOwner.py
+++ 1052_GrumpyBookstoreOwner.py
@@ -4,18 +4,21 @@ class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         n = len(customers)
         sums = [0 for _ in range(n + 1)]
-        counts = [0 for _ in range(n + 1)]
         for i in range(n):
-            counts[i+1] = counts[i]
+            sums[i+1] = sums[i]
             if grumpy[i] == 0:
-                counts[i+1] += customers[i]
-            sums[i+1] = sums[i] + customers[i]
+                sums[i+1] += customers[i]
 
         ans = 0
-        total = counts[-1]
+        total = sums[-1]
+        count = 0
         for i in range(n):
+            count += customers[i]
             j = max(0, i - minutes + 1)
-            t = sums[i + 1] - sums[j] + total - counts[i+1] + counts[j]
+            if j > 0:
+                count -= customers[j - 1]
+            t = count + total - sums[i+1] + sums[j]
             ans = max(ans, t)
+        
         return ans
             
