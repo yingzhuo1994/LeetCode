e9270e6f9983ad4900499a644e92e289f4459cfa
yingzhuo <yingzhuo1994@gmail.com>
2024-04-23 21:11:39 -0500

    2nd solution

--- 0786_K-thSmallestPrimeFraction.py
+++ 0786_K-thSmallestPrimeFraction.py
@@ -23,3 +23,33 @@ class Frac:
 
     def __lt__(self, other: "Frac") -> bool:
         return self.x * other.y < self.y * other.x
+
+
+# 2nd solution
+# O(n * log(C)) time | O(1) space
+# where C=10^8
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        n = len(arr)
+        left, right = 0.0, 1.0
+
+        while True:
+            mid = (left + right) / 2
+            i, count = -1, 0
+            # 记录最大的分数
+            x, y = 0, 1
+            
+            for j in range(1, n):
+                while arr[i + 1] / arr[j] < mid:
+                    i += 1
+                    if arr[i] * y > arr[j] * x:
+                        x, y = arr[i], arr[j]
+                count += i + 1
+
+            if count == k:
+                return [x, y]
+            
+            if count < k:
+                left = mid
+            else:
+                right = mid
