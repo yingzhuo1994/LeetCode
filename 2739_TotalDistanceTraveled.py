ad4f26c4862755f36b2b988ca4b770344e205531
yingzhuo <yingzhuo1994@gmail.com>
2024-04-24 20:06:02 -0500

    2nd solution

--- 2739_TotalDistanceTraveled.py
+++ 2739_TotalDistanceTraveled.py
@@ -11,3 +11,13 @@ class Solution:
                 mainTank += 1
         dist += mainTank * 10
         return dist
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
+        if mainTank < 5 or additionalTank == 0:
+            return mainTank * 10
+
+        q, r = divmod(mainTank, 5)
+        return q * 5 * 10 + self.distanceTraveled(r + min(q, additionalTank), max(0, additionalTank - q))
