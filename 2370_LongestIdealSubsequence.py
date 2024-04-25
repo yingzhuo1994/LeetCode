5fc8215f4f35d6faf060bd7b3de427240c43c457
yingzhuo <yingzhuo1994@gmail.com>
2024-04-24 19:57:22 -0500

    improve

--- 2370_LongestIdealSubsequence.py
+++ 2370_LongestIdealSubsequence.py
@@ -16,11 +16,8 @@ class Solution:
 # O(n) time | O(1) space
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        dic = {ch: 0 for ch in string.ascii_lowercase}
-        n = len(s)
-        for i in range(n):
-            val = dic[s[i]]
-            for j in range(max(ord("a"), ord(s[i]) - k), min(ord("z"), ord(s[i]) + k) + 1):
-                val = max(val, dic[chr(j)] + 1)
-            dic[s[i]] = val
-        return max(dic.values())
+        dp = [0] * 26
+        for c in s:
+            i = ord(c) - ord('a')
+            dp[i] = max(dp[max(0, i - k) : min(25, i + k) + 1]) + 1
+        return max(dp)
