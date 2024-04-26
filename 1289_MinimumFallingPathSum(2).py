19f83b56dc3f2c53c4c3e2f8d3ffff6daf6c05ec
yingzhuo <yingzhuo1994@gmail.com>
2024-04-25 20:06:34 -0500

    2nd solution

--- 1289_MinimumFallingPathSum(2).py
+++ 1289_MinimumFallingPathSum(2).py
@@ -12,3 +12,26 @@ class Solution:
                         continue
                     dp[i+1][k] = min(dp[i+1][k], dp[i][j] + grid[i+1][k])
         return min(dp[-1])
+
+
+# 2nd solution
+# O(n^2 * log(n)) time | O(n) space
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if n == 1:
+            return grid[0][0]
+        level = {-1: 0}
+        for row in grid:
+            pairs = [[val, i] for i, val in enumerate(row)]
+            pairs.sort()
+            newLevel = {}
+            for i, a in level.items():
+                for b, j in pairs[:3]:
+                    if i == j:
+                        continue
+                    newLevel[j] = min(newLevel.get(j, float("inf")), a + b)
+            items = sorted(list([val, col] for col, val in newLevel.items()))
+            level = {items[0][1]: items[0][0], items[1][1]: items[1][0]}
+
+        return min(level.values())
