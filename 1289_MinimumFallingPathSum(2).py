dc0158e2e2417ed2a34dd8a36d54868f83b9ed2a
yingzhuo <yingzhuo1994@gmail.com>
2024-04-25 20:12:04 -0500

    3rd solution

--- 1289_MinimumFallingPathSum(2).py
+++ 1289_MinimumFallingPathSum(2).py
@@ -35,3 +35,13 @@ class Solution:
             level = {items[0][1]: items[0][0], items[1][1]: items[1][0]}
 
         return min(level.values())
+
+# 3rd solution
+# O(n^2) time | O(1) space
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        for pre_row, cur_row in pairwise(grid):  # 枚举上一行和当前行
+            mn, mn2 = nsmallest(2, pre_row)  # 上一行的最小值和次小值
+            for j, pre in enumerate(pre_row):  # 枚举上一行的状态
+                cur_row[j] += mn if pre != mn else mn2  # 不是最小就加上最小，否则加上次小
+        return min(grid[-1])  # 第 n-1 行的最小值
