34cf2e86b43489bedd578995d4e552f39dbca1b4
yingzhuo <yingzhuo1994@gmail.com>
2024-04-27 20:48:59 -0500

    2nd solution

--- 1017_ConvertToBase-2.py
+++ 1017_ConvertToBase-2.py
@@ -14,3 +14,20 @@ class Solution:
             res.append(x & 1)
             x = x >> 1
         return "".join(map(str, res[::-1] or [0]))
+
+
+# 2nd solution
+# O(log(n)) time | O(log(n)) space
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        k = 1
+        ans = []
+        while n:
+            if n & 1:
+                ans.append('1')
+                n -= k
+            else:
+                ans.append('0')
+            n //= 2
+            k *= -1
+        return ''.join(ans[::-1]) or '0'
