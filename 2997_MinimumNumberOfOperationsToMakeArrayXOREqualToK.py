f295b7e6c24f7bd54b253b0b10c91e8c36fc997c
yingzhuo <yingzhuo1994@gmail.com>
2024-04-28 19:48:12 -0500

    1st solution

---
+++ 2997_MinimumNumberOfOperationsToMakeArrayXOREqualToK.py
@@ -0,0 +1,15 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        val = nums[0]
+        for i in range(1, len(nums)):
+            val ^= nums[i]
+        
+        ans = 0
+        for i in range(32):
+            a = (val >> i) & 1
+            b = (k >> i) & 1
+            if a != b:
+                ans += 1
+        return ans
