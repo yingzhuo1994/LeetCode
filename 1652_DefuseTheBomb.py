76ff309e6b89aa4feb1462ecf14a25a1bd67dd9e
yingzhuo <yingzhuo1994@gmail.com>
2024-05-04 19:23:10 -0500

    2nd solution

--- 1652_DefuseTheBomb.py
+++ 1652_DefuseTheBomb.py
@@ -21,3 +21,28 @@ class Solution:
                 curSum -= newCode[i+k]
                 curSum += newCode[i]
         return ans
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        ans = [0] * n
+        if k == 0:
+            return ans
+
+        curSum = 0
+        if k > 0:
+            curSum = sum(code[:k])
+            for i in reversed(range(n)):
+                ans[i] = curSum
+                curSum -= code[(i+k)%n]
+                curSum += code[i]
+        else:
+            curSum = sum(code[k:])
+            for i in range(n):
+                ans[i] = curSum
+                curSum -= code[i+k]
+                curSum += code[i]
+        return ans
