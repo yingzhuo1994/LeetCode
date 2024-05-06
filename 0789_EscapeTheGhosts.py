c813cf02fbd9ccd2602c04c8c602273dc7e9e43b
yingzhuo <yingzhuo1994@gmail.com>
2024-05-05 19:30:40 -0500

    improve

--- 0789_EscapeTheGhosts.py
+++ 0789_EscapeTheGhosts.py
@@ -2,11 +2,6 @@
 # O(n) time | O(1) space
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        tagetDist = abs(target[0]) + abs(target[1])
-        for x, y in ghosts:
-            a = x - target[0]
-            b = y - target[1]
-            dist = abs(a) + abs(b)
-            if dist <= tagetDist:
-                return False
-        return True
+        x, y = target
+        tagetDist = abs(x) + abs(y)
+        return all(tagetDist < abs(x - a) + abs(y - b) for a, b in ghosts)
