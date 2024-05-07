46c9a0567ba5a001f4a4622845a684cd3f5adcc6
yingzhuo <yingzhuo1994@gmail.com>
2024-05-06 20:38:58 -0500

    2nd solution

--- 2816_DoubleANumberRepresentedAsALinkedList.py
+++ 2816_DoubleANumberRepresentedAsALinkedList.py
@@ -23,3 +23,18 @@ class Solution:
             prev = node
             num //= 10
         return prev
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.val > 4:
+            head = ListNode(0, head)
+        node = head
+        while node:
+            node.val = (node.val * 2) % 10
+            if node.next and node.next.val > 4:
+                node.val += 1
+            node = node.next
+        return head
