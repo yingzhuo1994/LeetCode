f0721f521b82921906fed0c1272e17cc4c343b86
yingzhuo <yingzhuo1994@gmail.com>
2024-05-08 21:06:00 -0500

    improve

--- 2105_WateringPlants(2).py
+++ 2105_WateringPlants(2).py
@@ -8,21 +8,16 @@ class Solution:
         count = 0
         half = n // 2
         for i in range(half):
-            if alice >= plants[i]:
-                alice -= plants[i]
-            else:
-                alice = capacityA - plants[i]
+            if alice < plants[i]:
+                alice = capacityA
                 count += 1
-            if bob >= plants[n-1-i]:
-                bob -= plants[n-1-i]
-            else:
-                bob = capacityB - plants[n-1-i]
+            alice -= plants[i]
+            if bob < plants[n-1-i]:
+                bob = capacityB
                 count += 1
-        if n & 1:
-            if alice >= bob:
-                if alice < plants[n // 2]:
-                    count += 1
-            else:
-                if bob < plants[n // 2]:
-                    count += 1
+            bob -= plants[n-1-i]
+        
+        if n & 1 and max(alice, bob) < plants[n // 2]:
+            count += 1
+
         return count
