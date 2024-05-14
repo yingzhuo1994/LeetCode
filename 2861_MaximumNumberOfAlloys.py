cf5d098a5859cc63a7314aefcd89168b3473a309
yingzhuo <yingzhuo1994@gmail.com>
2024-05-13 21:31:49 -0500

    2nd solution

--- 2861_MaximumNumberOfAlloys.py
+++ 2861_MaximumNumberOfAlloys.py
@@ -24,3 +24,29 @@ class Solution:
             ans = left
         
         return ans
+
+
+# 2nd solution
+# O(kn * log(U)) time | O(1) space
+class Solution:
+    def maxNumberOfAlloys(self, n: int, k: int, budget: int, composition: List[List[int]], stock: List[int], cost: List[int]) -> int:
+        ans = 0
+        maxC = budget + sum(stock) + 1
+        for i in range(k):
+            machine = composition[i]
+            start = 0
+            end = maxC
+            count = 0
+            while start < end:
+                mid = start + (end - start) // 2
+                total = 0
+                for j in range(n):
+                    if stock[j] < mid * machine[j]:
+                        total += (mid * machine[j] - stock[j]) * cost[j]
+                if total <= budget:
+                    count = max(count, mid)
+                    start = mid + 1
+                else:
+                    end = mid
+            ans = max(ans, count)
+        return ans
