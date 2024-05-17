74a0c08c4904a97e12f580563b97ee9852a40236
yingzhuo <yingzhuo1994@gmail.com>
2024-05-16 20:32:10 -0500

    2nd solution

--- 0826_MostProfitAssigningWork.py
+++ 0826_MostProfitAssigningWork.py
@@ -1,5 +1,6 @@
 # 1st solution
-# O(n * log(n)) time | O(n) space
+# O(n * log(n) + m * log(n)) time | O(n) space
+# where n = len(profit) and m = len(worker)
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         tasks = [[d, p] for d, p in zip(difficulty, profit)]
@@ -15,4 +16,19 @@ class Solution:
             idx = bisect.bisect_right(keys, d) - 1
             if idx >= 0:
                 ans += dp[keys[idx]]
+        return ans
+
+# 2nd solution
+# O(n * log(n) + m * log(m)) time | O(n) space
+# where n = len(profit) and m = len(worker)
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+        ans = j = max_profit = 0
+        for w in worker:
+            while j < len(jobs) and jobs[j][0] <= w:
+                max_profit = max(max_profit, jobs[j][1])
+                j += 1
+            ans += max_profit
         return ans
