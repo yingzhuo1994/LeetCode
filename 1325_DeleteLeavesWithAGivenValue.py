1ca880f27f03b466fb2376ef092fde5e207e5e94
yingzhuo <yingzhuo1994@gmail.com>
2024-05-16 20:17:52 -0500

    2nd solution

--- 1325_DeleteLeavesWithAGivenValue.py
+++ 1325_DeleteLeavesWithAGivenValue.py
@@ -30,3 +30,12 @@ class Solution:
         return root
 
             
+# 2nd solution
+# O(n) time | O(h) space
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if root.left:
+            root.left = self.removeLeafNodes(root.left, target)
+        if root.right:
+            root.right = self.removeLeafNodes(root.right, target)
+        return None if root.left == root.right and root.val == target else root
