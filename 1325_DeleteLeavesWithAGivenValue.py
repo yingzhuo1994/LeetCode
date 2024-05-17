ad4601cb24fb9b7049b2ef61941d71ea1f7e638b
yingzhuo <yingzhuo1994@gmail.com>
2024-05-16 20:16:00 -0500

    1st solution

---
+++ 1325_DeleteLeavesWithAGivenValue.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 1st solution
+# O(n) time | O(h) space
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def dfs(node, parent):
+            if not node:
+                return None
+            if node.left:
+                dfs(node.left, node)
+            if node.right:
+                dfs(node.right, node)
+            if not node.left and not node.right:
+                if node.val == target:
+                    if parent:
+                        if parent.left == node:
+                            parent.left = None
+                        else:
+                            parent.right = None
+        dfs(root, None)
+        if not root.left and not root.right:
+            if root.val == target:
+                return None
+        return root
+
+            
