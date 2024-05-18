34dfd0fe9d5f75de2dc3186a9053d9c0ea3b4340
yingzhuo <yingzhuo1994@gmail.com>
2024-05-17 21:08:43 -0500

    2nd solution

--- 2644_FindTheMaximumDivisibilityScore.py
+++ 2644_FindTheMaximumDivisibilityScore.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n^2) time | O(1) space
+# O(mn) time | O(1) space
 class Solution:
     def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
         ans = divisors[0]
@@ -14,4 +14,26 @@ class Solution:
                 ans = divisor
             elif score == count:
                 ans = min(ans, divisor)
+        return ans
+
+
+# 2st solution
+# O(mn + m * log(m) + n * log(n)) time | O(1) space
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        nums.sort(reverse=True)
+        dup = sum(1 for x, y in pairwise(nums) if x == y)
+        divisors.sort()
+        max_cnt, ans = -1, 0
+        for d in divisors:
+            if (max_cnt - dup + 1) * d > nums[0]:
+                break
+            cnt = 0
+            for x in nums:
+                if x < d:
+                    break
+                if x % d == 0:
+                    cnt += 1
+            if cnt > max_cnt:
+                max_cnt, ans = cnt, d
         return ans
