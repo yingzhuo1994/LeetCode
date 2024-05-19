7f5013ef2e61f231a1aa9df72aa397db18e45da7
yingzhuo <yingzhuo1994@gmail.com>
2024-05-18 22:12:25 -0500

    update

--- 3068_FindTheMaximumSumOfNodeValues.py
+++ 3068_FindTheMaximumSumOfNodeValues.py
@@ -2,14 +2,14 @@
 # O(n) time | O(n) space
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        g = [[] for _ in nums]
+        graph = [[] for _ in nums]
         for x, y in edges:
-            g[x].append(y)
-            g[y].append(x)
+            graph[x].append(y)
+            graph[y].append(x)
 
         def dfs(x: int, fa: int) -> (int, int):
             f0, f1 = 0, -inf  # f[x][0] 和 f[x][1]
-            for y in g[x]:
+            for y in graph[x]:
                 if y != fa:
                     r0, r1 = dfs(y, x)
                     f0, f1 = max(f0 + r0, f1 + r1), max(f1 + r0, f0 + r1)
