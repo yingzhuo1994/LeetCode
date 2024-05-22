54128c30a32bb68368fb2ed53414dc74deec8762
yingzhuo <yingzhuo1994@gmail.com>
2024-05-21 20:59:07 -0500

    2nd solution

--- 0883_ProjectionAreaOf3DShapes.py
+++ 0883_ProjectionAreaOf3DShapes.py
@@ -7,3 +7,13 @@ class Solution:
         xz = sum([max(row) for row in grid])
         yz = sum([max([grid[i][j] for i in range(n)]) for j in range(n)])
         return xy + xz + yz
+
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def projectionArea(self, grid: List[List[int]]) -> int:
+        hor = sum(map(max, grid))
+        ver = sum(map(max, zip(*grid)))
+        top = sum(v > 0 for row in grid for v in row)
+        return ver + hor + top
