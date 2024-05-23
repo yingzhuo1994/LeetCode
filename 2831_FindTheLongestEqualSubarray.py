d027005e152921c8c348ac9f9e1a89ba88e5c41c
yingzhuo <yingzhuo1994@gmail.com>
2024-05-22 21:45:02 -0500

    2nd solution

--- 2831_FindTheLongestEqualSubarray.py
+++ 2831_FindTheLongestEqualSubarray.py
@@ -24,3 +24,23 @@ class Solution:
                 length = i - start + 1
                 ans = max(ans, length)
         return ans
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        pos_lists = defaultdict(list)
+        for i, x in enumerate(nums):
+            pos_lists[x].append(i - len(pos_lists[x]))
+
+        ans = 0
+        for pos in pos_lists.values():
+            if len(pos) <= ans:
+                continue  # 无法让 ans 变得更大
+            left = 0
+            for right, p in enumerate(pos):
+                while p - pos[left] > k:  # 要删除的数太多了
+                    left += 1
+                ans = max(ans, right - left + 1)
+        return ans
