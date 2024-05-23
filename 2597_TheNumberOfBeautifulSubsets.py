06421003abd147a36d5bed51d137b087b03afb90
yingzhuo <yingzhuo1994@gmail.com>
2024-05-22 21:16:48 -0500

    2nd solution

--- 2597_TheNumberOfBeautifulSubsets.py
+++ 2597_TheNumberOfBeautifulSubsets.py
@@ -15,3 +15,24 @@ class Solution:
             count += dfs(idx + 1, lst + [nums[idx]])
             return count
         return dfs(0, [])
+
+
+# 2nd solution
+# O(n * log(n) + k) time | O(n + k) space
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        count = [Counter() for i in range(k)]
+        for a in nums:
+            count[a % k][a] += 1
+        res = 1
+        for i in range(k):
+            prev, dp0, dp1 = 0, 1, 0
+            for a in sorted(count[i]):
+                v = pow(2, count[i][a])
+                if prev + k == a:
+                    dp0, dp1 = dp0 + dp1, dp0 * (v - 1)
+                else:
+                    dp0, dp1 = dp0 + dp1, (dp0 + dp1) * (v - 1)
+                prev = a
+            res *= dp0 + dp1
+        return res - 1
