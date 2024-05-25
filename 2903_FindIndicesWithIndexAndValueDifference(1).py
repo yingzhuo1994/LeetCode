70d59a5e0e84d917af258143e05b2f81ff1f3f16
yingzhuo <yingzhuo1994@gmail.com>
2024-05-24 22:00:49 -0500

    2nd solution

--- 2903_FindIndicesWithIndexAndValueDifference(1).py
+++ 2903_FindIndicesWithIndexAndValueDifference(1).py
@@ -8,3 +8,21 @@ class Solution:
                 if abs(nums[i] - nums[j]) >= valueDifference:
                     return [i, j]
         return [-1, -1]
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
+        max_idx = min_idx = 0
+        for j in range(indexDifference, len(nums)):
+            i = j - indexDifference
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+            elif nums[i] < nums[min_idx]:
+                min_idx = i
+            if nums[max_idx] - nums[j] >= valueDifference:
+                return [max_idx, j]
+            if nums[j] - nums[min_idx] >= valueDifference:
+                return [min_idx, j]
+        return [-1, -1]
