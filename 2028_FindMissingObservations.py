b893b26851e7be287cefd44b012017085f49bdc9
yingzhuo <yingzhuo1994@gmail.com>
2024-05-26 20:45:43 -0500

    1st solution

---
+++ 2028_FindMissingObservations.py
@@ -0,0 +1,21 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        total = mean * (m + n)
+        target = total - sum(rolls)
+        if target > 6 * n or target < n:
+            return []
+        ans = []
+        k = n
+        for val in reversed(range(1, 7)):
+            q, r = divmod(target, val)
+            while k - q > r:
+                q -= 1
+                r += val
+            if q > 0:
+                ans += [val] * q
+                target -= q * val
+                k -= q
+        return ans
