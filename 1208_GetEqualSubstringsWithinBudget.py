a42104a91cb8e843a6f7a6dc9dad1cdde6c9e11c
yingzhuo <yingzhuo1994@gmail.com>
2024-05-27 20:41:59 -0500

    improve 1st solution

--- 1208_GetEqualSubstringsWithinBudget.py
+++ 1208_GetEqualSubstringsWithinBudget.py
@@ -4,11 +4,10 @@ class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
         start = 0
         cost = 0
-        ans = 0
+        n = len(s)
         for i, ch in enumerate(s):
             cost += abs(ord(ch) - ord(t[i]))
-            while cost > maxCost:
+            if cost > maxCost:
                 cost -= abs(ord(s[start]) - ord(t[start]))
                 start += 1
-            ans = max(ans, i - start + 1)
-        return ans
+        return n - start
