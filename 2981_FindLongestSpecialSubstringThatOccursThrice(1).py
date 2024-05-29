7e714b83732a4e4a21bbb59aa246b701ea355a59
yingzhuo <yingzhuo1994@gmail.com>
2024-05-28 20:09:29 -0500

    2nd solution

--- 2981_FindLongestSpecialSubstringThatOccursThrice(1).py
+++ 2981_FindLongestSpecialSubstringThatOccursThrice(1).py
@@ -17,4 +17,24 @@ class Solution:
                 if s[j:j+len(sub)] == sub:
                     dic[sub] += 1
         return max([len(sub) for sub in dic if dic[sub] >= 3] + [-1])
-      
+
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        groups = defaultdict(list)
+        cnt = 0
+        for i, ch in enumerate(s):
+            cnt += 1
+            if i + 1 == len(s) or ch != s[i + 1]:
+                groups[ch].append(cnt)  # 统计连续字符长度
+                cnt = 0
+
+        ans = 0
+        for a in groups.values():
+            a.sort(reverse=True)
+            a.extend([0, 0])  # 假设还有两个空串
+            ans = max(ans, a[0] - 2, min(a[0] - 1, a[1]), a[2])
+
+        return ans if ans else -1
