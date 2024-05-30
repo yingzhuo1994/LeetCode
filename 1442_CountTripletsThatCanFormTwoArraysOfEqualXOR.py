1f1a59960268acc1ea7778181102b23726218f67
yingzhuo <yingzhuo1994@gmail.com>
2024-05-29 20:20:19 -0500

    2nd solution

--- 1442_CountTripletsThatCanFormTwoArraysOfEqualXOR.py
+++ 1442_CountTripletsThatCanFormTwoArraysOfEqualXOR.py
@@ -17,3 +17,24 @@ class Solution:
                     if a == b:
                         count += 1
         return count
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def countTriplets(self, arr: List[int]) -> int:
+        n = len(arr)
+        preXOR = [0 for _ in range(n+1)]
+        x = 0
+        for i, num in enumerate(arr):
+            x ^= num
+            preXOR[i] = x
+        count = 0
+        for j in range(1, n):
+            dic = {}
+            for i in range(j):
+                a = preXOR[j-1] ^ preXOR[i-1]
+                dic[a] = dic.get(a, 0) + 1
+            for k in range(j, n):
+                b = preXOR[k] ^ preXOR[j-1]
+                count += dic.get(b, 0)
+        return count
