8a926a167d5b069004ab0493cf6f7b97ff7bd98e
yingzhuo <yingzhuo1994@gmail.com>
2024-05-31 21:13:21 -0500

    2nd solution

--- 2928_DistributeCandiesAmongChildren(1).py
+++ 2928_DistributeCandiesAmongChildren(1).py
@@ -10,3 +10,12 @@ class Solution:
                 ans += dfs(k-1, left - i)
             return ans
         return dfs(3, n)
+
+
+# 2nd solution
+# O(1) time : O(1) space
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        def c2(n: int) -> int:
+            return n * (n - 1) // 2 if n > 1 else 0
+        return c2(n + 2) - 3 * c2(n - limit + 1) + 3 * c2(n - 2 * limit) - c2(n - 3 * limit - 1)
