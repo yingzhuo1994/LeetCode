c81448b3c1529b5a61a10b88e5bc80dfeeb11319
yingzhuo <yingzhuo1994@gmail.com>
2024-06-01 19:41:55 -0500

    1st solution

---
+++ 0884_UncommonWordsFromTwoSentences.py
@@ -0,0 +1,14 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        cnt1 = Counter(s1.split())
+        cnt2 = Counter(s2.split())
+        ans = []
+        for word in cnt1:
+            if word not in cnt2 and cnt1[word] == 1:
+                ans.append(word)
+        for word in cnt2:
+            if word not in cnt1 and cnt2[word] == 1:
+                ans.append(word)
+        return ans
