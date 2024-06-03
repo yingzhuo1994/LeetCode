7ee405a1da529d6cd485292715830a60b5e3862d
yingzhuo <yingzhuo1994@gmail.com>
2024-06-02 20:28:07 -0500

    3rd solution

--- 1103_DistributeCandiesToPeople.py
+++ 1103_DistributeCandiesToPeople.py
@@ -25,7 +25,7 @@ class Solution:
         c = n * n + n - 2 * candies
         k = (sqrt(b**2 - 4 * a * c) - b) / (2 * a)
         k = floor(k)
-        
+
         ans = [(i + k * n + i) * (k + 1) // 2 for i in range(1, n + 1)]
 
         candies -= ((k + 1) * n**2 + n) * (k + 1) // 2
@@ -39,3 +39,17 @@ class Solution:
             i += 1
 
         return ans
+
+# 3rd solution
+# O(n) time | O(n) space
+# where m = candies, n = num_people
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        n = num_people
+        x = int(math.sqrt(candies * 2 + 0.25) - 0.5)
+        res = [0] * n
+        for i in range(n):
+            m = x // n + (x % n > i)
+            res[i] = m * (i + 1) + m * (m - 1) // 2 * n
+        res[x % n] += candies - x * (x + 1) // 2
+        return res
