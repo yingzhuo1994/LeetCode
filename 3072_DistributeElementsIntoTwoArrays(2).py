82954887cc42a360dd6e359dc16e3b62032f42ce
yingzhuo <yingzhuo1994@gmail.com>
2024-06-05 21:49:48 -0500

    2nd solution

--- 3072_DistributeElementsIntoTwoArrays(2).py
+++ 3072_DistributeElementsIntoTwoArrays(2).py
@@ -69,3 +69,46 @@ class Fenwick:
             res += self.tree[i]
             i &= i - 1
         return res
+
+
+# 3rd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(set(nums))
+        m = len(sorted_nums)
+        a = [nums[0]]
+        b = [nums[1]]
+        t = Fenwick(m + 1)
+        t.add(m - bisect_left(sorted_nums, nums[0]), 1)
+        t.add(m - bisect_left(sorted_nums, nums[1]), -1)
+        for x in nums[2:]:
+            v = m - bisect_left(sorted_nums, x)
+            d = t.pre(v - 1)  # 转换成 < v 的元素个数之差
+            if d > 0 or d == 0 and len(a) <= len(b):
+                a.append(x)
+                t.add(v, 1)
+            else:
+                b.append(x)
+                t.add(v, -1)
+        return a + b
+
+class Fenwick:
+    __slots__ = 'tree'
+
+    def __init__(self, n: int):
+        self.tree = [0] * n
+
+    # 把下标为 i 的元素增加 v
+    def add(self, i: int, v: int) -> None:
+        while i < len(self.tree):
+            self.tree[i] += v
+            i += i & -i
+
+    # 返回下标在 [1,i] 的元素之和
+    def pre(self, i: int) -> int:
+        res = 0
+        while i > 0:
+            res += self.tree[i]
+            i &= i - 1
+        return res
