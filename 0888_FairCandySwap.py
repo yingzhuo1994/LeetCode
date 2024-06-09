500efee3e80995873ceb5f27829efdf9f6fb30ca
yingzhuo <yingzhuo1994@gmail.com>
2024-06-08 21:29:20 -0500

    2nd solution

--- 0888_FairCandySwap.py
+++ 0888_FairCandySwap.py
@@ -8,5 +8,25 @@ class Solution:
         target = sum(A) - sum(B)
         for i in range(len(A)):
             idx = bisect.bisect_left(B, A[i] - target // 2)
-            if 0 <= idx < len(B) and 2 *(A[i] - B[idx]) == target:
+            if idx < len(B) and 2 *(A[i] - B[idx]) == target:
                 return [A[i], B[idx]]
+
+# 2nd solution
+# O((m + n) * log(n)) time | O(n) space
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        A = aliceSizes
+        B = bobSizes
+        target = sum(A) - sum(B)
+        if len(A) <= len(B):
+            B.sort()
+            for i in range(len(A)):
+                j = bisect.bisect_left(B, A[i] - target // 2)
+                if j < len(B) and 2 * (A[i] - B[j]) == target:
+                    return [A[i], B[j]]
+        else:
+            A.sort()
+            for j in range(len(B)):
+                i = bisect.bisect_left(A, B[j] + target // 2)
+                if i < len(A) and 2 * (A[i] - B[j]) == target:
+                    return [A[i], B[j]]
