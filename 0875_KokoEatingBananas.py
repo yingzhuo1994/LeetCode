954774c3b83b54b80cc7f21e5c22727e958abd03
yingzhuo <yingzhuo1994@gmail.com>
2024-06-09 11:08:31 -0500

    2nd solution

--- 0875_KokoEatingBananas.py
+++ 0875_KokoEatingBananas.py
@@ -1,5 +1,6 @@
 # 1st solution
-# O(n * log(n)) time | O(1) space
+# O(n * log(U)) time | O(1) space
+# where U = max(piles)
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         n = len(piles)
@@ -22,3 +23,22 @@ class Solution:
         for pile in piles:
             count += (pile + k - 1) // k
         return count <= h
+
+
+# 2nd solution
+# O(n * log(U)) time | O(1) space
+# where U = max(piles)
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        start = 1
+        end = max(piles)
+        ans = end
+        while start <= end:
+            mid = start + (end - start) // 2
+            t = sum([(pile + mid - 1) // mid for pile in piles])
+            if t <= h:
+                ans = min(ans, mid)
+                end = mid - 1
+            else:
+                start = mid + 1
+        return ans
