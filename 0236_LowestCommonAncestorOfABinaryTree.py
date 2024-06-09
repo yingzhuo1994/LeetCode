187d211912fd34fe937a137b698a2d39be808db7
yingzhuo <yingzhuo1994@gmail.com>
2024-06-09 11:25:40 -0500

    simplify the 4th solution

--- 0236_LowestCommonAncestorOfABinaryTree.py
+++ 0236_LowestCommonAncestorOfABinaryTree.py
@@ -155,9 +155,12 @@ class Solution:
 # O(n) time | O(h) space
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root == None or root == p or root == q: return root
+        if root == None or root == p or root == q:
+            return root
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-        if left != None and right != None: return root
-        if left != None: return left
-        return right
+
+        if left != None and right != None:
+            return root
+
+        return left or right
