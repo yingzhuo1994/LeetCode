c8f7fa7b704190cac7bd92c3eebb06030118adb5
yingzhuo <yingzhuo1994@gmail.com>
2024-06-09 21:21:09 -0500

    2nd solution

--- 1051_HeightChecker.py
+++ 1051_HeightChecker.py
@@ -8,3 +8,23 @@ class Solution:
             if num != expected[i]:
                 ans += 1
         return ans
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        max_val = max(heights)
+        
+        # Create frequency table
+        freq = [0] * (max_val + 1)
+        for num in heights: freq[num] += 1
+        for num in range(1, len(freq)): freq[num] += freq[num-1]
+
+        # Create places table
+        places = [0] * len(heights)
+        for num in heights:
+            places[freq[num]-1] = num
+            freq[num] -= 1
+
+        return sum([a!=b for a, b in zip(heights, places)])
