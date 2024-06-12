3a7b6d927d3f32309bbcd1d5b0ce3641653c74a6
yingzhuo <yingzhuo1994@gmail.com>
2024-06-11 21:02:17 -0500

    2nd solution

--- 2806_AccountBalanceAfterRoundedPurchase.py
+++ 2806_AccountBalanceAfterRoundedPurchase.py
@@ -7,3 +7,9 @@ class Solution:
             return 100 - (q + 1) * 10
         else:
             return 100 - q * 10
+
+# 2nd solution
+# O(1) time | O(1) space 
+class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+        return 100 - (purchaseAmount + 5) // 10 * 10
