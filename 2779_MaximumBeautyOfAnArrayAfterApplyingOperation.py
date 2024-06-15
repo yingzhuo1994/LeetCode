c77a524f2bd624501d4deabee09ef0e27d458050
yingzhuo <yingzhuo1994@gmail.com>
2024-06-14 20:29:07 -0500

    3rd solution

--- 2779_MaximumBeautyOfAnArrayAfterApplyingOperation.py
+++ 2779_MaximumBeautyOfAnArrayAfterApplyingOperation.py
@@ -23,3 +23,15 @@ class Solution:
                 left += 1
             ans = max(ans, right - left + 1)
         return ans
+
+
+# 3rd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        for num in nums:
+            if num - nums[left] > k * 2:
+                left += 1
+        return len(nums) - left
