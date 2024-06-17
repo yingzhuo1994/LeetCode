85715ac3b5a46fa1f1d71db20a74102ca465eecf
yingzhuo <yingzhuo1994@gmail.com>
2024-06-16 20:38:45 -0500

    2nd solution

--- 0768_MaxChunksToMakeSorted(2).py
+++ 0768_MaxChunksToMakeSorted(2).py
@@ -14,3 +14,19 @@ class Solution:
             if len(cnt) == 0:
                 res += 1
         return res
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def maxChunksToSorted(self, arr: [int]) -> int:
+        stack = []
+        for num in arr:
+            if len(stack) == 0 or num >= stack[-1]:
+                stack.append(num)
+            else:
+                mx = stack.pop()
+                while stack and stack[-1] > num:
+                    stack.pop()
+                stack.append(mx)
+        return len(stack)
