8f99a63d6d6b1f84317506882332f24d31421bb1
yingzhuo <yingzhuo1994@gmail.com>
2024-06-16 19:58:54 -0500

    improve the 1st solution

--- 0768_MaxChunksToMakeSorted(2).py
+++ 0768_MaxChunksToMakeSorted(2).py
@@ -2,17 +2,15 @@
 # O(n * log(n)) time | O(n) space
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        sorted_arr = sorted(arr)
-        ans = 0
-        mask1 = 0
-        mask2 = 0
-        dic = {}
-        for i, num in enumerate(arr):
-            idx = bisect.bisect_left(sorted_arr, num, lo=dic.get(num, 0))
-            mask1 |= 1 << idx
-            mask2 |= 1 << i
-            dic[num] = idx + 1
-
-            if mask1 == mask2:
-                ans += 1
-        return ans
+        cnt = Counter()
+        res = 0
+        for x, y in zip(arr, sorted(arr)):
+            cnt[x] += 1
+            if cnt[x] == 0:
+                del cnt[x]
+            cnt[y] -= 1
+            if cnt[y] == 0:
+                del cnt[y]
+            if len(cnt) == 0:
+                res += 1
+        return res
