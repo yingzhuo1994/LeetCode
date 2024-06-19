aa7145329b9bd6e4703291694d1bda6f5463adee
yingzhuo <yingzhuo1994@gmail.com>
2024-06-18 22:05:23 -0500

    2nd solution

--- 2713_MaximumStrictlyIncreasingCellsInAMatrix.py
+++ 2713_MaximumStrictlyIncreasingCellsInAMatrix.py
@@ -36,3 +36,22 @@ class Solution:
 
         return max([max(line) for line in dp])
     
+
+# 2nd solution
+# O(mn * log(mn)) time | O(mn) space
+class Solution:
+    def maxIncreasingCells(self, mat: List[List[int]]) -> int:
+        g = defaultdict(list)
+        for i, row in enumerate(mat):
+            for j, x in enumerate(row):
+                g[x].append((i, j))  # 相同元素放在同一组，统计位置
+
+        row_max = [0] * len(mat)
+        col_max = [0] * len(mat[0])
+        for _, pos in sorted(g.items(), key=lambda p: p[0]):
+            # 先把最大值算出来，再更新 row_max 和 col_max
+            mx = [max(row_max[i], col_max[j]) + 1 for i, j in pos]
+            for (i, j), f in zip(pos, mx):
+                row_max[i] = max(row_max[i], f)  # 更新第 i 行的最大 f 值
+                col_max[j] = max(col_max[j], f)  # 更新第 j 列的最大 f 值
+        return max(row_max)
