a416f2e1bbd1b647497dd3b5e527ac5b42c9f182
yingzhuo <yingzhuo1994@gmail.com>
2024-06-19 21:25:09 -0500

    2nd solution

--- 2748_NumberOfBeautifulPairs.py
+++ 2748_NumberOfBeautifulPairs.py
@@ -14,3 +14,19 @@ class Solution:
                 if gcd(a, b) == 1:
                     ans += 1
         return ans
+
+# 2nd solution
+# O(n * (k + logU)) time | O(k) time
+# where n = len(nums), k = 10, U = max(nums)
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        ans = 0
+        cnt = [0] * 10
+        for x in nums:
+            for y, c in enumerate(cnt):
+                if c and gcd(y, x % 10) == 1:
+                    ans += c
+            while x >= 10: 
+                x //= 10
+            cnt[x] += 1  # 统计最高位的出现次数
+        return ans
