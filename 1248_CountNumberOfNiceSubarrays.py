363ddc9f466e5d95229424dd8db6b6a096ce6046
yingzhuo <yingzhuo1994@gmail.com>
2024-06-21 19:58:00 -0500

    2nd solution

--- 1248_CountNumberOfNiceSubarrays.py
+++ 1248_CountNumberOfNiceSubarrays.py
@@ -11,3 +11,20 @@ class Solution:
             dic[freq] = dic.get(freq, 0) + 1
             ans += dic.get(freq - k, 0)
         return ans
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        i = count = res = 0
+        for j in range(len(nums)):
+            if nums[j] & 1:
+                k -= 1
+                count = 0
+            while k == 0:
+                k += nums[i] & 1
+                i += 1
+                count += 1
+            res += count
+        return res
