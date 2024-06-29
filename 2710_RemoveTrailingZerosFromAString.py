5a81f3fbbcfdc36724d07b8d140ff2d5fa29b28c
yingzhuo <yingzhuo1994@gmail.com>
2024-06-28 21:04:45 -0500

    1st solution

---
+++ 2710_RemoveTrailingZerosFromAString.py
@@ -0,0 +1,8 @@
+# 1st solution
+# O(n) time | O(n) space
+class Solution:
+    def removeTrailingZeros(self, num: str) -> str:
+        i = len(num) - 1
+        while i > 0 and num[i] == "0":
+            i -= 1
+        return num[:i+1]
