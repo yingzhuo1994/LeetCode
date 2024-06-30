65e757ec6144289bf43c8156a70413fe70bf44ad
yingzhuo <yingzhuo1994@gmail.com>
2024-06-29 22:07:42 -0500

    3rd solution

--- 0795_NumberOfSubarraysWithBoundedMaximum.py
+++ 0795_NumberOfSubarraysWithBoundedMaximum.py
@@ -51,3 +51,17 @@ class Solution:
             ans += max(lastValidIdx - start + 1, 0)
             
         return ans
+    
+
+# 3rd solution
+# O(n) time | O(1) space
+class Solution:
+    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+        def count(bound):
+            ans = cnt = 0
+            for num in nums:
+                cnt = cnt + 1 if num <= bound else 0
+                ans += cnt
+            return ans
+
+        return count(right) - count(left - 1)
