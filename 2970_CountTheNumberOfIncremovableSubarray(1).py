05a90b11977cfc1a0c51d6ac16ca5d0f513995fe
yingzhuo <yingzhuo1994@gmail.com>
2024-07-09 21:01:18 -0500

    2nd solution

--- 2970_CountTheNumberOfIncremovableSubarray(1).py
+++ 2970_CountTheNumberOfIncremovableSubarray(1).py
@@ -22,3 +22,35 @@ class Solution:
                 array = front + back
                 ans += isIncreasingArray(array)
         return ans
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+        prefix = [True] * (n + 1)
+        for i in range(1, n):
+            if nums[i] <= nums[i-1]:
+                for j in range(i + 1, n):
+                    prefix[j] = False
+                break
+        suffix = [True] * (n + 1)
+        for i in reversed(range(n - 1)):
+            if nums[i] >= nums[i+1]:
+                for j in reversed(range(i)):
+                    suffix[j] = False
+                break
+
+        for i in range(n):
+            if not prefix[i]:
+                continue
+            for j in range(i, n):
+                if not suffix[j]:
+                    continue
+
+                if i == 0 or j == n - 1:
+                    ans += 1
+                else:
+                    ans += nums[i - 1] < nums[j + 1]
+        return ans
