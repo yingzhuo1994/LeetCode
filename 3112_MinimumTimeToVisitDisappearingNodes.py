8ebd2105c15d465e5efdee1f48b4cb40331b5ed5
yingzhuo <yingzhuo1994@gmail.com>
2024-07-17 21:04:31 -0500

    improve the answer

--- 3112_MinimumTimeToVisitDisappearingNodes.py
+++ 3112_MinimumTimeToVisitDisappearingNodes.py
@@ -6,34 +6,20 @@ class Solution:
         for a, b, w in edges:
             graph[a].append([b, w])
             graph[b].append([a, w])
-        disappearDic = {}
-        for i, t in enumerate(disappear):
-            if t not in disappearDic:
-                disappearDic[t] = []
-            disappearDic[t].append(i)
 
-
-        validNodes = [True for _ in range(n)]
-        visited = [False for _ in range(n)]
-        ans = [float("inf") for _ in range(n)]
-        ans[0] = 0
+        dist = [-1 for _ in range(n)]
+        dist[0] = 0
         minHeap = [[0, 0]] # time, node
-        for t in sorted(disappearDic.keys()):
-            lst = disappearDic[t]
-            while minHeap and minHeap[0][0] < t:
-                t1, node = heappop(minHeap)
-                if not validNodes[node] or visited[node]:
-                    continue
-                visited[node] = True
-                for neig, w in graph[node]:
-                    if not validNodes[neig] or visited[neig]:
-                        continue
-                    t2 = t1 + w
-                    if t2 < disappear[neig]:
-                        ans[neig] = min(ans[neig], t2)
-                    if t2 <= ans[neig]:
-                        heappush(minHeap, [t2, neig])
+
+        while minHeap:
+            t1, node = heappop(minHeap)
+            if t1 > dist[node]:
+                continue
+            for neig, w in graph[node]:
+
+                t2 = t1 + w
+                if t2 < disappear[neig] and (dist[neig] < 0 or t2 < dist[neig]):
+                    dist[neig] = t2
+                    heappush(minHeap, [t2, neig])
                     
-            for i in lst:
-                validNodes[i] = False
-        return [t if t != float("inf") else -1 for t in ans ]
+        return dist
