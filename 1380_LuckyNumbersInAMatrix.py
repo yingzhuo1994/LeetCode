aec157dc62bf56d1b0baa068952ba88a682e030f
yingzhuo <yingzhuo1994@gmail.com>
2024-07-18 20:48:12 -0500

    2nd solution

--- 1380_LuckyNumbersInAMatrix.py
+++ 1380_LuckyNumbersInAMatrix.py
@@ -11,3 +11,14 @@ class Solution:
                 if matrix[i][j] == minRow[i] and matrix[i][j] == maxCol[j]:
                     ans.append(matrix[i][j])
         return ans
+
+# 2nd solution
+# O(mn) time | O(1) space
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        rowMax, k = 0, 0
+        for row in matrix:
+            minRow = min(row)
+            if minRow > rowMax:
+                rowMax, k = minRow, row.index(minRow)
+        return [rowMax] if all(row[k] <= rowMax for row in matrix) else []
