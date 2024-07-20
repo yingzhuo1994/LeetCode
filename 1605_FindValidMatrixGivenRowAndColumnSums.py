9ed6ef364697e1ba77d03750694ad996b0da426c
yingzhuo <yingzhuo1994@gmail.com>
2024-07-19 22:30:14 -0500

    2nd solution

--- 1605_FindValidMatrixGivenRowAndColumnSums.py
+++ 1605_FindValidMatrixGivenRowAndColumnSums.py
@@ -39,4 +39,16 @@ class Solution:
 
         return matrix
         
-        
+
+# 2nd solution
+# O(mn) time | O(mn) space
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        m, n = len(rowSum), len(colSum)
+        matrix = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                matrix[i][j] = min(rowSum[i], colSum[j])
+                rowSum[i] -= matrix[i][j]
+                colSum[j] -= matrix[i][j]
+        return matrix
