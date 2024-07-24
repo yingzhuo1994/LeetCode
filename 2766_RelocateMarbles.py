741c867f31b1ae773ac3c8d80c950dff30f46f89
yingzhuo <yingzhuo1994@gmail.com>
2024-07-23 20:08:21 -0500

    2nd solution

--- 2766_RelocateMarbles.py
+++ 2766_RelocateMarbles.py
@@ -12,3 +12,15 @@ class Solution:
             locations[b] = 1
         ans = [i for i in range(n) if locations[i] > 0]
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
+        locations = {}
+        for num in nums:
+            locations[num] = 1
+        for a, b in zip(moveFrom, moveTo):
+            del locations[a]
+            locations[b] = 1
+        return sorted(list(locations.keys()))
