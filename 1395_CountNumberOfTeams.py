2013f113b492be4f67e45029468e0a5ef385bce1
yingzhuo <yingzhuo1994@gmail.com>
2024-07-28 20:44:55 -0500

    1st solution

---
+++ 1395_CountNumberOfTeams.py
@@ -0,0 +1,14 @@
+# 1st solution
+# O(n^2) time | O(n) space
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        ans = 0
+        for i, val in enumerate(rating):
+            left = [rating[j] for j in range(i) if rating[j] < val]
+            right = [rating[j] for j in range(i + 1, len(rating)) if rating[j] > val]
+            ans += len(left) * len(right)
+
+            left = [rating[j] for j in range(i) if rating[j] > val]
+            right = [rating[j] for j in range(i + 1, len(rating)) if rating[j] < val]
+            ans += len(left) * len(right)
+        return ans
