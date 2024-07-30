0a173ed0d4481a94cd744fd5d184a414ca19dcb5
yingzhuo <yingzhuo1994@gmail.com>
2024-07-29 19:28:06 -0500

    2nd solution

--- 1653_MinimumDeletionsToMakeStringBalanced.py
+++ 1653_MinimumDeletionsToMakeStringBalanced.py
@@ -20,3 +20,23 @@ class Solution:
                     dfs(idx + 1, "b", count + 1)
         dfs(0, "a", 0)
         return self.ans
+
+# 2nd
+# O(n) time | O(n) space
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        n = len(s)
+        dp_a = [0 for _ in range(n + 1)]
+        dp_b = [0 for _ in range(n + 1)]
+        for i, ch in enumerate(s):
+            dp_a[i] = dp_a[i - 1]
+            dp_b[i] = dp_b[i - 1]
+            if ch == "a":
+                dp_a[i] += 1
+            else:
+                dp_b[i] += 1
+        ans = float("inf")
+        for i in range(n + 1):
+            count = dp_b[i - 1] + dp_a[n-1] - dp_a[i - 1]
+            ans = min(ans, count)
+        return ans
