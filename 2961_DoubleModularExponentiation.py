7c0782f5b7c441f1d18a3be0693a2a5db4adf32d
yingzhuo <yingzhuo1994@gmail.com>
2024-07-29 19:52:10 -0500

    1st solution

---
+++ 2961_DoubleModularExponentiation.py
@@ -0,0 +1,6 @@
+# 1st solution
+# O(n * log(U)) time | O(1) space
+class Solution:
+    def getGoodIndices(self, variables: List[List[int]], target: int) -> List[int]:
+        return [i for i, (a, b, c, m) in enumerate(variables)
+                if pow(pow(a, b, 10), c, m) == target]
