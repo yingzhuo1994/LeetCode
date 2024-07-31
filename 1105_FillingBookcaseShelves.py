e9917ded570f47576a5dc846081974a2b7fac57f
yingzhuo <yingzhuo1994@gmail.com>
2024-07-30 19:51:49 -0500

    improve the 1st solution

--- 1105_FillingBookcaseShelves.py
+++ 1105_FillingBookcaseShelves.py
@@ -4,15 +4,16 @@ class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         n = len(books)
         books.append([0, 0])
-        widths = [0 for _ in range(n + 1)]
-        for i in range(n):
-            widths[i] = widths[i - 1] + books[i][0]
         minHeights = [float("inf") for _ in range(n + 1)]
         minHeights[-1] = 0
         for i in range(n):
             maxHeight = 0
+            curWidth = 0
             for j in reversed(range(i + 1)):
-                if widths[i] - widths[j - 1] <= shelfWidth:
+                curWidth += books[j][0]
+                if curWidth <= shelfWidth:
                     maxHeight = max(maxHeight, books[j][1])
                     minHeights[i] = min(minHeights[i], minHeights[j - 1] + maxHeight)
+                else:
+                    break
         return minHeights[n-1]
