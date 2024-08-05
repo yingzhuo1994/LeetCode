ce94172970958f3e55a5bad5fba4ed59e77e2a2c
yingzhuo <yingzhuo1994@gmail.com>
2024-08-04 20:32:56 -0500

    2nd solution

--- 2084_KthDistinctStringInAnArray.py
+++ 2084_KthDistinctStringInAnArray.py
@@ -12,3 +12,20 @@ class Solution:
                 order += 1
             if order == k:
                 return s
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        dic = {}
+        for i, s in enumerate(arr):
+            if s not in dic:
+                dic[s] = []
+            if len(dic[s]) > 1:
+                continue
+            dic[s].append(i)
+        distinct_strs = [[dic[s][0], s] for s in dic if len(dic[s]) == 1]
+        if len(distinct_strs) < k:
+            return ""
+        distinct_strs.sort()
+        return distinct_strs[k-1][1]
