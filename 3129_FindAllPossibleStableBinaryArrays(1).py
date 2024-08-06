d15fd08853fbdad78cbb2126a5773273fb02e075
yingzhuo <yingzhuo1994@gmail.com>
2024-08-05 20:56:59 -0500

    2nd solution

--- 3129_FindAllPossibleStableBinaryArrays(1).py
+++ 3129_FindAllPossibleStableBinaryArrays(1).py
@@ -16,3 +16,19 @@ class Solution:
         ans = (dfs(zero, one, 0) + dfs(zero, one, 1)) % MOD
         dfs.cache_clear()  # 防止爆内存
         return ans
+
+# 2nd solution
+# O(zero * one) time | O(zero * one) space
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 1_000_000_007
+        f = [[[0, 0] for _ in range(one + 1)] for _ in range(zero + 1)]
+        for i in range(1, min(limit, zero) + 1):
+            f[i][0][0] = 1
+        for j in range(1, min(limit, one) + 1):
+            f[0][j][1] = 1
+        for i in range(1, zero + 1):
+            for j in range(1, one + 1):
+                f[i][j][0] = (f[i - 1][j][0] + f[i - 1][j][1] - (f[i - limit - 1][j][1] if i > limit else 0)) % MOD
+                f[i][j][1] = (f[i][j - 1][0] + f[i][j - 1][1] - (f[i][j - limit - 1][0] if j > limit else 0)) % MOD
+        return sum(f[-1][-1]) % MOD
