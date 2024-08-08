18cc82653cd0a69dfa2dd7e80ab84a727593917a
yingzhuo <yingzhuo1994@gmail.com>
2024-08-07 20:43:40 -0500

    2nd solution

--- 0885_SpiralMatrix(3).py
+++ 0885_SpiralMatrix(3).py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(mn) time | O(mn) space
+# O(max(m, n)^2) time | O(mn) space
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -24,4 +24,23 @@ class Solution:
                         break
                 else:
                     break
+        return ans
+
+
+# 2nd solution
+# O(max(m, n)^2) time | O(mn) space
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        ans = []
+        dx, dy = 0, 1
+        r = 0
+        x, y = rStart, cStart
+        while len(ans) < rows * cols:
+            for _ in range(r // 2 + 1):
+                if 0 <= x < rows and 0 <= y < cols:
+                    ans.append([x, y])
+                x += dx
+                y += dy
+            dx, dy = dy, -dx
+            r += 1
         return ans
