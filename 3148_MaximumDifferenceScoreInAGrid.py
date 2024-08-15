49c531aec066f9a7feade003f1a71dd4b8d72de3
yingzhuo <yingzhuo1994@gmail.com>
2024-08-14 20:57:10 -0500

    2nd solution

--- 3148_MaximumDifferenceScoreInAGrid.py
+++ 3148_MaximumDifferenceScoreInAGrid.py
@@ -23,3 +23,17 @@ class Solution:
                 if j > 0:
                     diff = max(diff, grid[i][j] - grid[i][j-1])
         return diff
+
+# 2nd solution
+# O(mn) time | O(mn) space
+class Solution:
+    def maxScore(self, grid: List[List[int]]) -> int:
+        ans = -inf
+        m, n = len(grid), len(grid[0])
+        f = [[inf] * (n + 1) for _ in range(m + 1)]
+        for i, row in enumerate(grid):
+            for j, x in enumerate(row):
+                mn = min(f[i + 1][j], f[i][j + 1])
+                ans = max(ans, x - mn)
+                f[i + 1][j + 1] = min(mn, x)
+        return ans
