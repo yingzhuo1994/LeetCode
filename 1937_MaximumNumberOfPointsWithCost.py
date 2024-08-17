4408a53dedbbcffeb16c36d8e6334f59e1fa81a6
yingzhuo <yingzhuo1994@gmail.com>
2024-08-16 22:30:30 -0500

    2nd solution

--- 1937_MaximumNumberOfPointsWithCost.py
+++ 1937_MaximumNumberOfPointsWithCost.py
@@ -15,3 +15,22 @@ class Solution:
             for j in range(n):
                 dp[i][j] = max(left[j], right[j]) + points[i][j]
         return max(dp[-1])
+
+# 2nd solution
+# O(mn) time | O(n) space
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        m, n = len(points), len(points[0])
+        dp = points[0]
+        for i in range(1, m):
+            left = dp[:]
+            right = dp[:]
+
+            for j in range(1, n):
+                left[j] = max(left[j-1] - 1, left[j])
+            for j in reversed(range(n - 1)):
+                right[j] = max(right[j + 1] - 1, right[j])
+            for j in range(n):
+                dp[j] = max(left[j], right[j]) + points[i][j]
+        
+        return max(dp)
