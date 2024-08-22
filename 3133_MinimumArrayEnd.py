3376849d2ad670fd2f5c66b657f607adad9b249f
yingzhuo <yingzhuo1994@gmail.com>
2024-08-21 20:52:21 -0500

    2nd solution

--- 3133_MinimumArrayEnd.py
+++ 3133_MinimumArrayEnd.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(1) time | O(1) space
+# O(log(n) + log(x)) time | O(1) space
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
         mask = n - 1
@@ -12,4 +12,19 @@ class Solution:
                 x |= (1 << i)
             d += 1
             i += 1
+        return x
+
+
+# 2nd solution
+# O(log(n)) time | O(1) space
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        n -= 1
+        j = 0
+        t = ~x
+        while n >> j:
+            lb = t & -t
+            x |= (n >> j & 1) * lb
+            j += 1
+            t ^= lb
         return x
