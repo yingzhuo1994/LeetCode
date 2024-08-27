2a2b6af407608b1b90bb4fe2e394bde395c4616a
yingzhuo <yingzhuo1994@gmail.com>
2024-08-26 21:52:06 -0500

    2nd solution

--- 3154_FindNumberOfWaysToReachTheK-thStair.py
+++ 3154_FindNumberOfWaysToReachTheK-thStair.py
@@ -20,3 +20,16 @@ class Solution:
             return count
         ans = dfs(1, 0, False)
         return ans
+
+# 2nd solution
+# O(1) time | O(1) space
+import math
+class Solution:
+    def waysToReachStair(self, k: int) -> int:
+        ans = 0
+        for j in count(max(k - 1, 0).bit_length()):
+            m = (1 << j) - k
+            if m > j + 1:
+                break
+            ans += math.comb(j + 1, m)
+        return ans
