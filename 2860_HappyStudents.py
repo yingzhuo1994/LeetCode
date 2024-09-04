b6d14d8a7285fcb81b957cfd5e650c48ecf1b231
yingzhuo <yingzhuo1994@gmail.com>
2024-09-03 20:30:31 -0500

    2nd solution

--- 2860_HappyStudents.py
+++ 2860_HappyStudents.py
@@ -13,3 +13,17 @@ class Solution:
             if idx > nums[idx - 1] and (idx == n or nums[idx] > idx):
                 ans += 1
         return ans
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def countWays(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        ans = 0 + nums[0] != 0
+        idx = 0
+        while idx < n:
+            idx = bisect.bisect_left(nums, nums[idx] + 1)
+            if idx > nums[idx - 1] and (idx == n or nums[idx] > idx):
+                ans += 1
+        return ans
