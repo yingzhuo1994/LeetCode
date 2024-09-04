b9ed58e7d0f4ecab87d78b83b2fdcb6c1ca61c03
yingzhuo <yingzhuo1994@gmail.com>
2024-09-03 20:28:51 -0500

    1st solution

---
+++ 2860_HappyStudents.py
@@ -0,0 +1,15 @@
+# 1st solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def countWays(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        ans = 0 + nums[0] != 0
+        idx = 0
+        while idx < n:
+            target = nums[idx] + 1
+            while idx < n and nums[idx] < target:
+                idx += 1
+            if idx > nums[idx - 1] and (idx == n or nums[idx] > idx):
+                ans += 1
+        return ans
