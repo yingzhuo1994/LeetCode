6a8d5b2704449a01e48071199743e434aa87e495
yingzhuo <yingzhuo1994@gmail.com>
2024-09-06 22:13:27 -0500

    2nd solution

--- 1367_LinkedListInBinaryTree.py
+++ 1367_LinkedListInBinaryTree.py
@@ -43,3 +43,16 @@ class Solution:
             p = p.next
         return True
 
+
+# 2nd solution
+# O(N * min(L, H)) time | O(H) space
+# where N = tree size, H = tree height, L = list length
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        def dfs(head, root):
+            if not head: return True
+            if not root: return False
+            return root.val == head.val and (dfs(head.next, root.left) or dfs(head.next, root.right))
+        if not head: return True
+        if not root: return False
+        return dfs(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
