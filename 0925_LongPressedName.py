b205c329e59a6cc1f1ae25d3fdb34ea3dd252f61
yingzhuo <yingzhuo1994@gmail.com>
2024-09-07 19:21:28 -0500

    2nd solution

--- 0925_LongPressedName.py
+++ 0925_LongPressedName.py
@@ -18,3 +18,20 @@ class Solution:
         while j < len(typed) and typed[j] == name[-1]:
             j += 1
         return j == len(typed)
+
+# 2nd solution
+# O(max(m, n)) time | O(1) space
+# where m = len(name) and n = len(typed)
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i = 0
+        j = 0
+        while j < len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif j > 0 and typed[j] == typed[j-1]:
+                j += 1
+            else:
+                return False
+        return i == len(name)
