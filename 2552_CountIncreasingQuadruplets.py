e64c51a2dda6acf11d0e2c6481b66ca85a347992
yingzhuo <yingzhuo1994@gmail.com>
2024-09-09 21:40:43 -0500

    2nd solution

--- 2552_CountIncreasingQuadruplets.py
+++ 2552_CountIncreasingQuadruplets.py
@@ -17,3 +17,20 @@ class Solution:
                 if nums[j] > x:
                     ans += (x - n + 1 + j + great[j][x]) * great[k][nums[j]]
         return ans
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        cnt4 = 0
+        cnt3 = [0] * len(nums)
+        for l in range(2, len(nums)):
+            cnt2 = 0
+            for j in range(l):
+                if nums[j] < nums[l]:  # 3 < 4
+                    cnt4 += cnt3[j]
+                    # 把 j 当作 i，把 l 当作 k，现在 nums[i] < nums[k]，即 1 < 2
+                    cnt2 += 1
+                else:  # 把 l 当作 k，现在 nums[j] > nums[k]，即 3 > 2
+                    cnt3[j] += cnt2
+        return cnt4
