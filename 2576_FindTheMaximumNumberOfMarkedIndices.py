6b451ff50c4f57183b46f4893883318ea282c87e
yingzhuo <yingzhuo1994@gmail.com>
2024-09-11 20:06:17 -0500

    2nd solution

--- 2576_FindTheMaximumNumberOfMarkedIndices.py
+++ 2576_FindTheMaximumNumberOfMarkedIndices.py
@@ -16,3 +16,15 @@ class Solution:
             else:
                 end = mid - 1
         return end * 2
+
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        for x in nums[(len(nums) + 1) // 2:]:
+            if nums[i] * 2 <= x:  # 找到一个匹配
+                i += 1
+        return i * 2
