134917ecf43215c963a9fa5a1912986b0b3a2085
yingzhuo <yingzhuo1994@gmail.com>
2024-09-14 20:44:12 -0500

    2nd solution

--- 2848_PointsThatIntersectWithCars.py
+++ 2848_PointsThatIntersectWithCars.py
@@ -7,3 +7,21 @@ class Solution:
         for a, b in nums:
             points[a:b+1] = [True] * (b - a + 1)
         return sum(points)
+
+
+# 2nd solution
+# O(k * log(k)) time | O(k) space
+# where k = len(nums)
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        nums.sort(key=lambda v: [v[0], -v[1]])
+        stack = []
+        for a, b in nums:
+            if not stack or stack[-1][1] < a:
+                stack.append([a, b])
+            else:
+                stack[-1][1] = max(stack[-1][1], b)
+        ans = 0
+        for a, b in stack:
+            ans += b - a + 1
+        return ans
