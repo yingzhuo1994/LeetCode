5b826921bb8fe3b2146c2f0df9a8c5fb72ee44ac
yingzhuo <yingzhuo1994@gmail.com>
2024-09-19 21:08:32 -0500

    1st solution

---
+++ 2376_CountSpecialInteger.py
@@ -0,0 +1,26 @@
+# 1st solution
+# O(n * D * 2^D) time | O(n * 2^D) space
+class Solution:
+    def countSpecialNumbers(self, n: int) -> int:
+        n = min(n, 9876543210)
+        lst = list(map(int, list(str(n))))
+        @cache
+        def dfs(idx, zero, is_limit, mask):
+            if idx == len(lst):
+                if mask != 1:
+                    return 1
+                else:
+                    return 0
+            limit = 9 if not is_limit else min(9, lst[idx])
+            count = 0
+            for d in range(limit + 1):
+                if (1 << d) & mask:
+                    continue
+                if zero and d == 0:
+                    newMask = 0
+                else:
+                    newMask = mask | (1 << d)
+                v = dfs(idx + 1, zero and d == 0, is_limit and d == lst[idx], newMask)
+                count += v
+            return count
+        return dfs(0, True, True, 0) - 1
