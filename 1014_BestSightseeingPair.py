cc57efd60df5d34e9dbb41383d3fb93a660827b2
yingzhuo <yingzhuo1994@gmail.com>
2024-09-22 19:28:56 -0500

    2nd solution

--- 1014_BestSightseeingPair.py
+++ 1014_BestSightseeingPair.py
@@ -13,3 +13,18 @@ class Solution:
                 v = values[i]
         
         return max(dp[1:]) 
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        n = len(values)
+        v = values[0]
+        ans = 0
+        for i in range(1, n):
+            v -= 1
+            ans = max(ans, v + values[i])
+            if values[i] > v:
+                v = values[i]
+        
+        return ans
