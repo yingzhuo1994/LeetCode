71c7343d952476f5d3ab3bc8b739a12f44dc9301
yingzhuo <yingzhuo1994@gmail.com>
2024-09-23 19:58:54 -0500

    2nd solution

--- 3043_FindTheLengthOfTheLongestCommonPrefix.py
+++ 3043_FindTheLengthOfTheLongestCommonPrefix.py
@@ -1,4 +1,5 @@
 # 1st solution
+# O(m + n) time | O(m + n) space
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         tier1 = Tier()
@@ -30,3 +31,37 @@ class Tier:
             if ch not in dic:
                 dic[ch] = {}
             dic = dic[ch]
+
+# 2nd solution
+# O(m + n) time | O(m) space
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        tier1 = Tier()
+        for num in arr1:
+            tier1.add(str(num))
+        ans = 0
+        for num in arr2:
+            ans = max(ans, tier1.query(str(num)))
+        return ans
+
+
+class Tier:
+    def __init__(self):
+        self.root = {}
+    
+    def add(self, s):
+        dic = self.root
+        for ch in s:
+            if ch not in dic:
+                dic[ch] = {}
+            dic = dic[ch]
+    
+    def query(self, s):
+        dic = self.root
+        length = 0
+        for ch in s:
+            if ch not in dic:
+                break
+            dic = dic[ch]
+            length += 1
+        return length
