2a523aefde7913af4bd9bfd5d21f7d5cce6ca8e4
yingzhuo <yingzhuo1994@gmail.com>
2024-09-24 20:17:25 -0500

    3rd solution

--- 2306_NamingACompany.py
+++ 2306_NamingACompany.py
@@ -42,3 +42,26 @@ class Solution:
                 answer += 2 * (len(initial_groups[i]) - num_of_mutual) * (len(initial_groups[j]) - num_of_mutual)
                 
         return answer
+
+# 3rd solution
+# O(n) time | O(n) space
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+        size = [0] * 26  # 集合大小
+        intersection = [[0] * 26 for _ in range(26)]  # 交集大小
+        groups = defaultdict(list)  # 后缀 -> 首字母列表
+        for s in ideas:
+            b = ord(s[0]) - ord('a')
+            size[b] += 1  # 增加集合大小
+            g = groups[s[1:]]
+            for a in g:  # a 是和 s 有着相同后缀的字符串的首字母
+                intersection[a][b] += 1  # 增加交集大小
+                intersection[b][a] += 1
+            g.append(b)
+
+        ans = 0
+        for a in range(1, 26):  # 枚举所有组对
+            for b in range(a):
+                m = intersection[a][b]
+                ans += (size[a] - m) * (size[b] - m)
+        return ans * 2  # 乘 2 放到最后
