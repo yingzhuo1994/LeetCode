95520c667b07ab12a2968718b6d6fc104dc6b68e
yingzhuo <yingzhuo1994@gmail.com>
2024-09-25 20:49:24 -0500

    2nd solution

--- 2535_DifferenceBetweenElementSumAndDigitSumOfAnArray.py
+++ 2535_DifferenceBetweenElementSumAndDigitSumOfAnArray.py
@@ -5,3 +5,16 @@ class Solution:
         elementSum = sum(nums)
         digitSum = sum(sum(map(int, list(str(num)))) for num in nums)
         return abs(elementSum - digitSum)
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def differenceOfSum(self, nums: List[int]) -> int:
+        ans = 0
+        for x in nums:
+            ans += x  # 累加元素和
+            while x:
+                ans -= x % 10  # 减去数位和
+                x //= 10
+        return ans
