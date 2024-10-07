3a1acda6d551de12bbcdaa7fdce3b587edd83f2a
yingzhuo <yingzhuo1994@gmail.com>
2024-10-06 20:25:45 -0500

    improve the 1st solution

--- 0831_MaskingPersonalInformation.py
+++ 0831_MaskingPersonalInformation.py
@@ -3,8 +3,8 @@
 class Solution:
     def maskPII(self, s: str) -> str:
         if "@" in s:
-            name, domain = s.split("@")
-            email = name[0].lower() + "*****" + name[-1].lower() + "@" + domain.lower()
+            name, domain = s.lower().split("@")
+            email = name[0] + "*****" + name[-1] + "@" + domain
             return email
         else:
             number = s.translate(str.maketrans('', '', "+-() "))
