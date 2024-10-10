9c8d97bf469bb1696399e428735b548614b7abf8
yingzhuo <yingzhuo1994@gmail.com>
2024-10-09 19:55:37 -0500

    improve the 1st solution

--- 3162_FindTheNumberOfGoodPairs(1).py
+++ 3162_FindTheNumberOfGoodPairs(1).py
@@ -4,6 +4,8 @@ class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
         ans = 0
         for a in nums1:
+            if a % k != 0:
+                continue
             for b in nums2:
                 if a % (b * k) == 0:
                     ans += 1
