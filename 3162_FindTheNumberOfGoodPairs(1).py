875eaf0df11c9b3dcd8b7cc7322b4e24532a7d1c
yingzhuo <yingzhuo1994@gmail.com>
2024-10-09 19:54:39 -0500

    1st solution

---
+++ 3162_FindTheNumberOfGoodPairs(1).py
@@ -0,0 +1,10 @@
+# 1st solution
+# O(mn) time | O(1) space
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        ans = 0
+        for a in nums1:
+            for b in nums2:
+                if a % (b * k) == 0:
+                    ans += 1
+        return ans
