8aefd3f8ec806b20551402cf5be00ebaaf86009f
yingzhuo <yingzhuo1994@gmail.com>
2024-10-10 21:34:40 -0500

    2nd solution

--- 3164_FindTheNumberOfGoodPairs(2).py
+++ 3164_FindTheNumberOfGoodPairs(2).py
@@ -15,3 +15,21 @@ class Solution:
                 if d * d < x:
                     cnt[x // d] += 1  # 因子总是成对出现
         return sum(cnt[x] for x in nums2)
+
+
+# 2nd solution
+# O(n + m +  (U/k) * log(m)) time | O(n + m) space
+# where n = len(nums1), m = len(nums2), U = max(nums1)
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        cnt1 = Counter(x // k for x in nums1 if x % k == 0)
+        if not cnt1:
+            return 0
+
+        ans = 0
+        u = max(cnt1)
+        cnt2 = Counter(nums2)
+        for x, cnt in cnt2.items():
+            s = sum(cnt1[y] for y in range(x, u + 1, x))  # 枚举 x 的倍数
+            ans += s * cnt
+        return ans
