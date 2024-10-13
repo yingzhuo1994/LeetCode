5edd2ced80fd501944a66244dc37288ed33e1ccb
yingzhuo <yingzhuo1994@gmail.com>
2024-10-12 20:02:17 -0500

    2nd solution

--- 1884_EggDropWith2EggsAndNFloors.py
+++ 1884_EggDropWith2EggsAndNFloors.py
@@ -6,3 +6,10 @@ class Solution:
         for i in range(1, len(f)):
             f[i] = min(max(j, f[i - j] + 1) for j in range(1, i + 1))
         return f[n]
+
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        return ceil((sqrt(n * 8 + 1) - 1) / 2)
