7af1c05ca477606bc72706d3e103b842ff63d803
yingzhuo <yingzhuo1994@gmail.com>
2024-10-14 21:02:12 -0500

    2nd solution

--- 3200_MaximumHeightOfATriangle.py
+++ 3200_MaximumHeightOfATriangle.py
@@ -18,3 +18,19 @@ class Solution:
             return k - 1
         ans = max(helper(red, blue), helper(blue, red))
         return ans
+
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def maxHeightOfTriangle(self, red: int, blue: int) -> int:
+        def helper(first, second):
+            a = math.floor(math.sqrt(first))
+            b = math.floor((math.sqrt(1 + 4 * second) - 1) / 2)
+            if a > b:
+                return 2 * b + 1
+            else:
+                return 2 * a
+        
+        ans = max(helper(red, blue), helper(blue, red))
+        return ans
