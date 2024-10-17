4d8aa2dd3290c9a23e7f1eacb9c0b31ea6ea8dd9
yingzhuo <yingzhuo1994@gmail.com>
2024-10-16 21:41:29 -0500

    1st solution

---
+++ 3193_CountTheNumberOfInversions.py
@@ -0,0 +1,27 @@
+# 1st solution
+# O(mn) time | O(m + n) space
+class Solution:
+    def numberOfPermutations(self, n: int, requirements: List[List[int]]) -> int:
+        MOD = 1_000_000_007
+        req = [-1] * n
+        req[0] = 0
+        for end, cnt in requirements:
+            req[end] = cnt
+        if req[0]:
+            return 0
+
+        m = max(req)
+        f = [0] * (m + 1)
+        f[0] = 1
+        for i in range(1, n):
+            mx = m if req[i] < 0 else req[i]
+            r = req[i - 1]
+            if r >= 0:
+                for j in range(m + 1):
+                    f[j] = f[r] if r <= j <= min(i + r, mx) else 0
+            else:
+                for j in range(1, mx + 1):  # 计算前缀和
+                    f[j] = (f[j] + f[j - 1]) % MOD
+                for j in range(mx, i, -1):  # 计算子数组和
+                    f[j] = (f[j] - f[j - i - 1]) % MOD
+        return f[req[-1]]
