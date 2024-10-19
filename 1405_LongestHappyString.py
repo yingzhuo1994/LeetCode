1a048634694e8a5a2918be99f16475c07259e724
yingzhuo <yingzhuo1994@gmail.com>
2024-10-18 23:11:02 -0500

    2nd solution

--- 1405_LongestHappyString.py
+++ 1405_LongestHappyString.py
@@ -36,3 +36,25 @@ class Solution:
                 for i in range(1, c + 1):
                     lst[-i] = ch_c + lst[-i]
         return "".join(lst)
+
+
+# 2nd solution
+# O(a + b + c) time | O(a + b + c) space
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        ans = []
+        cnt = [[a, 'a'], [b, 'b'], [c, 'c']]
+        while True:
+            cnt.sort(key=lambda x: -x[0])
+            hasNext = False
+            for i, (c, ch) in enumerate(cnt):
+                if c <= 0:
+                    break
+                if len(ans) >= 2 and ans[-2] == ch and ans[-1] == ch:
+                    continue
+                hasNext = True
+                ans.append(ch)
+                cnt[i][0] -= 1
+                break
+            if not hasNext:
+                return ''.join(ans)
