9a4a3b4fb84e3a13852724db5b94274c772e2635
yingzhuo <yingzhuo1994@gmail.com>
2024-10-26 19:32:41 -0500

    1st solution

---
+++ 1277_CountSquareSubmatricesWithAllOnes.py
@@ -0,0 +1,22 @@
+# 1st solution
+# O(mn * min(m, n)) time | O(mn) space
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j]
+                if matrix[i][j] == 1:
+                    dp[i + 1][j + 1] += 1
+        
+        ans = 0
+        for i in range(1, m + 1):
+            for j in range(n + 1):
+                k = min(i, j)
+                for d in reversed(range(1, k + 1)):
+                    ones = dp[i][j] - dp[i-d][j] - dp[i][j-d] + dp[i-d][j-d]
+                    if ones == d * d:
+                        ans += d
+                        break
+        return ans
