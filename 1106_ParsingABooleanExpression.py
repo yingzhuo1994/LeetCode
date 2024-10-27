0e1c1b84a1c90b43399d85133339e010b6c4b2a2
yingzhuo <yingzhuo1994@gmail.com>
2024-10-26 20:25:19 -0500

    3rd solution

--- 1106_ParsingABooleanExpression.py
+++ 1106_ParsingABooleanExpression.py
@@ -78,3 +78,31 @@ class Solution:
                 nums.pop()
                 nums.append(cur)
         return nums[-1] == 't'
+
+
+# 3rd solution
+# O(n) time | O(n) space
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        stk = []
+        for c in expression:
+            if c == ',':
+                continue
+            if c != ')':
+                stk.append(c)
+                continue
+            t = f = 0
+            while stk[-1] != '(':
+                if stk.pop() == 't':
+                    t += 1
+                else:
+                    f += 1
+            stk.pop()
+            op = stk.pop()
+            if op == '!':
+                stk.append('t' if f == 1 else 'f')
+            elif op == '&':
+                stk.append('t' if f == 0 else 'f')
+            elif op == '|':
+                stk.append('t' if t else 'f')
+        return stk[-1] == 't'
