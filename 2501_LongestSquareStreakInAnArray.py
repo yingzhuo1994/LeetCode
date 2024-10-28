06b55e7153d16b3226004f7a8b908523c2529309
yingzhuo <yingzhuo1994@gmail.com>
2024-10-27 20:12:18 -0500

    2nd solution

--- 2501_LongestSquareStreakInAnArray.py
+++ 2501_LongestSquareStreakInAnArray.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n) time | O(n) space
+# O(n * log(n)) time | O(n) space
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         nums.sort()
@@ -12,3 +12,17 @@ class Solution:
         if ans == 1:
             return -1
         return ans
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        s = set(nums)
+        @cache
+        def dfs(x: int) -> int:
+            if x not in s: 
+                return 0
+            return 1 + dfs(x * x)
+        ans = max(map(dfs, s))
+        return ans if ans > 1 else -1
