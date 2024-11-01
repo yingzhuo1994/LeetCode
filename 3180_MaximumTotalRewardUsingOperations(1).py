c04c8d467ef601622126ba9a6a3cb19f528225f8
yingzhuo <yingzhuo1994@gmail.com>
2024-10-31 20:27:51 -0500

    2nd solution

--- 3180_MaximumTotalRewardUsingOperations(1).py
+++ 3180_MaximumTotalRewardUsingOperations(1).py
@@ -16,3 +16,22 @@ class Solution:
         for v in sorted(s):
             f |= (f & ((1 << v) - 1)) << v
         return f.bit_length() - 1
+
+
+# 2nd solution
+# O(n * (m + log(n))) time | O(m + log(n)) space
+class Solution:
+    def maxTotalReward(self, rewardValues: List[int]) -> int:
+        rewardValues.sort()
+        m = rewardValues[-1]
+        dp = [0] * (2 * m)
+        dp[0] = 1
+        for x in rewardValues:
+            for k in range(2 * x - 1, x - 1, -1):
+                if dp[k - x] == 1:
+                    dp[k] = 1
+        res = 0
+        for i in range(len(dp)):
+            if dp[i] == 1:
+                res = i
+        return res
