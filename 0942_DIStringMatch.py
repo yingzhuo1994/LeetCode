9a469601c06ba6867a48eab1d4c6e0e2525a264c
yingzhuo <yingzhuo1994@gmail.com>
2024-11-02 21:50:03 -0500

    2nd solution

--- 0942_DIStringMatch.py
+++ 0942_DIStringMatch.py
@@ -23,3 +23,20 @@ class Solution:
             ans[i] = v
             v += 1
         return ans
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        lo, hi = 0, len(s)
+        ans = []
+        for x in s:
+            if x == "I":
+                ans.append(lo)
+                lo += 1
+            else:
+                ans.append(hi)
+                hi -= 1
+
+        return ans + [lo]
