98d4cada06f40f6cf9b0c97b06510b6b425d300c
yingzhuo <yingzhuo1994@gmail.com>
2024-11-04 19:11:06 -0600

    2nd solution

--- 2914_MinimumNumberOfChangesToMakeBinaryStringBeautiful.py
+++ 2914_MinimumNumberOfChangesToMakeBinaryStringBeautiful.py
@@ -17,3 +17,15 @@ class Solution:
             one = 0
             zero = 0
         return ans
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def minChanges(self, s: str) -> int:
+        count = 0
+        i = 0
+        while i < len(s) - 1:
+            if s[i] != s[i + 1]:
+                count += 1
+            i += 2
+        return count
