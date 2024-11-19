f8d0c43957032f0a0c17e46ade48cfc25f7072b2
yingzhuo <yingzhuo1994@gmail.com>
2024-11-18 21:08:37 -0600

    2nd solution

--- 3243_ShortestDistanceAfterRoadAdditionQueries(1).py
+++ 3243_ShortestDistanceAfterRoadAdditionQueries(1).py
@@ -22,4 +22,30 @@ class Solution:
             dfs(u)
             ans.append(dp[0])
         return ans
-        
+
+
+# 2nd solution
+# O(k(n + k)) time | O(n + k) space
+# where k = len(queries)
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        neighbors = [[i + 1] for i in range(n)]
+        neighbors[-1] = []
+        res = []
+        for (u, v) in queries:
+            neighbors[u].append(v)
+            res.append(self.bfs(n, neighbors))
+        return res
+
+    def bfs(self, n: int, neighbors: List[List[int]]) -> int:
+        dist = [-1 for _ in range(n)]
+        dist[0] = 0
+        q = deque([0])
+        while len(q) > 0:
+            x = q.popleft()
+            for y in neighbors[x]:
+                if dist[y] >= 0:
+                    continue
+                q.append(y)
+                dist[y] = dist[x] + 1
+        return dist[n - 1]
