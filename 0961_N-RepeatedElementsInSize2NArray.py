3098a2e56f1c2631676c9779a95c58f8d171bad2
yingzhuo <yingzhuo1994@gmail.com>
2024-12-09 18:37:02 -0600

    2nd solution

--- 0961_N-RepeatedElementsInSize2NArray.py
+++ 0961_N-RepeatedElementsInSize2NArray.py
@@ -10,3 +10,13 @@ class Solution:
                 freq = cnt[num]
                 ans = num
         return ans
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        visited = set()
+        for num in nums:
+            if num in visited:
+                return num
+            visited.add(num)
