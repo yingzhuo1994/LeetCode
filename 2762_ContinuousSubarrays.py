4d9c4f3aef9d0b21e99730d86f0fe9dbdd65c629
yingzhuo <yingzhuo1994@gmail.com>
2024-12-13 19:22:45 -0600

    2nd solution

--- 2762_ContinuousSubarrays.py
+++ 2762_ContinuousSubarrays.py
@@ -20,3 +20,21 @@ class Solution:
                 while minHeap[0][1] < start:
                     heappop(minHeap)
         return ans
+
+
+# 2nd solution
+# O(n) time | O(1) space 
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        ans = left = 0
+        cnt = Counter()
+        for right, x in enumerate(nums):
+            cnt[x] += 1
+            while max(cnt) - min(cnt) > 2:
+                y = nums[left]
+                cnt[y] -= 1
+                if cnt[y] == 0:
+                    del cnt[y]
+                left += 1
+            ans += right - left + 1
+        return ans
