7fef9d02620cfdd2e0508e6b49ddf2292d34bb90
yingzhuo <yingzhuo1994@gmail.com>
2024-12-17 19:57:17 -0600

    2nd solution

--- 1475_FinalPricesWithASpecialDiscountInAShop.py
+++ 1475_FinalPricesWithASpecialDiscountInAShop.py
@@ -10,4 +10,18 @@ class Solution:
                     ans[i] = prices[i] - prices[j]
                     break
         return ans
-            
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        n = len(prices)
+        ans = [prices[i] for i in range(n)]
+        stack = []
+        for i, price in enumerate(prices):
+            while stack and stack[-1][0] >= price:
+                _, idx = stack.pop()
+                ans[idx] = prices[idx] - price
+            stack.append([price, i])
+        return ans
