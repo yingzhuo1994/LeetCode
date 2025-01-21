fa46cb09aaf83230f2d5a4deeadb88bf986cbba0
yingzhuo <yingzhuo1994@gmail.com>
2025-01-20 21:02:53 -0600

    2nd solution

--- 2017_GridGame.py
+++ 2017_GridGame.py
@@ -13,3 +13,21 @@ class Solution:
             b = curSum[0][n-1] - curSum[0][i]
             ans = min(ans, max(a, b))
         return ans
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        total1 = sum(grid[0])
+        curSum1 = 0
+        curSum2 = 0
+        ans = float("inf")
+        for i in range(n):
+            curSum1 += grid[0][i]
+            a = curSum2
+            b = total1 - curSum1
+            curSum2 += grid[1][i]
+            ans = min(ans, max(a, b))
+        return ans
