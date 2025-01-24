d4cf028b89ec67ae452251f0c6b1f609769adac5
yingzhuo <yingzhuo1994@gmail.com>
2025-01-23 20:53:35 -0600

    2nd solution

--- 2944_MinimumNumberOfCoinsForFruits.py
+++ 2944_MinimumNumberOfCoinsForFruits.py
@@ -13,3 +13,22 @@ class Solution:
                 cost = min(cost, dfs(i))
             return cost + prices[idx]
         return dfs(0)
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def minimumCoins(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 1:
+            return prices[0]
+        dp = [float("inf")] * (n + 1)
+        dp[-1] = 0
+        dp[n - 1] = prices[-1]
+        minHeap = [[prices[n - 1], n - 1]]
+        for i in reversed(range(n - 1)):
+            while minHeap and minHeap[0][1] > i + (i + 1):
+                heappop(minHeap)
+            cost = prices[i] + min(minHeap[0][0], dp[min(i + i + 2, n)])
+            dp[i] = cost
+            heappush(minHeap, [cost, i])
+        return dp[0]
