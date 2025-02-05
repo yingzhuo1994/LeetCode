78434a100e3f75c2698ae4804fe4d590d65d30b3
yingzhuo <yingzhuo1994@gmail.com>
2025-02-04 21:56:11 -0600

    2nd solution

--- 0873_LengthOfLongestFibonacciSubsequence.py
+++ 0873_LengthOfLongestFibonacciSubsequence.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n^ * log(n)) time | O(1) space
+# O(n^2 * log(n)) time | O(1) space
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         def dfs(i, j):
@@ -16,4 +16,25 @@ class Solution:
                 cnt = dfs(i, j)
                 if cnt > 0:
                     ans = max(ans, cnt + 2)
+        return ans
+
+
+# 2nd solution
+# O(n^2) time | O(n) space
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        vals = set(arr)
+        ans = 0
+        n = len(arr)
+        for i in range(n):
+            a = arr[i]
+            for j in range(i + 1, n):
+                b = arr[j]
+                c = a + b
+                cnt = 2
+                while c in vals:
+                    cnt += 1
+                    b, c = c, b + c
+                if cnt > 2:
+                    ans = max(ans, cnt)
         return ans
