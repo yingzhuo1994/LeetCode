8cc73917de836a5b25babd514d01c86c2a8116f9
yingzhuo <yingzhuo1994@gmail.com>
2025-03-01 19:09:26 -0600

    improve the solution

--- 2570_MergeTwo2DArraysBySummingValues.py
+++ 2570_MergeTwo2DArraysBySummingValues.py
@@ -6,31 +6,25 @@ class Solution:
         i = j = 0
         while i < len(nums1) and j < len(nums2):
             if nums1[i][0] < nums2[j][0]:
-                if array and array[-1][0] == nums1[i][0]:
-                    array[-1][1] += nums1[i][1]
-                else:
-                    array.append(nums1[i])
+                id, val = nums1[i]
                 i += 1
             elif nums1[i][0] > nums2[j][0]:
-                if array and array[-1][0] == nums2[j][0]:
-                    array[-1][1] += nums2[j][1]
-                else:
-                    array.append(nums2[j])
+                id, val = nums2[j]
                 j += 1
             else:
                 id = nums1[i][0]
                 val = nums1[i][1] + nums2[j][1]
-                array.append([id, val])
                 i += 1
                 j += 1
+            
+            if array and array[-1][0] == id:
+                array[-1][1] += val
+            else:
+                array.append([id, val])
+
         if i < len(nums1):
-            if array and array[-1][0] == nums1[i][0]:
-                array[-1][1] += nums1[i][1]
-                i += 1
             array.extend(nums1[i:])
         if j < len(nums2):
-            if array and array[-1][0] == nums2[j][0]:
-                array[-1][1] += nums2[j][1]
-                j += 1
             array.extend(nums2[j:])
+        
         return array            
