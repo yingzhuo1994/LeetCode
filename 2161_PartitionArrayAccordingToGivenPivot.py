8e5f700085e3c4f5148b7989d14596485bde4d11
yingzhuo <yingzhuo1994@gmail.com>
2025-03-02 20:20:11 -0600

    simplify

--- 2161_PartitionArrayAccordingToGivenPivot.py
+++ 2161_PartitionArrayAccordingToGivenPivot.py
@@ -2,9 +2,6 @@
 # O(n) time | O(n) space
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        def switch(array, i, j):
-            array[i], array[j] = array[j], array[i]
-        n = len(nums)
         front = []
         middle = []
         back = []
