38e93fcef91271f19bfa6aa794c383807a0d3c69
yingzhuo <yingzhuo1994@gmail.com>
2025-03-04 19:15:19 -0600

    2nd solution

--- 2579_CountTotalNumberOfColoredCells.py
+++ 2579_CountTotalNumberOfColoredCells.py
@@ -8,3 +8,9 @@ class Solution:
         for i in range(2, n + 1):
             ans += (i - 1) * 4
         return ans
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        return 1 + 2 * n * (n - 1)
