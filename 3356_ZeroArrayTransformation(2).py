f3e7cce61df0acd113f8e5ac4dd36ac633d875f2
yingzhuo <yingzhuo1994@gmail.com>
2025-03-12 19:50:56 -0500

    2nd solution

--- 3356_ZeroArrayTransformation(2).py
+++ 3356_ZeroArrayTransformation(2).py
@@ -16,3 +16,30 @@ class Solution:
             if zeros == n:
                 return i
         return -1
+
+
+# 2nd solution
+# O((n + q) * log(q)) time | O(n) space
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        # 3355. 零数组变换 I
+        def check(k: int) -> bool:
+            diff = [0] * (len(nums) + 1)
+            for l, r, val in queries[:k]:  # 前 k 个询问
+                diff[l] += val
+                diff[r + 1] -= val
+
+            for x, sum_d in zip(nums, accumulate(diff)):
+                if x > sum_d:
+                    return False
+            return True
+
+        q = len(queries)
+        left, right = -1, q + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid
+        return right if right <= q else -1
