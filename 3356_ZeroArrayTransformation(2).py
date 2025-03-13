a362e33e87f4969e85b3a38d2231b07d7786323d
yingzhuo <yingzhuo1994@gmail.com>
2025-03-12 19:59:34 -0500

    3rd solution

--- 3356_ZeroArrayTransformation(2).py
+++ 3356_ZeroArrayTransformation(2).py
@@ -43,3 +43,57 @@ class Solution:
             else:
                 left = mid
         return right if right <= q else -1
+
+
+# 3rd solution
+# O(n + q * log(n)) time | O(n) space
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n = len(nums)
+        m = 2 << n.bit_length()
+        mx = [0] * m
+        todo = [0] * m
+
+        def do(o: int, v: int) -> None:
+            mx[o] -= v
+            todo[o] += v
+
+        def spread(o: int) -> None:
+            if todo[o] != 0:
+                do(o * 2, todo[o])
+                do(o * 2 + 1, todo[o])
+                todo[o] = 0
+
+        def maintain(o: int) -> None:
+            mx[o] = max(mx[o * 2], mx[o * 2 + 1])
+
+        def build(o: int, l: int, r: int) -> None:
+            if l == r:
+                mx[o] = nums[l]
+                return
+            m = (l + r) // 2
+            build(o * 2, l, m)
+            build(o * 2 + 1, m + 1, r)
+            maintain(o)
+
+        def update(o: int, l: int, r: int, ql: int, qr: int, v: int) -> None:
+            if ql <= l and r <= qr:
+                do(o, v)
+                return
+            spread(o)
+            m = (l + r) // 2
+            if ql <= m:
+                update(o * 2, l, m, ql, qr, v)
+            if m < qr:
+                update(o * 2 + 1, m + 1, r, ql, qr, v)
+            maintain(o)
+
+        build(1, 0, n - 1)
+        if mx[1] <= 0:
+            return 0
+
+        for i, (ql, qr, v) in enumerate(queries):
+            update(1, 0, n - 1, ql, qr, v)
+            if mx[1] <= 0:
+                return i + 1
+        return -1
