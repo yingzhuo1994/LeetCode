585229686a11b2d526508eadb3b7d77585b1fd55
yingzhuo <yingzhuo1994@gmail.com>
2025-03-25 23:29:51 -0500

    2nd solution

--- 2829_DetermineTheMinimumSumOfAK-avoidingArray.py
+++ 2829_DetermineTheMinimumSumOfAK-avoidingArray.py
@@ -16,3 +16,11 @@ class Solution:
                 ans += i
             i += 1
         return ans
+
+
+# 2nd solution
+# O(1) time | O(1) space
+class Solution:
+    def minimumSum(self, n: int, k: int) -> int:
+        m = min(k // 2, n)
+        return (m * (m + 1) + (k * 2 + n - m - 1) * (n - m)) // 2
