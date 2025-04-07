60d4b7d527f7d60b80916cc479656a88b2bb0350
yingzhuo <yingzhuo1994@gmail.com>
2025-04-06 20:08:09 -0500

    2nd solution

--- 0954_ArrayOfDoubledPairs.py
+++ 0954_ArrayOfDoubledPairs.py
@@ -35,4 +35,17 @@ class Solution:
             cnt2[num] = 0
         
         return True
-            
+
+
+# 2nd solution
+# O(n * log(n)) time | O(n) space
+class Solution:
+    def canReorderDoubled(self, arr):
+        cnt = Counter(arr)
+        for num in sorted(arr, key = lambda x: abs(x)):
+            if cnt[num] == 0: continue
+            if cnt[2*num] == 0: return False
+            cnt[num] -= 1
+            cnt[2*num] -= 1
+        
+        return True
