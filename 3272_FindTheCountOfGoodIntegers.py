ef248c548d81e0f2fc8947f2d16be3d40792df1d
yingzhuo <yingzhuo1994@gmail.com>
2025-04-11 20:46:09 -0500

    1st solution

---
+++ 3272_FindTheCountOfGoodIntegers.py
@@ -0,0 +1,25 @@
+# 1st solution
+# O(10^m * n * log(n)) time | O(10^m * n) space
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        fac = [factorial(i) for i in range(n + 1)]
+        ans = 0
+        vis = set()
+        base = 10 ** ((n - 1) // 2)
+        for i in range(base, base * 10):  # 枚举回文数左半边
+            s = str(i)
+            s += s[::-1][n % 2:]
+            if int(s) % k:  # 回文数不能被 k 整除
+                continue
+
+            sorted_s = ''.join(sorted(s))
+            if sorted_s in vis:  # 不能重复统计
+                continue
+            vis.add(sorted_s)
+
+            cnt = Counter(sorted_s)
+            res = (n - cnt['0']) * fac[n - 1]
+            for c in cnt.values():
+                res //= fac[c]
+            ans += res
+        return ans
