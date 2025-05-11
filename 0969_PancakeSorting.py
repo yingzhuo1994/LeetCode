1edcc2eb529ebba856817a95766c5ced621334df
yingzhuo <yingzhuo1994@gmail.com>
2025-05-10 23:16:31 -0500

    1st solution

---
+++ 0969_PancakeSorting.py
@@ -0,0 +1,20 @@
+# 1st solution
+# O(n^2) time | O(1) space
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        ans = []
+        for n in range(len(arr), 1, -1):
+            index = 0
+            for i in range(n):
+                if arr[i] > arr[index]:
+                    index = i
+            if index == n - 1:
+                continue
+            m = index
+            for i in range((m + 1) // 2):
+                arr[i], arr[m - i] = arr[m - i], arr[i]  # 原地反转
+            for i in range(n // 2):
+                arr[i], arr[n - 1 - i] = arr[n - 1 - i], arr[i]  # 原地反转
+            ans.append(index + 1)
+            ans.append(n)
+        return ans
