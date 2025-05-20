3b878bb656479c6e64c4ff48bec626cbae97d1b9
yingzhuo <yingzhuo1994@gmail.com>
2025-05-19 21:37:48 -0500

    2nd solution

--- 3355_ZeroArrayTransformation(1).py
+++ 3355_ZeroArrayTransformation(1).py
@@ -17,3 +17,20 @@ class Solution:
             if num + val > 0:
                 return False
         return True
+
+# 2nd solution
+# (n + m) time | O(n) space
+# where n = len(nums) and m = len(queries)
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        diff = [0] * (len(nums) + 1)
+        for l, r in queries:
+            # 区间 [l,r] 中的数都加一
+            diff[l] += 1
+            diff[r + 1] -= 1
+
+        for x, sum_d in zip(nums, accumulate(diff)):
+            # 此时 sum_d 表示 x=nums[i] 要减掉多少
+            if x > sum_d:  # x 无法变成 0
+                return False
+        return True
