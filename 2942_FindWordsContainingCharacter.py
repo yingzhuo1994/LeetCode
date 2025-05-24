588023cd8e36e5ea97f2102f848b6a8820bac745
yingzhuo <yingzhuo1994@gmail.com>
2025-05-23 22:39:38 -0500

    1st solution

---
+++ 2942_FindWordsContainingCharacter.py
@@ -0,0 +1,9 @@
+# 1st solution
+# O(n * m) time | O(1) space
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        for i, word in enumerate(words):
+            if x in word:
+                ans.append(i)
+        return ans
