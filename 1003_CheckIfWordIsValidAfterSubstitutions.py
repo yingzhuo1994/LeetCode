dcf6e1e7aac1c5877a5eb4bfa70b27d4124a9f2b
yingzhuo <yingzhuo1994@gmail.com>
2025-06-01 22:34:08 -0500

    2nd solution

--- 1003_CheckIfWordIsValidAfterSubstitutions.py
+++ 1003_CheckIfWordIsValidAfterSubstitutions.py
@@ -9,3 +9,18 @@ class Solution:
         if idx < 0:
             return False
         return self.isValid(s[:idx] + s[idx+3:])
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == 'c':
+                if stack[-2:] != ['a', 'b']:
+                    return False
+                stack.pop()
+                stack.pop()
+            else:
+                stack.append(i)
+        return not stack
