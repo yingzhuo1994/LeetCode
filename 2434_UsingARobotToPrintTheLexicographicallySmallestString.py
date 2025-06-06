d194c5e55ffd99313accee1fe11ec442f8977b81
yingzhuo <yingzhuo1994@gmail.com>
2025-06-05 23:41:32 -0500

    2nd solution

--- 2434_UsingARobotToPrintTheLexicographicallySmallestString.py
+++ 2434_UsingARobotToPrintTheLexicographicallySmallestString.py
@@ -25,3 +25,22 @@ class Solution:
             i += 1
         ans += t[::-1]
         return "".join(ans)
+
+
+# 2nd solution
+# O(n) time | O(n) space
+class Solution:
+    def robotWithString(self, s: str) -> str:
+      n = len(s)
+      # 计算后缀最小值
+      suf_min = ['z'] * (n + 1)
+      for i in range(n - 1, -1, -1):
+          suf_min[i] = min(suf_min[i + 1], s[i])
+
+      ans = []
+      st = []
+      for i, ch in enumerate(s):
+          st.append(ch)
+          while st and st[-1] <= suf_min[i + 1]:
+              ans.append(st.pop())
+      return ''.join(ans)
