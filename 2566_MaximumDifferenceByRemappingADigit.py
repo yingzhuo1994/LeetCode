a73efd1946175f9175f6c157fe4dc491cb6311f7
yingzhuo <yingzhuo1994@gmail.com>
2025-06-13 21:29:42 -0500

    1st solution

---
+++ 2566_MaximumDifferenceByRemappingADigit.py
@@ -0,0 +1,10 @@
+# 1st solution
+# O(1) time | O(1) space
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        num = str(num)
+        i = 0
+        while num[i] == "9" and i < len(num)-1:
+            i += 1
+
+        return int(num.replace(num[i], "9")) - int(num.replace(num[0], "0"))
