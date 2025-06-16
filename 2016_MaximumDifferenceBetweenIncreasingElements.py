41aca51b9291b9df29d38f537eb84c2a28613b0a
yingzhuo <yingzhuo1994@gmail.com>
2025-06-15 22:11:51 -0500

    2nd solution

--- 2016_MaximumDifferenceBetweenIncreasingElements.py
+++ 2016_MaximumDifferenceBetweenIncreasingElements.py
@@ -1,5 +1,5 @@
 # 1st solution
-# O(n) time | O(1) space
+# O(n) time | O(n) space
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         rightMaxNums = nums[:]
@@ -9,5 +9,17 @@ class Solution:
         a = nums[0]
         for i in range(1, len(nums)):
             ans = max(ans, rightMaxNums[i] - a)
+            a = min(a, nums[i])
+        return ans if ans > 0 else -1
+
+
+# 2nd solution
+# O(n) time | O(1) space
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        ans = 0
+        a = nums[0]
+        for i in range(1, len(nums)):
+            ans = max(ans, nums[i] - a)
             a = min(a, nums[i])
         return ans if ans > 0 else -1
