cb7860dd69c4e246648793c576487fac1e1fd626
yingzhuo <yingzhuo1994@gmail.com>
2025-06-16 23:21:08 -0500

    1st solution

---
+++ 3405_CountTheNumberOfArraysWithKMatchingAdjacentElements.py
@@ -0,0 +1,7 @@
+# 1st solution
+# O(log(n - k)) time | O(1) space
+class Solution:
+    @cache
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        MOD = 10**9 + 7
+        return m * pow(m - 1, n - k - 1, MOD) * comb(n - 1, k) % MOD
