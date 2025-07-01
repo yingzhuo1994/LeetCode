a5b3ec99cabe177828b5da038019b13ca1175b47
yingzhuo <yingzhuo1994@gmail.com>
2025-06-30 21:41:19 -0500

    1st solution

---
+++ 3330_FindTheOriginalTypedString(1).py
@@ -0,0 +1,16 @@
+# 1st solution
+# O(n) time | O(1) space
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        ans = 0
+        last = word[0]
+        cnt = 0
+        for ch in word:
+            if ch == last:
+                cnt += 1
+            else:
+                ans += cnt - 1
+                cnt = 1
+                last = ch
+        ans += cnt - 1
+        return ans + 1
