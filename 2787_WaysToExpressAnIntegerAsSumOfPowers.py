f2db284ddcbe5ccb31b95560d85b7ddd89fd0879
yingzhuo <yingzhuo1994@gmail.com>
2025-08-12 09:17:40 -0500

    2nd solution

--- 2787_WaysToExpressAnIntegerAsSumOfPowers.py
+++ 2787_WaysToExpressAnIntegerAsSumOfPowers.py
@@ -23,3 +23,16 @@ class Solution:
             return cnt
         
         return helper(n, ceil(n**(1/x)))
+    
+# 2nd solution
+# O(n * n^(1/x)) time | O(n) space
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        f = [1] + [0] * n
+        for i in range(1, n + 1):
+            v = i ** x
+            if v > n:
+                break
+            for s in range(n, v - 1, -1):
+                f[s] += f[s - v]
+        return f[n] % 1_000_000_007
